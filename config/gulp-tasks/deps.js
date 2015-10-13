4a2b48dc3387c9cbafbdd9dcbf76ba1f4c177826
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2015-10-13 23:28:19 +0300

    chore: Improved gulp-deps script

--- config/gulp-tasks/deps.js
+++ config/gulp-tasks/deps.js
@@ -5,21 +5,28 @@ var
   config = require('../gulp-config'),
   plugins = config.plugins;
 
+function devDeps(config) {
+  return gulp.src(config.src)
+    .pipe(plugins.newer(config.dest + '/' + config.name))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.concat(config.name))
+    .pipe(plugins.sourcemaps.write())
+		.pipe(gulp.dest(config.dest));
+}
+
 /**
  * Scripts
  */
 gulp.task('dev:script:deps', function() {
-  return gulp.src(config.script.deps)
-    .pipe(plugins.newer(config.script.dest + '/' + config.script.depsName + '.js'))
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.concat(config.script.depsName + '.js'))
-    .pipe(plugins.sourcemaps.write())
-		.pipe(gulp.dest(config.script.dest));
+  return devDeps({
+    src: config.script.deps,
+    dest: config.script.dest,
+    name: config.script.depsName + '.js'
+  });
 });
 
 gulp.task('prod:script:deps', function() {
   return gulp.src(config.script.deps)
-    .pipe(plugins.newer(config.script.dest + '/' + config.script.depsName + '.min.js'))
     .pipe(plugins.concat(config.script.depsName + '.min.js'))
     .pipe(plugins.uglify())
     .pipe(gulp.dest(config.script.dest));
@@ -30,12 +37,11 @@ gulp.task('prod:script:deps', function() {
  * Styles
  */
 gulp.task('dev:style:deps', function() {
-  return gulp.src(config.style.deps)
-    .pipe(plugins.newer(config.style.dest + '/' + config.style.depsName + '.css'))
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.concat(config.style.depsName + '.css'))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest(config.style.dest));
+  return devDeps({
+    src: config.style.deps,
+    dest: config.style.dest,
+    name: config.style.depsName + '.css'
+  });
 });
 
 gulp.task('prod:style:deps', function() {
