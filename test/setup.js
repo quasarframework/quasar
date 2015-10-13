3f9eba39379c818f78b9215badf6c516e21609e5
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2015-10-13 12:50:49 +0300

    chore: Added Sinon-Chai

--- test/setup.js
+++ test/setup.js
@@ -7,8 +7,13 @@ if (typeof process !== 'undefined') {
   /**
    * We are in Node. Require modules.
    */
+  var chai = require('chai');
+  var sinonChai = require('sinon-chai');
+
   expect = require('chai').expect;
   sinon = require('sinon');
+
+  chai.use(sinonChai);
   inBrowser = false;
 }
 else {
