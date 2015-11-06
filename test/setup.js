e0100110d4be5408ffd06f7591080a89208c56be
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2015-11-06 18:36:36 +0200

    chore: Further testing improvements

--- test/setup.js
+++ test/setup.js
@@ -1,34 +1,15 @@
 'use strict';
 
-/**
- * Export modules to global scope as necessary
- * (only for testing)
- */
+window.expect = chai.expect;
+window.testing = {
+  phantomjs: (function() {
+    return navigator.userAgent.indexOf('PhantomJS') > -1;
+  }())
+};
 
-if (typeof process !== 'undefined') {
-  /**
-   * We are in Node. Require modules.
-   */
-  var chai = require('chai');
-  var sinonChai = require('sinon-chai');
-
-  expect = require('chai').expect;
-  sinon = require('sinon');
-
-  chai.use(sinonChai);
-}
-else {
-  /**
-   * We are in a browser.
-   * Set up variables like above using serves js files.
-   */
-  window.expect = chai.expect;
-  window.testing = {
-    phantomjs: (function() {
-      return navigator.userAgent.indexOf('PhantomJS') > -1;
-    }())
-  };
-}
+// Immediately throw error when using Sinon.
+// The default behavior is to throw error in a setTimeout().
+window.sinon.logError.useImmediateExceptions = true;
 
 if (typeof Function.prototype.bind != 'function') {
   Function.prototype.bind = function bind(obj) {
@@ -49,7 +30,3 @@ if (typeof Function.prototype.bind != 'function') {
     return bound;
   };
 }
-
-// Immediately throw error when using Sinon.
-// The default behavior is to throw error in a setTimeout().
-window.sinon.logError.useImmediateExceptions = true;
