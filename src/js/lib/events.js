62412993fdc1374469598678f5c666ab923b6579
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2015-11-13 01:31:50 +0200

    feat: Improve Events API

--- src/js/lib/events.js
+++ src/js/lib/events.js
@@ -1,8 +1,19 @@
 'use strict';
 
-function on(eventName, callback, context, once) {
-  if (!eventName) {
-    throw new Error('Missing event name');
+function parseEventNames(eventNames) {
+  return _.trim(eventNames).replace(/\s\s+/g, ' ').split(' ');
+}
+
+function remove(events, eventName, item) {
+  events[eventName] = _.without(events[eventName], item);
+  if (_.isEmpty(events[eventName])) {
+    delete events[eventName];
+  }
+}
+
+function on(eventNames, callback, context, once) {
+  if (!eventNames) {
+    throw new Error('Missing event(s) name(s)');
   }
   if (!callback) {
     throw new Error('Missing callback');
@@ -11,84 +22,95 @@ function on(eventName, callback, context, once) {
     throw new Error('Callback is not a function');
   }
 
-  if (!this.events.hasOwnProperty(eventName)) {
-    this.events[eventName] = [];
-  }
-
-  if (_.some(this.events[eventName], function(item) {
-    return item.cb === callback;
-  })) {
-    throw new Error('Event name already has specified callback');
-  }
-
-  this.events[eventName].push({
-    cb: callback,
-    context: context,
-    once: once
-  });
-}
-
-function off(eventName, callback) {
-  if (!eventName) {
-    throw new Error('Missing event name');
-  }
-
-  if (!this.events.hasOwnProperty(eventName)) {
-    throw new Error('Unregistered event name');
-  }
-
-  if (!callback) {
-    delete this.events[eventName];
-    return;
-  }
-
-  if (!_.isFunction(callback)) {
-    throw new Error('Callback is not a function');
-  }
-
-  var item = _.find(this.events[eventName], function(item) {
-    return item.cb === callback;
-  });
-
-  if (!item) {
-    throw new Error('Event is not registered with specified callback');
-  }
-
-  this.events[eventName] = _.without(this.events[eventName], item);
-}
-
-function once(eventName, callback, context) {
-  return on.call(this, eventName, callback, context, true);
-}
-
-function trigger(eventName) {
-  if (!eventName) {
-    throw new Error('Missing event name');
-  }
-
-  if (!this.events.hasOwnProperty(eventName)) {
-    // Nothing to trigger
-    return;
-  }
-
-  var args = Array.prototype.slice.call(arguments, 1);
-  var onceList = [];
-
-  _.forEach(this.events[eventName], function(item) {
-    item.cb.apply(item.context, args);
-    if (item.once) {
-      onceList.push(item);
+  _.forEach(parseEventNames(eventNames), function(eventName) {
+    if (!this.events.hasOwnProperty(eventName)) {
+      this.events[eventName] = [];
     }
-  });
 
-  _.forEach(onceList, function(item) {
-    this.events[eventName] = _.without(this.events[eventName], item);
+    if (_.some(this.events[eventName], function(item) {
+      return item.cb === callback;
+    })) {
+      // Event name already has specified callback
+      return;
+    }
+
+    this.events[eventName].push({
+      cb: callback,
+      context: context,
+      once: once
+    });
   }.bind(this));
 }
 
-function hasEvent(eventName, callback) {
+function off(eventNames, callback) {
+  if (!eventNames) {
+    this.events = {};
+    return;
+  }
+
+  if (callback && !_.isFunction(callback)) {
+    throw new Error('Callback is not a function');
+  }
+
+  _.forEach(parseEventNames(eventNames), function(eventName) {
+    if (!this.events.hasOwnProperty(eventName)) {
+      // Unregistered event name
+      return;
+    }
+
+    if (!callback) {
+      delete this.events[eventName];
+      return;
+    }
+
+    var item = _.find(this.events[eventName], function(item) {
+      return item.cb === callback;
+    });
+
+    if (!item) {
+      // Event is not registered with specified callback
+      return;
+    }
+
+    remove(this.events, eventName, item);
+  }.bind(this));
+}
+
+function once(eventNames, callback, context) {
+  on.call(this, eventNames, callback, context, true);
+}
+
+function trigger(eventNames) {
+  if (!eventNames) {
+    eventNames = getEventsList.call(this).join(' ');
+  }
+
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  _.forEach(parseEventNames(eventNames), function(eventName) {
+    if (!this.events.hasOwnProperty(eventName)) {
+      // Nothing to trigger
+      return;
+    }
+
+    var onceList = [];
+
+    _.forEach(this.events[eventName], function(item) {
+      item.cb.apply(item.context, args);
+      if (item.once) {
+        onceList.push(item);
+      }
+    });
+
+    _.forEach(onceList, function(item) {
+      remove(this.events, eventName, item);
+    }.bind(this));
+  }.bind(this));
+}
+
+function hasSubscriber(eventName, callback) {
   if (!eventName) {
-    throw new Error('Missing event name');
+    return !_.isEmpty(this.events);
   }
 
   var result = this.events.hasOwnProperty(eventName);
@@ -125,7 +147,7 @@ function createEventsEmitter() {
     once: once.bind(props),
     trigger: trigger.bind(props),
 
-    hasEvent: hasEvent.bind(props),
+    hasSubscriber: hasSubscriber.bind(props),
     getEventsList: getEventsList.bind(props)
   };
 }
@@ -133,6 +155,10 @@ function createEventsEmitter() {
 function makeEventsEmitter(object) {
   if (!_.isObject(object)) {
     throw new Error('Missing object');
+  }
+
+  if (isEventsEmitter(object)) {
+    throw new Error('Object is already an emitter');
   }
 
   _.merge(object, createEventsEmitter());
@@ -147,7 +173,7 @@ function isEventsEmitter(object) {
     _.isFunction(object.off) &&
     _.isFunction(object.once) &&
     _.isFunction(object.trigger) &&
-    _.isFunction(object.hasEvent) &&
+    _.isFunction(object.hasSubscriber) &&
     _.isFunction(object.getEventsList);
 }
 
--- test/01-lib/events.spec.js
+++ test/01-lib/events.spec.js
@@ -5,6 +5,8 @@ describe('Events', function() {
   beforeEach(function() {
     this.emitter = quasar.create.events.emitter();
     this.emptyFn = function() {};
+    this.times = 0;
+    this.fn = function() { this.times++; }.bind(this);
   });
 
   after(function() {
@@ -16,18 +18,18 @@ describe('Events', function() {
     expect(this.emitter).to.be.an('object');
   });
 
-  it('should be able to bind an event', function() {
+  it('should be able to register an event', function() {
     this.emitter.on('someEvent', this.emptyFn);
-    expect(this.emitter.hasEvent('someEvent')).to.equal(true);
+    expect(this.emitter.hasSubscriber('someEvent')).to.equal(true);
 
     this.emitter.on('someOtherEvent', this.emptyFn);
-    expect(this.emitter.hasEvent('someOtherEvent')).to.equal(true);
+    expect(this.emitter.hasSubscriber('someOtherEvent')).to.equal(true);
   });
 
   it('should throw error when supplying on() with faulty parameters', function() {
     expect(function() {
       this.emitter.on();
-    }.bind(this)).to.throw(/Missing event name/);
+    }.bind(this)).to.throw(/Missing event\(s\) name\(s\)/);
 
     expect(function() {
       this.emitter.on('event');
@@ -36,11 +38,13 @@ describe('Events', function() {
     expect(function() {
       this.emitter.on('event', 'string');
     }.bind(this)).to.throw(/Callback is not a function/);
+  });
 
-    this.emitter.on('event', this.emptyFn);
-    expect(function() {
-      this.emitter.on('event', this.emptyFn);
-    }.bind(this)).to.throw(/Event name already has specified callback/);
+  it('should not trigger callback multiple times if registered multiple times', function() {
+    this.emitter.on('event', this.fn);
+    this.emitter.on('event', this.fn);
+    this.emitter.trigger('event');
+    expect(this.times).to.equal(1);
   });
 
   it('should be able to get events list', function() {
@@ -49,36 +53,38 @@ describe('Events', function() {
     expect(this.emitter.getEventsList()).to.deep.equal(['someEvent', 'someOtherEvent']);
   });
 
-  it('should be able to unbind an event completely', function() {
+  it('should be able to remove an event completely', function() {
     this.emitter.on('someEvent', this.emptyFn);
     this.emitter.off('someEvent');
-    expect(this.emitter.hasEvent('someEvent')).to.equal(false);
+    expect(this.emitter.hasSubscriber('someEvent')).to.equal(false);
   });
 
-  it('should be able to unbind a callback from an event', function() {
+  it('should be able to remove a callback from an event', function() {
     this.emitter.on('someEvent', this.emptyFn);
     this.emitter.off('someEvent', this.emptyFn);
-    expect(this.emitter.hasEvent('someEvent', this.emptyFn)).to.equal(false);
+    expect(this.emitter.hasSubscriber('someEvent', this.emptyFn)).to.equal(false);
+  });
+
+  it('should be able to remove all events', function() {
+    this.emitter.on('event1', this.emptyFn);
+    this.emitter.on('event2', this.emptyFn);
+    this.emitter.off();
+    expect(this.emitter.hasSubscriber('event1')).to.equal(false);
+    expect(this.emitter.hasSubscriber('event2')).to.equal(false);
   });
 
   it('should throw error when supplying off() with faulty parameters', function() {
     expect(function() {
-      this.emitter.off();
-    }.bind(this)).to.throw(/Missing event name/);
-
-    expect(function() {
-      this.emitter.off('event');
-    }.bind(this)).to.throw(/Unregistered event name/);
-
-    this.emitter.on('event', this.emptyFn);
-
-    expect(function() {
       this.emitter.off('event', 'string');
     }.bind(this)).to.throw(/Callback is not a function/);
 
-    expect(function() {
-      this.emitter.off('event', function() {});
-    }.bind(this)).to.throw(/Event is not registered with specified callback/);
+    // Should not throw on unregistered event/callback
+    this.emitter.off('event', this.emptyFn);
+
+    // Should not throw on unregistered callback
+    this.emitter.on('event', this.emptyFn);
+    this.emitter.off('event', this.fn);
+    expect(this.emitter.hasSubscriber('event', this.emptyFn)).to.equal(true);
   });
 
   it('should be able to trigger event', function(done) {
@@ -97,25 +103,35 @@ describe('Events', function() {
   });
 
   it('should be able to register event callback with context', function(done) {
-    this.emitter.on(
+    var emitter = this.emitter;
+
+    emitter.on(
       'event',
       function(param) {
         expect(param).to.equal('param');
         expect(this.prop).to.equal(true);
-        done();
+        quasar.nextTick(function() {
+          // expect to still have this event registered
+          expect(emitter.hasSubscriber('event')).to.equal(true);
+          done();
+        });
       },
       {prop: true}
     );
-    this.emitter.trigger('event', 'param');
+    emitter.trigger('event', 'param');
   });
 
-  it('should throw error when supplying trigger() with faulty parameters', function() {
-    expect(function() {
-      this.emitter.trigger();
-    }.bind(this)).to.throw(/Missing event name/);
-
+  it('should be able to trigger all events at once', function() {
     // Should not throw for unregistered event:
     this.emitter.trigger('event');
+    // SHould not throw when no events are registered
+    this.emitter.trigger();
+
+    this.emitter.on('event1', this.fn);
+    this.emitter.on('event2', this.fn);
+    this.emitter.trigger();
+    expect(this.times).to.equal(2);
+    expect(this.emitter.getEventsList()).to.deep.equal(['event1', 'event2']);
   });
 
   it('should be able to trigger event callback once', function() {
@@ -131,20 +147,28 @@ describe('Events', function() {
     expect(times).to.equal(1);
   });
 
-  it('should be able to tell if event is registered with a specific callback', function() {
+  it('should be able to tell if emitter has subscribers', function() {
+    expect(this.emitter.hasSubscriber()).to.equal(false);
     this.emitter.on('event', this.emptyFn);
-    expect(this.emitter.hasEvent('event', this.emptyFn)).to.equal(true);
-    expect(this.emitter.hasEvent('event', function() {})).to.equal(false);
+    expect(this.emitter.hasSubscriber('event')).to.equal(true);
+    expect(this.emitter.hasSubscriber('event', this.emptyFn)).to.equal(true);
+    this.emitter.off('event');
+    expect(this.emitter.hasSubscriber()).to.equal(false);
+    this.emitter.on('event1 event2', this.emptyFn);
+    this.emitter.off();
+    expect(this.emitter.hasSubscriber()).to.equal(false);
   });
 
-  it('should throw error when supplying hasEvent() with faulty parameters', function() {
-    expect(function() {
-      this.emitter.hasEvent();
-    }.bind(this)).to.throw(/Missing event name/);
+  it('should be able to tell if emitter has event registered with callback', function() {
+    this.emitter.on('event', this.emptyFn);
+    expect(this.emitter.hasSubscriber('event', this.emptyFn)).to.equal(true);
+    expect(this.emitter.hasSubscriber('event', function() {})).to.equal(false);
+  });
 
+  it('should throw error when supplying hasSubscriber() with faulty parameters', function() {
     this.emitter.on('event', this.emptyFn);
     expect(function() {
-      this.emitter.hasEvent('event', 'string');
+      this.emitter.hasSubscriber('event', 'string');
     }.bind(this)).to.throw(/Callback is not a function/);
   });
 
@@ -160,7 +184,7 @@ describe('Events', function() {
     object.on('event', function() {
       done();
     });
-    expect(object.hasEvent('event'));
+    expect(object.hasSubscriber('event')).to.equal(true);
     object.trigger('event');
   });
 
@@ -175,10 +199,52 @@ describe('Events', function() {
     expect(quasar.is.events.emitter({some: 'object'})).to.equal(false);
   });
 
+  it('should throw error when trying to inject events methods to an object which is already an emitter', function() {
+    var object = {};
+
+    quasar.make.events.emitter(object);
+    expect(function() {
+      quasar.make.events.emitter(object);
+    }).to.throw(/Object is already an emitter/);
+  });
+
   it('should throw error when trying to tell if object is emitter and supplying faulty params', function() {
     expect(function() {
       quasar.is.events.emitter();
     }).to.throw(/Missing object/);
+  });
+
+  it('should be able to register multiple events at once', function() {
+    this.emitter.on('event1   event2 ', this.emptyFn);
+    expect(this.emitter.hasSubscriber('event1')).to.equal(true);
+    expect(this.emitter.hasSubscriber('event2')).to.equal(true);
+  });
+
+  it('should be able to remove multiple events at once', function() {
+    this.emitter.on(' event1   event2 ', this.emptyFn);
+    this.emitter.off('  event2    event1    ');
+    expect(this.emitter.hasSubscriber('event1')).to.equal(false);
+    expect(this.emitter.hasSubscriber('event2')).to.equal(false);
+
+    var
+      times = 0,
+      fn = function() {
+        times++;
+      },
+      secondFn = function() {
+        times++;
+      };
+
+    this.emitter.on('event1 event2', fn);
+    this.emitter.on('event1', secondFn);
+    this.emitter.trigger('   event1 event2');
+    expect(times).to.equal(3);
+  });
+
+  it('should be able to trigger multiple events at once', function() {
+    this.emitter.on('    event1 event2', this.fn);
+    this.emitter.trigger('event2         event1');
+    expect(this.times).to.equal(2);
   });
 
 });
