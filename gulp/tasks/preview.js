14882db577dfdfae9149e5bd239d28cb3ccc6d21
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2015-11-27 00:15:47 +0200

    feat: Add demo App native mobile wrapper

--- gulp/tasks/preview.js
+++ gulp/tasks/preview.js
@@ -1,21 +1,66 @@
 'use strict';
 
-var spawn = require('child_process').spawn;
+var
+  runSequence = require('run-sequence'),
+  spawn = require('child_process').spawn
+  ;
 
-function launchPreview(type) {
-  spawn('quasar', [type, '-d'], {cwd: config.preview.src, stdio: 'inherit'})
+/*
+ * Monitor
+ */
+
+function run(tasks) {
+  return function() {
+    runSequence(tasks);
+  };
+}
+
+function watchForChanges() {
+  /*
+   * Watch for CSS
+   */
+  plugins.watch(config.css.all, run('css:dev'));
+
+  /*
+   * Watch for JS
+   */
+  plugins.watch(config.js.all, run('js:dev'));
+
+  process.nextTick(function() {
+    plugins.util.log();
+    plugins.util.log(plugins.util.colors.magenta('Monitoring'), 'Quasar Framework source code for changes...');
+    plugins.util.log();
+  });
+}
+
+gulp.task('monitor', ['dev'], watchForChanges);
+
+/*
+ * Preview
+ */
+
+function launch(args) {
+  args.push('-d');
+  spawn('quasar', args, {cwd: config.preview.src, stdio: 'inherit'})
     .on('error', function() {
-      console.log();
-      console.log('!!! You need quasar-cli installed (npm install -g quasar-cli) !!!');
-      console.log();
+      console.log('\n!!! You need quasar-cli installed (npm install -g quasar-cli) !!!\n');
       process.exit(1);
     });
 }
 
 gulp.task('preview', ['monitor'], function() {
-  launchPreview('preview');
+  launch(['preview']);
 });
 
 gulp.task('preview-resp', ['monitor'], function() {
-  launchPreview('preview -r');
+  launch(['preview', '-r']);
+});
+
+/*
+ * Run Wrapper
+ */
+
+gulp.task('wrapper', ['dev'], function() {
+  console.log(plugins.util.colors.magenta('\nMake sure that you have added at least one platform (quasar wrap platform add <platform-name>).\n'));
+  launch(['wrap', 'run']);
 });
