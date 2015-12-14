30f53aeedce5ac0dede32bb229895b215c903cfe
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2015-12-14 19:25:06 +0200

    feat: Significantly enhanced environment detection

--- src/lib/core-js/environment/environment.js
+++ src/lib/core-js/environment/environment.js
@@ -1,34 +1,146 @@
 'use strict';
 
-var userAgent = navigator.userAgent.toLowerCase();
+/* istanbul ignore next */
+function runsOn() {
+  var userAgent = (navigator.userAgent || /* istanbul ignore next */ navigator.vendor || /* istanbul ignore next */ window.opera).toLowerCase();
 
-var env = {
+  var match = /(edge)\/([\w.]+)/.exec(userAgent) ||
+    /(opr)[\/]([\w.]+)/.exec(userAgent) ||
+    /(chrome)[\/]([\w.]+)/.exec(userAgent) ||
+    /(iemobile)[\/]([\w.]+)/.exec(userAgent) ||
+    /(version)(applewebkit)[\/]([\w.]+).*(safari)[\/]([\w.]+)/.exec(userAgent) ||
+    /(webkit)[\/]([\w.]+).*(version)[\/]([\w.]+).*(safari)[\/]([\w.]+)/.exec(userAgent) ||
+    /(webkit)[\/]([\w.]+)/.exec(userAgent) ||
+    /(opera)(?:.*version|)[\/]([\w.]+)/.exec(userAgent) ||
+    /(msie) ([\w.]+)/.exec(userAgent) ||
+    userAgent.indexOf('trident') >= 0 && /(rv)(?::| )([\w.]+)/.exec(userAgent) ||
+    userAgent.indexOf('compatible') < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(userAgent) ||
+    [];
+
+  var platformMatch = /(ipad)/.exec(userAgent) ||
+    /(ipod)/.exec(userAgent) ||
+    /(windows phone)/.exec(userAgent) ||
+    /(iphone)/.exec(userAgent) ||
+    /(kindle)/.exec(userAgent) ||
+    /(silk)/.exec(userAgent) ||
+    /(android)/.exec(userAgent) ||
+    /(win)/.exec(userAgent) ||
+    /(mac)/.exec(userAgent) ||
+    /(linux)/.exec(userAgent) ||
+    /(cros)/.exec(userAgent) ||
+    /(playbook)/.exec(userAgent) ||
+    /(bb)/.exec(userAgent) ||
+    /(blackberry)/.exec(userAgent) ||
+    [];
+
+  var
+    browser = {},
+    matched = {
+      browser: match[5] || match[3] || match[1] || '',
+      version: match[2] || match[4] || '0',
+      versionNumber: match[4] || match[2] || '0',
+      platform: platformMatch[0] || ''
+    };
+
+  if (matched.browser) {
+    browser[matched.browser] = true;
+    browser.version = matched.version;
+    browser.versionNumber = parseInt(matched.versionNumber, 10);
+  }
+
+  if (matched.platform) {
+    browser[matched.platform] = true;
+  }
+
+  // These are all considered mobile platforms, meaning they run a mobile browser
+  if (browser.android || browser.bb || browser.blackberry || browser.ipad || browser.iphone ||
+    browser.ipod || browser.kindle || browser.playbook || browser.silk || browser['windows phone']) {
+    browser.mobile = true;
+  }
+
+  if (browser['windows phone']) {
+    browser.winphone = true;
+    delete browser['windows phone'];
+  }
+
+  // These are all considered desktop platforms, meaning they run a desktop browser
+  if (browser.cros || browser.mac || browser.linux || browser.win) {
+    browser.desktop = true;
+  }
+
+  // Chrome, Opera 15+ and Safari are webkit based browsers
+  if (browser.chrome || browser.opr || browser.safari) {
+    browser.webkit = true;
+  }
+
+  // IE11 has a new token so we will assign it msie to avoid breaking changes
+  if (browser.rv || browser.iemobile) {
+    matched.browser = 'ie';
+    browser.ie = true;
+  }
+
+  // Edge is officially known as Microsoft Edge, so rewrite the key to match
+  if (browser.edge) {
+    matched.browser = 'edge';
+    browser.edge = true;
+  }
+
+  // Blackberry browsers are marked as Safari on BlackBerry
+  if (browser.safari && browser.blackberry || browser.bb) {
+    matched.browser = 'blackberry';
+    browser.blackberry = true;
+  }
+
+  // Playbook browsers are marked as Safari on Playbook
+  if (browser.safari && browser.playbook) {
+    matched.browser = 'playbook';
+    browser.playbook = true;
+  }
+
+  // Opera 15+ are identified as opr
+  if (browser.opr) {
+    matched.browser = 'opera';
+    browser.opera = true;
+  }
+
+  // Stock Android browsers are marked as Safari on Android.
+  if (browser.safari && browser.android) {
+    matched.browser = 'android';
+    browser.android = true;
+  }
+
+  // Kindle browsers are marked as Safari on Kindle
+  if (browser.safari && browser.kindle) {
+    var kindle = 'kindle';
+
+    matched.browser = 'kindle';
+    browser.kindle = true;
+  }
+
+   // Kindle Silk browsers are marked as Safari on Kindle
+  if (browser.safari && browser.silk) {
+    matched.browser = 'silk';
+    browser.silk = true;
+  }
+
+  // Assign the name and platform variable
+  browser.name = matched.browser;
+  browser.platform = matched.platform;
+
+  if (document.URL.indexOf('http://') === -1 && /* istanbul ignore next */ document.URL.indexOf('https://') === -1) {
+    browser.cordova = true;
+  }
+
+  return browser;
+}
+
+_.merge(q, {
   runs: {
-    on: {
-      mobile: (/* istanbul ignore next */function(a) {
-        return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4));
-      })(navigator.userAgent || /* istanbul ignore next */ navigator.vendor || /* istanbul ignore next */ window.opera),
-
-      cordova: (function() {
-        return document.URL.indexOf('http://') === -1 && /* istanbul ignore next */ document.URL.indexOf('https://') === -1;
-      }()),
-
-      android: !userAgent.match(/iemobile/) && userAgent.match(/android/),
-      ios: !userAgent.match(/iemobile/) && userAgent.match(/iphone|ipod|ipad/),
-      blackberry: userAgent.match(/blackberry/),
-      windows: userAgent.match(/iemobile/)
-    },
+    on: runsOn(),
     with: {
       touch: (function() {
         return !!('ontouchstart' in document.documentElement) || /* istanbul ignore next */ window.navigator.msMaxTouchPoints > 0;
       }())
     }
   }
-};
-
-_.merge(env.runs.on, {
-  desktop: !env.runs.on.mobile && !env.runs.on.cordova,
-  browser: !env.runs.on.cordova
 });
-
-_.merge(q, env);
--- test/01-lib/environment.spec.js
+++ test/01-lib/environment.spec.js
@@ -5,21 +5,7 @@ describe('Environment', function() {
   it('should have detectors', function() {
     expect(quasar.runs.on).to.exist;
     expect(quasar.runs.with).to.exist;
-
-    expect(quasar.runs.on.cordova).to.exist;
-    expect(quasar.runs.on.cordova).to.equal(false);
-
-    expect(quasar.runs.on.mobile).to.exist;
     expect(quasar.runs.with.touch).to.exist;
-  });
-
-  it('should have only boolean properties', function() {
-    _.each(quasar.runs.on, function(property) {
-      expect(property).to.be.boolean;
-    });
-    _.each(quasar.runs.with, function(property) {
-      expect(property).to.be.boolean;
-    });
   });
 
 });
