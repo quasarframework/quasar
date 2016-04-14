cfad13b5424baa4161da020d73d050200b7273e4
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-04-14 14:52:06 +0300

    feat: Table

---
+++ preview/src/pages/table/script.table.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var html = require('raw!./view.table.html');
+
+module.exports = {
+  template: html,
+  data: {
+    styles: [
+      '',
+      'inner-delimiter',
+      'bordered',
+      'striped',
+      'highlight',
+      'centered',
+      'responsive'
+    ]
+  }
+};
