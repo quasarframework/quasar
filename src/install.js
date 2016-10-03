b43034402776322ffe2046a970ecea456f1f509e
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-10-03 23:57:53 +0300

    feat: [vue2] Vue.partial

--- src/install.js
+++ src/install.js
@@ -49,7 +49,7 @@ import Search from './vue-components/search/search.vue'
 import Select from './vue-components/select/select.vue'
 import DialogSelect from './vue-components/select/dialog-select.vue'
 import Slider from './vue-components/slider/slider.vue'
-import registerSpinners from './vue-components/spinner/spinner'
+import Spinner from './vue-components/spinner/spinner.vue'
 import State from './vue-components/state/state.vue'
 import Stepper from './vue-components/stepper/stepper.vue'
 import Step from './vue-components/stepper/step.vue'
@@ -111,6 +111,7 @@ function registerComponents (_Vue) {
   _Vue.component('quasar-select', Select)
   _Vue.component('quasar-dialog-select', DialogSelect)
   _Vue.component('quasar-slider', Slider)
+  _Vue.component('spinner', Spinner)
   _Vue.component('quasar-state', State)
   _Vue.component('quasar-stepper', Stepper)
   _Vue.component('quasar-step', Step)
@@ -136,7 +137,6 @@ export default function (_Vue) {
   registerFilters(_Vue)
   registerDirectives(_Vue)
   registerComponents(_Vue)
-  registerSpinners(_Vue)
 
   Events.trigger('app:vue-ready', Vue)
 }
---
+++ src/vue-components/spinner/spinner.vue
@@ -0,0 +1,63 @@
+<template>
+  <component :is="name" :size="size" :color="color"></component>
+</template>
+
+<script>
+import Audio from './svg-spinners/audio.vue'
+import Ball from './svg-spinners/ball.vue'
+import Bars from './svg-spinners/bars.vue'
+import Circles from './svg-spinners/circles.vue'
+import Dots from './svg-spinners/dots.vue'
+import Facebook from './svg-spinners/facebook.vue'
+import Gears from './svg-spinners/gears.vue'
+import Grid from './svg-spinners/grid.vue'
+import Hearts from './svg-spinners/hearts.vue'
+import Hourglass from './svg-spinners/hourglass.vue'
+import Inf from './svg-spinners/infinity.vue'
+import Ios from './svg-spinners/ios.vue'
+import Oval from './svg-spinners/oval.vue'
+import Pie from './svg-spinners/pie.vue'
+import Puff from './svg-spinners/puff.vue'
+import Radio from './svg-spinners/radio.vue'
+import Rings from './svg-spinners/rings.vue'
+import Tail from './svg-spinners/tail.vue'
+
+import { current as theme } from '../../theme'
+
+export default {
+  props: {
+    name: {
+      type: String,
+      default: theme === 'ios' ? 'ios' : 'tail'
+    },
+    size: {
+      type: Number,
+      default: 64
+    },
+    color: {
+      type: String,
+      default: '#000'
+    }
+  },
+  components: {
+    Audio,
+    Ball,
+    Bars,
+    Circles,
+    Dots,
+    Facebook,
+    Gears,
+    Grid,
+    Hearts,
+    Hourglass,
+    Inf,
+    Ios,
+    Oval,
+    Pie,
+    Puff,
+    Radio,
+    Rings,
+    Tail
+  }
+}
+</script>
