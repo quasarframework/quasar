7bff59e97aec495eeb1a0163c890b16742aae950
SVogel <SVogel>
2016-10-04 10:30:35 +0200

    added address bar styling on mobile

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,6 @@
+import Platform from './platform.js'
+import rgbHex from 'rgb-hex'
+
 export function set (theme) {
   if (current) {
     document.body.classList.remove(current)
@@ -5,6 +8,46 @@ export function set (theme) {
 
   current = theme
   document.body.classList.add(theme)
+
+  // add ,eta tags for mobile address bar coloring
+  if (Platform.is.mobile && !Platform.is.cordova) {
+    let tempDiv = document.createElement('div')
+    tempDiv.style.height = '10px'
+    tempDiv.style.position = 'absolute'
+    tempDiv.style.top = '-100000px'
+    tempDiv.className = 'bg-primary'
+    document.body.appendChild(tempDiv)
+
+    let primaryColor = window.getComputedStyle(tempDiv).getPropertyValue('background-color')
+
+    document.body.removeChild(tempDiv)
+
+    let rgb = primaryColor.match(/\d+/g)
+    let hex = '#' + rgbHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]))
+
+    // http://stackoverflow.com/a/33193739
+    let metaTag = document.createElement('meta')
+
+    if (Platform.is.winphone) {
+      // <meta name="msapplication-navbutton-color" content="#4285f4">
+      metaTag.setAttribute('name', 'msapplicaiton-navbutton-color')
+    }
+
+    // Chrome, Firefox OS, Opera
+    // TODO: add Vivaldi browser detection to platform.js
+    if (Platform.is.webkit) {
+      // <meta name="theme-color" content="#4285f4">
+      metaTag.setAttribute('name', 'theme-color')
+    }
+
+    if (Platform.is.safari) {
+      // <meta name="apple-mobile-web-app-status-bar-style" content="#4285f4">
+      metaTag.setAttribute('name', 'apple-mobile-web-app-status-bar-style')
+    }
+    metaTag.setAttribute('content', hex)
+
+    document.getElementsByTagName('head')[0].appendChild(metaTag)
+  }
 }
 
 export var current
