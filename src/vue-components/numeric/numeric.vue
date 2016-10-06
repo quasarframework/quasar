f8e7264ad8092be2928e6bb6b39633f1812cf43c
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-10-06 23:41:24 +0300

    feat: [vue2] Numeric

--- src/vue-components/numeric/numeric.vue
+++ src/vue-components/numeric/numeric.vue
@@ -1,63 +1,87 @@
 <template>
   <div class="quasar-numeric textfield row inline items-center" :class="{disabled: disable}">
-    <i @click="setByOffset(-1)">remove</i>
+    <i @click="__setByOffset(-1)">remove</i>
+    {{min}}-{{max}}={{value}}
     <input
-      class="no-style auto"
+      class="no-style auto quasar-input-field"
       type="text"
       v-model.number="model"
-      class="quasar-input-field"
+      @keydown.enter="__updateValue()"
+      @blur="__updateValue()"
+      @keydown.up="__setByOffset(1)"
+      @keydown.down="__setByOffset(-1)"
+      :disabled="disable"
       :style="{width: (''+model).length * .7 + 'em'}"
-      v-attr="attrib"
     >
-    <i @click="setByOffset(1)">add</i>
+    <i v-show="value !== model">check</i>
+    <i @click="__setByOffset(1)">add</i>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    model: {
+    value: {
       type: Number,
-      default: 0,
-      coerce: value => parseFloat(value, 10) || 0
+      default: 0
     },
     step: {
       type: Number,
-      default: 1,
-      coerce: value => parseFloat(value, 10)
+      default: 1
     },
     min: Number,
     max: Number,
     disable: {
       type: Boolean,
-      default: false,
-      coerce: Boolean
+      default: false
     }
   },
   watch: {
-    model () {
-      this.$nextTick(this.validate)
+    value () {
+      console.log('watching')
+      this.model = this.value
     }
   },
-  computed: {
-    attrib () {
-      return this.disable ? 'disabled' : []
+  data () {
+    return {
+      model: this.value
     }
   },
   methods: {
-    validate () {
-      if (typeof this.min === 'number' && this.model < this.min && this.model !== 0) {
-        this.model = this.min
+    __normalize (value) {
+      if (typeof this.min === 'number' && value < this.min) {
+        return this.min
       }
-      else if (typeof this.max === 'number' && this.model > this.max) {
-        this.model = this.max
+      else if (typeof this.max === 'number' && value > this.max) {
+        return this.max
+      }
+      return value
+    },
+    __updateValue () {
+      this.model = this.__normalize(this.model)
+      if (!this.disable && this.value !== this.model) {
+        this.$emit('input', this.model)
       }
     },
-    setByOffset (direction) {
+    __setByOffset (direction) {
       if (!this.disable) {
-        this.model += direction * this.step
+        let newValue = this.model + direction * this.step
+        if (typeof this.min === 'number' && newValue < this.min && this.model === this.min) {
+          return
+        }
+        if (typeof this.max === 'number' && newValue > this.max && this.model === this.max) {
+          return
+        }
+        this.model = newValue
+        this.__updateValue()
       }
     }
+  },
+  beforeUpdate () {
+    this.__updateValue()
+  },
+  created () {
+    this.__updateValue()
   }
 }
 </script>
