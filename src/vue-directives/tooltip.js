0a72854599c4fb1d8aa93d0c48936892d80e117c
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-10-13 10:18:23 +0300

    refactor: [vue2] Updates to Tooltip

--- src/vue-directives/tooltip.js
+++ src/vue-directives/tooltip.js
@@ -1,31 +1,18 @@
-let data = {}
-
 export default {
   bind (el, binding) {
-    let ctx = {}
-
-    if (binding.modifiers.wrap) {
-      ctx.target = document.createElement('div')
-
-      el.parentNode.insertBefore(ctx.target, el)
-      ctx.target.appendChild(el)
-    }
-    else {
-      ctx.target = el
-    }
-
-    ctx.target.classList.add('quasar-tooltip')
+    el.setAttribute('quasar-tooltip', binding.value)
+    el.classList.add('quasar-tooltip')
 
     if (binding.modifiers.inline) {
-      ctx.target.classList.add('flex', 'inline')
+      el.classList.add('flex', 'inline')
     }
-
-    ctx.target.setAttribute('quasar-tooltip', binding.value)
-    data[el] = ctx
   },
   update (el, binding) {
     if (binding.value !== binding.oldValue) {
-      data[el].target.setAttribute('quasar-tooltip', binding.value)
+      el.setAttribute('quasar-tooltip', binding.value)
     }
+  },
+  unbind (el) {
+    el.removeAttribute('quasar-tooltip')
   }
 }
