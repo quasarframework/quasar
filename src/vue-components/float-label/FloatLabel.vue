aca595ef0a4ba46a1b7c0544c98812775879f4b2
CookieJon <cookie.jon@gmail.com>
2016-12-11 19:57:40 +1100

    WIP - Finalised name 'float-label'
    WIP - Finalised name 'float-label'

--- src/vue-components/float-label/FloatLabel.vue
+++ src/vue-components/float-label/FloatLabel.vue
@@ -1,69 +1,86 @@
 <template>
-  <div
-    class='floating-label'
-    :class='cssFloatingLabelActivity'
-  >
-    <slot ref='input'></slot>
+  <div :class='cssFloatingLabel'>
+    <i v-if='icon'>{{ icon }}</i>
+    <slot></slot>
     <label>{{ label }}</label>
-
-    <pre>{{ state }} </pre>
   </div>
 </template>
 
 <script>
-// import Utils from '../../utils'
-// import Platform from '../../features/platform'
-/* eslint-disable */
-
 // TODO:
-// - init state values during mount()
-// - tidy css
-// -
-
+/* eslint-disable */
+// - React to changes in input attributes: disabled/readonly/value, etc.
+// - iOS styles
 export default {
+  name: 'q-float-label',
   props: {
     label: {
       type: String,
       required: true
+    },
+    layout: {
+      type: String, // 'floating' | 'stacked' | 'inline'
+      default: 'floating'
+    },
+    icon: {
+      type: String
+    },
+    dense: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
     return {
+      input: null,
+      inputType: null,
+      isStacked: this.layout === 'stacked',
+      isInline: this.layout === 'inline',
       state: {
-        inputType: null,
         hasFocus: false,
         hasValue: false,
-        hasValid: false,
-        hasDisabled: false,
-        hasReadonly: false  // can have focus
+        hasValid: false, // (unused)
+        hasDisabled: false, // (unused)
+        hasReadOnly: false,  // can receive focus
       }
     }
   },
   computed: {
-    isActive () {
+    cssFloatingLabel () {
       let s = this.state
-      return s.hasValue || ( s.hasFocus && !s.hasReadOnly )  // Is it actually this simple!?
-    },
-    cssFloatingLabelActivity () {
-      return this.isActive === true ? 'floating-label-active' : 'floating-label-inactive'
+      let css = {
+        'float-label': true, // NB: Distinct from existing Quasar 'floating-label'
+        'label-active': this.isStacked || s.hasValue || s.hasReadOnly || s.hasFocus,
+        'label-focus': s.hasFocus,
+        'label-icon': this.icon,
+        'label-dense': this.dense,
+        'label-inline': this.isInline
+      }
+      return css
     }
   },
   methods: {
     __update (e) {
       let
         s = this.state,
-        input = e.target
-      s.hasFocus = e.type === 'focus' ? true : e.type === 'blur' ? false : s.hasFocus
-      s.hasValue = input.value ? true : false
-      s.hasValid = input.validity.valid
-      s.hasDisabled = input.disabled
-      s.hasReadonly = input.readOnly
+        etype = e ? e.type : ''
+      s.hasFocus = etype === 'focus' ? true : etype === 'blur' ? false : s.hasFocus
+      s.hasValue = this.input.value ? true : false
+      s.hasDisabled = this.input.disabled
+      s.hasReadOnly = this.input.readOnly
+      s.hasValid = this.input.validity.valid
     }
   },
   mounted () {
-    // this.state.inputType = this.$refs['input'].type
-    this.$el.addEventListener('focus', this.__update, true)
-    this.$el.addEventListener('blur', this.__update, true)
+      this.input = this.$el.querySelector('input, textarea')
+      if (!this.input) {
+        console.warn('"<q-floating-label>" missing required <input> or <textarea>.')
+        return
+      }
+      this.inputType = this.input.type
+      this.$el.addEventListener('focus', this.__update, true)
+      this.$el.addEventListener('blur', this.__update, true)
+      this.__update()
   },
   beforeDestroy () {
     this.$el.removeEventListener('focus', this.__update, true)
