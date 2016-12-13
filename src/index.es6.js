6074b0413b866f0cd62a712016d1fb9f6c2b905f
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-12-13 19:35:57 +0200

    chore: Bump to v0.10.2

--- src/index.es6.js
+++ src/index.es6.js
@@ -26,7 +26,7 @@ import Utils from './utils'
 import { LocalStorage, SessionStorage } from './features/web-storage'
 
 let Quasar = {
-  version: '0.10.1',
+  version: '0.10.2',
   install,
   start,
   theme
