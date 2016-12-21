5923c9dd656b81222d3466b90aa02bfe2f18fa1d
Francesco Saverio Giudice <info@fsgiudice.com>
2016-12-21 02:27:01 +0100

    Add back-to-top component

---
+++ dev/components/components/back-to-top.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="layout-padding">
+      <p class="caption">Scroll down to see it in action.</p>
+
+      <br>
+      <q-back-to-top> <!-- :handler="refresher"> -->
+        <p v-for="n in 100" class="caption">
+          <span class="label bg-secondary text-white shadow-1">
+            {{ n }}
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+
+      </q-back-to-top>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/vue-components/back-to-top/BackToTop.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="q-back-to-top">
+    <div ref="content" class="q-back-to-top">
+      <slot></slot>
+    </div>
+    <div class="q-back-to-top-fab" v-show="showBackButton">
+      <button class="circular raised" @click="__goTop()" :class="classNames" style="right: 18px; bottom: 18px;">
+        <i class="q-fab-icon">{{icon}}</i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Utils from '../../utils'
+
+export default {
+  props: {
+    classNames: {
+      default: 'primary fixed-bottom-right'
+    },
+    icon: {
+      type: String,
+      default: 'keyboard_arrow_up'
+    },
+    offset: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      showBackButton: false,
+      working: true
+    }
+  },
+  methods: {
+    __processResize () {
+      if (!this.scrollContainer) {
+        return
+      }
+
+      this.containerHeight = Utils.dom.height(this.scrollContainer)
+      this.containerTop = Utils.dom.offset(this.scrollContainer).top
+      this.containerBottom = Utils.dom.offset(this.scrollContainer).top + this.containerHeight
+
+      this.triggerPosition = Math.round(this.containerTop + this.containerBottom / 2 + this.offset)
+
+      this.__updatePosition()
+    },
+    __updatePosition (event) {
+      if (!this.working) {
+        return
+      }
+
+      this.position = window.scrollY
+
+      if ((this.position) > this.triggerPosition) { // < this.containerBottom) {
+        this.showBackButton = true
+      }
+      else {
+        this.showBackButton = false
+      }
+    },
+    __goTop () {
+      if (!this.working) {
+        return
+      }
+      this.stop()
+      Velocity(this.element, 'scroll', {offset: -this.position, mobileHA: false})
+      this.resume()
+    },
+    resume () {
+      this.working = true
+      this.scrollContainer.addEventListener('scroll', this.__updatePosition)
+      this.__updatePosition()
+    },
+    stop () {
+      this.working = false
+      this.scrollContainer.removeEventListener('scroll', this.__updatePosition)
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.element = this.$refs.content
+
+      this.scrollContainer = Utils.dom.getScrollTarget(this.$el)
+      this.resizeHandler = Utils.debounce(this.__processResize, 50)
+
+      window.addEventListener('resize', this.resizeHandler)
+      this.scrollContainer.addEventListener('scroll', this.__updatePosition)
+      this.__processResize()
+    })
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.resizeHandler)
+    this.scrollContainer.removeEventListener('scroll', this.__updatePosition)
+  }
+}
+</script>
