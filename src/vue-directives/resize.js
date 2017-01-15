ffcf3ae0e332d6c6a7dbc1dbb4f6268fa79c52a6
Francesco Saverio Giudice <info@fsgiudice.com>
2017-01-15 13:12:55 +0100

    2017-01-15 13:12 UTC+0100 Francesco Saverio Giudice (info/at/fsgiudice.com)   * src\vue-directives\resize.js     * Cleaned and fixed comments

--- src/vue-directives/resize.js
+++ src/vue-directives/resize.js
@@ -4,10 +4,8 @@ import Utils from '../utils'
   v-window-resize
   directive
 
-  args: id|tag|<none>
-  id = search if click is outside node with passed id - example: v-click-outside:id.myElementId
-  tag = search if click is outside node with passed selector - example: v-click-outside:tag.my-tag-element or v-click-outside:tag.div.layout
-  if no args is passed check if click is outside of element or its childs
+  args: <none>|window
+  if no args it returns element width, otherwise window width
 
 */
 
@@ -19,34 +17,29 @@ export default {
       context: vnode.context,
       action: binding.expression,
       hookEvent: Utils.debounce((event) => {
-        // debugger
 
         let arg = binding.arg
         let width
-        console.log('event resize')
+        // console.log('event resize')
 
-        // check if event.target is different from element itself
         if (arg) {
-          // v-ckick-outside:id.elementId
           if (arg === 'window') {
             width = Utils.dom.viewport().width
           }
         }
-          // v-ckick-outside - not self
         else {
           width = Utils.dom.width(ctx.element)
         }
-        // event.stopImmediatePropagation()
         ctx.context[ctx.action](width)
       }, 300)
     }
-    console.log('resize bind')
+    // console.log('resize bind')
     window.addEventListener('resize', ctx.hookEvent)
     Utils.store.add('windowresize', el, ctx)
   },
 
   unbind: function (el, binding, vnode) {
-    console.log('resize unbind')
+    // console.log('resize unbind')
     window.removeEventListener('resize', Utils.store.get('windowresize', el).hookEvent)
     Utils.store.remove('windowresize', el)
   }
