5b2bd618476856de8b44dbd32028b6d0adb30e2c
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-03-12 19:46:57 +0200

    feat: Add Slider props to GallerySlider

---
+++ src/vue-components/slider/slider-mixin.js
@@ -0,0 +1,14 @@
+export default {
+  props: {
+    arrows: Boolean,
+    dots: Boolean,
+    fullscreen: Boolean,
+    infinite: Boolean,
+    actions: Boolean,
+    animation: {
+      type: Boolean,
+      default: true
+    },
+    autoplay: [Number, Boolean]
+  }
+}
