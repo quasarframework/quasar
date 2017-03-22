0d98e4ab2ea503e597ee5a01925afea4ef33e950
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-03-22 13:33:43 +0200

    refactor: Addition to previous commit on Icon component

--- dev/components/components/icon.vue
+++ dev/components/components/icon.vue
@@ -3,10 +3,15 @@
     <div>
       <q-icon :icon="icon" class="gigi" style="font-size: 3rem;" /> Text
     </div>
-    <q-input-group type="radio" v-model="icon" :options="[
-      {value: 'cloud', label: 'Material'},
-      {value: 'fa-github', label: 'Github'}
-    ]" />
+    <q-input-group
+      type="radio"
+      v-model="icon"
+      :options="[
+        {value: 'cloud', label: 'A Material icon'},
+        {value: 'fa-github', label: 'A Font Awesome icon'}
+      ]"
+      style="margin-top: 25px"
+    />
   </div>
 </template>
 
