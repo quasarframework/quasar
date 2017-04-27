5e33988977f172d4cb149224aff09e7448822a53
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-04-27 18:55:07 +0800

    fix isNumber

--- src/utils/is.js
+++ src/utils/is.js
@@ -20,7 +20,7 @@ export function isRegexp (v) {
 }
 
 export function isNumber (v) {
-  return typeof v === 'boolean'
+  return !isNaN(parseFloat(v)) && isFinite(v);
 }
 
 export function isString (v) {
