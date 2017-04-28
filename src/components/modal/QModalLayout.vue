915ff5333371d4f87a2d06358cc714f4a399620d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-04-28 11:51:07 +0300

    feat: Improving QModalLayout styling

--- src/components/modal/QModalLayout.vue
+++ src/components/modal/QModalLayout.vue
@@ -1,19 +1,45 @@
 <template>
-  <div class="column absolute-full">
-    <slot name="header"></slot>
-    <slot v-if="$q.theme !== 'ios'" name="navigation"></slot>
+  <div
+    class="q-modal-layout column absolute-full"
+    :style="headerStyle"
+    :class="headerClass"
+  >
+    <div class="layout-header">
+      <slot name="header"></slot>
+      <slot v-if="$q.theme !== 'ios'" name="navigation"></slot>
+    </div>
 
-    <div class="auto scroll">
+    <div
+      class="q-modal-layout-content auto scroll"
+      :style="contentStyle"
+      :class="contentClass"
+    >
       <slot></slot>
     </div>
 
-    <slot name="footer"></slot>
-    <slot v-if="$q.theme === 'ios'" name="navigation"></slot>
+    <div
+      class="layout-footer"
+      :style="footerStyle"
+      :class="footerClass"
+    >
+      <slot name="footer"></slot>
+      <slot v-if="$q.theme === 'ios'" name="navigation"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'q-modal-layout'
+  name: 'q-modal-layout',
+  props: {
+    headerStyle: [String, Object, Array],
+    headerClass: [String, Object, Array],
+
+    contentStyle: [String, Object, Array],
+    contentClass: [String, Object, Array],
+
+    footerStyle: [String, Object, Array],
+    footerClass: [String, Object, Array]
+  }
 }
 </script>
