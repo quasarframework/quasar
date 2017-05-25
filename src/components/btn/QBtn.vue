08c462b056ef98d447ade0b6e579f92f3b03835f
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-05-25 20:32:04 +0300

    feat: Small tweak to QButton

--- src/components/btn/QBtn.vue
+++ src/components/btn/QBtn.vue
@@ -3,18 +3,7 @@
     v-ripple.mat
     @click="click"
     class="q-btn row inline items-center justify-center q-focusable q-hoverable"
-    :class="{
-      disabled: disable || loading,
-      'q-btn-outline': outline,
-      'q-btn-flat': flat,
-      'q-btn-rounded': rounded,
-      'q-btn-push': push,
-      'q-btn-no-uppercase': noCaps,
-      [shape]: true,
-      [size]: true,
-      [`bg-${color}`]: color && !flat && !outline,
-      [`text-${flat || outline ? color : 'white'}`]: color
-    }"
+    :class="classes"
   >
     <div class="q-focus-helper"></div>
 
@@ -83,6 +72,37 @@ export default {
     },
     shape () {
       return `q-btn-${this.round ? 'round' : 'rectangle'}`
+    },
+    classes () {
+      const cls = [this.shape, this.size]
+
+      if (this.disable || this.loading) {
+        cls.push('disabled')
+      }
+      if (this.flat) {
+        cls.push('q-btn-flat')
+      }
+      else if (this.outline) {
+        cls.push('q-btn-outline')
+      }
+      else if (this.push) {
+        cls.push('q-btn-push')
+      }
+
+      this.noCaps && cls.push('q-btn-no-uppercase')
+      this.rounded && cls.push('q-btn-rounded')
+
+      if (this.color) {
+        if (this.flat || this.outline) {
+          cls.push(`text-${this.color}`)
+        }
+        else {
+          cls.push(`bg-${this.color}`)
+          cls.push(`text-white`)
+        }
+      }
+
+      return cls
     }
   },
   methods: {
