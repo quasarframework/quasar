d5916dca038bfd7053887d1748cced99ab915402
Dan Popescu <pdan.popescu@gmail.com>
2017-05-30 10:30:28 +0300

    fix: IE - add Array.prototype.findIndex polyfill

--- src/features/polyfills.js
+++ src/features/polyfills.js
@@ -106,3 +106,27 @@ if (!Array.prototype.find) {
     }
   })
 }
+
+if (!Array.prototype.findIndex) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    value (predicate) {
+      'use strict'
+      if (this == null) {
+        throw new TypeError('Array.prototype.findIndex called on null or undefined')
+      }
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function')
+      }
+      var list = Object(this)
+      var length = list.length >>> 0
+      var thisArg = arguments[1]
+
+      for (var i = 0; i < length; i++) {
+        if (predicate.call(thisArg, list[i], i, list)) {
+          return i
+        }
+      }
+      return -1
+    }
+  })
+}
