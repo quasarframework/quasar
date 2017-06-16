768cbff1b6e5a1cd4a51d53369e613988f28ae25
Dan Popescu <pdan.popescu@gmail.com>
2017-06-16 16:06:29 +0300

    Fix Object.values polyfill

--- dev/polyfills.js
+++ dev/polyfills.js
@@ -2,6 +2,10 @@
 
 require('es6-promise').polyfill()
 
+if (!Object.values) {
+  require('object.values').shim()
+}
+
 if (!Number.isInteger) {
   Number.isInteger = function (value) {
     return typeof value === 'number' &&
@@ -48,14 +52,4 @@ if (!Array.prototype.findIndex) {
       return -1
     }
   })
-}
-
-if (!Object.values) {
-  const reduce = Function.bind.call(Function.call, Array.prototype.reduce)
-  const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable)
-  const concat = Function.bind.call(Function.call, Array.prototype.concat)
-  const keys = Reflect.ownKeys
-  Object.values = function values (O) {
-    return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), [])
-  }
 }
