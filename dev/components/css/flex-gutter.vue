027d761d2d7ed998e414df342a18bacf9b9e3aea
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-06-27 03:36:41 +0300

    feat: Flex gutter classes #653

---
+++ dev/components/css/flex-gutter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout-padding gutter-demo">
+    <div class="container" v-for="n in ['xs', 'sm', 'md', 'lg', 'xl']">
+      {{n}}-gutter
+      <div class="row" :class="`${n}-gutter`">
+        <div class="col-4" v-for="n in 5">
+          <div>&nbsp;</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="stylus">
+.gutter-demo
+  .container
+    border 1px solid black
+  .container + .container
+    margin-top 64px
+  .row
+    // border 1px solid rgba(0, 0, 0, .3)
+    > div > div
+      background rgba(0, 0, 0, .3)
+</style>
