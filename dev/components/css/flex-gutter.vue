f5e39e2ed52151b44de8bec50c33b9487bb721a5
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-06-27 19:33:16 +0300

    chore: Addition to gutter demo

--- dev/components/css/flex-gutter.vue
+++ dev/components/css/flex-gutter.vue
@@ -26,6 +26,30 @@
         </div>
       </div>
     </div>
+
+    <h1>QInput example</h1>
+    <div>
+      <div class="row sm-gutter">
+        <div class="col-12">
+          <q-input inverted v-model="model" class="no-margin" float-label="col-12" />
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <q-input inverted v-model="model" class="no-margin" float-label="col-xs-12 col-sm-6 TOP LEFT" />
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <q-input inverted v-model="model" class="no-margin" float-label="col-xs-12 col-sm-6 TOP RIGHT" />
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <q-input inverted v-model="model" class="no-margin" float-label="col-xs-12 col-sm-6 BOTTOM LEFT" />
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <q-input inverted v-model="model" class="no-margin" float-label="col-xs-12 col-sm-6 BOTTOM RIGHT" />
+        </div>
+        <div class="col-12">
+          <q-input inverted v-model="model" class="no-margin" float-label="col-12" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,7 +57,7 @@
 export default {
   data () {
     return {
-      gigi: 'some text'
+      model: 'some text'
     }
   }
 }
