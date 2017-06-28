23f29b2e714b062e20cc1ccd717227a1978b7845
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-06-29 00:49:46 +0300

    fix: getDateDiff(_, _, 'hours') returns 10x more

--- src/utils/date.js
+++ src/utils/date.js
@@ -5,7 +5,7 @@ import { pad, capitalize } from './format'
 
 const
   MILLISECONDS_IN_DAY = 86400000,
-  MILLISECONDS_IN_HOUR = 360000,
+  MILLISECONDS_IN_HOUR = 3600000,
   MILLISECONDS_IN_MINUTE = 60000,
   token = /d{1,4}|M{1,4}|m{1,2}|w{1,2}|D{1,4}|YY(?:YY)?|H{1,2}|h{1,2}|s{1,2}|S{1,3}|Z{1,2}|[QExX]'/g
 
