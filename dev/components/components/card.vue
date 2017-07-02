44bfea0128ff65590e924429047b6d604de874e9
Jonathan Feldman <JonathanSFeldman@gmail.com>
2017-07-02 00:20:49 -0400

    Fixed issue with padding if the title was colored and the body wasn't

--- dev/components/components/card.vue
+++ dev/components/components/card.vue
@@ -47,15 +47,6 @@
       <q-card inline>
         <q-card-title color="primary">
           Title
-        </q-card-title>
-        <q-card-main color="secondary">
-          {{lorem}}
-        </q-card-main>
-      </q-card>
-
-      <q-card inline>
-        <q-card-title color="primary">
-          Title
           <span slot="subtitle">Subtitle</span>
         </q-card-title>
         <q-card-main color="secondary">
