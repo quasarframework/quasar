5021aa4f228ecde6fc6601f8b6d1fc256a9b9e23
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-07-05 13:45:07 +0300

    fix: debounce helper breaks if immediate is set to true #689

--- dev/components/web-tests/fast-test.vue
+++ dev/components/web-tests/fast-test.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="layout-padding">
-    <!--
-      This is for fast tests.
-      Use this page but don't add it into your commits (leave it outside
-      of your commit).
-
-      For some test that you think it should be persistent,
-      make a new *.vue file here or in another folder under /dev/components.
-    -->
+  <div>
+    <q-btn @click="handleClick">True</q-btn>
+    <q-btn @click="handleClick2">False</q-btn>
+    <q-btn @click="handleClick3">0 delay</q-btn>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
+  import {
+    QBtn,
+    debounce
+  } from 'quasar'
+
+  export default {
+    methods: {
+      handleClick: debounce(function () {
+        console.log('tClick handled')
+      }, 1000, true),
+      handleClick2: debounce(function () {
+        console.log('fClick handled')
+      }, 1000, false),
+      handleClick3: debounce(function () {
+        console.log('fClick handled')
+      }, 0, true)
+    },
+
+    components: {
+      QBtn
     }
-  },
-  methods: {
   }
-}
 </script>
--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,57 +1,31 @@
 
-const now = Date.now
-
 export function debounce (fn, wait = 250, immediate) {
-  let
-    timeout, params, context, timestamp, result,
-    later = () => {
-      let last = now() - timestamp
-
-      if (last < wait && last >= 0) {
-        timeout = setTimeout(later, wait - last)
-      }
-      else {
-        timeout = null
-        if (!immediate) {
-          result = fn.apply(context, params)
-          if (!timeout) {
-            context = params = null
-          }
-        }
-      }
-    }
-
+  let timeout
   return function (...args) {
-    context = this
-    timestamp = now()
-    params = args
-
-    if (!timeout) {
-      timeout = setTimeout(later, wait)
+    const later = () => {
+      timeout = null
+      if (!immediate) {
+        fn.apply(this, args)
+      }
     }
+
+    clearTimeout(timeout)
     if (immediate && !timeout) {
-      result = fn.apply(context, args)
-      context = params = null
+      fn.apply(this, args)
     }
-
-    return result
+    timeout = setTimeout(later, wait)
   }
 }
 
 export function frameDebounce (fn) {
-  let
-    wait = false,
-    param
+  let wait = false
 
   return function (...args) {
-    param = args
-    if (wait) {
-      return
-    }
+    if (wait) { return }
 
     wait = true
     window.requestAnimationFrame(() => {
-      fn.apply(this, param)
+      fn.apply(this, args)
       wait = false
     })
   }
