541427c15573536a03f18956b5ee641989c71190
Popescu Dan <pdan.popescu@gmail.com>
2017-07-13 15:55:33 +0300

    Fix QInputFrame defaultHandler call (#708)

--- src/components/input-frame/QInputFrame.vue
+++ src/components/input-frame/QInputFrame.vue
@@ -13,7 +13,7 @@
         class="q-if-control q-if-control-before"
         :class="{hidden: __additionalHidden(item, hasError, length)}"
         :name="item.icon"
-        @click="item.handler || __defaultHander"
+        @click="(item.handler || __defaultHandler)($event)"
       ></q-icon>
     </template>
 
@@ -50,7 +50,7 @@
         class="q-if-control"
         :class="{hidden: __additionalHidden(item, hasError, length)}"
         :name="item.icon"
-        @click="item.handler || __defaultHandler"
+        @click="(item.handler || __defaultHandler)($event)"
       ></q-icon>
     </template>
   </div>
