146b75b2217a8dc6cb8296b12105d16933528b9a
Jonathan Feldman <JonathanSFeldman@gmail.com>
2017-07-15 00:44:40 -0400

    Added input-frame-color variable to core.variables.styl and colors.styl

--- src/components/input-frame/input-frame-mixin.js
+++ src/components/input-frame/input-frame-mixin.js
@@ -24,7 +24,7 @@ export default {
     disable: Boolean,
     color: {
       type: String,
-      default: 'primary'
+      default: 'input-frame-color'
     },
     dark: Boolean,
     before: marginal,
