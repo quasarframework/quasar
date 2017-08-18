695bf3a9b49012273907641dfb1c5ee1b47b30f4
wagner <wagner.el.lima@gmail.com>
2017-08-17 23:57:48 -0400

    - translation for French

---
+++ src/i18n/fr.js
@@ -0,0 +1,22 @@
+export const i18n = {
+  dayNames: 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
+  monthNames: 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
+  clearLabel: 'Clair',
+  okLabel: 'Ensemble',
+  cancelLabel: 'Annuler',
+  selectLabel: 'Sélectionner',
+  dataTable: {
+    columns: 'Colonnes',
+    allCols: 'Toutes les colonnes',
+    rows: 'Lignes',
+    selected: {
+      singular: 'élément sélectionné.',
+      plural: 'éléments sélectionnés.'
+    },
+    clear: 'Clair',
+    search: 'Rechercher',
+    all: 'Tout',
+    noDataAfterFiltering: '<i class="material-icons">warning</i> Aucun résultat. Veuillez affiner les termes de votre recherche.',
+    noData: '<i class="material-icons">warning</i> Aucune donnée disponible pour montrer.'
+  }
+}
