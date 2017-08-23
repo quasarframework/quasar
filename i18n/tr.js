708ce3cb548dfa541b29da0abfee2a79941b3980
guyulmaz <guyulmaz@gmail.com>
2017-08-23 13:41:39 +0300

    i18n for Turkish is added, also a code contribution guide text is added.

---
+++ i18n/tr.js
@@ -0,0 +1,22 @@
+export const i18n = {
+    dayNames: 'Pazar_Pazartesi_Salı_Çarşamba_Perşembe_Cuma_Cumartesi'.split('_'),
+    monthNames: 'Ocak_Şubat_Mart_Nisan_Mayıs_Haziran_Temmuz_Ağustos_Eylül_Ekim_Kasım_Aralık'.split('_'),
+    clearLabel: 'Temizle',
+    okLabel: 'Tamam',
+    cancelLabel: 'İptal',
+    selectLabel: 'Seç',
+    dataTable: {
+        columns: 'Sütunlar',
+        allCols: 'Tüm Sütunlar',
+        rows: 'Satırlar',
+        selected: {
+            singular: 'öğe seçildi.',
+            plural: 'öğeler seçildi.'
+        },
+        clear: 'Temizle',
+        search: 'Ara',
+        all: 'Tümü',
+        noDataAfterFiltering: '<i class="material-icons">warning</i> Sonuç yok. Lütfen arama terimlerini düzenleyiniz.',
+        noData: '<i class="material-icons">warning</i> Gösterilecek veri yok.'
+    }
+}
