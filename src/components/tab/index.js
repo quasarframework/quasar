1bd35322a42c0a1d927632f2c46fb104b236cc99
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-08-31 12:48:15 +0300

    feat: "keep-alive" property for QTabPane #846

--- src/components/tab/index.js
+++ src/components/tab/index.js
@@ -1,6 +1,6 @@
 import QRouteTab from './QRouteTab.vue'
 import QTab from './QTab.vue'
-import QTabPane from './QTabPane.vue'
+import QTabPane from './QTabPane'
 import QTabs from './QTabs.vue'
 
 export {
