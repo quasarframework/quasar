dcb1906ae41ac99912a8d60c75ba11e85c6f2c92
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-09-13 17:49:39 +0300

    fix: Slight label misalignment in QTab when hide prop is set to "icon" (on narrow windows only) #881

--- dev/components/test-layout/layout.vue
+++ dev/components/test-layout/layout.vue
@@ -25,7 +25,7 @@
 
     <q-tabs slot="navigation">
       <q-route-tab slot="title" icon="view_quilt" to="/test-layout/about" replace hide="icon" label="About" />
-      <q-route-tab slot="title" icon="view_day" to="/test-layout/toolbar" replace hide="icon" label="Toolbar" />
+      <q-route-tab slot="title" icon="view_day" to="/test-layout/toolbar" replace hide="label" label="Toolbar" />
       <q-route-tab slot="title" icon="view_day" to="/test-layout/tabs" replace label="Tabs" />
       <q-route-tab slot="title" icon="input" to="/test-layout/drawer" replace label="Drawer" />
     </q-tabs>
