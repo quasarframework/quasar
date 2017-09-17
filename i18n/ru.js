cde8d1c90ba6f6ebfa379de6fc9215adc914cf2c
olegvakulenko <30389307+olegvakulenko@users.noreply.github.com>
2017-09-17 15:36:09 +0300

    Create ru.js

---
+++ i18n/ru.js
@@ -0,0 +1,22 @@
+export default {
+  dayNames: 'Воскресенье_Понедельник_Вторник_Среда_Четверг_Пятница_Суббота'.split('_'),
+  monthNames: 'Январь_Февраль_Март_Апрель_Май_Июнь_Июль_Август_Сентябрь_Октябрь_Ноябрь_Декабрь'.split('_'),
+  clearLabel: 'Очистить',
+  okLabel: 'OK',
+  cancelLabel: 'Отменить',
+  selectLabel: 'Выбрать',
+  dataTable: {
+    columns: 'Колонки',
+    allCols: 'Все колонки',
+    rows: 'Столбцы',
+    selected: {
+      singular: 'объект выбран.',
+      plural: 'объекты выбраны.'
+    },
+    clear: 'Очистить',
+    search: 'Поиск',
+    all: 'Все',
+    noDataAfterFiltering: '<i class="material-icons">warning</i> Результаты отсутствуют. Измените критерии поиска.',
+    noData: '<i class="material-icons">warning</i> Нет данных для отображения.'
+  }
+}
