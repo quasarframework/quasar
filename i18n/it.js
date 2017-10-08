3885404bfa8c8847d3c6642f8d1554d8d67d937e
Laurent <laurent.aapro@gmail.com>
2017-10-08 19:16:11 +0200

    italian translation (clean file)

---
+++ i18n/it.js
@@ -0,0 +1,22 @@
+export default {
+  dayNames: 'Domenica_Lunedì_Martedì_Mercoledì_Giovedì_Venerdì_Sabato'.split('_'),
+  monthNames: 'Gennaio_Febbraio_Marzo_Aprile_Maggio_Giugno_Luglio_Settembre_Ottobre_Novembre_Dicembre'.split('_'),
+  clearLabel: 'Cancella',
+  okLabel: 'Valida',
+  cancelLabel: 'Annulla',
+  selectLabel: 'Seleziona',
+  dataTable: {
+    columns: 'Colonne',
+    allCols: 'Tutte le colonne',
+    rows: 'Righe',
+    selected: {
+      singular: 'elemento selezionato.',
+      plural: 'elementi selezionati.'
+    },
+    clear: 'Cancella',
+    search: 'Cerca',
+    all: 'Tutto',
+    noDataAfterFiltering: '<i class="material-icons">warning</i> Nessun risultato. La preghiamo di controllare i termini di ricerca.',
+    noData: '<i class="material-icons">warning</i> Nessun dato da visualizzare.'
+  }
+}
