63c40ba052d337517eb6cab13553df26b5d15b62
BenoitRanque <ranque.benoit@gmail.com>
2017-10-10 08:34:34 -0400

    spanish i18n (#971)

---
+++ i18n/es.js
@@ -0,0 +1,22 @@
+export default {
+  dayNames: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sábado'.split('_'),
+  monthNames: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Augosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+  clearLabel: 'Limpiar',
+  okLabel: 'Ok',
+  cancelLabel: 'Cancel',
+  selectLabel: 'Seleccionar',
+  dataTable: {
+    columns: 'Columnas',
+    allCols: 'Todas las Columnas',
+    rows: 'Filas',
+    selected: {
+      singular: 'item seleccionado.',
+      plural: 'items seleccionados.'
+    },
+    clear: 'Limpiar',
+    search: 'Buscar',
+    all: 'Todo',
+    noDataAfterFiltering: '<i class="material-icons">warning</i> Sin resultados. Por favor refina sus terminos de busqueda.',
+    noData: '<i class="material-icons">warning</i> No hay datos que mostrar.'
+  }
+}
