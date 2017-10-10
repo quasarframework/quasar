f0bf21806183c27deb866da43f4c784a4b44ee2d
John <git@jcharante.com>
2017-10-10 14:25:40 -0400

    i18n add Esperanto translations

---
+++ i18n/eo.js
@@ -0,0 +1,22 @@
+export default {
+  dayNames: 'Dimanĉo_Lundo_Mardo_Merkredo_Ĵaŭdo_Vendredo_Sabato'.split('_'),
+  monthNames: 'Januaro_Februaro_Marto_Aprilo_Majo_Junio_Julio_Aŭgusto_Septembro_Oktobro_Novembro_Decembro'.split('_'),
+  clearLabel: 'Klara',
+  okLabel: 'Meti',
+  cancelLabel: 'Nuligi',
+  selectLabel: 'Elektu',
+  dataTable: {
+    columns: 'Kolumnoj',
+    allCols: 'Ĉiuj Kolumnoj',
+    rows: 'Vicoj',
+    selected: {
+      singular: 'ero elektita.',
+      plural: 'eroj elektitaj.'
+    },
+    clear: 'Klara',
+    search: 'Serĉi',
+    all: 'Ĉio',
+    noDataAfterFiltering: '<i class="material-icons">warning</i> Ne estas rezultoj. Bonvolu rafini vian serĉon.',
+    noData: '<i class="material-icons">warning</i> Ne estas datumoj por vidi.'
+  }
+}
