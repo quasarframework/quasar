9796e670c005b02ff74f0d3f1eb20fa33a32814e
徐昱琪 <yockii@users.noreply.github.com>
2017-10-19 15:01:15 +0800

    i18n add Simplified Chinese translations (#1017)

---
+++ i18n/zh-CN.js
@@ -0,0 +1,22 @@
+export default {
+  dayNames: '周日_周一_周二_周三_周四_周五_周六'.split('_'),
+  monthNames: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
+  clearLabel: '清除',
+  okLabel: '确定',
+  cancelLabel: '取消',
+  selectLabel: '选择',
+  dataTable: {
+    columns: '列',
+    allCols: '所有列',
+    rows: '行',
+    selected: {
+      singular: '行被选中.',
+      plural: '行被选中.'
+    },
+    clear: '清除',
+    search: '搜索',
+    all: '全部',
+    noDataAfterFiltering: '<i class="material-icons">警告</i> 没有结果. 请改进你的搜索条件.',
+    noData: '<i class="material-icons">警告</i> 没有数据可供显示.'
+  }
+}
