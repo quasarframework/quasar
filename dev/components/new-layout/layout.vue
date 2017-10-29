85c0ef969e4e179248f79ef0e05daed1ed433e03
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-10-29 20:33:48 +0200

    HIST #2

--- dev/components/new-layout/layout.vue
+++ dev/components/new-layout/layout.vue
@@ -52,38 +52,76 @@ export default {
     },
     __popstate () {
       console.log('popstate', window.history.state)
-      if (window.history.state && window.history.state.modal) {
+      if (this.manual) {
+        console.log('popstate[manual] modals', this.modals)
+        if (this.modals > 0) {
+          console.log('popstate: manual; going back one more')
+          this.modals--
+          window.history.state.modal = null
+          window.history.go(-1)
+          return
+        }
+        else {
+          console.log('popstate: resetting manual')
+          window.history.state.modal = null
+          console.log('popstate: remove popstate')
+          this.manual = false
+          window.removeEventListener('popstate', this.__popstate)
+          this.$router.push(this.to)
+          return
+        }
+      }
+      else if (window.history.state && window.history.state.modal) {
         this.modals--
         window.history.state.modal = null
         if (this.modals === 0) {
           console.log('remove popstate')
           window.removeEventListener('popstate', this.__popstate)
         }
-      }
-      if (this.push) {
-        const to = this.push
-        console.log('pushing', to)
-        this.push = null
-        this.$router.push(to)
-      }
-      else if (this.replace) {
-        const to = this.replace
-        console.log('replacing', to)
-        this.replace = null
-        this.$router.replace(to)
+        else {
+          console.log('popstate: going back one more')
+          window.history.go(-1)
+          return
+        }
       }
       else {
-        console.log('back detected; replacing state')
-        // window.history.replaceState({}, '')
+        // manually changed route
+        console.log('manually changed route')
+        this.manual = true
+        this.next(false)
+        return
       }
+
+      if (this.next) {
+        const fn = this.next
+        this.next = null
+        console.log('calling next')
+        fn()
+        return
+      }
+
+      console.log('back detected')
     },
     __clearBogusHistory () {
       console.log('__clearBogusHistory')
       window.removeEventListener('popstate', this.__clearBogusHistory)
       if (window.history.state && window.history.state.hasBack) {
         console.log('__clearBogusHistory: hasBack; going back in history')
+        const state = window.history.state
+        state.hasBack = null
+        state.forceBack = true
+        window.history.replaceState(state, '')
         window.addEventListener('popstate', this.__clearBogusHistory)
         window.history.go(-1)
+      }
+      else {
+        window.addEventListener('popstate', () => {
+          console.log('generic popstate')
+          if (window.history.state && window.history.state.forceBack) {
+            console.log('generic popstate: detected forceBack')
+            window.history.go(-1)
+          }
+        })
       }
     }
   },
@@ -92,17 +130,9 @@ export default {
     this.__clearBogusHistory()
     this.beforeEach = this.$router.beforeEach((to, from, next) => {
       console.log('route change', from, to)
-      console.log('HISTORY length', window.history.length)
       if (this.modals) {
-        if (window.history.state) {
-          console.log('HISTORY. modal: ', window.history.state.modal, 'hasBack:', window.history.state.hasBack)
-        }
-        const prop = window.history.state && window.history.state.hasBack
-          ? 'push'
-          : 'replace'
-        console.log('going back', prop)
-        this[prop] = to
-        next(false)
+        this.next = next
+        this.to = to
         window.history.go(-1)
       }
       else {
