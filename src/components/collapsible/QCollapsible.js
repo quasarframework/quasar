bb0c5813c1278843a4312b177fa2baefb6b8af00
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-12-02 15:02:57 +0200

    chore: Small addition to previous QCollapsible commit

--- src/components/collapsible/QCollapsible.js
+++ src/components/collapsible/QCollapsible.js
@@ -99,7 +99,7 @@ export default {
       return [
         h(QItemTile, {
           slot: slot ? 'right' : undefined,
-          staticClass: 'cursor-pointer transition-generic',
+          staticClass: 'cursor-pointer transition-generic relative-position',
           'class': {
             'rotate-180': this.showing,
             invisible: this.disable
@@ -151,7 +151,7 @@ export default {
       this.$slots.header
         ? h(QItem, this.__getItemProps(), [
           this.$slots.header,
-          h(QItemSide, { props: { right: true } }, this.__getToggleSide(h))
+          h(QItemSide, { props: { right: true }, staticClass: 'relative-position' }, this.__getToggleSide(h))
         ])
         : h(QItemWrapper, this.__getItemProps(true), this.__getToggleSide(h, true)),
 
