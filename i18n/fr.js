73f8fc575ca1276533530cb6fed2af6917d7216a
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-12-07 15:25:21 +0200

    refactor(i18n): Update some languages

--- i18n/fr.js
+++ i18n/fr.js
@@ -1,5 +1,3 @@
-import { declOfNum } from '../src/utils/plural'
-
 export default {
   lang: 'fr',
   label: {
@@ -34,7 +32,7 @@ export default {
     noData: 'Aucune donnée à afficher',
     noResults: 'Aucune donnée trouvée',
     loader: 'Chargement...',
-    selectedRows: rows => rows > 0 ? `${rows} ${declOfNum(rows, ['ligne sélectionnée', 'lignes sélectionnées'])}.` : 'Non lignes sélectionnées.',
+    selectedRows: rows => rows > 0 ? `${rows} ${rows === 1 ? 'ligne sélectionnée' : 'lignes sélectionnées'}.` : 'Non lignes sélectionnées.',
     rowsPerPage: 'Lignes par page:',
     allRows: 'Tous',
     pagination: (start, end, total) => `${start}-${end} sur ${total}`,
