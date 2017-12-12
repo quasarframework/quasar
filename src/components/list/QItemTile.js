50ff844972662cddcd1373b2e6ffef297d8481f0
Dan Popescu <pdan.popescu@gmail.com>
2017-12-12 17:40:14 +0200

    feat: color and inverted for QItemSide/QItemTile letter and icon

--- src/components/list/QItemTile.js
+++ src/components/list/QItemTile.js
@@ -29,16 +29,32 @@ export default {
       prop = ctx.props,
       cls = data.staticClass,
       type = getType(prop),
-      icon = prop.icon || prop.invertedIcon
+      icon = prop.icon,
+      textColor = prop.color ? ` text-${prop.color}` : ''
 
-    data.staticClass = `q-item-${type}${prop.color ? ` text-${prop.color}` : ''}${cls ? ` ${cls}` : ''}`
+    data.staticClass = `q-item-${type}${cls ? ` ${cls}` : ''}`
 
     if (icon) {
       data.props = { name: icon }
       if (prop.inverted) {
         data.staticClass += ' q-item-icon-inverted'
+        if (prop.color) {
+          data.staticClass += ` bg-${prop.color}`
+        }
+      }
+      else {
+        data.staticClass += textColor
       }
       return h(QIcon, data, ctx.children)
+    }
+    else if (prop.letter && prop.inverted) {
+      data.staticClass += ' q-item-letter-inverted'
+      if (prop.color) {
+        data.staticClass += ` bg-${prop.color}`
+      }
+    }
+    else {
+      data.staticClass += textColor
     }
     if ((prop.label || prop.sublabel) && prop.lines) {
       if (prop.lines === '1' || prop.lines === 1) {
