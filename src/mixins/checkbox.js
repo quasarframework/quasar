53ab7c5ab0148d9b39606ab6dafe00c3bc315b18
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-12-12 19:22:03 +0200

    chore: Improve QCheckbox animation

--- src/mixins/checkbox.js
+++ src/mixins/checkbox.js
@@ -93,7 +93,7 @@ export default {
       if (ref) {
         ref.classList.add('active')
         setTimeout(() => {
-          ref.classList.remove('active')
+          ref && ref.classList.remove('active')
         }, 10)
       }
       this.$nextTick(() => {
