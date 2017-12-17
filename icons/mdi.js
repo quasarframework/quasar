742e09d496cd92b0bd37e31e5e48b5e55c28420d
mstaack <maxmatteostaack@gmail.com>
2017-12-17 19:57:53 +0100

    search update

--- icons/mdi.js
+++ icons/mdi.js
@@ -24,7 +24,7 @@ export default {
   search: {
     icon: 'mdi-magnify',
     clear: 'mdi-close-circle',
-    clearInverted: ''
+    clearInverted: 'mdi-close-circle-outline'
   },
   carousel: {
     left: 'mdi-chevron-left',
