0e17fb55101a66afda4d8e303ff0bd67eca73ca7
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-01-09 16:01:01 +0200

    chore: Small design updates to QTable

--- dev/components/components/data-table2.vue
+++ dev/components/components/data-table2.vue
@@ -103,7 +103,7 @@
       <template slot="top-right" slot-scope="props">
         <q-search hide-underline color="primary" v-model="filter" />
         <q-table-columns color="primary" class="on-right" v-model="visibleColumns" :columns="columns" />
-        <q-btn color="primary" flat round size="sm" icon="more_vert" class="on-right">
+        <q-btn color="primary" flat round dense icon="more_vert" class="on-right">
           <q-popover ref="popover">
             <q-list link>
               <q-item @click.native="$refs.popover.hide()">
