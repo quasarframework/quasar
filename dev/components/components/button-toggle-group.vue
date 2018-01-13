113d753f855ea610381f3e5d22fa1868d550393e
Réal Carbonneau <contact@realcarbonneau.com>
2018-01-12 20:29:17 -0500

    QBtnToggleGroup disable bug fix (#1411)

--- dev/components/components/button-toggle-group.vue
+++ dev/components/components/button-toggle-group.vue
@@ -11,59 +11,62 @@
           <div v-for="glossy in options" :key="glossy" v-if="!(push || flat) || !outline">
             <div v-for="rounded in options" :key="rounded">
               <div v-for="size in sizes" :key="size" class="q-ma-sm">
-                <p class="caption">
-                  {{push ? 'push ' : ''}}
-                  {{rounded ? 'rounded ' : ''}}
-                  {{outline ? 'outline ' : ''}}
-                  {{flat ? 'flat ' : ''}}
-                  {{glossy ? 'glossy ' : ''}}
-                  {{size}}
-                </p>
+                <div v-for="disable in options.slice().reverse()" :key="disable">
+                  <p class="caption">
+                    {{push ? 'push ' : ''}}
+                    {{rounded ? 'rounded ' : ''}}
+                    {{outline ? 'outline ' : ''}}
+                    {{flat ? 'flat ' : ''}}
+                    {{glossy ? 'glossy ' : ''}}
+                    {{size}}
+                    {{disable ? 'disable ' : ''}}
+                  </p>
 
-                <q-btn-toggle-group v-model="model" toggle-color="primary"
-                  :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size"
-                  :options="[
-                    {label: 'One', value: 'one'},
-                    {label: 'Two', value: 'two'},
-                    {label: 'Three', value: 'three'}
-                  ]"
-                />
+                  <q-btn-toggle-group v-model="model" toggle-color="primary"
+                    :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size" :disable="disable"
+                    :options="[
+                      {label: 'One', value: 'one'},
+                      {label: 'Two', value: 'two'},
+                      {label: 'Three', value: 'three'}
+                    ]"
+                  />
 
-                <q-btn-toggle-group v-model="model" toggle-color="primary"
-                  :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size"
-                  :options="[
-                    {label: 'One', icon: 'filter_1', value: 'one'},
-                    {label: 'Two', icon: 'filter_2', value: 'two'},
-                    {label: 'Three', icon: 'filter_3', value: 'three'}
-                  ]"
-                />
+                  <q-btn-toggle-group v-model="model" toggle-color="primary"
+                    :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size" :disable="disable"
+                    :options="[
+                      {label: 'One', icon: 'filter_1', value: 'one'},
+                      {label: 'Two', icon: 'filter_2', value: 'two'},
+                      {label: 'Three', icon: 'filter_3', value: 'three'}
+                    ]"
+                  />
 
-                <q-btn-toggle-group v-model="model" toggle-color="primary"
-                  :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size"
-                  :options="[
-                    {icon: 'filter_1', value: 'one'},
-                    {icon: 'filter_2', value: 'two'},
-                    {icon: 'filter_3', value: 'three'}
-                  ]"
-                />
+                  <q-btn-toggle-group v-model="model" toggle-color="primary"
+                    :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size" :disable="disable"
+                    :options="[
+                      {icon: 'filter_1', value: 'one'},
+                      {icon: 'filter_2', value: 'two'},
+                      {icon: 'filter_3', value: 'three'}
+                    ]"
+                  />
 
-                <q-btn-toggle-group v-model="model" toggle-color="primary"
-                  :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size"
-                  :options="[
-                    {label: 'One', iconRight: 'filter_1', value: 'one'},
-                    {label: 'Two', iconRight: 'filter_2', value: 'two'},
-                    {label: 'Three', iconRight: 'filter_3', value: 'three'}
-                  ]"
-                />
+                  <q-btn-toggle-group v-model="model" toggle-color="primary"
+                    :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size" :disable="disable"
+                    :options="[
+                      {label: 'One', iconRight: 'filter_1', value: 'one'},
+                      {label: 'Two', iconRight: 'filter_2', value: 'two'},
+                      {label: 'Three', iconRight: 'filter_3', value: 'three'}
+                    ]"
+                  />
 
-                <q-btn-toggle-group v-model="model" toggle-color="primary"
-                  :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size"
-                  :options="[
-                    {label: 'One', value: 'one'},
-                    {label: 'Two', toggleColor: 'yellow', value: 'two'},
-                    {label: 'Three', toggleColor: 'red', value: 'three'}
-                  ]"
-                />
+                  <q-btn-toggle-group v-model="model" toggle-color="primary"
+                    :push="push" :flat="flat" :outline="outline" :glossy="glossy" :rounded="rounded" :size="size" :disable="disable"
+                    :options="[
+                      {label: 'One', value: 'one'},
+                      {label: 'Two', toggleColor: 'yellow', value: 'two'},
+                      {label: 'Three', toggleColor: 'red', value: 'three'}
+                    ]"
+                  />
+                </div>
               </div>
             </div>
           </div>
