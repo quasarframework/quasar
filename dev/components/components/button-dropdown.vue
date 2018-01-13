7181850b4fd63d194161215219850e6c6c886a41
Réal Carbonneau <contact@realcarbonneau.com>
2018-01-12 20:33:06 -0500

    QBtnDropdown Disable Bug Fixes (#1408)

--- dev/components/components/button-dropdown.vue
+++ dev/components/components/button-dropdown.vue
@@ -1,11 +1,11 @@
 <template>
   <div style="padding: 25px">
-    <div v-for="(cfg, index1) in conf" :key="`${cfg.split}-${cfg.dense}`">
+    <div v-for="(cfg, index1) in conf" :key="`${cfg.split}-${cfg.dense}-${cfg.disable}`">
       <div v-for="(size, index2) in sizes" :key="size">
         <p class="caption">{{ label(cfg) }} - {{ size }}</p>
         text
-        <q-btn :size="size" color="primary" glossy :dense="cfg.dense" label="Test" />
-        <q-btn-dropdown ref="first" :size="size" :split="cfg.split" :dense="cfg.dense" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy label="Dropdown Button" style="margin: 15px">
+        <q-btn :size="size" color="primary" glossy :dense="cfg.dense" :disable="cfg.disable" label="Test" />
+        <q-btn-dropdown ref="first" :size="size" :split="cfg.split" :dense="cfg.dense" :disable="cfg.disable" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy label="Dropdown Button" style="margin: 15px">
           <q-list link>
             <q-list-header inset>Folders X</q-list-header>
             <q-item v-for="n in 3" :key="`1.${n}`" @click.native="hideDropdown(index1 * 3 + index2)">
@@ -28,7 +28,7 @@
             </q-item>
           </q-list>
         </q-btn-dropdown>
-        <q-btn-dropdown ref="second" :size="size" :split="cfg.split" :dense="cfg.dense" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" icon="map" glossy label="Dropdown Button" style="margin: 15px">
+        <q-btn-dropdown ref="second" :size="size" :split="cfg.split" :dense="cfg.dense" :disable="cfg.disable" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" icon="map" glossy label="Dropdown Button" style="margin: 15px">
           <q-list link>
             <q-list-header inset>Folders</q-list-header>
             <q-item v-for="n in 3" :key="`1.${n}`" @click.native="$refs.second[index1 * 3 + index2].hide()">
@@ -51,7 +51,7 @@
             </q-item>
           </q-list>
         </q-btn-dropdown>
-        <q-btn-dropdown ref="third" :size="size" :split="cfg.split" :dense="cfg.dense" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" icon="map" glossy style="margin: 15px">
+        <q-btn-dropdown ref="third" :size="size" :split="cfg.split" :dense="cfg.dense" :disable="cfg.disable" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" icon="map" glossy style="margin: 15px">
           <q-list link>
             <q-list-header inset>Folders</q-list-header>
             <q-item v-for="n in 3" :key="`1.${n}`" @click.native="$refs.third[index1 * 3 + index2].hide()">
@@ -74,7 +74,7 @@
             </q-item>
           </q-list>
         </q-btn-dropdown>
-        <q-btn-dropdown ref="fourth" :size="size" :split="cfg.split" :dense="cfg.dense" @show="log('open')" @hide="log('close')" @click="log('click')" color="yellow" glossy icon="map" label="Dropdown Button" style="margin: 15px">
+        <q-btn-dropdown ref="fourth" :size="size" :split="cfg.split" :dense="cfg.dense" :disable="cfg.disable" @show="log('open')" @hide="log('close')" @click="log('click')" color="yellow" glossy icon="map" label="Dropdown Button" style="margin: 15px">
           <q-list link>
             <q-list-header inset>Folders</q-list-header>
             <q-item v-for="n in 3" :key="`1.${n}`" @click.native="$refs.fourth[index1 * 3 + index2].hide()">
@@ -107,10 +107,14 @@ export default {
   data () {
     return {
       conf: [
-        {split: false, dense: false},
-        {split: false, dense: true},
-        {split: true, dense: false},
-        {split: true, dense: true}
+        {split: false, dense: false, disable: false},
+        {split: false, dense: true, disable: false},
+        {split: true, dense: false, disable: false},
+        {split: true, dense: true, disable: false},
+        {split: false, dense: false, disable: true},
+        {split: false, dense: true, disable: true},
+        {split: true, dense: false, disable: true},
+        {split: true, dense: true, disable: true}
       ],
       sizes: ['sm', 'md', 'lg']
     }
@@ -126,6 +130,9 @@ export default {
       }
       if (cfg.dense) {
         label += ' dense'
+      }
+      if (cfg.disable) {
+        label += ' disable'
       }
       return label
     },
