0536e99990089d8b9895a4cecaf7912d8d2842cc
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-01-18 16:35:07 +0200

    chore: Add flex-test page

--- dev/components/web-tests/c.vue
+++ dev/components/web-tests/c.vue
@@ -2,39 +2,5 @@
   <div class="layout-padding">
     <span>CCC</span>
     <q-btn @click="$router.replace('/web-tests/a')">Go to A</q-btn>
-
-    <div class="xs">XS (<strong>{{first}}</strong>)</div>
-    <div class="sm">SM (<strong>{{second}}</strong>)</div>
-    <div class="md">MD (<strong>{{last}}</strong>)</div>
-    <div>{{conf}}</div>
-    <div class="row ex">
-      <div :class="conf">First</div>
-      <div :class="conf">Second</div>
-      <div :class="conf">Last</div>
-    </div>
   </div>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      conf: 'col-xs-4 col-sm-auto col-md-6'
-    }
-  },
-  computed: {
-    split () {
-      return this.conf.split(' ')
-    },
-    first () {
-      return this.split[0]
-    },
-    second () {
-      return this.split[1]
-    },
-    last () {
-      return this.split[2]
-    }
-  }
-}
-</script>
