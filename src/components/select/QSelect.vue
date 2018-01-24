2f3d61023b96e005f0aaa460da27f4dd1629405b
ROUL <sebastien.roul.0@gmail.com>
2018-01-24 16:37:39 +0100

    Add specific coloring of chips on optColor of selectOptions

--- src/components/select/QSelect.vue
+++ src/components/select/QSelect.vue
@@ -33,11 +33,11 @@
       :class="alignClass"
     >
       <q-chip
-        v-for="{label, value, disable: optDisable} in selectedOptions"
+        v-for="{label, value, color: optColor, disable: optDisable} in selectedOptions"
         :key="label"
         small
         :closable="!disable && !optDisable"
-        :color="color"
+        :color="optColor || color"
         @click.native.stop
         @hide="__toggleMultiple(value, disable || optDisable)"
       >
