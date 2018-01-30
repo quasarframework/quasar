892d4a19fbdc00bfcd41b30c896bd6c187d04a2f
Dan Popescu <pdan.popescu@gmail.com>
2018-01-30 12:29:56 +0200

    Remove horizontal modifier for slider drag

--- src/components/slider/slider-utils.js
+++ src/components/slider/slider-utils.js
@@ -149,7 +149,6 @@ export let SliderMixin = {
         ? [{
           name: 'touch-pan',
           modifiers: {
-            horizontal: true,
             prevent: true,
             stop: true
           },
