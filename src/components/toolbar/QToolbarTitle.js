c72241ce79c29929d7a866894fee8c56c484c75d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-02-25 00:58:31 +0200

    feat: QToolbarTitle "shrink" prop #1629

--- src/components/toolbar/QToolbarTitle.js
+++ src/components/toolbar/QToolbarTitle.js
@@ -1,8 +1,12 @@
 export default {
   name: 'q-toolbar-title',
+  props: {
+    shrink: Boolean
+  },
   render (h) {
     return h('div', {
-      staticClass: 'q-toolbar-title'
+      staticClass: 'q-toolbar-title',
+      'class': this.shrink ? 'col-auto' : null
     }, [
       this.$slots.default,
       this.$slots.subtitle
