8105db25acac0241f72c23ed26bb64cadc958f7e
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-02-25 17:00:43 +0200

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -17,5 +17,5 @@ rm -rf deploy
 mkdir deploy
 cp -R dist i18n icons LICENSE package.json deploy
 cp "build/README-$1.md" deploy/README.md
-node_modules/git-directory-deploy/bin/git-directory-deploy.sh -ddeploy -bmaster "-rgit@github.com:pdanpdan/quasar.git"
+node_modules/git-directory-deploy/bin/git-directory-deploy.sh -ddeploy -bmaster "-rgit@github.com:quasarframework/quasar-$1.git"
 rm -rf deploy
