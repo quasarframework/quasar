556288085af8ec4c4810b87747edfdf6f942433f
Popescu Dan <pdan.popescu@gmail.com>
2018-03-07 20:46:00 +0200

    fix IE: QResizeObservable object visibility and QList main flex (#1720)
    * fix IE: QResizeObservable object visibility and QList main flex
    
    * Update QResizeObservable.js

--- src/components/observables/QResizeObservable.js
+++ src/components/observables/QResizeObservable.js
@@ -33,7 +33,7 @@ export default {
 
     this.object = object
     object.setAttribute('aria-hidden', true)
-    object.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; right: 0; bottom: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1;')
+    object.setAttribute('style', 'display:block;position:absolute;top:0;left:0;right:0;bottom:0;height:100%;width:100%;overflow:hidden;pointer-events:none;z-index:-1;visibility:hidden;')
     object.onload = () => {
       object.contentDocument.defaultView.addEventListener('resize', this.onResize, listenOpts.passive)
     }
