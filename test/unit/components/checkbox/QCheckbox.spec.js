7c43244b8fb1b70a63adc076aeb34cabd8642271
Denny Headrick <denny.headrick@gmail.com>
2018-03-15 21:40:47 -0500

    add tests

---
+++ test/unit/components/checkbox/QCheckbox.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import { QCheckbox } from '@/components/checkbox'
+
+describe('QCheckbox', () => {
+    test('updates value on click', () => {
+        const wrapper = mount(QCheckbox, {
+            
+        })
+    })
+})
