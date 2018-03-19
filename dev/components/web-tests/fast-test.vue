ac7a839056892bec7ea0351b0d6ed96b7e42b296
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-03-19 18:36:31 +0200

    fix(QTabs): default white background should go to tabs head instead -- when using inverted

--- dev/components/web-tests/fast-test.vue
+++ dev/components/web-tests/fast-test.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout-padding">
+  <div class="layout-padding bg-grey-3">
     <!--
       This is for fast tests.
       Use this page but don't add it into your commits (leave it outside
@@ -8,6 +8,15 @@
       For some test that you think it should be persistent,
       make a new *.vue file here or in another folder under /dev/components.
     -->
+    <q-tabs color="primary">
+      <q-tab default slot="title" hide="label" name="three" label="Oaua" icon="bluetooth" />
+      <q-tab alert slot="title" hide="icon" name="one" label="Gogu" icon="wifi" />
+      <q-tab alert slot="title" name="two" label="Gigiiiiiiiii sdfsdfs aadsf asfsda" icon="important_devices" />
+
+      <q-tab-pane name="one" class="no-border bg-primary text-white">Tab One</q-tab-pane>
+      <q-tab-pane name="two" class="no-border">Tab Two</q-tab-pane>
+      <q-tab-pane name="three" class="no-border">Tab Three</q-tab-pane>
+    </q-tabs>
   </div>
 </template>
 
