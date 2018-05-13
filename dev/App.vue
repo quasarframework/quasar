edff63ef96143c8392b57752ae8e14f1711b2eea
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-05-13 21:27:10 +0300

    Update App.vue

--- dev/App.vue
+++ dev/App.vue
@@ -29,7 +29,7 @@
           ,{ label: 'Russian', value: 'ru' }
           ,{ label: 'Ukrainian', value: 'uk' }
           ,{ label: 'Polish', value: 'pl' }
-          ,{ label: 'Czech', value: 'cs-cz' }
+          ,{ label: 'Czech', value: 'cs' }
         ]"
         v-model="lang"
       />
