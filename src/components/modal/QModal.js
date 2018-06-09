9dec032316582b62d4c5ca99f24d8030e276a1c2
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-06-09 19:18:05 +0300

    chore: Preparation for keyboard navigation

--- src/components/modal/QModal.js
+++ src/components/modal/QModal.js
@@ -94,6 +94,7 @@ export default {
       default: false
     },
     noRouteDismiss: Boolean,
+    noRefocus: Boolean,
     minimized: Boolean,
     maximized: Boolean
   },
@@ -168,6 +169,9 @@ export default {
       })
     },
     __show () {
+      if (!this.noRefocus) {
+        this.__refocusTarget = document.activeElement
+      }
       const body = document.body
 
       body.appendChild(this.$el)
@@ -199,6 +203,9 @@ export default {
       EscapeKey.pop()
       this.__preventScroll(false)
       this.__register(false)
+      if (!this.noRefocus && this.__refocusTarget) {
+        this.__refocusTarget.focus()
+      }
     },
     __stopPropagation (e) {
       e.stopPropagation()
