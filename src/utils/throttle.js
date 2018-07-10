441fe7e408d71f662a249791b2b06c435e1d262c
panstromek <panstromek@seznam.cz>
2018-07-10 17:02:15 +0200

    make throttle return last return value of throttled function (#2246)

--- src/utils/throttle.js
+++ src/utils/throttle.js
@@ -1,15 +1,17 @@
 export default function (fn, limit = 250) {
   let wait = false
+  let result
 
   return function (...args) {
     if (wait) {
-      return
+      return result
     }
 
     wait = true
-    fn.apply(this, args)
+    result = fn.apply(this, args)
     setTimeout(() => {
       wait = false
     }, limit)
+    return result
   }
 }
