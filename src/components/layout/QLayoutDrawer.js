afcee2b1b08fc3bb1f2d8dffd12f17097c625ea8
Dan Popescu <pdan.popescu@gmail.com>
2018-08-22 19:18:52 +0300

    QLayoutDrawer: fix user select and visibility when in container

--- src/components/layout/QLayoutDrawer.js
+++ src/components/layout/QLayoutDrawer.js
@@ -236,7 +236,8 @@ export default {
         `q-layout-drawer-${this.side}`,
         this.layout.container ? 'overflow-auto' : 'scroll',
         this.contentClass,
-        this.mobileView ? this.belowClass : this.aboveClass
+        this.mobileView ? this.belowClass : this.aboveClass,
+        this.showing ? void 0 : 'q-layout-drawer-invisible'
       ]
     },
     stateDirection () {
