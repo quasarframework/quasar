2dda4b4595d979bc72a7a29c36b5f72462416585
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-09-26 02:15:48 +0300

    fix: Step content is inserted multiple times when switching from Vertical to Horizontal #2601

--- src/components/slide-transition/QSlideTransition.js
+++ src/components/slide-transition/QSlideTransition.js
@@ -1,5 +1,6 @@
 export default {
   name: 'QSlideTransition',
+
   props: {
     appear: Boolean,
     duration: {
@@ -7,14 +8,19 @@ export default {
       default: 300
     }
   },
+
   methods: {
-    __begin (el, height) {
+    __begin (el, height, done) {
       el.style.overflowY = 'hidden'
       if (height !== void 0) {
         el.style.height = `${height}px`
       }
       el.style.transition = `height ${this.duration}ms cubic-bezier(.25, .8, .50, 1)`
+
+      this.animating = true
+      this.done = done
     },
+
     __end (el, event) {
       el.style.overflowY = null
       el.style.height = null
@@ -22,15 +28,22 @@ export default {
       this.__cleanup()
       event !== this.lastEvent && this.$emit(event)
     },
+
     __cleanup () {
+      this.done && this.done()
+      this.done = null
+      this.animating = false
+
       clearTimeout(this.timer)
       this.el.removeEventListener('transitionend', this.animListener)
-      delete this.animListener
+      this.animListener = null
     }
   },
+
   beforeDestroy () {
-    this.animListener && this.animListener()
+    this.animating && this.__cleanup()
   },
+
   render (h) {
     return h('transition', {
       props: {
@@ -42,22 +55,21 @@ export default {
           let pos = 0
           this.el = el
 
-          if (this.animListener) {
-            pos = el.offsetHeight
-            this.animListener()
+          if (this.animating === true) {
+            this.__cleanup()
+            pos = el.offsetHeight === el.scrollHeight ? 0 : void 0
           }
           else {
             this.lastEvent = 'hide'
           }
 
-          this.__begin(el, pos)
+          this.__begin(el, pos, done)
 
-          this.animListener = () => {
-            this.__end(el, 'show')
-            done()
-          }
           this.timer = setTimeout(() => {
             el.style.height = `${el.scrollHeight}px`
+            this.animListener = () => {
+              this.__end(el, 'show')
+            }
             el.addEventListener('transitionend', this.animListener)
           }, 100)
         },
@@ -65,23 +77,21 @@ export default {
           let pos
           this.el = el
 
-          if (this.animListener) {
-            pos = el.offsetHeight
-            this.animListener()
+          if (this.animating === true) {
+            this.__cleanup()
           }
           else {
             this.lastEvent = 'show'
             pos = el.scrollHeight
           }
 
-          this.__begin(el, pos)
+          this.__begin(el, pos, done)
 
-          this.animListener = () => {
-            this.__end(el, 'hide')
-            done()
-          }
           this.timer = setTimeout(() => {
             el.style.height = 0
+            this.animListener = () => {
+              this.__end(el, 'hide')
+            }
             el.addEventListener('transitionend', this.animListener)
           }, 100)
         }
