cfb7c3a667bf8ae6f94e1fb295c0c73cb87de184
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-09-26 20:03:26 +0300

    First day of week for Thailand is Monday

--- i18n/th.js
+++ i18n/th.js
@@ -20,7 +20,7 @@ export default {
     daysShort: 'อา._จ._อ._พ._พฤ_.ศ._ส.'.split('_'),
     months: 'มกราคม_กุมภาพันธ์_มีนาคม_เมษายน_พฤษภาคม_มิถุนายน_กรกฎาคม_สิงหาคม_กันยายน_ตุลาคม_พฤศจิกายน_ธันวาคม'.split('_'),
     monthsShort: 'ม.ค._ก.พ._มี.ค._เม.ย._พ.ค._มิ.ย._ก.ค._ส.ค._ก.ย._ต.ค._พ.ย._ธ.ค.'.split('_'),
-    firstDayOfWeek: 0, // 0-6, 0 - Sunday, 1 Monday, ...
+    firstDayOfWeek: 1, // 0-6, 0 - Sunday, 1 Monday, ...
     format24h: true
   },
   pullToRefresh: {
