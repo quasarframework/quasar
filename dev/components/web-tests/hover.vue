2f63b0015d6044a62997571c970828c1c9a12a6c
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-09-28 21:22:03 +0300

    feat: Improve focusable/hoverable

--- dev/components/web-tests/hover.vue
+++ dev/components/web-tests/hover.vue
@@ -1,8 +1,37 @@
 <template>
   <div class="layout-padding">
     <h1>Focus helper test</h1>
-    <q-list>
+    <q-btn class="q-ma-sm" color="black" label="Test" />
+    <q-btn class="q-ma-sm" color="black" label="Test" />
+    <q-btn class="q-ma-sm" color="primary" label="Test" />
+    <q-btn class="q-ma-sm" color="primary" label="Test" />
+    <q-btn class="q-ma-sm" color="orange" label="Test" />
+    <q-btn class="q-ma-sm" color="orange" label="Test" />
+    <q-btn class="q-ma-sm" color="yellow" text-color="black" label="Test" />
+    <q-btn class="q-ma-sm" color="yellow" text-color="black" label="Test" />
+    <q-btn class="q-ma-sm" label="Test" />
+    <q-btn class="q-ma-sm" label="Test" />
+
+    <q-list class="q-mt-md">
+      <q-item v-for="n in 3" :key="'1.'+n" clickable>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item</q-item-section>
+      </q-item>
       <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item - ripple</q-item-section>
+      </q-item>
+      <q-item class="text-primary" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item - ripple</q-item-section>
+      </q-item>
+      <q-item class="text-primary" v-for="n in 3" :key="'2.'+n" clickable>
         <q-item-section avatar>
           <q-icon color="primary" name="bluetooth" />
         </q-item-section>
@@ -11,7 +40,25 @@
     </q-list>
 
     <q-list dark class="bg-black">
+      <q-item v-for="n in 5" :key="'3.'+n" clickable>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item</q-item-section>
+      </q-item>
       <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item - ripple</q-item-section>
+      </q-item>
+      <q-item class="text-orange" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item - ripple</q-item-section>
+      </q-item>
+      <q-item class="text-orange" v-for="n in 5" :key="'4.'+n" clickable>
         <q-item-section avatar>
           <q-icon color="primary" name="bluetooth" />
         </q-item-section>
