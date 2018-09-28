c2d571d3765ac1a9147bfcf074c66f728265efaf
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-09-28 15:10:57 +0300

    chore: Focus helper test page

---
+++ dev/components/web-tests/hover.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout-padding">
+    <h1>Focus helper test</h1>
+    <q-list>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item</q-item-section>
+      </q-item>
+    </q-list>
+
+    <q-list dark class="bg-black">
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item</q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
