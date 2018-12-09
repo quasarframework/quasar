af0997080a67012aa538a6ba9ba92a4e41b3103c
Popescu Dan <pdan.popescu@gmail.com>
2018-12-09 19:40:38 +0200

    [v1] QSplitter: increase transparent size on hover, prevent scroll (#2837)
    * [v1] QSplitter: increase transparent size on hover, prevent scroll, always large on touch
    
    * Update QSplitter.js
    
    * Update QSplitter.js
    
    * Update splitter.styl

--- src/components/splitter/QSplitter.js
+++ src/components/splitter/QSplitter.js
@@ -50,7 +50,9 @@ export default Vue.extend({
 
   computed: {
     classes () {
-      return (this.horizontal ? 'column' : 'row') + ` q-splitter--${this.horizontal ? 'horizontal' : 'vertical'}` + ` q-splitter--${this.disable === true ? 'disabled' : 'workable'}`
+      return (this.horizontal ? 'column' : 'row') +
+        ` q-splitter--${this.horizontal ? 'horizontal' : 'vertical'}` +
+        ` q-splitter--${this.disable === true ? 'disabled' : 'workable'}`
     },
 
     prop () {
@@ -120,16 +122,21 @@ export default Vue.extend({
       h('div', {
         staticClass: 'q-splitter__separator',
         style: this.separatorStyle,
-        class: this.separatorClass,
-        directives: this.disable === false ? [{
-          name: 'touch-pan',
-          value: this.__pan,
-          modifiers: {
-            horizontal: !this.horizontal,
-            vertical: this.horizontal
-          }
-        }] : null
-      }),
+        class: this.separatorClass
+      }, this.disable === false ? [
+        h('div', {
+          staticClass: 'absolute-full',
+          directives: [{
+            name: 'touch-pan',
+            value: this.__pan,
+            modifiers: {
+              horizontal: !this.horizontal,
+              vertical: this.horizontal,
+              prevent: true
+            }
+          }]
+        })
+      ] : null),
 
       h('div', {
         ref: 'after',
