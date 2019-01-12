17e4d6988c1c82739c6ba3a9fc9bc27e9294b7bf
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-01-12 12:42:11 +0200

    feat(docs): Finalize "how to use vue" page

--- docs/src/pages/getting-started/how-to-use-vue.vue
+++ docs/src/pages/getting-started/how-to-use-vue.vue
@@ -89,16 +89,87 @@ doc-page(
     | Notify.create('My message')
 
   doc-section.h2(title="Importing All Components and Directives for Quick Test")
+
   div Referencing all Quasar Components, Directives and Plugins can be tiresome when you just want to do a quick test. In this case, you can tell Quasar to import them all by editing <doc-token>/quasar.conf.js</doc-token> like this:
   code-markup framework: 'all'
   doc-note(title="IMPORTANT")
     | This <strong>will not</strong> take advantage of tree shaking, causing your bundle to become bloated with unnescesary/unused code. <strong>Not recommended for production</strong>. Use this only for quick testing purposes.
+
+  doc-section.h2(title="Self Closing Tags")
+
+  div Some Quasar components do not need you to include HTML content inside of them. In this case, you can use them as self closing tags. One example with QIcon below:
+  code-markup(lang="html") {{ markup.selfClosing }}
+
+  div Self-closing means the above template is the equivalent to:
+  code-markup(lang="html") {{ markup.selfClosingEquiv }}
+
+  div Both forms are valid and can be used. It works the same with regular DOM elements:
+  code-markup(lang="html") {{ markup.selfClosingExtra }}
+
+  div Some eslint-plugin-vue linting rules actually enforce using the self-closing syntax.
+
+  doc-section.h1(title="Handling Vue Properties")
+
+  div Let’s take some examples of Vue properties with a bogus Quasar component (we will call it QBogus) that supports the properties above. We will discuss each of the types of Vue properties in the below sections.
+
+  doc-section.h2(title="Boolean Property")
+
+  div A boolean property means it only accepts a strictly Boolean value. The values will not be cast to Boolean, so you must ensure you are using a true Boolean.
+
+  div If you are trying to control that property and change it dynamically at runtime, then bind it to a variable in your scope:
+
+  code-markup(lang="html") {{ markup.boolean }}
+
+  div If, on the other hand, you know this Boolean value is not going to change, you can use the shorthand version of the variable like a component attribute and just specify it. In other words, if you don’t bind the variable to a variable in the component’s scope as it will always be <doc-token>true</doc-token>:
+
+  code-markup(lang="html") {{ markup.boolean2 }}
+
+  doc-section.h2(title="String Property")
+
+  div As you can imagine, Strings are required as a value for this type of property.
+
+  code-markup(lang="html") {{ markup.string }}
+
+  doc-section.h2(title="Number Property")
+  code-markup(lang="html") {{ markup.number }}
+
+  doc-section.h2(title="Object Property")
+  code-markup(lang="html") {{ markup.object }}
+
+  doc-section.h2(title="Array Property")
+  code-markup(lang="html") {{ markup.array }}
+
+  doc-section.h1(title="Handling Vue Methods")
+  div Example of props on a bogus QBogus component: <doc-token>next()</doc-token>, <doc-token>previous(doneFn)</doc-token>, <doc-token>toggleFullscreen()</doc-token>
+  div In order for you to access these methods, you will need to set a Vue reference on the component first. Here’s an example:
+  code-markup(lang="html") {{ markup.vueMethod }}
+
+  doc-section.h1(title="Handling Vue Events")
+
+  div You will notice throughout the documentation that some Quasar components have a section called “Vue Events”. <strong>Do not confuse</strong> these Vue events with the <doc-link to="/options-and-helpers/global-event-bus">Global Event Bus</doc-link> as these two have nothing in common.
+
+  div Example of “Vue Events” on a bogus QBogus component: <doc-token>@show</doc-token>, <doc-token>@hide</doc-token>.
+  div In order for you to catch these events, when they are triggered, you will need to add listeners for them on the component itself in the HTML template. Here’s an example:
+
+  code-markup(lang="html") {{ markup.vueEvents }}
+
+  div There are times when you need to access native DOM events on a Quasar component too, like the native <doc-token>@click</doc-token>. Do not confuse native events with the Vue events emitted by the component. They are different things. Let’s take an example: let’s say we have a component (QBogus) that emits <doc-token>@show</doc-token> and <doc-token>@hide</doc-token>, but doesn’t emit a <doc-token>@click</doc-token> event. <doc-token>@click</doc-token> being a native DOM event, we can still catch it with the <doc-token>.native</doc-token> modifier:
+
+  code-markup(lang="html") {{ markup.native }}
 </template>
 
 <script>
 import sfc from '!raw-loader!markup/sfc.html'
 import sfcStyle from '!raw-loader!markup/sfc-style.html'
 import notify from '!raw-loader!markup/notify.html'
+import boolean from '!raw-loader!markup/boolean.html'
+import boolean2 from '!raw-loader!markup/boolean2.html'
+import string from '!raw-loader!markup/string.html'
+import number from '!raw-loader!markup/number.html'
+import object from '!raw-loader!markup/object.html'
+import array from '!raw-loader!markup/array.html'
+import vueMethod from '!raw-loader!markup/vue-method.html'
+import vueEvents from '!raw-loader!markup/vue-events.html'
 
 export default {
   name: 'HowToPage',
@@ -111,15 +182,37 @@ export default {
     this.markup = {
       sfc,
       sfcStyle,
+
       directive: `<div v-ripple>Click Me</div>`,
       directiveLast: `<div v-touch-pan="handler">...</div>
 <div v-touch-swipe="handler">...</div>
 <div v-ripple>Click me. I got ripples.</div>`,
+
       component: `<div>
   <q-btn @click="doSomething">Do something</q-btn>
   <q-icon name="alarm" />
 </div>`,
-      notify
+
+      notify,
+
+      selfClosing: `<q-icon name="cloud" />`,
+      selfClosingEquiv: `<q-icon name="cloud"></q-icon>`,
+      selfClosingExtra: `<div class="col" />
+<!-- equivalent to: -->
+<div class="col"></div>`,
+
+      boolean,
+      boolean2,
+      string,
+      number,
+      object,
+      array,
+
+      vueMethod,
+      vueEvents,
+
+      native: `<!-- Notice "@click.native" -->
+<q-bogus @click.native="myMethod" />`
     }
   }
 }
