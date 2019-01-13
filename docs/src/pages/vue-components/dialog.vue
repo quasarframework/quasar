ce705651b872ba23ab9d30409a4468884a1a2d83
Scott Molinari <scott.molinari@adduco.de>
2019-01-13 10:03:31 +0100

    Adds QDialog

--- docs/src/pages/vue-components/dialog.vue
+++ docs/src/pages/vue-components/dialog.vue
@@ -1,8 +1,38 @@
 <template lang="pug">
 doc-page(
-  title="Sample Page"
+  title="Dialog"
 )
-  div Introduction - <doc-token>token</doc-token>
+
+  div Quasar Dialogs are a great way to offer the user the ability to choose a specific action or list of actions. They also can provide the user with important information, or require them to make a decision (or multiple decisions).
+
+  div From a UI perspective, you can think of Dialogs as a type of floating modal, which covers only a portion of the screen. This means Dialogs should only be used for quick user actions, like verifying a password, getting a short App notification or selecting an option or options quickly. For more in depth user interactions, you should use QModal (add link to QModal).
+
+  doc-note(title="Note")
+    div Dialogs can be used either as a component in your Vue file templates (for complex use-cases, like specific form components with validation, selectable options, etc.), or they can be used as a globally available method for more basic use cases, like the native JS alert(), prompt(), etc.
+
+  doc-note(title="IMPORTANT")
+   div When the user hits the phone/tablet back button (only for Cordova apps), the Dialog will be closed automatically.
+   div Also, when on a desktop browser, hitting the <doc-token>ESCAPE</doc-token> key will also close the Dialog, as does clicking or touching outside of the Dialog.
+
+  doc-section.h1(title="Installation as a Plugin")
+  InstallationCard(
+    plugins="QDialog"
+  )
+
+  doc-section.h1(title="Usage as a Plugin")
+  code-example(title="Examples", file="QDialog/Plugin")
+
+  doc-section.h1(title="Installation as a Component")
+  InstallationCard(
+    components="QDialog"
+  )
+
+  doc-section.h1(title="Usage as a Component")
+  code-example(title="Examples", file="QDialog/Component")
+
+  doc-section.h1(title="API")
+  ApiCard(file="QDialog")
+
   div.
     Introduction - another paragraph.
     <doc-link to="https://github.com/quasarframework/quasar">External link</doc-link>.
@@ -63,10 +93,10 @@ doc-page(
 
 <script>
 export default {
-  name: 'NamePage',
+  name: 'QDialog',
 
   meta: {
-    title: 'Name'
+    title: 'QDialog'
   }
 }
 </script>
