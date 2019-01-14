35d22953d6c908da8bc0a19188b6f722fa98540d
panstromek <panstromek@seznam.cz>
2019-01-14 23:13:58 +0100

    WIP q-select doc page

---
+++ docs/src/examples/QSelect/Types.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-select
+        :options="selectOptions"
+        filled
+        label="Filled"
+        v-model="select"
+    />
+
+    <q-select
+        :options="selectOptions"
+        label="Outlined"
+        outlined
+        v-model="select"
+    />
+
+    <q-select
+        :options="selectOptions"
+        label="Standout"
+        standout
+        v-model="select"
+    />
+
+    <q-select
+        :options="selectOptions"
+        label="Standard"
+        standard
+        v-model="select"
+    />
+
+    <q-select
+        :options="selectOptions"
+        borderless
+        label="Borderless"
+        v-model="select"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selectOptions: [
+        {
+          label: 'Google',
+          value: 'goog'
+        },
+        {
+          label: 'Facebook',
+          value: 'fb'
+        }
+      ],
+      select: null
+    }
+  }
+}
+</script>
