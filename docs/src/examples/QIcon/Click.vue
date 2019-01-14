bdc8bbc4fcc89026c57e5bda58e77fd9cae2fb54
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-01-14 16:21:11 -0200

    Added QIcon Documentation

---
+++ docs/src/examples/QIcon/Click.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-icon :name="iconName" color="red" size="2rem" @click.native="favorite = !favorite"/>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    favorite: false
+  }),
+  computed: {
+    iconName () {
+      return this.favorite ? 'favorite' : 'favorite_border'
+    }
+  }
+}
+</script>
---
+++ docs/src/examples/QIcon/Size.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <!-- Sizes can be declared as a prop size= -->
+    <q-icon name="wifi" size="2rem" />
+    <q-icon name="delete" size="24px" />
+  </div>
+</template>
