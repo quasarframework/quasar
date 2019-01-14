a77c8145efecaff8a88be186846d0e55dc5ea953
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-14 13:40:39 -0700

    feat(docs): QCheckbox page (#2980)
    * feat(docs): QCheckbox page
    
    * feat(docs): Minor improvements to QRadio page
    
    * Update checkbox.md

---
+++ docs/src/examples/QCheckbox/IndeterminateState.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-checkbox toggle-indeterminate indeterminate-value="maybe" v-model="theModel" label="Did you eat lunch today?" />
+    </div>
+
+    <div class="q-px-sm">
+      The model data: <strong>'{{ theModel }}'</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      theModel: false
+    }
+  }
+}
+</script>
