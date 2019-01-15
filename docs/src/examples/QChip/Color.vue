1014144cde52ef23cda5ee3dbf44107c7d6c0852
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-15 09:37:58 -0700

    feat(docs): QChip (#2985)
    * feat(docs): QChip
    
    * Update QChip.json
    
    Removed 'pointing' and it failed to merge :)

---
+++ docs/src/examples/QChip/Color.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-chip icon="alarm" color="primary" text-color="white">
+        Alarm
+      </q-chip>
+      <q-chip icon-right="map" color="orange" text-color="white">
+        Map
+      </q-chip>
+      <q-chip icon="mail" color="red" text-color="white">
+        10k
+      </q-chip>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      color: 'cyan'
+    }
+  }
+}
+</script>
---
+++ docs/src/examples/QChip/Removable.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-chip removable @remove="onRemove('Alarm')" square icon="alarm" color="primary" text-color="white">
+        Alarm
+      </q-chip>
+      <q-chip removable @remove="onRemove('Map')" square icon-right="map" color="secondary" text-color="white">
+        Map
+      </q-chip>
+      <q-chip removable @remove="onRemove('10k')" square color="warning" text-color="white">
+        10k
+      </q-chip>
+      <q-chip removable @remove="onRemove('5k')" outline square color="info" text-color="white">
+        5k
+      </q-chip>
+    </div>
+
+    <div class="q-px-sm">
+      Received signal to remove: <strong>{{ JSON.stringify(value) }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onRemove (data) {
+      this.value = data
+    }
+  }
+}
+</script>
