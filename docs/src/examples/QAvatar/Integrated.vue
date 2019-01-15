c9bff96238b2b5fc3d0b93cd024900007dcf83ac
nothingismagick <35242872+nothingismagick@users.noreply.github.com>
2019-01-15 18:49:00 +0100

    feat/docs/QAvatar (#2989)
    * QEditor.json api and minor cleanup in QEditor.js
    
    * Update [WIP]
    
    * Good to go
    
    * QAvatar docs page

---
+++ docs/src/examples/QAvatar/Integrated.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <div class="row">
+      <p class="col-3">Within QChip:</p>
+      <div class="col-auto">
+        <q-chip>
+          <q-avatar class="q-pl-xs" size="24px" color="red" icon="wifi" />
+          Offline
+        </q-chip>
+      </div>
+    </div>
+    <div class="row">
+      <p class="col-3">Within QButton:</p>
+      <div class="col-auto">
+        <q-btn round>
+          <q-avatar size="28px">
+            <img src="/statics/quasar-logo.png" />
+          </q-avatar>
+        </q-btn>
+        &nbsp;
+        <q-btn round>
+          <q-avatar size="32px">
+            <img src="/statics/quasar-logo.png" />
+          </q-avatar>
+        </q-btn>
+        &nbsp;
+        <q-btn round>
+          <q-avatar size="40px">
+            <img src="/statics/quasar-logo.png" />
+          </q-avatar>
+        </q-btn>
+      </div>
+    </div>
+  </div>
+</template>
