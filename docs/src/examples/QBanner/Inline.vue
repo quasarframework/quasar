3e35598a0a74815c871908f92873272f3fa64304
D.C. Thompson <denjell@kinokabaret.com>
2019-01-16 18:14:04 +0100

    Banner
    Signed-off-by: D.C. Thompson <denjell@kinokabaret.com>

---
+++ docs/src/examples/QBanner/Inline.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-banner inline-actions dens>
+    <q-avatar slot="avatar" icon="signal_wifi_off" text-color="primary" />
+
+    This app is offline.
+
+      <q-btn slot="action" flat color="primary" label="Turn ON Wifi" />
+    </q-banner>
+  </div>
+</template>
