8a86df40b81c7c35dd0edf2be8f02bb9af238fc9
panstromek <panstromek@seznam.cz>
2019-01-16 01:43:52 +0100

    QToggle docs (#2994)
    * [wip] examples
    
    * array value example
    
    * fix custom values + dark background example
    
    * add option group example
    
    * list example
    
    * QToggle doc text + remove empty script tag
    
    * Update toggle.md

---
+++ docs/src/examples/QToggle/Labels.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="q-pa-md q-gutter-lg">
+    <q-toggle
+      v-model="value"
+      label="On Right"
+    />
+    <q-toggle
+      v-model="value"
+      color="green"
+      label="On Right"
+    />
+    <q-toggle
+      v-model="value"
+      color="yellow"
+      label="On Right"
+    />
+    <q-toggle
+      v-model="value"
+      color="red"
+      label="On Right"
+    />
+    <br>
+    <q-toggle
+      v-model="value"
+      label="On Left"
+      left-label
+    />
+    <q-toggle
+      v-model="value"
+      color="green"
+      label="On Left"
+      left-label
+    />
+    <q-toggle
+      v-model="value"
+      color="yellow"
+      label="On Left"
+      left-label
+    />
+    <q-toggle
+      v-model="value"
+      color="red"
+      label="On Left"
+      left-label
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: true
+    }
+  }
+}
+</script>
