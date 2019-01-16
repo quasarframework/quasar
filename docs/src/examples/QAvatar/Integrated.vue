4defed20b15766f1d03a25a69ddf083aa2e0bc39
nothingismagick <35242872+nothingismagick@users.noreply.github.com>
2019-01-16 17:07:42 +0100

    feat/docs/QAvatar (#2999)
    * QEditor.json api and minor cleanup in QEditor.js
    
    * Update [WIP]
    
    * Good to go
    
    * QAvatar docs page
    
    * QAvatar docs  + QBanner typo fix
    
    Signed-off-by: D.C. Thompson <denjell@kinokabaret.com>
    
    * add CDN
    
    Signed-off-by: D.C. Thompson <denjell@kinokabaret.com>
    
    * add CDN
    
    Signed-off-by: D.C. Thompson <denjell@kinokabaret.com>
    
    * remove (default) description mistake
    
    Signed-off-by: D.C. Thompson <denjell@kinokabaret.com>

--- docs/src/examples/QAvatar/Integrated.vue
+++ docs/src/examples/QAvatar/Integrated.vue
@@ -1,35 +1,42 @@
 <template>
-  <div class="q-pa-md q-gutter-sm">
-    <div class="row">
-      <p class="col-3">Within QChip:</p>
-      <div class="col-auto">
-        <q-chip>
-          <q-avatar class="q-pl-xs" size="24px" color="red" icon="wifi" />
-          Offline
-        </q-chip>
-      </div>
+  <div>
+    <div class="row q-pa-md q-gutter-sm">
+      <q-chip>
+        <q-avatar class="q-pl-xs" size="24px" font-size="16px" color="positive" icon="wifi" />
+        Online
+      </q-chip>
     </div>
-    <div class="row">
-      <p class="col-3">Within QButton:</p>
-      <div class="col-auto">
-        <q-btn round>
-          <q-avatar size="28px">
-            <img src="https://cdn.quasar-framework.org/img/quasar-logo.png" />
-          </q-avatar>
-        </q-btn>
-        &nbsp;
-        <q-btn round>
-          <q-avatar size="32px">
-            <img src="https://cdn.quasar-framework.org/img/quasar-logo.png" />
-          </q-avatar>
-        </q-btn>
-        &nbsp;
-        <q-btn round>
-          <q-avatar size="40px">
-            <img src="https://cdn.quasar-framework.org/img/quasar-logo.png" />
-          </q-avatar>
-        </q-btn>
-      </div>
+    <div class="row q-pa-md q-gutter-sm">
+      <q-btn round>
+        <q-avatar size="28px">
+          <img src="https://cdn.quasar-framework.org/app-icons/icon-128x128.png" />
+        </q-avatar>
+      </q-btn>
+      <q-btn round>
+        <q-avatar size="32px">
+          <img src="https://cdn.quasar-framework.org/app-icons/icon-128x128.png" />
+        </q-avatar>
+      </q-btn>
+      <q-btn round>
+        <q-avatar size="40px">
+          <img src="https://cdn.quasar-framework.org/app-icons/icon-128x128.png" />
+        </q-avatar>
+      </q-btn>
     </div>
+    <div class="row q-pa-md q-gutter-sm">
+      <q-item color="blue">
+        <q-avatar rounded size="48px">
+          <img src="https://cdn.quasar-framework.org/img/avatar.png" />
+        </q-avatar>
+        <q-badge floating>1.0</q-badge>
+      </q-item>
+    </div>
+    <q-banner :dense="dense" class="bg-primary text-white q-my-md">
+      <q-avatar slot="avatar" icon="signal_wifi_off" color="white" text-color="primary" />
+
+      You have lost connection to the internet. This app is offline.
+
+      <q-btn slot="action" flat color="white" label="Turn ON Wifi" />
+    </q-banner>
   </div>
 </template>
