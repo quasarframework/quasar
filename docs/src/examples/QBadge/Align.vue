6b9d38a07fbb49d21fb14b6491c1710863e2c845
Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>
2019-01-17 12:13:18 +0100

    feat(docs): QBadge

---
+++ docs/src/examples/QBadge/Align.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <h5>
+      @quasar
+      <q-badge align="top">cli v1.0.0</q-badge>
+    </h5>
+    <h5>
+      @quasar
+      <q-badge align="middle">app v1.0.0</q-badge>
+    </h5>
+    <h5>
+      @quasar
+      <q-badge align="bottom">docs v1.0.0</q-badge>
+    </h5>
+  </div>
+</template>
---
+++ docs/src/examples/QBadge/Basic.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <div>
+      This blue:
+      <q-badge color="blue">
+        #4D96F2
+      </q-badge>
+    </div>
+    <div>
+      Not blue:
+      <q-badge color="orange" text-color="black">
+        2
+      </q-badge>
+    </div>
+    <div>
+      Also not blue:
+      <q-badge color="green-3" text-color="purple-12">
+        orange
+      </q-badge>
+    </div>
+    <div>
+      Technically blue:
+      <q-badge color="blue">
+        <q-icon name="fab fa-bluetooth-b" color="white" />
+      </q-badge>
+    </div>
+  </div>
+</template>
