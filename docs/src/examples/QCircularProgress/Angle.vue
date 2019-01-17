769691b8639a8ec3634dc896bfa346c1eab16c81
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-16 20:13:11 -0700

    feat(docs): QCircularProgress (#3000)
    * feat(docs): QCircularProgress
    
    * Updated QCircularProgress examples

---
+++ docs/src/examples/QCircularProgress/Angle.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        :value="count"
+        size="25px"
+        :show-value="true"
+        :thickness="1"
+        color="grey-8"
+        center-color="white"
+        track-color="orange"
+        :angle="45"
+      />
+
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        :value="count"
+        size="50px"
+        :show-value="true"
+        :thickness="1"
+        color="grey-8"
+        center-color="blue"
+        track-color="orange"
+        :angle="90"
+      />
+
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        :value="count"
+        size="100px"
+        :show-value="true"
+        :thickness="1"
+        color="grey-8"
+        center-color="red"
+        track-color="orange"
+        :angle="270"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0,
+      interval: null
+    }
+  },
+
+  mounted () {
+    this.interval = setInterval(() => {
+      this.count += 1
+      if (this.count > 100) {
+        this.count = 0
+      }
+    }, 100)
+  },
+
+  beforeDestroy () {
+    clearInterval(this.interval)
+  }
+}
+</script>
---
+++ docs/src/examples/QCircularProgress/Indeterminate.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        size="25px"
+        color="orange"
+        center-color="grey-8"
+        indeterminate
+      />
+
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        size="50px"
+        color="orange"
+        center-color="grey-8"
+        indeterminate
+      />
+
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        size="100px"
+        color="orange"
+        center-color="grey-8"
+        indeterminate
+      />
+    </div>
+
+    <div class="q-gutter-sm">
+      <q-circular-progress
+        size="25px"
+        color="red"
+        indeterminate
+      />
+
+      <q-circular-progress
+        class="q-ma-sm"
+        size="50px"
+        color="blue"
+        indeterminate
+      />
+
+      <q-circular-progress
+        class="q-ma-sm"
+        size="100px"
+        color="green"
+        indeterminate
+      />
+    </div>
+
+    <div class="q-gutter-sm">
+      <q-circular-progress
+        class="q-ma-sm text-black"
+        size="25px"
+        color="grey-8"
+        track-color="orange"
+        indeterminate
+      />
+
+      <q-circular-progress
+        class="q-ma-sm text-black"
+        size="50px"
+        color="red"
+        track-color="grey-8"
+        indeterminate
+      />
+
+      <q-circular-progress
+        class="q-ma-sm text-black"
+        size="100px"
+        color="blue"
+        track-color="green"
+        indeterminate
+      />
+    </div>
+    <div class="q-gutter-sm">
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        size="25px"
+        :thickness="1"
+        color="grey-8"
+        center-color="white"
+        track-color="orange"
+        indeterminate
+      />
+
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        size="50px"
+        :thickness="1"
+        color="grey-8"
+        center-color="blue"
+        track-color="orange"
+        indeterminate
+      />
+
+      <q-circular-progress
+        class="q-ma-sm text-white"
+        size="100px"
+        :thickness="1"
+        color="grey-8"
+        center-color="red"
+        track-color="orange"
+        indeterminate
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
