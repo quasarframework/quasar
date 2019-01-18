cafc705dd3b0d3d7196a26c00ed22a32bb65647b
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-01-18 07:56:32 +0200

    feat(docs): QLinearProgress page curation

---
+++ docs/src/examples/QLinearProgress/Reverse.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="q-pa-md">
+    <q-btn size="sm" color="primary" @click="randomize" label="Change Model" />
+
+    <q-linear-progress reverse :value="progress" class="q-mt-md" />
+
+    <q-linear-progress reverse :value="progress" color="warning" class="q-mt-sm" />
+
+    <q-linear-progress reverse :value="progress" color="secondary" class="q-mt-sm" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      progress: 0.4
+    }
+  },
+
+  methods: {
+    randomize () {
+      this.progress = Math.random()
+    }
+  }
+}
+</script>
