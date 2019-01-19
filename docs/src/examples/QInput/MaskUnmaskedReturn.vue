808be956e6bdbb8da32fdb5834f663e9fc5e2a4b
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-01-19 15:03:49 -0200

    Added masked input docs

---
+++ docs/src/examples/QInput/MaskUnmaskedReturn.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="q-pa-md q-gutter-md">
+    <div class="row justify-start gutter-sm">
+      <div class="col-xs-12 col-md-6">
+        <q-input v-model="maskedInput" label="Credit Card" unmasked-value hint="Don't type a real card number, just for example." mask="card"/>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <q-input v-model="maskedInput" label="Returned Value" readonly />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    maskedInput: ''
+  })
+}
+</script>
