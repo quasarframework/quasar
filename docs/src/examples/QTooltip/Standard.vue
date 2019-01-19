d6157caa14b840e5869110bd6d89def82944625e
panstromek <panstromek@seznam.cz>
2019-01-19 22:58:16 +0100

    tooltip docs

---
+++ docs/src/examples/QTooltip/Standard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="q-pa-md">
+    <p>
+      The target element
+      must be direct parent of QTooltip on the
+      DOM hierarchy.
+    </p>
+    <q-btn label="Hover me" color="primary">
+      <q-tooltip>
+        Some text as content of Tooltip
+      </q-tooltip>
+    </q-btn>
+  </div>
+</template>
