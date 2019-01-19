b1f01dcbbbc4522859971d79c11f8105636759c6
panstromek <panstromek@seznam.cz>
2019-01-19 13:42:23 +0100

    QRange (#3011)

---
+++ docs/src/examples/QRange/DragSnap.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="q-pa-md">
+    <q-chip
+      square
+      color="secondary"
+      text-color="white"
+    >
+      Model: {{ rangeSnap.min }} to {{ rangeSnap.max }} (0 to 100, step 5)
+    </q-chip>
+
+    <q-range
+      v-model="rangeSnap"
+      :min="0"
+      :max="100"
+      :step="5"
+      drag-range
+      label
+      markers
+      snap
+      color="lime"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      rangeSnap: {
+        min: 35,
+        max: 60
+      }
+    }
+  }
+}
+</script>
---
+++ docs/src/examples/QRange/LabelAlways.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="q-pa-md">
+    <q-chip
+      square
+      color="secondary"
+      text-color="white"
+    >
+      Model: {{ label.min }} to {{ label.max }} (-20 to 20, step 4)
+    </q-chip>
+
+    <q-range
+      v-model="label"
+      :min="-20"
+      :max="20"
+      :step="4"
+      label-always
+      color="brown"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      label: {
+        min: -12,
+        max: 8
+      }
+    }
+  }
+}
+</script>
