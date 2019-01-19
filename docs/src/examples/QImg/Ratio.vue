98b94aa4d29c2095256569c022cf0df1993e0e75
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-19 10:34:14 -0700

    feat(docs): QImg

---
+++ docs/src/examples/QImg/Ratio.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-center">
+      <q-btn push color="teal" label="Refresh" @click="refresh" class="q-my-lg" />
+    </div>
+    <div class="row justify-center">
+      Ratio = 16/9
+    </div>
+    <div class="row justify-center">
+      <div class="q-gutter-md" style="width: 50%;">
+        <q-img
+          :src="url"
+          :ratio="16/9"
+        />
+      </div>
+    </div>
+    <div class="row justify-center">
+      Ratio = 4/3
+    </div>
+    <div class="row justify-center">
+      <div class="q-gutter-md" style="width: 50%;">
+        <q-img
+          :src="url"
+          :ratio="4/3"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: 'https://placeimg.com/500/300/nature'
+    }
+  },
+
+  methods: {
+    refresh () {
+      this.url = 'https://placeimg.com/500/300/nature?t=' + Math.random()
+    }
+  }
+}
+</script>
