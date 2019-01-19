5276da5de7ba0817e2cd0e40f1ad5a4de1a73f20
panstromek <panstromek@seznam.cz>
2019-01-19 13:41:58 +0100

    Qslider docs (basic) (#3008)
    * QSlider docs - Scaffold examples
    
    * QSlider docs - more examples work
    
    * more slider work

---
+++ docs/src/examples/QSlider/Markers.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="q-pa-md">
+    <q-slider
+      v-model="basicModel"
+      markers
+      :min="0"
+      :max="10"
+    />
+    <q-slider
+      v-model="greenModel"
+      color="green"
+      markers
+      :min="0"
+      :max="10"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      basicModel: 2,
+      greenModel: 0
+    }
+  }
+}
+</script>
---
+++ docs/src/examples/QSlider/Snap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-pa-md">
+    <q-slider
+      v-model="value"
+      :min="-20"
+      :max="20"
+      :step="4"
+      label
+      label-always
+      color="purple"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: 0
+    }
+  }
+}
+</script>
