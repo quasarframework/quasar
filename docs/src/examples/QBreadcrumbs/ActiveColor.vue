d5f72a475e998936dc9aab0b315ee365410c6eba
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-19 05:38:56 -0700

    feat(docs): QBreadcrumbs (#3013)
    * feat(docs): QBreadcrumbs
    
    * Removing obsolete 'color' property from QBreadcrumbs and QBreadcrumbsEl.

---
+++ docs/src/examples/QBreadcrumbs/ActiveColor.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-breadcrumbs active-color="secondary" class="text-orange">
+      <q-breadcrumbs-el label="Home" />
+      <q-breadcrumbs-el label="Components" />
+      <q-breadcrumbs-el label="Breadcrumbs" />
+    </q-breadcrumbs>
+  </div>
+</template>
---
+++ docs/src/examples/QBreadcrumbs/SeparatorColor.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-breadcrumbs
+      separator-color="red"
+    >
+      <q-breadcrumbs-el label="Home" icon="home" />
+      <q-breadcrumbs-el label="Components" icon="widgets" />
+      <q-breadcrumbs-el label="Breadcrumbs" icon="navigation" />
+    </q-breadcrumbs>
+  </div>
+</template>
