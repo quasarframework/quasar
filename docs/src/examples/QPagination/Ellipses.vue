01ee3dad9fb846837538bd93737be0c032d47457
Ross <w.ross.ball@gmail.com>
2019-01-20 12:07:07 -0500

    Pagination docs (#3051)
    * Paginations documentation update:
    * Updated validation for maxPages on paginations component
    *Updated styling of input box on paginations to remove number arrows
    
    * Shouldn't be in this commit

---
+++ docs/src/examples/QPagination/Ellipses.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="q-pa-lg flex flex-center">
+    <q-pagination
+      v-model="current"
+      :max="10"
+      :maxPages="5"
+      :ellipses="false"
+      :boundaryNumbers="false"
+    >
+    </q-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      current: 5
+    }
+  }
+}
+</script>
