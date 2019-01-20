80ff6bdfbf57bb7ee440608b02343f2a1e733b5a
panstromek <panstromek@seznam.cz>
2019-01-20 16:23:45 +0100

    form validation first iter. finished

---
+++ docs/src/examples/FormValidation/Lazy.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-input
+      ref="input"
+      filled
+      v-model="model"
+      label="Required field with length < 2"
+      hint="Validation starts after first blur"
+      counter
+      :rules="[
+          val => !!val || '* Required',
+          val => val.length < 2 || 'Please use maximum 1 character',
+        ]"
+      lazy-rules
+    />
+    <q-btn label="Reset" @click="reset" color="primary"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: ''
+    }
+  },
+  methods: {
+    reset () {
+      this.$refs.input.resetValidation()
+    }
+  }
+}
+</script>
---
+++ docs/src/examples/FormValidation/MaxLength.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-input
+      ref="input"
+      filled
+      v-model="model"
+      label="Maximum 3 characters"
+      :rules="[ val => val.length <= 3 || 'Please use maximum 3 characters']"
+    />
+    <q-btn label="Reset" @click="reset" color="primary"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: ''
+    }
+  },
+  methods: {
+    reset () {
+      this.$refs.input.resetValidation()
+    }
+  }
+}
+</script>
