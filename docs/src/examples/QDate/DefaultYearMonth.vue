cd9ae8adf21067f117c08a1cc4db0a4178eb6af4
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-20 07:55:20 -0700

    feat(docs): QDate (#3046)

---
+++ docs/src/examples/QDate/DefaultYearMonth.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-center">
+      <q-date
+        v-model="date"
+        default-year-month="1964/08"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: null
+    }
+  }
+}
+</script>
