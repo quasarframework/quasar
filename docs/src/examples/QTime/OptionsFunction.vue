90efaa524a21a41cff33cd370cc6d0a8c2e89a59
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-19 17:45:41 -0700

    feat(docs): QTime (#3037)
    * feat(docs): QTime
    
    * Update time.md

---
+++ docs/src/examples/QTime/OptionsFunction.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-center">
+      <q-time
+        v-model="time"
+        :options="optionsFn"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      time: '10:56'
+    }
+  },
+
+  methods: {
+    optionsFn (hr, min, sec) {
+      if (hr < 6 || hr > 15 || hr % 2 !== 0) {
+        return false
+      }
+      if (min !== null && (min <= 25 || min >= 58)) {
+        return false
+      }
+      if (sec !== null && sec % 10 !== 0) {
+        return false
+      }
+      return true
+    }
+  }
+
+}
+</script>
