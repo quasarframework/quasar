8e90ed906e73ef2c060e4b9c4fccdd79a1d44839
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-20 17:15:12 -0700

    feat(docs): QChatMessage (#3060)

---
+++ docs/src/examples/QChatMessage/Avatar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-center">
+      <div class="q-layout-padding" style="width: 80%; max-width: 500px; border: 1px solid lightgrey">
+
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar-framework.org/img/boy-avatar.png"
+          :text="['hey, how are you?']"
+          sent
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar-framework.org/img/linux-avatar.png"
+          :text="[`doing fine, how r you?`]"
+        />
+
+      </div>
+    </div>
+  </div>
+</template>
---
+++ docs/src/examples/QChatMessage/Size.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-center">
+      <div class="q-layout-padding" style="width: 80%; max-width: 500px; border: 1px solid lightgrey">
+
+        <q-chat-message
+          label="Sunday, 19th"
+        />
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar-framework.org/img/boy-avatar.png"
+          :text="['hey, how are you?']"
+          stamp="7 minutes ago"
+          sent
+          :size="2"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar-framework.org/img/linux-avatar.png"
+          :text="[`doing fine, how r you?`, 'I just feel like typing a really, really, REALY long message to annoy you...']"
+          stamp="4 minutes ago"
+          :size="10"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar-framework.org/img/linux-avatar.png"
+          :text="[`Did it work?`]"
+          stamp="1 minutes ago"
+          :size="10"
+        />
+
+      </div>
+    </div>
+  </div>
+</template>
