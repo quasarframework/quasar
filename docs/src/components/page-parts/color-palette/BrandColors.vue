091fb6f05974fced09e7c169019e4bf6893d8a19
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-01-22 09:56:42 +0200

    feat(docs): Updates to Color Palette page

---
+++ docs/src/components/page-parts/color-palette/BrandColors.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="q-gutter-sm">
+    <div
+      v-for="color in brandColors"
+      :key="color"
+      class="brand-color shadow-1 text-center row inline flex-center text-white generic-border-radius"
+      :class="`bg-${color}`"
+    >
+      <div class="col">{{ color }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      brandColors: [
+        'primary',
+        'secondary',
+        'tertiary',
+        'positive',
+        'negative',
+        'info',
+        'warning'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.brand-color
+  width 130px
+  height 40px
+</style>
