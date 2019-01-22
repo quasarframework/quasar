c221bb16e3c18a6c7d96b00947df67ae5cd09ac9
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-21 21:05:20 -0700

    feat(docs): QParallax (#3069)
    Parallax possibly has a bug with <video> tag.

---
+++ docs/src/examples/QParallax/Basic.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-between">
+
+    <q-parallax
+      src="https://cdn.quasar-framework.org/img/parallax2.jpg"
+    >
+      <h1 class="text-white">Basic</h1>
+    </q-parallax>
+
+    </div>
+  </div>
+</template>
