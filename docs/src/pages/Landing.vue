4219392cde576071bf5ffcacaa927af0e2504cac
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-01-26 12:11:26 -0700

    Updated landing page.
    Not production quality but meant to inspire...

--- docs/src/pages/Landing.vue
+++ docs/src/pages/Landing.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
 q-page(padding)
-  div Todo... Meanwhile, go to <doc-link to="/docs">Documentation pages</doc-link>.
+  div.flex
+    div.row
+      h4 Quasar - Empowering your websites and apps
+      div.row.col-12
+        img(src='https://cdn.quasar-framework.org/img/responsive-logo.png')
+
+      div.col-12.q-pt-sm Todo... Meanwhile, go to <doc-link to="/docs">Documentation pages</doc-link>.
+
+      div.col-12.q-pt-md If you encounter a problem you can’t solve, you can also get help from:
+        li the Quasar <doc-link to="https://forum.quasar-framework.org/">forum</doc-link>.
+        li the <doc-link to="https://discord.gg/5TDhbDg">Discord chat server</doc-link>.
+        li You can also get updates on new versions and features by following us on <doc-link to="https://twitter.com/quasarframework">Twitter</doc-link>.
 </template>
 
 <script>
