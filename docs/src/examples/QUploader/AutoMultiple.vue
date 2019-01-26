fe4a43a84fa2e61053bf87ea8f253eb087e6d4c6
Adam Purdy <adamp@kpapro.com>
2019-01-25 21:26:55 -0500

    feat(docs): Uploader Component (#3082)
    * feat(docs): Uploader
    
    - Added Examples
    - Updated uploader.md
    
    * feat(docs): Uplaoder Component
    
    - Update uploader.md
    
    * feat(docs): Uploader
    
    - Update Styling.vue
    
    * Update uploader.md

---
+++ docs/src/examples/QUploader/AutoMultiple.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="q-layout-padding">
+      <div class="q-gutter-sm">
+        <q-uploader class="q-mb-lg" label="Auto Uploader" auto url="http://localhost:4444/upload" />
+        <q-uploader label="Multiple File Uploader" multiple url="http://localhost:4444/upload" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onChange (val) {
+      console.log('@change', JSON.stringify(val))
+    },
+    onInput (val) {
+      console.log('@input', JSON.stringify(val))
+    }
+  }
+}
+</script>
