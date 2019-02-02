aeaa474497693f8335530ad85ebe8281b8eceb64
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-02-02 12:43:30 +0200

    feat(docs): QItem page

---
+++ docs/src/examples/QItem/Basic.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list bordered separator>
+      <q-item clickable v-ripple>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Item with caption</q-item-label>
+          <q-item-label caption>Caption</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label overline>OVERLINE</q-item-label>
+          <q-item-label>Item with caption</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
---
+++ docs/src/examples/QItem/Dense.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list dense bordered padding class="rounded-borders">
+      <q-item clickable v-ripple>
+        <q-item-section>
+          Item
+        </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          Item
+        </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          Item
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
