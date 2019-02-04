c4093f5bfeb04f4f41149caae4918c3575287608
Tobias de Abreu Mesquita <tobias.mesquita@gmail.com>
2019-02-04 20:35:15 -0200

    Revert "validation improvements"
    This reverts commit b16af53f41925945172b00f1f36bb3cc71dc74b0.

--- quasar/src/mixins/validate.js
+++ quasar/src/mixins/validate.js
@@ -6,9 +6,7 @@ export default {
     errorMessage: String,
 
     rules: Array,
-    lazyRules: Boolean,
-    disabledRules: Boolean,
-    validationLabel: String
+    lazyRules: Boolean
   },
 
   data () {
@@ -22,8 +20,9 @@ export default {
   watch: {
     value (v) {
       if (this.rules === void 0) { return }
-      if (this.isDirty === false) { return }
-      this.validate(v, lazy = false, disabled = false)
+      if (this.lazyRules === true && this.isDirty === false) { return }
+
+      this.validate(v)
     }
   },
 
@@ -53,71 +52,39 @@ export default {
       this.innerError = false
       this.innerErrorMessage = void 0
     },
-    getRule (original) {
-      let rule = {}
-      rule.handler = () => true
-      rule.args = undefined
-      rule.lazy = this.lazyRules
-      rule.disabled = this.disabledRules
-      rule.message = undefined
-      rule.label = this.validationLabel || this.stackedLabel || this.floatLabel
 
-      if (typeof original === 'object') {
-        rule.args = original.args || rule.args
-        rule.lazy = original.lazy || rule.lazy
-        rule.disabled = original.disabled || rule.disabled
-        rule.message = original.message || rule.message
-        rule.label = original.label || rule.label
-        original = original.handler
-      }
+    validate (val = this.value) {
+      let msg, error = false
 
-      if (typeof original === 'function') {
-        rule.handler = original
-      }
-
-      if (typeof original === 'string') {
-        if (testPattern[original] !== void 0) {
-          rule.handler = testPattern[original]
-        }
-      }
-      return rule
-    },
-    async validate (val = this.value, lazy = true, disabled = true) {
-      let results = []
       for (let i = 0; i < this.rules.length; i++) {
-        const rule = this.getRule(this.rules[i])
-        if ((!rule.disabled || disabled) && (!rule.lazy || lazy)) {
-          const result = await this.validateRule(rule, val)
-          if (result.error) {
-            results.push(result)
+        const rule = this.rules[i]
+        if (typeof rule === 'function') {
+          const res = rule(val)
+
+          if (typeof res === 'string') {
+            error = true
+            msg = res
+            break
+          }
+        }
+        else if (typeof rule === 'string' && testPattern[rule] !== void 0) {
+          if (testPattern[rule](val) !== true) {
+            error = true
+            break
           }
         }
       }
-      if (results.length > 0) {
-        this.innerError = results[0].error
-        this.innerErrorMessage = results[0].msg
+
+      if (this.innerError !== error) {
+        this.innerError = error
+        this.innerErrorMessage = msg
       }
     },
-    async validateRule (rule) {
-      let msg, error = false
-      const args = typeof rule.args === 'function' ? rule.args() : rule.args
-      const res = await Promise.resolve().then(() => rule.handler(val, args))
-      var error = res !== true
-      if (error) {
-        if (!rule.message) {
-          msg = res
-        } else if (typeof rule.message === 'function') {
-          msg = rule.message(rule.label)
-        } else {
-          msg = rule.message
-        }
-      }
-      return { error, msg }
-    },
+
     __triggerValidation () {
       if (this.isDirty === false && this.rules !== void 0) {
         this.isDirty = true
-        this.validate(this.value, lazy = true, disabled = false)
+        this.validate(this.value)
       }
     }
   }
