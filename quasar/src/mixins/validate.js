1f0c22c6c428622205a3edb917d6a132dad093ea
Tobias de Abreu Mesquita <tobias.mesquita@gmail.com>
2019-02-04 20:14:28 -0200

    replacing async/await by promises

--- quasar/src/mixins/validate.js
+++ quasar/src/mixins/validate.js
@@ -23,7 +23,7 @@ export default {
     value (v) {
       if (this.rules === void 0) { return }
       if (this.isDirty === false) { return }
-      this.validate(v, lazy = false, disabled = false)
+      this.validate(v, false, false)
     }
   },
 
@@ -82,42 +82,57 @@ export default {
       }
       return rule
     },
-    async validate (val = this.value, lazy = true, disabled = true) {
+    validate (val = this.value, lazy = true, disabled = true) {
       let results = []
+      let promise = Promise.resolve()
       for (let i = 0; i < this.rules.length; i++) {
-        const rule = this.getRule(this.rules[i])
-        if ((!rule.disabled || disabled) && (!rule.lazy || lazy)) {
-          const result = await this.validateRule(rule, val)
-          if (result.error) {
-            results.push(result)
+        promise = promise.then(() => {
+          const rule = this.getRule(this.rules[i])
+          if ((!rule.disabled || disabled) && (!rule.lazy || lazy)) {
+            return this.validateRule(rule, val).then(result => {
+              if (result.error) {
+                results.push(result)
+              }
+            })
           }
+          else {
+            return Promise.resolve()
+          }
+        })
+      }
+
+      return promise.then(() => {
+        if (results.length > 0) {
+          this.innerError = results[0].error
+          this.innerErrorMessage = results[0].msg
         }
-      }
-      if (results.length > 0) {
-        this.innerError = results[0].error
-        this.innerErrorMessage = results[0].msg
-      }
+      })
     },
-    async validateRule (rule) {
+    validateRule (rule, val) {
       let msg, error = false
       const args = typeof rule.args === 'function' ? rule.args() : rule.args
-      const res = await Promise.resolve().then(() => rule.handler(val, args))
-      var error = res !== true
-      if (error) {
-        if (!rule.message) {
-          msg = res
-        } else if (typeof rule.message === 'function') {
-          msg = rule.message(rule.label)
-        } else {
-          msg = rule.message
+      return Promise.resolve().then(() => {
+        return rule.handler(val, args)
+      }).then(res => {
+        error = res !== true
+        if (error) {
+          if (!rule.message) {
+            msg = res
+          }
+          else if (typeof rule.message === 'function') {
+            msg = rule.message(rule.label)
+          }
+          else {
+            msg = rule.message
+          }
         }
-      }
-      return { error, msg }
+        return { error, msg }
+      })
     },
     __triggerValidation () {
       if (this.isDirty === false && this.rules !== void 0) {
         this.isDirty = true
-        this.validate(this.value, lazy = true, disabled = false)
+        return this.validate(this.value, true, false)
       }
     }
   }
