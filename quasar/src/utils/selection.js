e8cf17fbdf85bce4ecfcf08033b515243b597722
Dan Popescu <pdan.popescu@gmail.com>
2019-02-13 11:06:38 +0200

    fix(quasar): Unify clear selection and remove obsolete selection prevention

---
+++ quasar/src/utils/selection.js
@@ -0,0 +1,15 @@
+export function clearSelection () {
+  if (window.getSelection !== void 0) {
+    const selection = window.getSelection()
+    if (selection.empty !== void 0) {
+      selection.empty()
+    }
+    else if (selection.removeAllRanges !== void 0) {
+      selection.removeAllRanges()
+      selection.addRange(document.createRange())
+    }
+  }
+  else if (document.selection !== void 0) {
+    document.selection.empty()
+  }
+}
