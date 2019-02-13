2f30a17be2d3e65d083cd9408611eb1b4015068e
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-02-13 13:53:19 -0700

    feat(docs): Adding Splitter examples with images

---
+++ docs/src/examples/QSplitter/ReactiveImages.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <q-splitter
+      v-model="splitterModel"
+      style="height: 400px"
+    >
+
+      <template v-slot:before>
+        <q-img
+          src="https://cdn.quasar-framework.org/img/parallax1.jpg"
+          :ratio="16/9"
+        />
+      </template>
+
+      <template v-slot:after>
+        <q-img
+          src="https://cdn.quasar-framework.org/img/parallax1-inverted.jpg"
+          :ratio="16/9"
+        />
+      </template>
+
+    </q-splitter>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      splitterModel: 50 // start at 50%
+    }
+  }
+}
+</script>
