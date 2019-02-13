945790068071971946587b80327241d19da551d5
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-02-13 08:25:41 -0700

    feat(docs): Select - update "Create new values" examples. (#3246)

---
+++ docs/src/examples/QSelect/NoFiltering.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="q-pa-md">
+    <q-select
+      filled
+      v-model="model"
+      use-input
+      use-chips
+      multiple
+      input-debounce="0"
+      @new-value="createValue"
+      style="width: 250px"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: null
+    }
+  },
+
+  methods: {
+    createValue (val, done) {
+      if (val.length > 0) {
+        done(val)
+      }
+    }
+  }
+}
+</script>
