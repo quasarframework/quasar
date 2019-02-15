1f5cbeb77da148f4ed6cbb991e747ccef76b1c35
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-02-15 22:57:18 +0200

    feat(docs): Updates for Scroll Area page

--- docs/src/examples/QScrollArea/Basic.vue
+++ docs/src/examples/QScrollArea/Basic.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="row q-ma-md">
     <div class="col-12">
-      <q-scroll-area style="height: 100px; max-width: 300px;">
-        <div v-for="n in 100" :key="n">
+      <q-scroll-area style="height: 200px; max-width: 300px;">
+        <div v-for="n in 100" :key="n" class="q-py-xs">
           Lorem ipsum dolor sit amet, consectetur adipisicing
           elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua.
--- docs/src/examples/QScrollArea/Delay.vue
+++ docs/src/examples/QScrollArea/Delay.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="row q-ma-md">
-    <div class="col-12">
-      <q-scroll-area
-        :delay="200"
-        style="height: 100px; max-width: 300px;"
-      >
-        <p>The bar will show in 200ms scrollbars become visible after mouse is hovering the container.</p>
-        <div v-for="n in 100" :key="n">
-          Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit, sed do eiusmod tempor incididunt ut labore et
-          dolore magna aliqua.
-        </div>
-      </q-scroll-area>
-    </div>
+  <div class="q-pa-md">
+    <q-btn-group class="q-mb-md">
+      <q-btn color="primary" @click="less">Less</q-btn>
+      <q-btn color="secondary" @click="more">More</q-btn>
+    </q-btn-group>
+
+    <q-scroll-area
+      :delay="1200"
+      style="height: 200px; max-width: 300px;"
+    >
+      <div v-for="n in number" :key="n">
+        Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua.
+      </div>
+    </q-scroll-area>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      number: 4
+    }
+  },
+
+  methods: {
+    less () {
+      if (this.number > 1) {
+        this.number--
+      }
+    },
+
+    more () {
+      this.number++
+    }
+  }
+}
 </script>
