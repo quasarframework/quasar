aff0ff49551547b79559afdea44e452f28de1c7b
Ilya Apasov <apasov@users.noreply.github.com>
2019-02-27 16:11:03 +0200

    [v1] Add more cryptographic quality to uuid generator
    Based on the most upvoted answer to StackOverflow question about UUID generation in Javascript
    https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript/2117523#2117523

--- quasar/src/utils/uid.js
+++ quasar/src/utils/uid.js
@@ -1,10 +1,4 @@
-function s4 () {
-  return Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1)
-}
-
 export default function () {
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4()
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16))
 }
