fc983fe436288ea23eba921a36e5b896731f5432
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-03-13 00:38:49 +0200

    fix(@quasar/app): same AE chain/extendWebpack were called for main electron process too; async chain/extendWebpack support

--- app/lib/helpers/get-quasar-ctx.js
+++ app/lib/helpers/get-quasar-ctx.js
@@ -11,7 +11,7 @@ module.exports = function (opts) {
     archName: opts.arch,
     bundler: {},
     bundlerName: opts.bundler,
-    debug: opts.debug
+    debug: opts.debug || false
   }
   ctx.mode[opts.mode] = true
 
