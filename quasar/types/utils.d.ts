c1ac519f1d4c826b0c4e2de2fd281c1a241cc119
Kerry Huguet <k_huguet@hotmail.com>
2019-03-21 11:23:18 -0500

    Remove commented code
    Remove commented code from utils.d.ts

--- quasar/types/utils.d.ts
+++ quasar/types/utils.d.ts
@@ -5,38 +5,9 @@ export * from './utils/format';
 export * from './utils/scroll';
 export * from './utils/event';
 
-
-
 // others utils
 export function openURL(url: string): void;
 export function debounce(fn: Function, wait?: Number, immediate?: boolean) : Function
 export function extend(...args: any[]): any;
 export function throttle(fn: Function, limit: Number): Function; 
 export function uid(): string;
-
-
-
-// export interface NotifyAction {
-//   label: string;
-//   icon?: string;
-//   noDismiss?: boolean;
-//   handler: Function;
-// }
-
-// export interface NotifyOptions {
-// message: string;
-// timeout?: number;
-// type?: string;
-// color?: string;
-// textColor?: string;
-// icon?: string;
-// avitar?: string;
-// detail?: string;
-// position?: string; 
-// closeBtn?: boolean;
-// actions?: NotifyAction[];
-// onDismiss?: Function;
-// }
-// export namespace Notify{
-// function create(message: string | NotifyOptions): void;
-// }
