fddb72551be343a2bba90d4b629cb3d5e285ced1
Dan Popescu <pdan.popescu@gmail.com>
2019-03-23 17:00:39 +0200

    wip

---
+++ docs/src/examples/QField/ValidationExternal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-pa-md" style="max-width: 300px">
+    <q-input
+      ref="input"
+      filled
+      v-model="model"
+      label="Type here"
+      bottom-slots
+      hint="Max 3 characters"
+      error-message="Please use maximum 3 characters"
+      :error="!isValid"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: ''
+    }
+  },
+
+  computed: {
+    isValid () {
+      return this.model.length <= 3
+    }
+  }
+}
+</script>
