bc0ba497a3b18f72a0e21a0cc983a9ed5b6b2efe
Popescu Dan <pdan.popescu@gmail.com>
2019-03-23 17:36:18 +0200

    Add QField docs - WIP (#3659)
    * fix(quasar): Fix condition for using q-field--auto-height in QField
    
    * fix(quasar): Use value instead of innerValue in QField with custom control
    
    * fix(quasar): Move computedCounter to QField from QInput
    
    * docs(quasar): Add docs for QField - part 1
    
    * wip
    
    * Update menu.js

---
+++ docs/src/examples/QField/ValidationLazy.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="q-pa-md" style="max-width: 300px">
+    <q-input
+      ref="input"
+      filled
+      v-model="model"
+      label="Required field with length < 2"
+      hint="Validation starts after first blur"
+      counter
+      :rules="[
+          val => !!val || '* Required',
+          val => val.length < 2 || 'Please use maximum 1 character',
+        ]"
+      lazy-rules
+    />
+
+    <q-btn class="q-mt-sm" label="Reset Validation" @click="reset" color="primary"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: ''
+    }
+  },
+
+  methods: {
+    reset () {
+      this.$refs.input.resetValidation()
+    }
+  }
+}
+</script>
---
+++ docs/src/examples/QField/ValidationRequired.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="q-pa-md" style="max-width: 300px">
+    <q-input
+      ref="input"
+      filled
+      v-model="model"
+      label="Required Field"
+      :rules="[val => !!val || 'Field is required']"
+    />
+
+    <q-btn class="q-mt-sm" label="Reset Validation" @click="reset" color="primary"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: ''
+    }
+  },
+
+  methods: {
+    reset () {
+      this.$refs.input.resetValidation()
+    }
+  }
+}
+</script>
