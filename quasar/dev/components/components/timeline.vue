101086bb1cb6843d22508843dcae8da97d3ca08c
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-04-02 09:39:22 +0300

    feat(QTimelineEntry): New prop ("body"), new slots: title, subtitle #3777

--- quasar/dev/components/components/timeline.vue
+++ quasar/dev/components/components/timeline.vue
@@ -23,7 +23,7 @@
     </div>
     <q-timeline :layout="layout" :side="side" color="secondary" style="padding: 0 24px;">
       <q-timeline-entry heading>
-        November, 2017
+        Novermber, 2017
       </q-timeline-entry>
 
       <q-timeline-entry
@@ -35,6 +35,15 @@
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </q-timeline-entry>
+
+      <q-timeline-entry heading body="Novermber, 2017" />
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        side="left"
+        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      />
 
       <q-timeline-entry
         title="Event Title Event Title Event Title Event Title Event Title Event Title Event Title Event Title "
