b18c407ba4579cc8febf764eab939884f8545e70
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-04-23 16:45:29 +0300

    Update QUploaderBase.js

--- quasar/src/components/uploader/QUploaderBase.js
+++ quasar/src/components/uploader/QUploaderBase.js
@@ -334,7 +334,7 @@ export default {
           ])
         ]),
 
-        this.__getBtn(h, this.editable && (this.multiple || this.queuedFiles.length == 0), 'add', this.pickFiles),
+        this.__getBtn(h, this.editable && this.queuedFiles.length == 0, 'add', this.pickFiles),
         this.__getBtn(h, !this.hideUploadButton && this.editable && this.queuedFiles.length > 0, 'upload', this.upload),
         this.__getBtn(h, this.editable && this.isUploading, 'clear', this.abort)
       ])
