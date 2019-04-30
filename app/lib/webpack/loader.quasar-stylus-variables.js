3ea80ef7ffcdb7b174ae2723adb11788c9e581fb
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-04-30 17:36:08 +0300

    chore(quasar/app): Remove debugging statement

--- app/lib/webpack/loader.quasar-stylus-variables.js
+++ app/lib/webpack/loader.quasar-stylus-variables.js
@@ -1,5 +1,4 @@
 module.exports = function (content) {
-  console.log(this.resource)
   return content.indexOf('$') !== -1
     ? `@import '~src/css/quasar.variables.styl'\n@import '~quasar/src/css/variables.styl'\n` +
       content
