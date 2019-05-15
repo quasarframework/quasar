51065c8a330fa187a48b5bdb56f120fd6972fbf9
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-05-16 01:01:50 +0300

    perf(date): improve S, SS, SS mask param

--- ui/src/utils/date.js
+++ ui/src/utils/date.js
@@ -89,10 +89,10 @@ function getRegexData (mask, locale) {
         map.S = index
         return '(\\d{1})'
       case 'SS':
-        map.SS = index
+        map.S = index // bump to S
         return '(\\d{2})'
       case 'SSS':
-        map.SSS = index
+        map.S = index // bump to S
         return '(\\d{3})'
       case 'A':
         map.A = index
@@ -251,13 +251,7 @@ export function __splitDate (str, mask, locale, calendar) {
   }
 
   if (map.S !== void 0) {
-    date.millisecond = parseInt(match[map.S], 10) * 100
-  }
-  else if (map.SS !== void 0) {
-    date.millisecond = parseInt(match[map.SS], 10) * 10
-  }
-  else if (map.SSS !== void 0) {
-    date.millisecond = parseInt(match[map.SSS], 10)
+    date.millisecond = parseInt(match[map.S], 10) * 10 ^ (3 - match[map.S].length)
   }
 
   date.dateHash = date.year + '/' + pad(date.month) + '/' + pad(date.day)
