96bfc4e75362006688c903fb974e88e4c5fce079
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-05-22 15:07:17 +0300

    feat(TouchHold): Add touch/mouse sensitivity; "capture" and "mouseCapture" modifiers; early exit

--- ui/dev/components/touch-directives/touch-hold.vue
+++ ui/dev/components/touch-directives/touch-hold.vue
@@ -11,7 +11,13 @@
 
       <div
         v-touch-hold.mouse="handleHold"
-        @click="onClick"
+        @click="onEvt('click')"
+        @mousedown="onEvt('mousedown')"
+        @mousemove="onEvt('mousemove')"
+        @mouseup="onEvt('mouseup')"
+        @touchstart="onEvt('touchstart')"
+        @touchmove="onEvt('touchmove')"
+        @touchend="onEvt('touchend')"
         class="custom-area row flex-center"
       >
         <div v-if="info" class="custom-info">
@@ -27,7 +33,13 @@
       </p>
       <div
         v-touch-hold:3000.mouse="holdExtended"
-        @click="onClick"
+        @click="onEvt('click')"
+        @mousedown="onEvt('mousedown')"
+        @mousemove="onEvt('mousemove')"
+        @mouseup="onEvt('mouseup')"
+        @touchstart="onEvt('touchstart')"
+        @touchmove="onEvt('touchmove')"
+        @touchend="onEvt('touchend')"
         class="custom-area row flex-center"
       >
         <div v-if="infoExtended" class="custom-info">
@@ -35,6 +47,80 @@
         </div>
         <div v-else class="q-pa-xl custom-area-placeholder">
           Click/touch and hold for 3 seconds
+        </div>
+      </div>
+
+      <p class="caption">
+        Custom duration (1500 ms) and sensitivities (100px/100px):
+      </p>
+      <div
+        v-touch-hold:1500:100:100.mouse="holdCustom"
+        @click="onEvt('click')"
+        @mousedown="onEvt('mousedown')"
+        @mousemove="onEvt('mousemove')"
+        @mouseup="onEvt('mouseup')"
+        @touchstart="onEvt('touchstart')"
+        @touchmove="onEvt('touchmove')"
+        @touchend="onEvt('touchend')"
+        class="custom-area row flex-center"
+      >
+        <div v-if="infoCustom" class="custom-info">
+          <pre>{{ infoCustom }}</pre>
+        </div>
+        <div v-else class="q-pa-xl custom-area-placeholder">
+          Click/touch and hold for 3 seconds
+        </div>
+      </div>
+
+      <p class="caption">
+        test (preventing it from inner square)
+      </p>
+      <div
+        v-touch-hold.mouse="holdTest"
+        @click="onEvt('click')"
+        @mousedown="onEvt('mousedown')"
+        @mousemove="onEvt('mousemove')"
+        @mouseup="onEvt('mouseup')"
+        @touchstart="onEvt('touchstart')"
+        @touchmove="onEvt('touchmove')"
+        @touchend="onEvt('touchend')"
+        class="row flex-center"
+      >
+        <div @touchstart="handleEvt" @mousedown="handleEvt" style="padding: 24px" class="cursor-pointer bg-primary text-white rounded-borders shadow-2">
+          <div>
+            <q-toggle dark color="black" v-model="holdTestStopPropagation" label="Stop propagation" />
+          </div>
+          <div v-if="infoTest" class="custom-info">
+            <pre>{{ infoTest }}</pre>
+          </div>
+          <div v-else class="text-center q-pa-xl custom-area-placeholder">
+            Touch/click and hold
+          </div>
+        </div>
+      </div>
+
+      <p class="caption">
+        test (capture + preventing it from inner square)
+        -- should still work
+      </p>
+      <div
+        v-touch-hold.capture.mouse.mouseCapture="holdTestCapture"
+        @click="onEvt('click')"
+        @mousedown="onEvt('mousedown')"
+        @mousemove="onEvt('mousemove')"
+        @mouseup="onEvt('mouseup')"
+        @touchstart="onEvt('touchstart')"
+        @touchmove="onEvt('touchmove')"
+        @touchend="onEvt('touchend')"
+        class="row flex-center"
+      >
+        <div @touchstart.stop @mousedown.stop style="padding: 24px" class="cursor-pointer bg-primary text-white rounded-borders shadow-2">
+          <div v-if="infoTestCapture" class="custom-info">
+            <pre>{{ infoTestCapture }}</pre>
+          </div>
+          <div v-else class="text-center q-pa-xl custom-area-placeholder">
+            Touch/click and hold
+          </div>
         </div>
       </div>
     </div>
@@ -48,25 +134,59 @@ export default {
   data () {
     return {
       info: null,
-      infoExtended: null
+      infoExtended: null,
+      infoCustom: null,
+
+      infoTest: null,
+      holdTestStopPropagation: false,
+
+      infoTestCapture: null
     }
   },
   methods: {
-    handleHold ({ position, duration, evt }) {
-      this.info = { position, duration }
+    handleHold ({ evt, ...info }) {
+      this.info = info
 
       // native Javascript event
-      console.log(evt)
+      console.log('TRIGGER', evt)
     },
-    holdExtended ({ position, duration, evt }) {
-      this.infoExtended = { position, duration }
+
+    holdExtended ({ evt, ...info }) {
+      this.infoExtended = info
 
       // native Javascript event
-      console.log(evt)
+      console.log('TRIGGER', evt)
     },
 
-    onClick (ev) {
-      console.log('onClick', ev.type, ev)
+    holdCustom ({ evt, ...info }) {
+      this.infoCustom = info
+
+      // native Javascript event
+      console.log('TRIGGER', evt)
+    },
+
+    holdTest ({ evt, ...info }) {
+      this.infoTest = info
+
+      // native Javascript event
+      console.log('TRIGGER', evt)
+    },
+
+    holdTestCapture ({ evt, ...info }) {
+      this.infoTestCapture = info
+
+      // native Javascript event
+      console.log('TRIGGER', evt)
+    },
+
+    onEvt (reason) {
+      console.log('@' + reason)
+    },
+
+    handleEvt (e) {
+      if (this.holdTestStopPropagation) {
+        e.stopPropagation()
+      }
     }
   }
 }
