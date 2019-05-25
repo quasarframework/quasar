db848b2b7deb6efa9efb29cb0dc9b70fa6b66351
Allan Gaunt <allangaunt@gmail.com>
2019-05-25 20:32:07 +0100

    Changed menu option to title case so it's inline with the others.

--- docs/src/assets/menu.js
+++ docs/src/assets/menu.js
@@ -964,7 +964,7 @@ module.exports = [
     path: 'sponsors-and-backers'
   },
   {
-    name: 'Why donations are important',
+    name: 'Why Donations Are Important',
     icon: 'assignment_late',
     path: 'why-donate'
   },
