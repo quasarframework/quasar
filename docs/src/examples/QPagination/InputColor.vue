ff51c48fee3773547bdf9a578cba77d5fd24a14a
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-06-02 13:42:06 -0600

    feat(QPagination): new props input-style and input-class for input prop

---
+++ docs/src/examples/QPagination/InputColor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="q-pa-lg flex flex-center">
+    <q-pagination
+      v-model="current"
+      :max="5"
+      :input="true"
+      :input-class="inputClass"
+    >
+    </q-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      current: 3,
+      inputClass: 'text-orange-10'
+    }
+  }
+}
+</script>
