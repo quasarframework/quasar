8d2d2a40e636d4f4b6090a6460a711ffb0f4c284
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2019-06-21 19:01:41 -0300

    remove unecessary limit chunk count plugin
    it was breaking the build mode

--- app/lib/webpack/webview/index.js
+++ app/lib/webpack/webview/index.js
@@ -2,8 +2,7 @@ const
   injectHtml = require('../inject.html'),
   injectClientSpecifics = require('../inject.client-specifics'),
   injectHotUpdate = require('../inject.hot-update'),
-  HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin'),
-  webpack = require('webpack')
+  HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 
 module.exports = function (chain, cfg) {
   injectHtml(chain, cfg)
@@ -11,8 +10,4 @@ module.exports = function (chain, cfg) {
   injectHotUpdate(chain, cfg)
   chain.plugin('html-webpack-inline-source')
     .use(HtmlWebpackInlineSourcePlugin)
-  chain.plugin('limit-chunk-count')
-    .use(new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    }))
 }
