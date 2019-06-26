71a48502aefdcbcf33af195af9f5e18de3d94278
Noah Klayman <noahklayman@gmail.com>
2019-06-26 15:09:22 -0700

    feat(app/capacitor): add dev support

--- app/lib/capacitor/index.js
+++ app/lib/capacitor/index.js
@@ -7,29 +7,60 @@ const log = require('../helpers/logger')('app:capacitor'),
   appPaths = require('../app-paths')
 
 class CapacitorRunner {
-  constructor() {
-    this.pid = 0
-
-    onShutdown(() => {
-      this.stop()
+  run(quasarConfig) {
+    return new Promise(async resolve => {
+      const cfg = quasarConfig.getBuildConfig(),
+        url = cfg.build.APP_URL,
+        platform = cfg.ctx.targetName
+      // Make sure there is an index.html, otherwise Capacitor will crash
+      fse.ensureFileSync(appPaths.resolve.app('./dist/capacitor/index.html'))
+      // Copy app data
+      await this.__runCapacitorCommand(['sync', cfg.ctx.targetName])
+      this.__setCapacitorConfig(platform, url)
+      // Make sure cleartext is enabled
+      if (cfg.ctx.targetName === 'android') {
+        const androidManifest = fse.readFileSync(
+          appPaths.resolve.capacitorAndroid('app/src/main/AndroidManifest.xml'),
+          'utf8'
+        )
+        if (!androidManifest.match('android:usesCleartextTraffic="true"')) {
+          warn(
+            'Cleartext must be enabled to connect to the dev server on Android!'
+          )
+          warn(
+            'Add `android:usesCleartextTraffic="true"` to android/app/src/main/AndroidManifest.xml in the `application` tag'
+          )
+          // TODO: actual url with instructions
+          warn('See [need url] for more instructions')
+          process.exit(1)
+        }
+      }
+      await this.__runCapacitorCommand(['open', cfg.ctx.targetName])
+      if (!/^192/.test(url)) {
+        warn(
+          'It appears your dev server is not hosted on your local network. You will only be able to run your app on an emulator.'
+        )
+      }
+      log(
+        `Launching ${
+          platform === 'android' ? 'Android Studio' : 'XCode'
+        }. Run your app here, and it will automatically connect to the dev server.\n`
+      )
+      resolve()
     })
   }
 
-  run(quasarConfig) {
-    // TODO: add support for development
-  }
-
   build(quasarConfig) {
-    return new Promise(async (resolve, reject) => {
+    return new Promise(async resolve => {
       const cfg = quasarConfig.getBuildConfig()
 
-      await this.__runCapacitorCommand(cfg, ['sync', cfg.ctx.targetName])
+      await this.__runCapacitorCommand(['sync', cfg.ctx.targetName])
 
       if (
         process.argv.includes('--openIde') ||
         cfg.ctx.targetName !== 'android'
       ) {
-        return this.__runCapacitorCommand(cfg, ['open', cfg.ctx.targetName])
+        return this.__runCapacitorCommand(['open', cfg.ctx.targetName])
       } else {
         const basePath = appPaths.resolve.capacitorAndroid(
           'app/build/outputs/apk/release'
@@ -59,7 +90,7 @@ class CapacitorRunner {
               warn(
                 'Could not find outputted apk. Please resolve any errors with Capacitor. To run the build in Android Studio, pass the "--openIde" argument to this command.'
               )
-              reject()
+              process.exit(1)
             }
             resolve()
           }
@@ -68,36 +99,43 @@ class CapacitorRunner {
     })
   }
 
-  stop() {
-    if (!this.pid) {
-      return
-    }
-
-    log('Shutting down Capacitor process...')
-    process.kill(this.pid)
-    this.__cleanup()
-  }
-
-  __runCapacitorCommand(cfg, args) {
+  __runCapacitorCommand(args) {
     return new Promise(resolve => {
-      this.pid = spawn(
-        'node_modules/.bin/capacitor',
-        args,
-        appPaths.appDir,
-        code => {
-          this.__cleanup()
-          if (code) {
-            warn(`⚠️  [FAIL] Capacitor CLI has failed`)
-            process.exit(1)
-          }
-          resolve(code)
+      spawn('node_modules/.bin/capacitor', args, appPaths.appDir, code => {
+        if (code) {
+          warn(`⚠️  [FAIL] Capacitor CLI has failed`)
+          process.exit(1)
         }
-      )
+        resolve(code)
+      })
     })
   }
 
-  __cleanup() {
-    this.pid = 0
+  __setCapacitorConfig(platform, serverUrl, webDir) {
+    let configPath
+    // Read existing config
+    if (platform === 'android') {
+      configPath = './android/app/src/main/assets/capacitor.config.json'
+    } else if (platform === 'ios') {
+      configPath = './ios/App/App/capacitor.config.json'
+    } else {
+      configPath = './capacitor.config.json'
+    }
+    const capacitorConfig = JSON.parse(
+      fse.readFileSync(appPaths.resolve.app(configPath), 'utf8')
+    )
+    if (serverUrl) {
+      capacitorConfig.server = capacitorConfig.server || {}
+      capacitorConfig.server.url = serverUrl
+    }
+    if (webDir) {
+      capacitorConfig.webDir = webDir
+    }
+    // Write updated config
+    fse.writeFileSync(
+      appPaths.resolve.app(configPath),
+      JSON.stringify(capacitorConfig)
+    )
   }
 }
 
