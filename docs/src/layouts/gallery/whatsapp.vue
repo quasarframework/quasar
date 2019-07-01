2bfa989fe3bff46ec797c042cdb8a7d547e9ab2e
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-07-01 17:45:27 +0300

    feat(docs): Add style > breakpoints page; Update Whatsapp layout

--- docs/src/layouts/gallery/whatsapp.vue
+++ docs/src/layouts/gallery/whatsapp.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="col position-relative whatsapp-container bg-grey-4">
-    <q-layout view="lHh Lpr lFf" class="shadow-3" container
-      :style="{
-        height: (this.$q.screen.height - (this.$q.screen.gt.md ? 40 : 0)) + 'px',
-        width: (this.$q.screen.width - (this.$q.screen.gt.md ? 350 : 0)) + 'px',
-        top: this.$q.screen.gt.md ? '20px' : 0
-      }"
-    >
+  <div class="WAL position-relative bg-grey-4">
+    <q-layout view="lHh Lpr lFf" class="WAL__layout shadow-3" container>
       <q-header elevated>
         <q-toolbar class="bg-grey-3 text-black">
+          <q-btn
+            round
+            flat
+            icon="keyboard_arrow_left"
+            class="WAL__drawer-open q-mr-sm"
+            @click="leftDrawerOpen = true"
+          />
+
           <q-btn round flat>
             <q-avatar>
               <img :src="currentConversation.avatar">
             </q-avatar>
           </q-btn>
+
           <span class="q-subtitle-1 q-pl-md">
             {{ currentConversation.person }}
           </span>
+
           <q-space/>
-          <q-btn round flat icon="search"/>
-          <q-btn round flat icon="attachment" class="rotate-135"/>
+
+          <q-btn round flat icon="search" />
+          <q-btn round flat>
+            <q-icon name="attachment" class="rotate-135" />
+          </q-btn>
           <q-btn round flat icon="more_vert">
             <q-menu auto-close :offset="[110, 0]">
               <q-list style="min-width: 150px">
@@ -49,14 +56,17 @@
 
       <q-drawer
         v-model="leftDrawerOpen"
-        bordered :breakpoint="0"
-        :width="350"
+        bordered
+        show-if-above
+        :breakpoint="690"
       >
         <q-toolbar class="bg-grey-3">
           <q-avatar class="cursor-pointer">
             <img src="https://cdn.quasar.dev/app-icons/icon-128x128.png" />
           </q-avatar>
+
           <q-space />
+
           <q-btn round flat icon="message" />
           <q-btn round flat icon="more_vert">
             <q-menu auto-close :offset="[110, 8]">
@@ -82,10 +92,18 @@
               </q-list>
             </q-menu>
           </q-btn>
+
+          <q-btn
+            round
+            flat
+            icon="close"
+            class="WAL__drawer-close"
+            @click="leftDrawerOpen = false"
+          />
         </q-toolbar>
 
         <q-toolbar class="bg-grey-2">
-          <q-input rounded outlined dense class="full-width" bg-color="white" v-model="search" placeholder="Search or start a new conversation">
+          <q-input rounded outlined dense class="WAL__field full-width" bg-color="white" v-model="search" placeholder="Search or start a new conversation">
             <template slot="prepend">
               <q-icon name="search" />
             </template>
@@ -94,59 +112,63 @@
 
         <q-scroll-area style="height: calc(100% - 100px)">
           <q-list>
-            <q-item v-for="(conversation, index) in conversations" :key="conversation.id" clickable ripple @click="currentConversationIndex = index">
+            <q-item
+              v-for="(conversation, index) in conversations"
+              :key="conversation.id"
+              clickable
+              v-ripple
+              @click="currentConversationIndex = index"
+            >
               <q-item-section avatar>
                 <q-avatar>
                   <img :src="conversation.avatar">
-                </q-avatar>{{          }}
-              </q-item-section>{{  }}
+                </q-avatar>
+              </q-item-section>
+
               <q-item-section>
-                <div class="row justify-between items-center">
-                  <q-item-label>
-                    {{ conversation.person }}
-                  </q-item-label>
-                  <q-item-label caption>
-                    {{ conversation.time }}
-                  </q-item-label>
-                </div>
-                <div class="row justify-between items-center">
-                  <q-item-label class="conversation__summary" caption>
-                    <q-icon name="check" v-if="conversation.sent"/>
-                    <q-icon name="fas fa-ban" v-if="conversation.deleted"/>
-                    {{ conversation.caption }}
-                  </q-item-label>
-                  <q-item-label class="conversation__more" caption>
-                    <q-icon name="keyboard_arrow_down"/>
-                  </q-item-label>
-                </div>
+                <q-item-label lines="1">
+                  {{ conversation.person }}
+                </q-item-label>
+                <q-item-label class="conversation__summary" caption>
+                  <q-icon name="check" v-if="conversation.sent" />
+                  <q-icon name="fas fa-ban" v-if="conversation.deleted" />
+                  {{ conversation.caption }}
+                </q-item-label>
+              </q-item-section>
+
+              <q-item-section side>
+                <q-item-label caption>
+                  {{ conversation.time }}
+                </q-item-label>
+                <q-icon name="keyboard_arrow_down" />
               </q-item-section>
             </q-item>
           </q-list>
         </q-scroll-area>
       </q-drawer>
 
-      <q-footer>
-        <q-toolbar class="bg-grey-3 text-black row">
-          <q-btn round flat icon="insert_emoticon"/>
-          <q-input rounded outlined dense class="col-grow" bg-color="white" v-model="message" placeholder="Type a message"/>
-          <q-btn round flat icon="mic"/>
-        </q-toolbar>
-      </q-footer>
-
       <q-page-container class="bg-grey-2">
         <router-view />
       </q-page-container>
+
+      <q-footer>
+        <q-toolbar class="bg-grey-3 text-black row">
+          <q-btn round flat icon="insert_emoticon" class="q-mr-sm" />
+          <q-input rounded outlined dense class="WAL__field col-grow q-mr-sm" bg-color="white" v-model="message" placeholder="Type a message" />
+          <q-btn round flat icon="mic" />
+        </q-toolbar>
+      </q-footer>
     </q-layout>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MyLayout',
+  name: 'WhatsappLayout',
 
   data () {
     return {
-      leftDrawerOpen: true,
+      leftDrawerOpen: false,
       search: '',
       message: '',
       currentConversationIndex: 0,
@@ -186,34 +208,69 @@ export default {
       ]
     }
   },
+
   computed: {
     currentConversation () {
       return this.conversations[this.currentConversationIndex]
+    }
+  },
+
+  watch: {
+    leftDrawerOpen (val) {
+      // if user opens drawer in mobile mode
+      // then widens the window and closes drawer,
+      // we should still show drawer on page in standard mode
+      if (val === false && this.$q.screen.width >= 690) {
+        this.$nextTick(() => {
+          this.leftDrawerOpen = true
+        })
+      }
     }
   }
 }
 </script>
 
 <style lang="stylus">
-.q-layout-container
-  margin 0 auto
-  z-index 4000
+.WAL
+  width 100%
+  height 100%
+  padding-top 20px
+  padding-bottom 20px
 
-.whatsapp-container
-  height 100vh
-  &::after
-    content ""
+  &:before
+    content ''
     height 127px
     position fixed
     top 0
     width 100%
     background-color #009688
 
-.q-field--outlined .q-field__control:before
-  border none
+  &__layout
+    margin 0 auto
+    z-index 4000
+    height 100%
+    width 90%
+    max-width 950px
+    border-radius 5px
 
-.q-icon
-  opacity .45
+  &__field.q-field--outlined .q-field__control:before
+    border none
+
+  .q-drawer--standard
+    .WAL__drawer-close
+      display none
+
+@media (max-width 850px)
+  .WAL
+    padding 0
+    &__layout
+      width 100%
+      border-radius 0
+
+@media (min-width 691px)
+  .WAL
+    &__drawer-open
+      display none
 
 .conversation__summary
   margin-top 4px
