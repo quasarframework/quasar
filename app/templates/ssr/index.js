56cd7fb09afe0cfb2389ab4b5065c90ab21d4068
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-07-02 20:45:29 +0300

    feat(app): Further PWA enhancement

--- app/templates/ssr/index.js
+++ app/templates/ssr/index.js
@@ -31,7 +31,6 @@ app.use(compression({ threshold: 0 }))
 // serve this with no cache, if built with PWA:
 if (ssr.settings.pwa) {
   app.use('/service-worker.js', serve('service-worker.js'))
-  app.use('/vue-ssr-client-manifest.json', serve('../vue-ssr-client-manifest.json', true))
 }
 
 // serve "www" folder
