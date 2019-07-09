216054d0fcba05eead107875f9d68577eff4708a
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2019-07-09 10:00:57 -0300

    feat(proton) use tiny-http instead of rouille (#4593)

--- app/templates/proton/rust/src/main.rs
+++ app/templates/proton/rust/src/main.rs
@@ -6,8 +6,7 @@ extern crate clap;
 extern crate proton;
 
 #[cfg(not(feature = "dev"))]
-#[macro_use]
-extern crate rouille;
+extern crate tiny_http;
 
 #[cfg(feature = "dev")]
 use clap::{Arg, App};
@@ -16,8 +15,7 @@ use clap::{Arg, App};
 use std::thread;
 
 mod cmd;
-
-include!(concat!(env!("OUT_DIR"), "/data.rs"));
+mod server;
 
 fn main() {
     let debug;
@@ -53,48 +51,19 @@ fn main() {
     #[cfg(not(feature="dev"))]
     {
         if let Some(available_port) = proton::tcp::get_available_port() {
-            let server_url = format!("{}:{}", "0.0.0.0", available_port);
+            let server_url = format!("{}:{}", "127.0.0.1", available_port);
             content = web_view::Content::Url(format!("http://{}", server_url));
             debug = cfg!(debug_assertions);
 
             thread::spawn(move || {
-                rouille::start_server(server_url, move |request| {
-                    let url = request.url();
-                    if url.starts_with("/statics/") || url.starts_with("/img/")
-                    {
-                        let asset = ASSETS.get(&format!("./target/compiled-web{}", url)).unwrap().into_owned();
-                        if url.ends_with(".svg")
-                        {
-                            rouille::Response::svg(String::from_utf8(asset).unwrap())
-                        }
-                        else
-                        {
-                            rouille::Response::from_data("application/octet-stream", asset)
-                        }
+                let server = tiny_http::Server::http(server_url).unwrap();
+                for request in server.incoming_requests() {
+                    let mut url = request.url().to_string();
+                    if url == "/" {
+                        url = "/index.html".to_string();
                     }
-                    else
-                    {
-                        router!(request,
-                            (GET) (/) => {
-                                rouille::Response::html(String::from_utf8(ASSETS.get("./target/compiled-web/index.html").unwrap().into_owned()).unwrap())
-                            },
-
-                            (GET) (/js/{id: String}) => {
-                                rouille::Response::text(String::from_utf8(ASSETS.get(&format!("./target/compiled-web/js/{}", id)).unwrap().into_owned()).unwrap())
-                            },
-
-                            (GET) (/css/{id: String}) => {
-                                rouille::Response::from_data("text/css;charset=utf-8", ASSETS.get(&format!("./target/compiled-web/css/{}", id)).unwrap().into_owned())
-                            },
-
-                            (GET) (/fonts/{id: String}) => {
-                                rouille::Response::from_data("application/octet-stream", ASSETS.get(&format!("./target/compiled-web/fonts/{}", id)).unwrap().into_owned())
-                            },
-
-                            _ => rouille::Response::empty_404()
-                    )
-                    }
-                });
+                    request.respond(server::asset_response(&url)).unwrap();
+                }
             });
         }
         else
