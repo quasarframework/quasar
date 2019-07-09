d084632dffdfc72cb2f9543009f0faa55fa60eac
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-07-09 11:52:50 +0300

    feat(docs): Update sponsor list

--- docs/src/components/page-parts/sponsors-and-backers/SponsorList.vue
+++ docs/src/components/page-parts/sponsors-and-backers/SponsorList.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   .row.q-gutter-sm
     sponsor(img="campus-cloud-services.png" name="Campus Cloud Services" url="http://campuscloudservices.com/")
+    sponsor(img="platform-purple.png" name="Platform Purple" url="https://platformpurple.com/")
     sponsor(img="truelogic.png" name="Truelogic" url="https://truelogic.com/")
     sponsor(img="jqwidgets.png" name="jQWidgets" url="https://www.jqwidgets.com/")
     sponsor(img="juggle-street.png" name="Juggle Street" url="https://www.jugglestreet.com.au/")
     sponsor(img="com-com-services.png" name="Com Com Services" url="http://comcomservices.com/")
     sponsor(img="kalisio.png" name="Kalisio" url="https://kalisio.com/")
-    sponsor(img="platform-purple.png" name="Platform Purple" url="https://platformpurple.com/")
     sponsor(img="bgasoft.png" name="BGASoft" url="https://www.bgasoft.com/")
     sponsor(img="letsbutterfly.png" name="LetsButterfly" url="https://www.letsbutterfly.com/")
     sponsor(img="project-finance.png" name="Project Finance" url="https://www.projectfinance.io/")
