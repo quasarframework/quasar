3c3eff638195121db0130f31f5c272a5cf62bee7
Daniel Thompson-Yvetot <denjell@quasar.dev>
2019-07-21 17:04:41 +0200

    feat(whitelist) Object.freeze and JSDocs
    Signed-off-by: Daniel Thompson-Yvetot <denjell@quasar.dev>

--- app/templates/entry/proton.js
+++ app/templates/entry/proton.js
@@ -6,10 +6,25 @@
  *
  **/
 
+/**
+ * @module proton
+ * @description This API interface makes powerful interactions available
+ * to be run on client side applications. They are opt-in features, and
+ * must be enabled in quasar.conf.js
+ *
+ * Each binding MUST provide these interfaces in order to be compliant,
+ * and also whitelist them based upon the developer's settings.
+ */
+
 import { uid } from 'quasar'
+
+Object.freeze(proton) // freeze proton so no tampering
+
 <% if (ctx.dev) { %>
 /**
- *
+ * @name __whitelistWarning
+ * @description Present a stylish warning to the developer that their API
+ * call has not been whitelisted in quasar.conf.js
  * @param {String} func - function name to warn
  * @private
  */
@@ -19,45 +34,38 @@ const __whitelistWarning = function (func) {
 <% } %>
 
 /**
- *
- * @type {Promise<any>}
+ * @name __reject
+ * @description is a private promise used to deflect un-whitelisted proton API calls
+ * Its only purpose is to maintain thenable structure in client code without
+ * breaking the application
+ *  * @type {Promise<any>}
  * @private
  */
 const __reject = new Promise((reject) => { reject })
 
-/**
- *
- * @param name
- * @returns {boolean|*}
- * @private
- */
-const __whitelist = function (name) {
-  // seems that this isn't available in lodash.template :(
-  return proton.whitelist[name] === true;
-}
-
 export default class Proton {
-
 <% if (ctx.dev) { %>
   /**
-   *
+   * @name invoke
+   * @description Calls a Quasar Core feature, such as setTitle
    * @param {Object} args
    */
 <% } %>
   static invoke (args) {
+  Object.freeze(args)
     external.invoke(JSON.stringify(args))
   }
 
-
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param callback
+   * @name transformCallback
+   * @description Registers a callback with a uid
+   * @param {Function} callback
    * @returns {*}
    */
 <% } %>
   static transformCallback (callback) {
-    const identifier = uid()
+    const identifier = Object.freeze(uid())
     window[identifier] = (result) => {
       delete window[identifier]
       callback(result)
@@ -67,8 +75,9 @@ export default class Proton {
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param args
+   * @name promisified
+   * @description Turns a request into a chainable promise
+   * @param {Object} args
    * @returns {Promise<any>}
    */
 <% } %>
@@ -84,90 +93,144 @@ export default class Proton {
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param path
+   * @name readTextFile
+   * @description Accesses a non-binary file on the user's filesystem
+   * and returns the content. Permissions based on the app's PID owner
+   * @param {String} path
    * @returns {*|Promise<any>|Promise}
    */
 <% } %>
   static readTextFile (path) {
-  <% if (proton.whitelist.readTextFile === true) { %>
+  <% if (proton.whitelist.readTextFile === true || proton.whitelist.all === true) { %>
+    Object.freeze(path)
     return this.promisified({ cmd: 'readAsString', path })
-  <% } %>
+      <% } else { %>
+  <% if (ctx.dev) { %>
+      __whitelistWarning('readTextFile')
+      <% } %>
+    return __reject
+      <% } %>
   }
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param path
+   * @name readBinaryFile
+   * @description Accesses a binary file on the user's filesystem
+   * and returns the content. Permissions based on the app's PID owner
+   * @param {String} path
    * @returns {*|Promise<any>|Promise}
    */
 <% } %>
   static readBinaryFile (path) {
-  <% if (proton.whitelist.readBinaryFile === true) { %>
+  <% if (proton.whitelist.readBinaryFile === true || proton.whitelist.all === true) { %>
+    Object.freeze(path)
     return this.promisified({ cmd: 'readAsBinary', path })
-  <% } %>
+      <% } else { %>
+  <% if (ctx.dev) { %>
+      __whitelistWarning('readBinaryFile')
+      <% } %>
+    return __reject
+      <% } %>
   }
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param cfg
+   * @name writeFile
+   * @description Write a file to the Local Filesystem.
+   * Permissions based on the app's PID owner
+   * @param {Object} cfg
+   * @param {String} cfg.file
+   * @param {String|Binary} cfg.contents
    */
 <% } %>
   static writeFile (cfg) {
-  <% if (proton.whitelist.writeFile === true) { %>
+  Object.freeze(cfg)
+  <% if (proton.whitelist.writeFile === true || proton.whitelist.all === true) { %>
     this.invoke({ cmd: 'write', file: cfg.file, contents: cfg.contents })
-  <% } %>
-  }
+    <% } else { %>
+  <% if (ctx.dev) { %>
+      __whitelistWarning('writeFile')
+      <% } %>
+    return __reject
+      <% } %>  }
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param path
+   * @name listFiles
+   * @description Get the files in a path.
+   * Permissions based on the app's PID owner
+   * @param {String} path
    * @returns {*|Promise<any>|Promise}
    */
 <% } %>
   static listFiles (path) {
-  <% if (proton.whitelist.listFiles === true) { %>
+  <% if (proton.whitelist.listFiles === true || proton.whitelist.all === true) { %>
+    Object.freeze(path)
     return this.promisified({ cmd: 'list', path })
-  <% } %>
+      <% } else { %>
+  <% if (ctx.dev) { %>
+      __whitelistWarning('listFiles')
+      <% } %>
+    return __reject
+      <% } %>
   }
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param path
+   * @name listDirs
+   * @description Get the directories in a path.
+   * Permissions based on the app's PID owner
+   * @param {String} path
    * @returns {*|Promise<any>|Promise}
    */
 <% } %>
   static listDirs (path) {
-  <% if (proton.whitelist.listDirs === true) { %>
+  <% if (proton.whitelist.listDirs === true || proton.whitelist.all === true) { %>
+    Object.freeze(path)
     return this.promisified({ cmd: 'read', path })
-  <% } %>
+      <% } else { %>
+  <% if (ctx.dev) { %>
+      __whitelistWarning('listDirs')
+      <% } %>
+    return __reject
+      <% } %>
   }
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param title
+   * @name setTitle
+   * @description Set the application's title
+   * @param {String} title
    */
 <% } %>
   static setTitle (title) {
-    <% if (proton.whitelist.setTitle === true) { %>
-      this.invoke({ cmd: 'setTitle', title })
-    <% } %>
+    <% if (proton.whitelist.setTitle === true || proton.whitelist.all === true) { %>
+    Object.freeze(title)
+    this.invoke({ cmd: 'setTitle', title })
+      <% } else { %>
+  <% if (ctx.dev) { %>
+      __whitelistWarning('setTitle')
+      <% } %>
+    return __reject
+      <% } %>
   }
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param command
-   * @param args
+   * @name execute
+   * @description Execute a program with arguments.
+   * Permissions based on the app's PID owner
+   * @param {String} command
+   * @param {String|Array} args
    * @returns {*|Promise<any>|Promise}
    */
 <% } %>
   static execute (command, args) {
-    <% if (proton.whitelist.execute === true) { %>
+    <% if (proton.whitelist.execute === true || proton.whitelist.all === true) { %>
+    Object.freeze(command)
+    if (typeof args === 'string' || typeof args === 'object') {
+      Object.freeze(args)
+    }
     return this.promisified({ cmd: 'call', command, args: typeof (args) === 'string' ? [args] : args })
   <% } else { %>
   <% if (ctx.dev) { %>
@@ -179,14 +242,22 @@ export default class Proton {
 
 <% if (ctx.dev) { %>
   /**
-   *
-   * @param command
-   * @param payload
+   * @name bridge
+   * @description Securely pass a message to the backend.
+   * @example
+   *  this.$q.proton.bridge('QBP/1/ping/client-1', 'pingback')
+   * @param {String} command - a compressed, slash-delimited and
+   * versioned API call to the backend.
+   * @param {String|Object}payload
    * @returns {*|Promise<any>|Promise}
    */
 <% } %>
   static bridge (command, payload) {
-<% if (proton.whitelist.bridge === true) { %>
+<% if (proton.whitelist.bridge === true || proton.whitelist.all === true) { %>
+    Object.freeze(command)
+    if (typeof args === 'string' || typeof args === 'object') {
+      Object.freeze(payload)
+    }
     return this.promisified({ cmd: 'bridge', command, payload: typeof (payload) === 'object' ? [payload] : payload })
 <% } else { %>
 <% if (ctx.dev) { %>
