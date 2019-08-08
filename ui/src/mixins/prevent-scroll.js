c23954ccae482c0deac3afaa5038fac75c456efd
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-08-08 21:01:22 +0300

    Update prevent-scroll.js

--- ui/src/mixins/prevent-scroll.js
+++ ui/src/mixins/prevent-scroll.js
@@ -1,10 +1,49 @@
-import { listenOpts } from '../utils/event.js'
+import { getEventPath, listenOpts, stopAndPrevent } from '../utils/event.js'
+import { hasScrollbar } from '../utils/scroll.js'
 import Platform from '../plugins/Platform.js'
 
 let
   registered = 0,
   scrollPosition,
   bodyTop
+
+function onWheel (e) {
+  if (shouldPreventScroll(e)) {
+    stopAndPrevent(e)
+  }
+}
+
+function shouldPreventScroll (e) {
+  if (e.target === document.body || e.target.classList.contains('q-layout__backdrop')) {
+    return true
+  }
+
+  const
+    path = getEventPath(e),
+    shift = e.shiftKey && !e.deltaX,
+    scrollY = !shift && Math.abs(e.deltaX) <= Math.abs(e.deltaY),
+    delta = shift || scrollY ? e.deltaY : e.deltaX
+
+  for (let index = 0; index < path.length; index++) {
+    const el = path[index]
+
+    if (hasScrollbar(el, scrollY)) {
+      return scrollY
+        ? (
+          delta < 0 && el.scrollTop === 0
+            ? true
+            : delta > 0 && el.scrollTop + el.clientHeight === el.scrollHeight
+        )
+        : (
+          delta < 0 && el.scrollLeft === 0
+            ? true
+            : delta > 0 && el.scrollLeft + el.clientWidth === el.scrollWidth
+        )
+    }
+  }
+
+  return true
+}
 
 function onAppleScroll (e) {
   if (e.target === document) {
@@ -42,6 +81,11 @@ function prevent (register) {
   }
 
   body.classList[action]('q-body--prevent-scroll')
+
+  if (Platform.is.desktop === true && Platform.is.mac === true) {
+    // ref. https://developers.google.com/web/updates/2017/01/scrolling-intervention
+    window[`${action}EventListener`]('wheel', onWheel, listenOpts.notPassive)
+  }
 
   if (register !== true && typeof window !== 'undefined') {
     Platform.is.ios === true && window.removeEventListener('scroll', onAppleScroll, listenOpts.passiveCapture)
