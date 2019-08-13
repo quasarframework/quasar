a58ebfd0bda81ed4e971905e809cd1df45d53cff
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-08-13 21:11:22 +0300

    chore(dev): fix description on model-toggle test with accordeon

--- ui/dev/components/other/model-toggle.vue
+++ ui/dev/components/other/model-toggle.vue
@@ -88,7 +88,7 @@
         <q-expansion-item
           group="group1"
           default-opened
-          label="Expansion Item - Accordion (group1) - defaultOpened - should start closed because of the next defaultOpened"
+          label="Expansion Item - Accordion (group1) - defaultOpened - should start open even if next defaultOpened"
         >
           <q-card>
             <q-card-section>{{ lorem }}</q-card-section>
