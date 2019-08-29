1b92edfe95f6cd7cf9a2a6e2f88cab5702211282
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2019-08-29 02:40:08 -0300

    feat(docs) add releases search & markdown parsing (#4987)
    * feat(releases) add search & parse markdown
    
    * feat(releases) addition to previous commit
    
    * feat(releases) add links parsing

--- docs/src/components/page-parts/releases/PackageReleases.vue
+++ docs/src/components/page-parts/releases/PackageReleases.vue
@@ -2,19 +2,22 @@
 q-splitter.release__splitter(:value="20")
   template(#before)
     q-scroll-area
+      q-input(v-model="search" dense square standout="bg-primary text-white" placeholder="Search...")
       q-tabs.text-primary(vertical v-model="selectedVersion")
-        q-tab(v-for="releaseInfo in releases" :key="releaseInfo.key" :label="releaseInfo.version" :name="releaseInfo.key")
+        q-tab(v-for="releaseInfo in filteredReleases" :key="releaseInfo.key" :label="releaseInfo.version" :name="releaseInfo.key")
   template(#after)
     q-tab-panels.releases-container(v-model="selectedVersion" animated)
-      q-tab-panel(v-for="releaseInfo in releases" :key="releaseInfo.key" :name="releaseInfo.key")
+      q-tab-panel(v-for="releaseInfo in filteredReleases" :key="releaseInfo.key" :name="releaseInfo.key")
         q-scroll-area
-          .release__body(v-html="releaseInfo.body")
+          .release__body(v-html="currentReleaseBody")
 </template>
 
 <script>
+import sanitize from './sanitize'
 export default {
   data () {
     return {
+      search: '',
       selectedVersion: null
     }
   },
@@ -23,6 +26,36 @@ export default {
     releases: {
       type: Array,
       required: true
+    }
+  },
+  computed: {
+    filteredReleases () {
+      return this.search
+        ? this.releases.filter(release => release.body.toLowerCase().includes(this.search))
+        : this.releases
+    },
+    currentReleaseBody () {
+      return this.parse(this.releases.find(r => r.key === this.selectedVersion).body)
+    }
+  },
+  methods: {
+    parse (body) {
+      let content = sanitize(body) + '\n'
+      if (this.search) {
+        content = content.replace(new RegExp(`(${this.search})`, 'ig'), `<span class="bg-accent text-white">$1</span>`)
+      }
+      content = content
+        .replace(/###### ([\S ]+)/g, '<h6>$1</h6>')
+        .replace(/##### ([\S ]+)/g, '<h5>$1</h5>')
+        .replace(/#### ([\S ]+)/g, '<h4>$1</h4>')
+        .replace(/### ([\S ]+)/g, '<h3>$1</h3>')
+        .replace(/## ([\S ]+)/g, '<h2>$1</h2>')
+        .replace(/# ([\S ]+)g/, '<h1>$1</h1>')
+        .replace(/\*\*([\S ]+)\*\*/g, '<strong>$1</strong>')
+        .replace(/\*([\S ]+)\*/g, '<em>$1</em>')
+        .replace(/\* ([\S ]+)\n/g, '<li>$1</li>')
+        .replace(/\[(\S+)\]\((\S+)\)/g, '<a href="$2" target="_blank">$1</a>')
+      return content
     }
   },
   watch: {
@@ -42,7 +75,10 @@ export default {
 .release__body
   white-space pre-line
   margin-top 16px
-  & h2, & h3
+  & h2
     font-size 3rem
-    padding-left 20px
+    margin 0
+  & h3
+    font-size 2.5rem
+    margin 0
 </style>
