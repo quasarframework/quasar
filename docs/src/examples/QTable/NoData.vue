2bb8291ca0fcb4b18d9d3ae31890bff1e9afcd1c
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-09-03 23:47:24 +0300

    Update NoData.vue

--- docs/src/examples/QTable/NoData.vue
+++ docs/src/examples/QTable/NoData.vue
@@ -4,7 +4,7 @@
       title="Treats"
       :data="[]"
       :columns="columns"
-      no-data-label="I didn't find anything to you"
+      no-data-label="I didn't find anything for you"
       row-key="name"
     />
   </div>
