10d0aa21f475c038470c74849985235eb0374a9b
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-09-14 15:40:28 +0300

    feat(docs): Redesign the 404 error page

--- docs/src/pages/Landing.vue
+++ docs/src/pages/Landing.vue
@@ -187,34 +187,21 @@
 
       div Copyright © 2015 - {{ year }} PULSARDEV SRL, Razvan Stoenescu
 
-  section.fixed-top.landing__toolbar.text-white
-    q-toolbar.q-pa-none
-      q-btn.quasar-logo.text-bold(flat, no-caps, no-wrap, to="/start")
-        q-avatar.doc-landing-avatar
-          img(src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg")
-        q-toolbar-title Docs
-
-      q-space
-
-      q-btn.q-mr-xs(type="a", flat, icon="fab fa-github", href="https://github.quasar.dev", target="_blank", rel="noopener")
-      q-btn.q-mr-xs.gt-xs(type="a", flat, icon="fab fa-medium", href="https://blog.quasar.dev", target="_blank", rel="noopener")
-      q-btn.q-mr-xs(type="a", flat, icon="fab fa-discord", href="https://chat.quasar.dev", target="_blank", rel="noopener")
-      q-btn.q-mr-xs(type="a", flat, icon="fas fa-comments", href="https://forum.quasar.dev/", target="_blank", rel="noopener")
-      q-btn.q-mr-xs.gt-xs(type="a", flat, icon="fab fa-twitter", href="https://twitter.quasar.dev", target="_blank", rel="noopener")
-      q-btn.q-mr-xs.gt-xs(type="a", flat, icon="fab fa-facebook", href="https://facebook.quasar.dev", target="_blank", rel="noopener")
-      q-btn.gt-xs(type="a", flat, icon="email", to="/newsletter", rel="noopener")
+  landing-top-bar
 </template>
 
 <script>
 import Sponsor from 'components/page-parts/sponsors-and-backers/Sponsor'
 import SponsorList from 'components/page-parts/sponsors-and-backers/SponsorList'
+import LandingTopBar from 'components/page-parts/landing/LandingTopBar'
 
 export default {
   name: 'Landing',
 
   components: {
     Sponsor,
-    SponsorList
+    SponsorList,
+    LandingTopBar
   },
 
   meta: {
@@ -229,9 +216,6 @@ export default {
 </script>
 
 <style lang="stylus">
-.doc-landing-avatar > div
-  border-radius 0
-
 .landing
   background #000 url('https://cdn.quasar.dev/img/quasar-cover.jpg') no-repeat center center fixed
   background-size cover
@@ -248,14 +232,6 @@ export default {
       position relative
       max-width 1040px
       width 100%
-
-  &__toolbar
-    background linear-gradient(to bottom, #000, transparent)
-    padding 0 !important
-
-    .q-btn
-      border-radius 0 0 5px 5px
-      align-self stretch
 
   &__hero
     min-height 100vh
