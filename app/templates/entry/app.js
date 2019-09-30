aee76139f6d4d4ecbd1f83b4a897a8018af9b2e5
webnoob <allangaunt@gmail.com>
2019-09-30 15:53:29 +0100

    fix (bex): Remove custom q-app tag for bex.

--- app/templates/entry/app.js
+++ app/templates/entry/app.js
@@ -42,8 +42,7 @@ export default function (<%= ctx.mode.ssr ? 'ssrContext' : '' %>) {
   // Here we inject the router, store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    <% if (!ctx.mode.ssr && !ctx.mode.bex) { %>el: '#q-app',<% } %>
-    <% if (ctx.mode.bex) { %>el: '#q-bex-app',<% } %>
+    <% if (!ctx.mode.ssr) { %>el: '#q-app',<% } %>
     router,
     <%= store ? 'store,' : '' %>
     render: h => h(App)
