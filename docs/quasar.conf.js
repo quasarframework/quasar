3efc78539395db812a3b257266d2c7540b1aa560
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-10-16 11:44:32 +0300

    feat(docs): GDPR notice

--- docs/quasar.conf.js
+++ docs/quasar.conf.js
@@ -6,6 +6,7 @@ module.exports = function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     boot: [
+      { path: 'gdpr', server: false },
       'components'
     ],
 
