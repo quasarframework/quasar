d62f198e417244ba8d47dbf3966c67c92e0ee41a
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-10-21 14:05:20 +0300

    feat(docs): Update to a better example for LinearProgress with label

--- docs/src/examples/QLinearProgress/Label.vue
+++ docs/src/examples/QLinearProgress/Label.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="q-pa-md">
-    <q-linear-progress style="height: 25px" :value="progress" color="accent" class="q-mt-sm q-mt-sm row flex-center">
-      <div class="text-black">
-        {{ progressLabel }}
+    <q-linear-progress size="25px" :value="progress1" color="accent">
+      <div class="absolute-full flex flex-center">
+        <q-badge color="white" text-color="accent" :label="progressLabel1" />
+      </div>
+    </q-linear-progress>
+
+    <q-linear-progress size="25px" :value="progress2" color="accent" class="q-mt-sm">
+      <div class="absolute-full flex flex-center">
+        <q-badge color="white" text-color="accent" :label="progressLabel2" />
       </div>
     </q-linear-progress>
   </div>
@@ -12,13 +18,18 @@
 export default {
   data () {
     return {
-      progress: 0.4
+      progress1: 0.3,
+      progress2: 0.9
     }
   },
 
   computed: {
-    progressLabel () {
-      return (this.progress * 100).toFixed(2) + '%'
+    progressLabel1 () {
+      return (this.progress1 * 100).toFixed(2) + '%'
+    },
+
+    progressLabel2 () {
+      return (this.progress2 * 100).toFixed(2) + '%'
     }
   }
 }
