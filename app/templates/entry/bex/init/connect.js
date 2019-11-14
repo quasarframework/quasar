9f3ce9cbd50cc1804e32829e48778963ed80693d
Allan Gaunt <allangaunt@gmail.com>
2019-11-14 16:23:15 +0000

    fix(bex): Popup comms: Tab not defined just because chrome.tabs is.

--- app/templates/entry/bex/init/connect.js
+++ app/templates/entry/bex/init/connect.js
@@ -33,19 +33,27 @@ export default function connect () {
     cb(bridge)
   }
 
+  const fallbackConnection = cb => {
+    // If we're not in a proper web browser tab, generate an id so we have a unique connection to whatever it is.
+    // this could be the popup window or the options window (As they don't have tab ids)
+    // If dev tools is available, it means we're on it. Use that for the id.
+    const tabId = chrome.devtools ? chrome.devtools.inspectedWindow.tabId : uid()
+    buildConnection(tabId, cb)
+  }
+
   window.QBexInit({
     connect (cb) {
       if (chrome.tabs && !chrome.devtools) {
         // If we're on a web browser tab, use the current tab id to connect to the app.
         chrome.tabs.getCurrent(tab => {
-          buildConnection(tab.id, cb)
+          if (tab && tab.id) {
+            buildConnection(tab.id, cb)
+          } else {
+            fallbackConnection(cb)
+          }
         })
       } else {
-        // If we're not in a proper web browser tab, generate an id so we have a unique connection to whatever it is.
-        // this could be the popup window or the options window (As they don't have tab ids)
-        // If dev tools is available, it means we're on it. Use that for the id.
-        const tabId = chrome.devtools ? chrome.devtools.inspectedWindow.tabId : uid()
-        buildConnection(tabId, cb)
+        fallbackConnection(cb)
       }
 
     },
