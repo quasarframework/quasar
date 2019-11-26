a634c5baaefa15a61ab7229918a9aa9894344df9
Lucas Fernandes Gonçalves Nogueira <lucasfernandesnog@gmail.com>
2019-11-26 10:56:05 -0300

    fix(quasar) code review updates

--- docs/src/components/DocApi.vue
+++ docs/src/components/DocApi.vue
@@ -198,7 +198,7 @@ export default {
   },
 
   methods: {
-    parseJson (name, { type, behavior, docs, ...api }) {
+    parseJson (name, { type, behavior, meta, ...api }) {
       this.aggregationModel = {}
 
       if (type === 'component' && api.props !== void 0) {
