2081daa5a35cba84b549cbb7eddb9d476fd1a473
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-11-29 20:30:03 +0200

    fix(QDialog/QMenu): On iOS, components ignore first touch of the screen #5624

--- ui/src/components/dialog/QDialog.js
+++ ui/src/components/dialog/QDialog.js
@@ -200,6 +200,9 @@ export default Vue.extend({
           document.activeElement.scrollIntoView()
         }
 
+        // required in order to avoid the "double-tap needed" issue
+        this.$q.platform.is.ios === true && this.__portal.$el.click()
+
         this.$emit('show', evt)
       }, 300)
     },
