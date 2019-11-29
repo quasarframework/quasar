97d132091de9b274aa9058bd4650b1f030a907c5
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2019-11-29 11:42:14 -0300

    feat(docs) add copy button to flex playground (#5749)
    * feat(docs) add copy button to flex playground
    
    * Update CopyButton.vue

---
+++ docs/src/components/CopyButton.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+div.relative
+  q-btn(
+    color="primary"
+    round
+    dense
+    flat
+    icon="content_copy"
+    @click="copyToClipboard"
+  )
+    q-tooltip Copy to Clipboard
+
+  transition(
+    enter-active-class="animated fadeIn"
+    leave-active-class="animated fadeOut"
+  )
+    q-badge.absolute(
+      v-show="copied"
+      style="top: 12px; right: 58px;"
+    ) Copied to clipboard
+</template>
+
+<script>
+
+import { copyToClipboard } from 'quasar'
+
+export default {
+  props: {
+    text: [Function, String]
+  },
+
+  data () {
+    return {
+      copied: false
+    }
+  },
+
+  methods: {
+    copyToClipboard () {
+      copyToClipboard(typeof this.text === 'string' ? this.text : this.text())
+      this.copied = true
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.copied = false
+        this.timer = null
+      }, 2000)
+    }
+  }
+}
+</script>
