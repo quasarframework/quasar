4eb689efba4ea76c08ad5c1052997462e17fa4da
Paolo Caleffi <p.caleffi@dreamonkey.com>
2019-12-06 14:23:16 +0100

    fix(types): fix passive/notPassive usage when removing a listener

--- ui/types/utils/event.d.ts
+++ ui/types/utils/event.d.ts
@@ -1,7 +1,14 @@
+// Allow using `passive` and `notPassive` with `removeEventListener`
+// See https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener#Matching_event_listeners_for_removal
+// See https://github.com/quasarframework/quasar/pull/5729#issuecomment-559588257
+interface RemoveEventListenerFix {
+  capture: undefined;
+}
+
 export interface ListenOpts {
   hasPassive: boolean;
-  passive: undefined | { passive: true };
-  notPassive: undefined | { passive: false };
+  passive: undefined | ({ passive: true } & RemoveEventListenerFix);
+  notPassive: undefined | ({ passive: false } & RemoveEventListenerFix);
   passiveCapture: true | { passive: true; capture: true };
   notPassiveCapture: true | { passive: false; capture: true };
 }
