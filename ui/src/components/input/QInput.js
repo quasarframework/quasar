8c34486e913c64956d235f9d696c7014b1912400
Dan Popescu <pdan.popescu@gmail.com>
2019-12-06 22:34:12 +0200

    fix(QInput): Update input value with empry string instead of undefined #5820

--- ui/src/components/input/QInput.js
+++ ui/src/components/input/QInput.js
@@ -207,7 +207,7 @@ export default Vue.extend({
 
       this.type !== 'file' && this.$nextTick(() => {
         if (this.$refs.input !== void 0) {
-          this.$refs.input.value = this.innerValue
+          this.$refs.input.value = this.innerValue !== void 0 ? this.innerValue : ''
         }
       })
     },
@@ -260,7 +260,7 @@ export default Vue.extend({
           ? {
             value: this.hasOwnProperty('tempValue') === true
               ? this.tempValue
-              : this.innerValue
+              : (this.innerValue !== void 0 ? this.innerValue : '')
           }
           : null
       })
