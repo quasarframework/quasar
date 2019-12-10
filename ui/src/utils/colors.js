fd47304b31fa4559ca6787a89dbf9bdf34a30940
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-12-10 20:01:34 +0200

    Update colors.js

--- ui/src/utils/colors.js
+++ ui/src/utils/colors.js
@@ -220,8 +220,10 @@ export function brightness (color) {
     throw new TypeError('Expected a string or a {r, g, b} object as color')
   }
 
-  const
-    rgb = typeof color === 'string' ? textToRgb(color) : color
+  const rgb = typeof color === 'string'
+    ? textToRgb(color)
+    : color
+
   return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000
 }
 
