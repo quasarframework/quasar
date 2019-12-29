5b49b1892765c57f97eedeed18fbd1e4e3fc1d2b
Scott <scott.molinari@adduco.de>
2019-12-29 18:43:56 +0100

    update(docs): add new videos
    Adds 3 new videos from codeguru

--- docs/src/components/page-parts/video-tutorials/quasar-tutorials.js
+++ docs/src/components/page-parts/video-tutorials/quasar-tutorials.js
@@ -15,5 +15,32 @@ export default [
     lang: 'Spanish',
     link: 'https://click.linksynergy.com/link?id=V56OaeB17aU&offerid=507388.2075016&type=2&murl=https%3A%2F%2Fwww.udemy.com%2Fcourse%2Faplicaciones-pwa-con-vuejs-y-quasar-framework%2F',
     imgUrl: 'https://i.udemycdn.com/course/480x270/2075016_4ea3_2.jpg'
+  },
+
+  {
+    name: 'Getting started with Quasar Framework',
+    desc: 'In this video we will see how to get started with Quasar Framework by setting up a development environment and quickly creating a hello world quasar app for both web and desktop.',
+    author: 'codeguru',
+    lang: 'English',
+    link: 'https://www.youtube.com/watch?v=BK66mQTSl7U',
+    imgUrl: 'https://i.ytimg.com/vi/BK66mQTSl7U/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCG_P7GvHFaoEzScLX83FBKt49sUw'
+  },
+
+  {
+    name: 'How to create Android and iOS applications using Quasar Framework',
+    desc: 'In this video tutorial, codeguru shows you how to create Android and iOS applications using Quasar Framework. He will go in depth in setting up the environments for both Android and iOS applications.',
+    author: 'codeguru',
+    lang: 'English',
+    link: 'https://www.youtube.com/watch?v=As5lzor2NQ8',
+    imgUrl: 'https://i.ytimg.com/vi/As5lzor2NQ8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLANOzkdmYe6Ng5jdSfPGMSuL7HFlQ'
+  },
+
+  {
+    name: 'How to Host a Quasar Framework application on AWS in 10 mins using AWS-Amplify',
+    desc: 'In this video codeguru discusses hosting a Quasar application on AWS using the aws-amplify framework.',
+    author: 'codeguru',
+    lang: 'English',
+    link: 'https://www.youtube.com/watch?v=1F0SfYmrSbA',
+    imgUrl: 'https://i.ytimg.com/vi/1F0SfYmrSbA/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA2kGICdkby-h6xUpCKhj763uYSwg'
   }
 ]
