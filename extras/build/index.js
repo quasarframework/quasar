72b9dc7531d1b636db483d8430a1006cb0dde37b
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-01-15 16:37:47 +0200

    chore(extras): re-enable all builds

--- extras/build/index.js
+++ extras/build/index.js
@@ -1,11 +1,9 @@
-// require('./webfonts.js')
+require('./webfonts.js')
 require('./animate')
 
-/*
 require('./material-icons.js')
 require('./mdi-v4.js')
 require('./fontawesome-v5.js')
 require('./ionicons-v4.js')
 require('./eva-icons.js')
 require('./themify.js')
-*/
