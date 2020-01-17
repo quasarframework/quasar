fdc364f9f035ae76843098747f045f498fc6287b
Dan Popescu <pdan.popescu@gmail.com>
2020-01-17 14:37:09 +0200

    fix(QIcon): Misc fixes for SVG icon support in IE

--- ui/src/ie-compat/ie.js
+++ ui/src/ie-compat/ie.js
@@ -201,6 +201,46 @@ typeof window !== 'undefined' && (function (window) {
       }
     })
   }
+
+  if (!('classList' in SVGElement.prototype)) {
+    Object.defineProperty(SVGElement.prototype, 'classList', {
+      get: function get () {
+        var _this = this
+
+        return {
+          contains: function contains (className) {
+            return _this.className.baseVal.split(' ').indexOf(className) !== -1
+          },
+          add: function add (className) {
+            return _this.setAttribute(
+              'class',
+              _this.getAttribute('class') + ' ' + className
+            )
+          },
+          remove: function remove (className) {
+            var removedClass = _this
+              .getAttribute('class')
+              .replace(
+                new RegExp('(\\s|^)'.concat(className, '(\\s|$)'), 'g'),
+                '$2'
+              )
+
+            if (_this.classList.contains(className)) {
+              _this.setAttribute('class', removedClass)
+            }
+          },
+          toggle: function toggle (className) {
+            if (this.contains(className)) {
+              this.remove(className)
+            }
+            else {
+              this.add(className)
+            }
+          }
+        }
+      }
+    })
+  }
 }(window))
 
 /* eslint-disable */
