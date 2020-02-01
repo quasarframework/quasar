a7ee096cc2c770ea6d2416a893ddee0bdef2ec6a
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-02-01 15:07:59 +0200

    feat(ClickOutside): Only close on click when the mousedown was not inside the monitored element #5791

--- ui/src/components/menu/ClickOutside.js
+++ ui/src/components/menu/ClickOutside.js
@@ -76,7 +76,7 @@ export default {
 
     if (handlers.click.length === 0) {
       // use click to be able to prevent click in handler
-      document.addEventListener('click', globalHandler, notPassiveCapture)
+      document.addEventListener('mousedown', globalHandler, notPassiveCapture)
       document.addEventListener('touchstart', globalHandler, notPassiveCapture)
       document.addEventListener('focusin', globalHandler, passiveCapture)
     }
@@ -113,7 +113,7 @@ export default {
 
       if (handlers.click.length === 0) {
         clearTimeout(timer)
-        document.removeEventListener('click', globalHandler, notPassiveCapture)
+        document.removeEventListener('mousedown', globalHandler, notPassiveCapture)
         document.removeEventListener('touchstart', globalHandler, notPassiveCapture)
         document.removeEventListener('focusin', globalHandler, passiveCapture)
       }
