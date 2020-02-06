1494745e4e515f20db8cacc71f980b5ed7df10bb
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-02-06 17:33:56 +0200

    Update types-feature-flags.js

--- app/lib/helpers/types-feature-flags.js
+++ app/lib/helpers/types-feature-flags.js
@@ -1,19 +1,19 @@
-const logger = require("./logger");
-const path = require("path");
-const warn = logger("app:mode", "yellow");
-const fs = require("fs");
-const fse = require("fs-extra");
-const getMode = require("../mode/index");
-const appPaths = require("../app-paths");
+const logger = require('./logger')
+const path = require('path')
+const warn = logger('app:mode', 'yellow')
+const fs = require('fs')
+const fse = require('fs-extra')
+const getMode = require('../mode/index')
+const appPaths = require('../app-paths')
 
 function getStoreFlagPath(storeIndexPath) {
-  return path.join(path.parse(storeIndexPath).dir, "store-flag.d.ts");
+  return path.join(path.parse(storeIndexPath).dir, 'store-flag.d.ts')
 }
 
 module.exports = function regenerateTypesFeatureFlags(buildConfig) {
   const storeFeatureData = [
     buildConfig.store,
-    appPaths.resolve.cli(`templates/app/store/store-flag.d.ts`),
+    appPaths.resolve.cli('templates/app/store/store-flag.d.ts'),
     appPaths.resolve.app(getStoreFlagPath(buildConfig.sourceFiles.store))
   ];
 
@@ -24,26 +24,24 @@ module.exports = function regenerateTypesFeatureFlags(buildConfig) {
   //    every time `quasar dev` and `quasar build` are run:
   //    this automatize the upgrade for existing codebases
   for (const feature of [
-    "pwa",
-    "cordova",
-    "capacitor",
-    "electron",
-    "ssr",
-    "store"
+    'pwa',
+    'cordova',
+    'capacitor',
+    'electron',
+    'ssr',
+    'store'
   ]) {
-    const [isFeatureInstalled, sourceFlagPath, destFlagPath] =
-      feature === "store"
-        ? storeFeatureData
-        : [
-            getMode(feature).isInstalled,
-            appPaths.resolve.cli(`templates/${feature}/${feature}-flag.d.ts`),
-            appPaths.resolve[feature](`${feature}-flag.d.ts`)
-          ];
+    const [isFeatureInstalled, sourceFlagPath, destFlagPath] = feature === 'store'
+      ? storeFeatureData
+      : [
+        getMode(feature).isInstalled,
+        appPaths.resolve.cli(`templates/${feature}/${feature}-flag.d.ts`),
+        appPaths.resolve[feature](`${feature}-flag.d.ts`)
+      ]
 
     if (isFeatureInstalled && !fs.existsSync(destFlagPath)) {
-      fse.copySync(sourceFlagPath, destFlagPath);
-
-      warn(`'${feature}' feature flag was missing and has been regenerated`);
+      fse.copySync(sourceFlagPath, destFlagPath)
+      warn(`'${feature}' feature flag was missing and has been regenerated`)
     }
   }
 };
