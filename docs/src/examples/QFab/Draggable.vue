b5abbef8d168dbfb51655809e2bc7efd9781be86
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-02-14 16:29:00 +0200

    fix(docs): QFab draggable example on mobile devices

--- docs/src/examples/QFab/Draggable.vue
+++ docs/src/examples/QFab/Draggable.vue
@@ -13,7 +13,7 @@
               direction="up"
               color="accent"
               :disable="draggingFab"
-              v-touch-pan.mouse="moveFab"
+              v-touch-pan.prevent.mouse="moveFab"
             >
               <q-fab-action @click="onClick" color="primary" icon="person_add" :disable="draggingFab" />
               <q-fab-action @click="onClick" color="primary" icon="mail" :disable="draggingFab" />
