42be585e371cc97cfb190dfd127e79be6e9ca7a4
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-02-18 09:00:25 -0700

    fix(docs): use material icons for codepen to work

--- docs/src/examples/QPagination/Icons.vue
+++ docs/src/examples/QPagination/Icons.vue
@@ -5,33 +5,21 @@
       :max="5"
       :direction-links="true"
       :boundary-links="true"
-      :icon-first="laAngleDoubleLeftSolid"
-      :icon-last="laAngleDoubleRightSolid"
-      :icon-prev="lasAngleLeft"
-      :icon-next="lasAngleRight"
+      icon-first="skip_previous"
+      icon-last="skip_next"
+      icon-prev="fast_rewind"
+      icon-next="fast_forward"
     >
     </q-pagination>
   </div>
 </template>
 
 <script>
-import {
-  laAngleDoubleLeftSolid, laAngleDoubleRightSolid, lasAngleLeft, lasAngleRight
-} from '@quasar/extras/line-awesome'
-
 export default {
   data () {
     return {
       current: 3
     }
-  },
-
-  created () {
-    // these icons to not need be Vue reactive
-    this.laAngleDoubleLeftSolid = laAngleDoubleLeftSolid
-    this.laAngleDoubleRightSolid = laAngleDoubleRightSolid
-    this.lasAngleLeft = lasAngleLeft
-    this.lasAngleRight = lasAngleRight
   }
 }
 </script>
