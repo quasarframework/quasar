e702d5a8300b47fde6cf7018c69a1af709290f2b
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-02-28 13:01:07 +0200

    feat(uid): small tweaks

--- ui/src/utils/uid.js
+++ ui/src/utils/uid.js
@@ -2,6 +2,8 @@
  * Based on the work of https://github.com/jchook/uuid-random
  */
 
+import { isSSR } from '../plugins/Platform.js'
+
 let
   buf,
   bufIdx = 0,
@@ -12,70 +14,61 @@ for (let i = 0; i < 256; i++) {
   hexBytes[i] = (i + 0x100).toString(16).substr(1)
 }
 
-// Buffer random numbers for speed
-// Reduce memory usage by decreasing this number (min 16)
-// or improve speed by increasing this number (try 16384)
-const BUFFER_SIZE = 4096
-
-// Node & Browser support
-let crypt0
-if (typeof crypto !== 'undefined') {
-  crypt0 = crypto
-}
-else if (typeof window !== 'undefined' && window.msCrypto !== void 0) {
-  crypt0 = window.msCrypto // IE11
-}
-
 // Use best available PRNG
-// Also expose this so you can override it.
-const randomBytes = (function () {
-  if (crypt0 !== void 0) {
-    if (crypt0.randomBytes !== void 0) {
-      return crypt0.randomBytes
+const randomBytes = (() => {
+  // Node & Browser support
+  const lib = isSSR === true
+    ? (typeof module !== 'undefined' && typeof require === 'function' ? require('crypto') : void 0)
+    : (
+      typeof crypto !== 'undefined'
+        ? crypto
+        : window.msCrypto // IE11
+    )
+
+  if (lib !== void 0) {
+    if (lib.randomBytes !== void 0) {
+      return lib.randomBytes
     }
-    if (crypt0.getRandomValues !== void 0) {
-      return function (n) {
+    if (lib.getRandomValues !== void 0) {
+      return n => {
         var bytes = new Uint8Array(n)
-        crypt0.getRandomValues(bytes)
+        lib.getRandomValues(bytes)
         return bytes
       }
     }
   }
-  return function (n) {
+
+  return n => {
     const r = []
-    for (let i = 0; i < n; i++) {
+    for (let i = n; i > 0; i -= 1) {
       r.push(Math.floor(Math.random() * 256))
     }
     return r
   }
 })()
 
-// Buffer some random bytes for speed
-function randomBytesBuffered (n) {
-  if (buf === void 0 || ((bufIdx + n) > BUFFER_SIZE)) {
+// Buffer random numbers for speed
+// Reduce memory usage by decreasing this number (min 16)
+// or improve speed by increasing this number (try 16384)
+const BUFFER_SIZE = 4096
+
+export default function () {
+  // Buffer some random bytes for speed
+  if (buf === void 0 || (bufIdx + 16 > BUFFER_SIZE)) {
     bufIdx = 0
     buf = randomBytes(BUFFER_SIZE)
   }
-  return buf.slice(bufIdx, (bufIdx += n))
-}
 
-// uuid.bin
-function uuidBin () {
-  const b = randomBytesBuffered(16)
+  const b = buf.slice(bufIdx, (bufIdx += 16))
   b[6] = (b[6] & 0x0f) | 0x40
   b[8] = (b[8] & 0x3f) | 0x80
-  return b
-}
 
-// String UUIDv4 (Random)
-export default function () {
-  const b = uuidBin()
   return hexBytes[b[0]] + hexBytes[b[1]] +
-        hexBytes[b[2]] + hexBytes[b[3]] + '-' +
-        hexBytes[b[4]] + hexBytes[b[5]] + '-' +
-        hexBytes[b[6]] + hexBytes[b[7]] + '-' +
-        hexBytes[b[8]] + hexBytes[b[9]] + '-' +
-        hexBytes[b[10]] + hexBytes[b[11]] +
-        hexBytes[b[12]] + hexBytes[b[13]] +
-        hexBytes[b[14]] + hexBytes[b[15]]
+    hexBytes[b[2]] + hexBytes[b[3]] + '-' +
+    hexBytes[b[4]] + hexBytes[b[5]] + '-' +
+    hexBytes[b[6]] + hexBytes[b[7]] + '-' +
+    hexBytes[b[8]] + hexBytes[b[9]] + '-' +
+    hexBytes[b[10]] + hexBytes[b[11]] +
+    hexBytes[b[12]] + hexBytes[b[13]] +
+    hexBytes[b[14]] + hexBytes[b[15]]
 }
