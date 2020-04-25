e36796e1a432aac45499330084c06945a4500efb
Allan Gaunt <allangaunt@gmail.com>
2020-04-25 23:59:06 +0100

    fix(bex): Tweak type location to fit inline with other modes #6883

--- ui/types/globals.d.ts
+++ ui/types/globals.d.ts
@@ -5,11 +5,11 @@ import { QuasarLanguage } from "./lang";
 // We cannot reference directly Capacitor/Cordova/Electron types
 //  or they would generate TS errors for Vue CLI users
 // We also cannot move feature-flags system into `@quasar/app`
-//  because `QVueGlobals` augmentations won't be transfered to
+//  because `QVueGlobals` augmentations won't be transferred to
 //  the `vue/types/vue` augmentation for unknown reason (probably TS limitations)
 //  and the system will just stop working
 // To workaround these problems we define an empty holder interface
-//  and augment it into `@quasar/app` with currect typings
+//  and augment it into `@quasar/app` with current typings
 export interface GlobalsTypesHolder {
   [index: string]: any;
 }
@@ -28,21 +28,15 @@ type GlobalQuasarIconMapFn = (
   iconName: string
 ) => { icon: string } | { cls: string; content?: string } | void;
 
-interface GlobalQuasarBex {
-  send(eventName: string, payload?: object): Promise<any>;
-  on(eventName: string, eventListener: (payload?: object) => any): void;
-  off(eventName: string, eventListener: (payload?: object) => any): void;
-}
-
 export interface QVueGlobals
   extends HasCapacitor<{ capacitor: any }>,
+    HasBex<{ bex: GlobalsTypesHolder["bex"] }>,
     HasCordova<{ cordova: GlobalsTypesHolder["cordova"] }>,
     HasElectron<{ electron: GlobalsTypesHolder["electron"] }>,
     HasSsr<
       { iconMapFn?: GlobalQuasarIconMapFn },
       { iconMapFn: GlobalQuasarIconMapFn }
-    >,
-    HasBex<{ bex: GlobalQuasarBex }> {
+    > {
   version: string;
   lang: GlobalQuasarLanguage;
   iconSet: GlobalQuasarIconSet;
