7b16690d522cf0c54263a1ad1884d21cbe9dfe7f
bias-santa <tobiaslehner@me.com>
2020-05-24 23:01:14 +0200

    Fixes typo and changes an example
    I tried to come up with a new example, that better demonstrates "column-wrapping". I found the second example confusing for new readers since it also requires knowledge about breakpoints, and to see the effect, you need to resize the browser window. Or you may not even know which rule is applied on the screen you are viewing the docs. So I tried to come up with something more intuitive :) Also fixed a typo.

--- docs/src/examples/grid/RowColumnWrapping.vue
+++ docs/src/examples/grid/RowColumnWrapping.vue
@@ -3,16 +3,19 @@
 
     <div class="row">
       <div class="col-9">.col-9</div>
-      <div class="col-6">.col-6<br>Since 9 + 4 = 13 &gt; 12, this 4-column-wide div gets wrapped onto a new line as one contiguous unit.</div>
+      <div class="col-4">.col-4<br>Since 9 + 4 = 13 &gt; 12, this 4-column-wide div gets wrapped onto a new line as one contiguous unit.</div>
       <div class="col-5">.col-5<br>Subsequent columns continue along the new line.</div>
     </div>
 
     <div class="row">
-      <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
-      <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
-
-      <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
-      <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+      <div class="col-2">.col-2</div>
+      <div class="col-3">.col-3</div>
+      <div class="col-4">.col-4</div>
+      <div class="col-5">.col-5</div>
+      <div class="col-6">.col-6</div>
+      <div class="col-5">.col-5</div>
+      <div class="col-4">.col-4</div>
+      <div class="col-3">.col-3</div>
     </div>
 
   </div>
