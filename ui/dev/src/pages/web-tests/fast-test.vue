43f53ed58444b613d1a6ad2bd4e34d7e9296a9ba
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-06-03 20:43:41 +0300

    chore(ui/dev): reset fast-test page

--- ui/dev/src/pages/web-tests/fast-test.vue
+++ ui/dev/src/pages/web-tests/fast-test.vue
@@ -8,7 +8,6 @@
       For some test that you think it should be persistent,
       make a new *.vue file here or in another folder under /dev/components.
     -->
-    <q-btn-toggle value="off" :options="options" @click="notify" @mouseover="notify" class="q-ma-md" />
   </div>
 </template>
 
@@ -18,13 +17,9 @@
 export default {
   data () {
     return {
-      options: [{label:'Popup', value: 'on'}, {label:'No popup', value: 'off'}]
     }
   },
   methods: {
-    notify: function () {
-      this.$q.notify('Running on Quasar v' + this.$q.version)
-    }
   }
 }
 </script>
