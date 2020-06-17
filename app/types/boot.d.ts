b6fab30065c6880765ad3b60d6c2275c3fc4b076
Paolo Caleffi <p.caleffi@dreamonkey.com>
2020-06-17 13:01:53 +0200

    feat(app): support TS 3.9.x (#7237)

--- app/types/boot.d.ts
+++ app/types/boot.d.ts
@@ -1,18 +1,17 @@
-import { HasSsrParam, HasStoreParam } from "quasar";
 import Vue, { ComponentOptions, VueConstructor } from "vue";
 import VueRouter, { RawLocation } from "vue-router";
+import { HasSsrParam } from "./ssr";
+import { HasStoreParam } from "./store";
 
-declare module "quasar" {
-  interface BootFileParams<TStore> extends HasSsrParam, HasStoreParam<TStore> {
-    app: ComponentOptions<Vue>;
-    Vue: VueConstructor;
-    router: VueRouter;
-    urlPath: string;
-    publicPath: string;
-    redirect: (url: string | RawLocation) => void;
-  }
-
-  type BootCallback<TStore> = (
-    params: BootFileParams<TStore>
-  ) => void | Promise<void>;
+interface BootFileParams<TStore> extends HasSsrParam, HasStoreParam<TStore> {
+  app: ComponentOptions<Vue>;
+  Vue: VueConstructor;
+  router: VueRouter;
+  urlPath: string;
+  publicPath: string;
+  redirect: (url: string | RawLocation) => void;
 }
+
+export type BootCallback<TStore> = (
+  params: BootFileParams<TStore>
+) => void | Promise<void>;
