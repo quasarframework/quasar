2b258a4257d5a3289490c5e3469bf7daa5b4e3ce
Allan Gaunt <allangaunt@gmail.com>
2020-07-08 06:23:36 +0100

    fix(app): Source maps not working when importStrategy: 'auto' (#7328)

--- app/lib/webpack/loader.transform-quasar-imports.js
+++ app/lib/webpack/loader.transform-quasar-imports.js
@@ -3,18 +3,22 @@ const importTransform = getDevlandFile('quasar/dist/babel-transforms/imports.js'
 
 const regex = /import\s*\{([\w,\s]+)\}\s*from\s*['"]{1}quasar['"]{1}/g
 
-module.exports = content => content.replace(
-  regex,
-  (_, match) => match.split(',')
-    .map(identifier => {
-      const data = identifier.split(' as ')
+module.exports = function (content, map) {
+  const newContent = content.replace(
+    regex,
+    (_, match) => match.split(',')
+      .map(identifier => {
+        const data = identifier.split(' as ')
 
-      if (data[1] !== void 0) {
-        return `import ${data[1].trim()} from '${importTransform(data[0].trim())}';`
-      }
+        if (data[1] !== void 0) {
+          return `import ${data[1].trim()} from '${importTransform(data[0].trim())}';`
+        }
 
-      const name = data[0].trim()
-      return `import ${name} from '${importTransform(name)}';`
-    })
-    .join('')
-)
+        const name = data[0].trim()
+        return `import ${name} from '${importTransform(name)}';`
+      })
+      .join('')
+  )
+
+  return this.callback(null, newContent, map)
+}
