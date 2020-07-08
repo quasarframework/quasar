2e68d1429cbdd84d0b623402789e87208d08ee9d
Dan Popescu <pdan.popescu@gmail.com>
2020-07-08 11:22:50 +0300

    feat(QTabs): add outside arrow positioning and force arrows on mobile #7104, #4019

---
+++ docs/src/examples/QTabs/ArrowsModifiers.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 300px">
+      <q-tabs
+        v-model="tab"
+        inline-label
+        outside-arrows
+        mobile-arrows
+        class="bg-primary text-white shadow-2"
+      >
+        <q-tab name="mails" icon="mail" label="Mails" />
+        <q-tab name="alarms" icon="alarm" label="Alarms" />
+        <q-tab name="movies" icon="movie" label="Movies" />
+        <q-tab name="photos" icon="photo" label="Photos" />
+        <q-tab name="videos" icon="slow_motion_video" label="Videos" />
+        <q-tab name="addressbook" icon="people" label="Address Book" />
+      </q-tabs>
+
+      <q-tabs
+        v-model="tab"
+        inline-label
+        mobile-arrows
+        class="bg-purple text-white shadow-2"
+      >
+        <q-tab name="mails" icon="mail" label="Mails" />
+        <q-tab name="alarms" icon="alarm" label="Alarms" />
+        <q-tab name="movies" icon="movie" label="Movies" />
+        <q-tab name="photos" icon="photo" label="Photos" />
+        <q-tab name="videos" icon="slow_motion_video" label="Videos" />
+        <q-tab name="addressbook" icon="people" label="Address Book" />
+      </q-tabs>
+
+      <q-tabs
+        v-model="tab"
+        no-caps
+        outside-arrows
+        mobile-arrows
+        class="bg-orange text-white shadow-2"
+      >
+        <q-tab name="mails" label="Mails" />
+        <q-tab name="alarms" label="Alarms" />
+        <q-tab name="movies" label="Movies" />
+      </q-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 'mails'
+    }
+  }
+}
+</script>
