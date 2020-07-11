76ceeea5aaf0e09fc3886f99341d3bb1bd85a3f9
Dan Popescu <pdan.popescu@gmail.com>
2020-07-11 12:44:07 +0300

    fix(QBtn): stretch is for vertical stretch, so do not grow in horizontal direction #7406

--- ui/dev/src/pages/components/button-dropdown.vue
+++ ui/dev/src/pages/components/button-dropdown.vue
@@ -362,10 +362,9 @@
 
     <div class="row q-gutter-x-md q-my-md">
       <q-toggle v-model="fullWidth" label="Full width" />
-      <q-toggle v-model="stretch" label="Stretch" />
     </div>
-    <div class="column q-gutter-y-md" :class="fullWidth ? 'items-stretch' : 'items-start'">
-      <q-btn-dropdown :stretch="stretch" persistent v-model="toggle" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy label="Dropdown Button" style="margin: 15px">
+    <div class="column q-gutter-y-md items-start">
+      <q-btn-dropdown :class="fullWidth ? 'self-stretch' : ''" persistent v-model="toggle" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy label="Dropdown Button" style="margin: 15px">
         <q-list>
           <q-item clickable v-for="n in 2" :key="`1.${n}`" v-close-popup @click="showNotification">
             <q-item-section avatar>
@@ -402,7 +401,7 @@
         </q-list>
       </q-btn-dropdown>
 
-      <q-btn-dropdown :stretch="stretch" no-caps @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy style="margin: 15px">
+      <q-btn-dropdown :class="fullWidth ? 'self-stretch' : ''" no-caps @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy style="margin: 15px">
         <template v-slot:label>
           <div class="row items-center col-grow justify-center no-wrap">
             <q-icon left name="map" />
@@ -447,7 +446,7 @@
         </q-list>
       </q-btn-dropdown>
 
-      <q-btn-dropdown :stretch="stretch" split no-caps @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy style="margin: 15px">
+      <q-btn-dropdown :class="fullWidth ? 'self-stretch' : ''" split no-caps @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy style="margin: 15px">
         <template v-slot:label>
           <div class="col row items-center no-wrap">
             <q-icon left name="map" />
@@ -492,7 +491,171 @@
         </q-list>
       </q-btn-dropdown>
 
-      <q-btn-dropdown :stretch="stretch" dropdown-icon="change_history" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy label="Custom Dropdown Icon" style="margin: 15px">
+      <q-btn-dropdown :class="fullWidth ? 'self-stretch' : ''" dropdown-icon="change_history" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy label="Custom Dropdown Icon" style="margin: 15px">
+        <q-list>
+          <q-item clickable v-for="n in 2" :key="`1.${n}`" v-close-popup @click="showNotification">
+            <q-item-section avatar>
+              <q-avatar icon="folder" color="primary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Photos</q-item-label>
+              <q-item-label caption>
+                February 22, 2016
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" color="amber" />
+            </q-item-section>
+          </q-item>
+          <q-separator inset="item" spaced />
+          <q-item-label header inset>
+            Files
+          </q-item-label>
+          <q-item clickable v-close-popup @click="showNotification">
+            <q-item-section avatar>
+              <q-avatar icon="assignment" color="secondary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Vacation</q-item-label>
+              <q-item-label caption>
+                February 22, 2016
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" color="amber" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+
+      <q-btn-dropdown :class="fullWidth ? 'full-width' : ''" persistent v-model="toggle" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy label="Dropdown Button" style="margin: 15px">
+        <q-list>
+          <q-item clickable v-for="n in 2" :key="`1.${n}`" v-close-popup @click="showNotification">
+            <q-item-section avatar>
+              <q-avatar icon="folder" color="primary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Photos</q-item-label>
+              <q-item-label caption>
+                February 22, 2016
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" color="amber" />
+            </q-item-section>
+          </q-item>
+          <q-separator inset="item" spaced />
+          <q-item-label header inset>
+            Files
+          </q-item-label>
+          <q-item clickable v-close-popup @click="showNotification">
+            <q-item-section avatar>
+              <q-avatar icon="assignment" color="secondary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Vacation</q-item-label>
+              <q-item-label caption>
+                February 22, 2016
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" color="amber" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+
+      <q-btn-dropdown :class="fullWidth ? 'full-width' : ''" no-caps @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy style="margin: 15px">
+        <template v-slot:label>
+          <div class="row items-center col-grow justify-center no-wrap">
+            <q-icon left name="map" />
+            <div class="text-center">
+              Custom<br>Content
+            </div>
+          </div>
+        </template>
+        <q-list>
+          <q-item clickable v-for="n in 2" :key="`1.${n}`" v-close-popup @click="showNotification">
+            <q-item-section avatar>
+              <q-avatar icon="folder" color="primary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Photos</q-item-label>
+              <q-item-label caption>
+                February 22, 2016
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" color="amber" />
+            </q-item-section>
+          </q-item>
+          <q-separator inset="item" spaced />
+          <q-item-label header inset>
+            Files
+          </q-item-label>
+          <q-item clickable v-close-popup @click="showNotification">
+            <q-item-section avatar>
+              <q-avatar icon="assignment" color="secondary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Vacation</q-item-label>
+              <q-item-label caption>
+                February 22, 2016
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" color="amber" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+
+      <q-btn-dropdown :class="fullWidth ? 'full-width' : ''" split no-caps @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy style="margin: 15px">
+        <template v-slot:label>
+          <div class="col row items-center no-wrap">
+            <q-icon left name="map" />
+            <div class="text-center">
+              Custom<br>Content
+            </div>
+          </div>
+        </template>
+        <q-list>
+          <q-item clickable v-for="n in 2" :key="`1.${n}`" v-close-popup @click="showNotification">
+            <q-item-section avatar>
+              <q-avatar icon="folder" color="primary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Photos</q-item-label>
+              <q-item-label caption>
+                February 22, 2016
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" color="amber" />
+            </q-item-section>
+          </q-item>
+          <q-separator inset="item" spaced />
+          <q-item-label header inset>
+            Files
+          </q-item-label>
+          <q-item clickable v-close-popup @click="showNotification">
+            <q-item-section avatar>
+              <q-avatar icon="assignment" color="secondary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Vacation</q-item-label>
+              <q-item-label caption>
+                February 22, 2016
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" color="amber" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+
+      <q-btn-dropdown :class="fullWidth ? 'full-width' : ''" dropdown-icon="change_history" @show="log('open')" @hide="log('close')" @click="log('click')" color="primary" glossy label="Custom Dropdown Icon" style="margin: 15px">
         <q-list>
           <q-item clickable v-for="n in 2" :key="`1.${n}`" v-close-popup @click="showNotification">
             <q-item-section avatar>
@@ -539,7 +702,6 @@ export default {
       toggle: false,
 
       fullWidth: true,
-      stretch: false,
 
       conf: [
         { split: false, dense: false, disable: false },
