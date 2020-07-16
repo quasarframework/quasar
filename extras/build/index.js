388b7c436f3086480f8daca4aed495d8fdffcd10
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-07-16 07:19:58 -0600

    chore(extras): updated svg icons (count 1293)

--- extras/build/index.js
+++ extras/build/index.js
@@ -5,9 +5,6 @@ const { join } = require('path')
 runJob(join(__dirname, './webfonts.js'))
 runJob(join(__dirname, './animate.js'))
 
-// runJob(join(__dirname, './mdi-v5.js'))
-// runJob(join(__dirname, './fontawesome-v5.js'))
-
 runJob(join(__dirname, './mdi-v5.js'))
 runJob(join(__dirname, './fontawesome-v5.js'))
 runJob(join(__dirname, './ionicons-v5.js'))
