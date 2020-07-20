a965bfa3f226a8bdbc3dd339e943cc08a6fdd29b
Allan Gaunt <allangaunt@gmail.com>
2020-07-20 08:12:57 +0100

    fix(icongenie): Incorrect validation when generating profile #7441

--- icongenie/lib/utils/validate-profile-object.js
+++ icongenie/lib/utils/validate-profile-object.js
@@ -5,7 +5,7 @@ const generatorsList = Object.keys(require('../generators'))
 const modesList = [ 'all' ].concat(Object.keys(require('../modes')))
 const platformsList = [ 'cordova-ios', 'cordova-android' ]
 
-const paramsSchema = Joi.object({
+const baseParamsSchema = {
   include: Joi.array().min(1).items(
     Joi.string().valid(...modesList)
   ),
@@ -21,13 +21,22 @@ const paramsSchema = Joi.object({
     Joi.number().integer().min(0)
   ).min(1).max(2),
 
+  splashscreenIconRatio: Joi.number().integer().min(0).max(100)
+}
+
+const colorParamsSchemaProfileSave = {
+  themeColor: Joi.string().pattern(/^[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/),
+  pngColor: Joi.string().pattern(/^[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/),
+  splashscreenColor: Joi.string().pattern(/^[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/),
+  svgColor: Joi.string().pattern(/^[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/),
+}
+
+const colorParamsSchema = {
   themeColor: Joi.string().pattern(/^#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/),
   pngColor: Joi.string().pattern(/^#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/),
   splashscreenColor: Joi.string().pattern(/^#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/),
   svgColor: Joi.string().pattern(/^#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/),
-
-  splashscreenIconRatio: Joi.number().integer().min(0).max(100)
-})
+}
 
 const assetsSchema = Joi.array().items({
   generator: Joi.string().required().valid(...generatorsList),
@@ -58,13 +67,23 @@ const assetsSchema = Joi.array().items({
   tag: Joi.string()
 })
 
-const profileSchema = Joi.object({
-  params: paramsSchema,
-  assets: assetsSchema
-})
+module.exports = function validateProfileObject (profileObject, generatingProfileFile = false) {
+  // ? - When generating the profile file, we don't want to validate with # on the hex color.
+  // : - When generating the icon, we're expecting a hash on the color (automatically added to user input via the CLI)
+  const colorParams = generatingProfileFile
+    ? colorParamsSchemaProfileSave
+    : colorParamsSchema
 
-module.exports = function validateProfileObject (profileObject) {
+  const profileSchema = Joi.object({
+    params: {
+      ...baseParamsSchema,
+      ...colorParams
+    },
+    assets: assetsSchema
+  })
+
   const { error } = profileSchema.validate(profileObject)
+
   if (error) {
     console.error(` ${red('ERROR')}: Input parameters are not valid. Please correct them.`)
     console.error(` ${error}`)
