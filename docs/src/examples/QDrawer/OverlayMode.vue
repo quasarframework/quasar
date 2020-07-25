da22a0aa979bddb092f8af0650518c6573891111
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-07-25 15:08:31 -0600

    fix(docs): don't put QList in v-for loop
    Nothing visually changes with the site. This just prevents unnecessary overhead.

--- docs/src/examples/QDrawer/OverlayMode.vue
+++ docs/src/examples/QDrawer/OverlayMode.vue
@@ -17,18 +17,17 @@
         content-class="bg-grey-3"
       >
         <q-scroll-area class="fit">
-          <q-list v-for="(menuItem, index) in menuList" :key="index">
+          <q-list>
 
-            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+            <q-item v-for="(menuItem, index) in menuList" :key="index" clickable :active="menuItem.label === 'Outbox'" v-ripple>
               <q-item-section avatar>
                 <q-icon :name="menuItem.icon" />
               </q-item-section>
               <q-item-section>
                 {{ menuItem.label }}
               </q-item-section>
+             <q-separator v-if="menuItem.separator" />
             </q-item>
-
-           <q-separator v-if="menuItem.separator" />
 
           </q-list>
         </q-scroll-area>
