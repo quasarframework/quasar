2d823861eee9ad1dd67ee352ad02dd27f58cbde2
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-07-29 06:04:27 -0600

    chore(docs): Additional fixes to QDrawer examples

--- docs/src/examples/QDrawer/Menu.vue
+++ docs/src/examples/QDrawer/Menu.vue
@@ -20,17 +20,15 @@
           <q-list>
 
             <template v-for="(menuItem, index) in menuList">
-              <div :key="index">
-                <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
-                  <q-item-section avatar>
-                    <q-icon :name="menuItem.icon" />
-                  </q-item-section>
-                  <q-item-section>
-                    {{ menuItem.label }}
-                  </q-item-section>
-                </q-item>
-                <q-separator v-if="menuItem.separator" />
-              </div>
+              <q-item :key="index" clickable :active="menuItem.label === 'Outbox'" v-ripple>
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" />
+                </q-item-section>
+                <q-item-section>
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
             </template>
 
           </q-list>
