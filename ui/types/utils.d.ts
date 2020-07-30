eaeed124dac10283e333382e5a4f6d7756ba9031
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-07-30 20:52:20 +0300

    feat(ts/morph): temporary change of the definition

--- ui/types/utils.d.ts
+++ ui/types/utils.d.ts
@@ -26,29 +26,30 @@ export function throttle<F extends (...args: any[]) => any>(
 ): F;
 export function uid(): string;
 
-export function morph<F extends () => any, R extends (end: 'to' | 'from') => any, E extends Element | string | (() => Element)>(
-  elements: E | { from: E, to: E },
-  logic?: F,
-  options?: number | F | Partial<{
-    waitFor: number | 'transitionend' | Promise<any>,
+// interface MorphUtil {
+//   from: Element | string | (() => Element);
+//   to?: Element | string | (() => Element);
+//   onToggle?: () => any;
+//   waitFor?: number | 'transitionend' | Promise<any>;
 
-    duration: number,
-    easing: string,
-    delay: number,
-    fill: string,
+//   duration?: number;
+//   easing?: string;
+//   delay?: number;
+//   fill?: string;
 
-    style: string | Partial<CSSStyleDeclaration>,
-    class: string,
+//   style?: string | Partial<CSSStyleDeclaration>;
+//   classes?: string;
 
-    forceResize: boolean,
-    forceCssAnimation: boolean,
-    hideFromClone: boolean,
-    keepToClone: boolean,
+//   resize?: boolean;
+//   useCSS?: boolean;
+//   hideFromClone?: boolean;
+//   keepToClone?: boolean;
 
-    tween: boolean,
-    tweenFromOpacity: number,
-    tweenToOpacity: number
+//   tween?: boolean;
+//   tweenFromOpacity?: number;
+//   tweenToOpacity?: number;
 
-    onReady: R
-  }>
-): () => boolean
+//   onReady?: (end: 'to' | 'from') => any;
+// }
+
+// export function morph(): () => boolean
