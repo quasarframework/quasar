d90eb2b9e2e2b14c5205ae34faa6ddded7ef1fc6
FragsterAt <fragster@mail.ru>
2020-08-07 16:47:16 +0300

    playground for toggle  false-color property

--- ui/dev/src/pages/form/toggle.vue
+++ ui/dev/src/pages/form/toggle.vue
@@ -2,31 +2,61 @@
   <div>
     <div class="q-layout-padding" :class="dark ? 'bg-black text-white' : ''">
       <div class="label bg-secondary text-white">
-        Model <span class="right-detail"><em>{{ checked }}</em></span>
+        Model
+        <span class="right-detail">
+          <em>{{ checked }}</em>
+        </span>
       </div>
       <q-toggle v-model="dark" :dark="dark" :dense="dense" label="Dark" :false-value="null" />
       <q-toggle v-model="keepColor" :dark="dark" :dense="dense" label="Keep Color" />
       <q-toggle v-model="dense" :dark="dark" :dense="dense" label="Dense" />
 
-      <p class="caption">
-        Standalone
-      </p>
-      <q-toggle @change="onChange" @input="onInput" v-model="checked" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" color="orange" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" color="teal" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
+      <p class="caption">Standalone</p>
+      <q-toggle
+        @change="onChange"
+        @input="onInput"
+        v-model="checked"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        color="orange"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        color="teal"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
 
-      <p class="caption">
-        Sizes
-      </p>
+      <p class="caption">Sizes</p>
       <div>
         <q-toggle
           v-for="size in ['xs', 'sm', 'md', 'lg', 'xl', '150px']"
           :key="size"
           :size="size"
           :label="size"
-          unchecked-icon="visibility_off" checked-icon="visibility"
-          v-model="checked" :dark="dark" :dense="dense" :keep-color="keepColor"
+          unchecked-icon="visibility_off"
+          checked-icon="visibility"
+          v-model="checked"
+          :dark="dark"
+          :dense="dense"
+          :keep-color="keepColor"
         />
       </div>
       <div>
@@ -35,52 +65,236 @@
           :key="size"
           :size="size"
           :label="size"
-          :unchecked-icon="mdiEyeOff" :checked-icon="mdiEye"
-          v-model="checked" :dark="dark" :dense="dense" :keep-color="keepColor"
+          :unchecked-icon="mdiEyeOff"
+          :checked-icon="mdiEye"
+          v-model="checked"
+          :dark="dark"
+          :dense="dense"
+          :keep-color="keepColor"
         />
       </div>
 
-      <p class="caption">
-        Indeterminate
-      </p>
-      <q-toggle v-model="indModel" toggle-indeterminate :dark="dark" :dense="dense" :keep-color="keepColor" label="Three states" />
-      <q-toggle v-model="indModel" toggle-indeterminate :dark="dark" :dense="dense" keep-color label="Three states" />
-      <q-toggle v-model="indModel" toggle-indeterminate :dark="dark" :dense="dense" :keep-color="keepColor" color="orange" unchecked-icon="visibility_off" checked-icon="visibility" indeterminate-icon="help" label="Three states" />
-      <q-toggle v-model="indModel" toggle-indeterminate :dark="dark" :dense="dense" keep-color color="orange" unchecked-icon="visibility_off" checked-icon="visibility" indeterminate-icon="help" label="Three states" size="100px" />
+      <p class="caption">Indeterminate</p>
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+        label="Three states"
+      />
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        keep-color
+        label="Three states"
+      />
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+        color="orange"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        indeterminate-icon="help"
+        label="Three states"
+      />
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        keep-color
+        color="orange"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        indeterminate-icon="help"
+        label="Three states"
+        size="100px"
+      />
 
-      <p class="caption">
-        Indeterminate + icon-color
-      </p>
-      <q-toggle v-model="indModel" toggle-indeterminate :dark="dark" :dense="dense" :keep-color="keepColor" unchecked-icon="visibility_off" checked-icon="visibility" indeterminate-icon="help" label="Three states" icon-color="red" />
-      <q-toggle v-model="indModel" toggle-indeterminate :dark="dark" :dense="dense" :keep-color="keepColor" unchecked-icon="visibility_off" checked-icon="visibility" indeterminate-icon="help" label="Three states" icon-color="red" />
-      <q-toggle v-model="indModel" toggle-indeterminate :dark="dark" :dense="dense" keep-color color="orange" unchecked-icon="visibility_off" checked-icon="visibility" indeterminate-icon="help" label="Three states" size="100px" icon-color="black" />
+      <p class="caption">Indeterminate + icon-color</p>
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        indeterminate-icon="help"
+        label="Three states"
+        icon-color="red"
+      />
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        indeterminate-icon="help"
+        label="Three states"
+        icon-color="red"
+      />
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        keep-color
+        color="orange"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        indeterminate-icon="help"
+        label="Three states"
+        size="100px"
+        icon-color="black"
+      />
 
-      <p class="caption">
-        Label on the left side
-      </p>
-      <q-toggle v-model="checked" color="orange" left-label label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" color="teal" left-label label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" color="blue" left-label label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
+      <p class="caption">False color</p>
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        indeterminate-icon="help"
+        label="Three states"
+        icon-color="red"
+        false-color="teal"
+      />
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        indeterminate-icon="help"
+        label="Three states"
+        icon-color="red"
+        false-color="teal"
+      />
+      <q-toggle
+        v-model="indModel"
+        toggle-indeterminate
+        :dark="dark"
+        :dense="dense"
+        keep-color
+        color="orange"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        indeterminate-icon="help"
+        label="Three states"
+        size="100px"
+        icon-color="black"
+        false-color="teal"
+      />
 
-      <p class="caption">
-        Array Model
-      </p>
+      <p class="caption">Label on the left side</p>
+      <q-toggle
+        v-model="checked"
+        color="orange"
+        left-label
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        color="teal"
+        left-label
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        color="blue"
+        left-label
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+
+      <p class="caption">Array Model</p>
       <div class="label bg-secondary text-white">
-        Model <span class="right-detail"><em>{{ selection }}</em></span>
+        Model
+        <span class="right-detail">
+          <em>{{ selection }}</em>
+        </span>
       </div>
 
-      <q-toggle @change="onChange" v-model="selection" val="one" label="One" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <br>
-      <q-toggle @change="onChange" v-model="selection" val="two" label="Two" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <br>
-      <q-toggle @change="onChange" v-model="selection" val="three" label="Three" :dark="dark" :dense="dense" :keep-color="keepColor" />
+      <q-toggle
+        @change="onChange"
+        v-model="selection"
+        val="one"
+        label="One"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <br />
+      <q-toggle
+        @change="onChange"
+        v-model="selection"
+        val="two"
+        label="Two"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <br />
+      <q-toggle
+        @change="onChange"
+        v-model="selection"
+        val="three"
+        label="Three"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
 
-      <p class="caption">
-        With Icon
-      </p>
-      <q-toggle v-model="checked" icon="alarm" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" icon="mail" color="secondary" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" unchecked-icon="visibility_off" checked-icon="visibility" color="red" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
+      <p class="caption">With Icon</p>
+      <q-toggle
+        v-model="checked"
+        icon="alarm"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        icon="mail"
+        color="secondary"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        unchecked-icon="visibility_off"
+        checked-icon="visibility"
+        color="red"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
 
       <div>
         <q-toggle icon="fas fa-street-view" v-model="checked" />
@@ -92,23 +306,44 @@
         <q-toggle icon="las la-phone-volume" v-model="checked" />
       </div>
 
-      <p class="caption">
-        Disabled State
-      </p>
-      <q-toggle v-model="checked" disable color="primary" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" disable color="accent" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
-      <q-toggle v-model="checked" disable color="teal" label="Toggle Label" :dark="dark" :dense="dense" :keep-color="keepColor" />
+      <p class="caption">Disabled State</p>
+      <q-toggle
+        v-model="checked"
+        disable
+        color="primary"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        disable
+        color="accent"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
+      <q-toggle
+        v-model="checked"
+        disable
+        color="teal"
+        label="Toggle Label"
+        :dark="dark"
+        :dense="dense"
+        :keep-color="keepColor"
+      />
 
-      <p class="caption">
-        Option Group
-      </p>
+      <p class="caption">Option Group</p>
       <q-option-group
         inline
         type="toggle"
         v-model="group"
         @change="onChange"
         @input="onInput"
-        :dark="dark" :dense="dense"
+        :dark="dark"
+        :dense="dense"
         :keep-color="keepColor"
         :options="[
           { label: 'Option 2 Option 2 Option 2 Option 2 Option 2 Option 2 Option 2 ', value: 'op2' },
@@ -117,14 +352,13 @@
         ]"
       />
 
-      <p class="caption">
-        Another Option Group
-      </p>
+      <p class="caption">Another Option Group</p>
       <q-option-group
         type="toggle"
         v-model="group"
         size="lg"
-        :dark="dark" dense
+        :dark="dark"
+        dense
         :keep-color="keepColor"
         :options="[
           { label: 'Option 2 Option 2 Option 2 Option 2 Option 2 Option 2 Option 2 ', value: 'op2', dark, keepColor },
@@ -133,26 +367,48 @@
         ]"
       />
 
-      <p class="caption">
-        Inside a Label
-      </p>
+      <p class="caption">Inside a Label</p>
       <div class="column q-gutter-y-sm">
         <label>
-          <q-toggle v-model="checked" color="orange" label="Toggle - own label" :dark="dark" :dense="dense" :keep-color="keepColor" />
+          <q-toggle
+            v-model="checked"
+            color="orange"
+            label="Toggle - own label"
+            :dark="dark"
+            :dense="dense"
+            :keep-color="keepColor"
+          />
         </label>
 
         <label tabindex="0">
-          <q-toggle v-model="checked" color="orange" label="Toggle - own label (tabindex)" :dark="dark" :dense="dense" :keep-color="keepColor" />
+          <q-toggle
+            v-model="checked"
+            color="orange"
+            label="Toggle - own label (tabindex)"
+            :dark="dark"
+            :dense="dense"
+            :keep-color="keepColor"
+          />
         </label>
 
         <label>
-          <q-toggle v-model="checked" color="orange" :dark="dark" :dense="dense" :keep-color="keepColor" />
-          Toggle - external label
+          <q-toggle
+            v-model="checked"
+            color="orange"
+            :dark="dark"
+            :dense="dense"
+            :keep-color="keepColor"
+          />Toggle - external label
         </label>
 
         <label tabindex="0">
-          <q-toggle v-model="checked" color="orange" :dark="dark" :dense="dense" :keep-color="keepColor" />
-          Toggle - external label (tabindex)
+          <q-toggle
+            v-model="checked"
+            color="orange"
+            :dark="dark"
+            :dense="dense"
+            :keep-color="keepColor"
+          />Toggle - external label (tabindex)
         </label>
 
         <q-field v-model="checked" label="Toggle field" stack-label :dark="dark" :dense="dense">
@@ -169,9 +425,7 @@
         </q-field>
       </div>
 
-      <p class="caption">
-        Inside a List
-      </p>
+      <p class="caption">Inside a List</p>
       <q-list :dark="dark" :dense="dense">
         <q-item tag="label">
           <q-item-section>
@@ -184,23 +438,33 @@
         <q-item tag="label" multiline>
           <q-item-section>
             <q-item-label>Events and reminders</q-item-label>
-            <q-item-label caption>
-              Lorem ipsum
-            </q-item-label>
+            <q-item-label caption>Lorem ipsum</q-item-label>
           </q-item-section>
           <q-item-section avatar>
-            <q-toggle v-model="checked" class="purple" :dark="dark" :dense="dense" :keep-color="keepColor" />
+            <q-toggle
+              v-model="checked"
+              class="purple"
+              :dark="dark"
+              :dense="dense"
+              :keep-color="keepColor"
+            />
           </q-item-section>
         </q-item>
         <q-item tag="label" multiline>
           <q-item-section>
             <q-item-label>Events and reminders</q-item-label>
-            <q-item-label caption>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </q-item-label>
+            <q-item-label
+              caption
+            >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</q-item-label>
           </q-item-section>
           <q-item-section avatar>
-            <q-toggle v-model="checked" class="red" :dark="dark" :dense="dense" :keep-color="keepColor" />
+            <q-toggle
+              v-model="checked"
+              class="red"
+              :dark="dark"
+              :dense="dense"
+              :keep-color="keepColor"
+            />
           </q-item-section>
         </q-item>
       </q-list>
