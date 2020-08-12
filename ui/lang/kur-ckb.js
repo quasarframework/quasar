c83b5af260277b2c73523626014ceffab4366008
r4wand <26229485+r4wand@users.noreply.github.com>
2020-08-12 19:21:07 +0300

    kur-ckb.js lang bug fix (#7614)
    * first day of the week fix
    
    i made a silly mistake of counting the days from right to left but .split() function separates it from left to right. based on that first day of the week should be index 6/Saturday(شەممە) but I previously set it as index 0 which would make it Friday(هەینی). this is a fix for my mistake.
    
    * set rtl to true

--- ui/lang/kur-ckb.js
+++ ui/lang/kur-ckb.js
@@ -1,6 +1,7 @@
 export default {
   isoName: "kur-ckb",
   nativeName: "کوردی سۆرانی",
+  rtl: true,
   label: {
     clear: "پاککردنەوە",
     ok: "باشە",
@@ -29,7 +30,7 @@ export default {
     monthsShort: "مانگی یەک_مانگی دوو_مانگی سێ_مانگی چوار_مانگی پێنج_مانگی شەش_مانگی حەوت_مانگی هەشت_مانگی نۆ_مانگی دە_مانگی یانزە_مانگی دووانزە".split(
       "_"
     ),
-    firstDayOfWeek: 0, // 0-6, 0 - Sunday, 1 Monday, ...
+    firstDayOfWeek: 6, // 0-6, 0 - Sunday, 1 Monday, ...
     format24h: false,
     singleDay: "ڕۆژ",
     pluralDay: "ڕۆژەکان"
