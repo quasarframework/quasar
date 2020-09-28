7ec3f2645503a7d33207bbe48022f29d878adc39
Paolo Caleffi <p.caleffi@dreamonkey.com>
2020-09-28 15:37:40 +0200

    Revert "fix: QuasarIconSet type import (fix #7640) (#7648)" (#7831)
    This reverts commit eda69ba39384409a41acc89fa3b9781707d80e12.

--- ui/types/shim-icon-set.d.ts
+++ ui/types/shim-icon-set.d.ts
@@ -1,7 +1,7 @@
 declare module "quasar/icon-set/*" {
   // We know "quasar" will exists at runtime, we can safely ignore the TS error
   // @ts-ignore
-  import { QuasarIconSet } from "./extras/icon-set";
+  import { QuasarIconSet } from "quasar";
   const iconSet: QuasarIconSet;
   export default iconSet;
 }
