89be447a3a47b18487032cbf85f0059dd3046a94
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-10-31 09:44:41 -0600

    feat(extras): automatically generate ts types for animate

--- extras/build/animate.js
+++ extras/build/animate.js
@@ -64,4 +64,6 @@ else {
 
   writeFileSync(join(dist, 'animate-list.js'), getList(`export const `), 'utf-8')
   writeFileSync(join(dist, 'animate-list.common.js'), getList(`module.exports.`), 'utf-8')
+  writeFileSync(join(dist, 'animate-list.d.ts'), getList(`export type `), 'utf-8')
+  writeFileSync(join(dist, 'animate-list.common.d.ts'), getList(`export type `), 'utf-8')
 }
