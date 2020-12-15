cdb03bdf7dba8f92f830a9a147df6af4a0c3c821
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-12-15 20:58:01 +0200

    fix(QTable): duplicate headerClasses added #8205

--- ui/src/components/table/table-header.js
+++ ui/src/components/table/table-header.js
@@ -51,9 +51,7 @@ export default {
           ? slot(props)
           : h(QTh, {
             key: col.name,
-            props,
-            style: col.headerStyle,
-            class: col.headerClasses
+            props
           }, () => col.label)
       })
 
