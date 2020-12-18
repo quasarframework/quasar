44cbc00e8c02b23153edc4a67ee436984936e5a4
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-12-18 12:11:47 -0700

    fix: "Extension server error: Object not found: <top>" (#8188)

--- app/templates/electron/main-process/electron-main.dev.js
+++ app/templates/electron/main-process/electron-main.dev.js
@@ -7,20 +7,35 @@
 
 import electronDebug from 'electron-debug'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-import { app } from 'electron'
+import { app, BrowserWindow } from 'electron'
 
-// Install `electron-debug` with `devtron`
-electronDebug({ showDevTools: true })
-
-// Install vuejs devtools
 app.whenReady().then(() => {
-  installExtension(VUEJS_DEVTOOLS)
-    .then(name => {
-      console.log(`Added Extension: ${name}`)
-    })
-    .catch(err => {
-      console.log('An error occurred: ', err)
-    })
+  // allow for a small delay for mainWindow to be created
+  setTimeout(() => {
+    // Install `electron-debug` with `devtron`
+    electronDebug({ showDevTools: false })
+
+    // Install vuejs devtools
+    installExtension(VUEJS_DEVTOOLS)
+      .then(name => {
+        console.log(`Added Extension: ${name}`)
+        // get main window
+        const win = BrowserWindow.getFocusedWindow()
+        if (win) {
+          win.webContents.on('did-frame-finish-load', () => {
+            win.webContents.once('devtools-opened', () => {
+              win.webContents.focus()
+            })
+            // open electron debug
+            console.log('Opening dev tools')
+            win.webContents.openDevTools()
+          })
+        }
+      })
+      .catch(err => {
+        console.log('An error occurred: ', err)
+      })
+  }, 250)
 })
 
 import './electron-main'
