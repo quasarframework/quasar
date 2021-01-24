7fbdbeda71c491869ab2c658ff5e56b29a1834f5
Popescu Dan <pdan.popescu@gmail.com>
2021-01-24 04:34:05 +0200

    feat(QSlideItem): add @slide event to allow customizing slots while sliding #7282 (#8369)
    * feat(QSlideItem): add @slide event to allow customizing slots while sliding #7282
    
    #7282
    
    * Update QSlideItem.js
    
    * Update QSlideItem.json
    
    * Update QSlideItem.json
    
    Co-authored-by: Razvan Stoenescu <razvan.stoenescu@gmail.com>

--- ui/src/components/slide-item/QSlideItem.js
+++ ui/src/components/slide-item/QSlideItem.js
@@ -42,6 +42,11 @@ export default Vue.extend({
   methods: {
     reset () {
       this.$refs.content.style.transform = `translate(0,0)`
+      this.__emitSlide(this.__showing, 0, true)
+    },
+
+    __emitSlide (side, ratio, isReset) {
+      this.qListeners.slide !== void 0 && this.$emit('slide', { side, ratio, isReset })
     },
 
     __pan (evt) {
@@ -79,6 +84,7 @@ export default Vue.extend({
         }
         else {
           node.style.transform = `translate(0,0)`
+          this.__emitSlide(this.__showing, 0, true)
         }
 
         return
@@ -132,6 +138,8 @@ export default Vue.extend({
 
       node.style.transform = `translate${this.__axis}(${dist * dir / Math.abs(dir)}px)`
       this.$refs[`${showing}Content`].style.transform = `scale(${this.__scale})`
+
+      this.__emitSlide(showing, this.__scale, false)
     }
   },
 
