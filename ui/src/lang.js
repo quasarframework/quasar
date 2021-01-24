b43990cd517925ccd794dcd6a31e596753a06b39
Dan Popescu <pdan.popescu@gmail.com>
2021-01-24 10:37:24 +0200

    feat(lang): improve getLocale to return same case strings on browsers

--- ui/src/lang.js
+++ ui/src/lang.js
@@ -3,17 +3,32 @@ import { reactive } from 'vue'
 import langEn from '../lang/en-US.js'
 import { isRuntimeSsrPreHydration } from './plugins/Platform.js'
 
+const localeRe = /^\s*([^-]+)(?:-(.+))\s*$/
+
+function normalizeLocale (_, p1, p2) {
+  const locale = p1.toLowerCase()
+
+  if (typeof p2 === 'string' && p2.length > 0) {
+    return locale + '-' + (
+      p2.length < 4
+        ? p2.toUpperCase()
+        : (p2[0].toUpperCase() + p2.slice(1).toLowerCase())
+    )
+  }
+
+  return locale
+}
+
 function getLocale () {
   if (__QUASAR_SSR_SERVER__) { return }
 
-  const val
-    = navigator.language
-    || navigator.languages[ 0 ]
-    || navigator.browserLanguage
-    || navigator.userLanguage
-    || navigator.systemLanguage
+  const val = Array.isArray(navigator.languages) === true && navigator.languages.length > 0
+    ? navigator.languages[ 0 ]
+    : navigator.language
 
-  return val.toLowerCase()
+  return typeof val === 'string'
+    ? val.replace(localeRe, normalizeLocale)
+    : void 0
 }
 
 const Plugin = {
