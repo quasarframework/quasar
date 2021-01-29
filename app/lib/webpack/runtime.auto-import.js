4d875ed644542eaca0726b18d9029bf9552381f2
Kerry <outofmemoryagain@gmail.com>
2021-01-29 13:03:51 -0600

    fix(ts): Fix auto import when using vue-class-component. (#8390)
    Co-authored-by: Kerry Huguet <kerry@seatzapp.com>

--- app/lib/webpack/runtime.auto-import.js
+++ app/lib/webpack/runtime.auto-import.js
@@ -10,10 +10,11 @@
  * @param {items}     Object containing components or directives
  */
 module.exports = function qInstall (component, type, items) {
-  const target = component[type]
+  const targetComponent = component.__vccOpts ? component.__vccOpts : component
+  const target = targetComponent[type]
 
   if (target === void 0) {
-    component[type] = items
+    targetComponent[type] = items
   }
   else {
     for (var i in items) {
