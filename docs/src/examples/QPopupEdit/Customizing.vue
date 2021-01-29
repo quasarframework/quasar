6c3f1e32e4acea93dd227c1615e7a4b777737e5f
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-01-29 16:19:30 +0200

    feat(QPopupEdit): update docs and upgrade guide

--- docs/src/examples/QPopupEdit/Customizing.vue
+++ docs/src/examples/QPopupEdit/Customizing.vue
@@ -3,8 +3,8 @@
     <div class="q-gutter-md">
       <div class="cursor-pointer" style="width: 100px">
         {{ label }}
-        <q-popup-edit v-model="label" class="bg-accent text-white">
-          <q-input dark color="white" v-model="label" dense autofocus counter>
+        <q-popup-edit v-model="label" class="bg-accent text-white" v-slot="scope">
+          <q-input dark color="white" v-model="scope.value" dense autofocus counter @keyup.enter="scope.set">
             <template v-slot:append>
               <q-icon name="edit" />
             </template>
@@ -14,8 +14,8 @@
 
       <div class="cursor-pointer" style="width: 100px">
         {{ label2 }}
-        <q-popup-edit v-model="label2" :cover="false" :offset="[0, 10]">
-          <q-input color="accent" v-model="label2" dense autofocus counter>
+        <q-popup-edit v-model="label2" :cover="false" :offset="[0, 10]" v-slot="scope">
+          <q-input color="accent" v-model="scope.value" dense autofocus counter @keyup.enter="scope.set">
             <template v-slot:prepend>
               <q-icon name="record_voice_over" color="accent" />
             </template>
