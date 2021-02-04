14b7ad60fd4afbf69f436d0e92bf9caec7c473e1
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-02-04 16:59:29 +0200

    fix(app): linting on store/index.ts #8412

--- app/templates/store/ts/index.ts
+++ app/templates/store/ts/index.ts
@@ -1,5 +1,5 @@
-import { store } from 'quasar/wrappers';
-import { createStore } from 'vuex';
+import { store } from 'quasar/wrappers'
+import { createStore } from 'vuex'
 
 // import example from './module-example'
 // import { ExampleStateInterface } from './module-example/state';
@@ -28,8 +28,8 @@ export default store(function (/* { ssrContext } */) {
 
     // enable strict mode (adds overhead!)
     // for dev mode and --debug builds only
-    strict: !!process.env.DEBUGGING,
-  });
+    strict: !!process.env.DEBUGGING
+  })
 
-  return Store;
-});
+  return Store
+})
