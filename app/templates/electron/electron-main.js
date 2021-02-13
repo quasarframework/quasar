d7cb45054e5382d0be9697383ec7ef35b08b3b02
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-02-13 23:47:34 +0200

    feat(app): Improve electron template on devtools usage

--- app/templates/electron/electron-main.js
+++ app/templates/electron/electron-main.js
@@ -26,6 +26,16 @@ function createWindow () {
 
   mainWindow.loadURL(process.env.APP_URL)
 
+  if (process.env.DEBUGGING) {
+    // if on DEV or Production with debug enabled
+    mainWindow.webContents.openDevTools()
+  } else {
+    // we're on production; no access to devtools pls
+    mainWindow.webContents.on('devtools-opened', () => {
+      mainWindow.webContents.closeDevTools()
+    })
+  }
+
   mainWindow.on('closed', () => {
     mainWindow = null
   })
