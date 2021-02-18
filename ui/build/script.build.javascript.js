58700f6e45b822c4a0b1547480e7e899ebc3abf3
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-02-18 19:05:16 +0200

    fix(ui/build): Minify options #8497

--- ui/build/script.build.javascript.js
+++ ui/build/script.build.javascript.js
@@ -31,6 +31,42 @@ const rollupPluginsModern = [
   nodeResolve()
 ]
 
+const uglifyJsOptions = {
+  compress: {
+    // turn off flags with small gains to speed up minification
+    arrows: false,
+    collapse_vars: false,
+    comparisons: false,
+    computed_props: false,
+    hoist_funs: false,
+    hoist_props: false,
+    hoist_vars: false,
+    inline: false,
+    loops: false,
+    negate_iife: false,
+    properties: false,
+    reduce_funcs: false,
+    reduce_vars: false,
+    switches: false,
+    toplevel: false,
+    typeofs: false,
+
+    // a few flags with noticable gains/speed ratio
+    booleans: true,
+    if_return: true,
+    sequences: true,
+    unused: true,
+
+    // required features to drop conditional branches
+    conditionals: true,
+    dead_code: true,
+    evaluate: true
+  },
+  mangle: {
+    safari10: true
+  }
+}
+
 const builds = [
   { // Generic prod entry (client-side only; NOT used by Quasar CLI)
     rollup: {
@@ -239,11 +275,7 @@ function buildEntry (config) {
         return code
       }
 
-      const minified = uglify.minify(code, {
-        compress: {
-          ecma: 6
-        }
-      })
+      const minified = uglify.minify(code, uglifyJsOptions)
 
       if (minified.error) {
         return Promise.reject(minified.error)
