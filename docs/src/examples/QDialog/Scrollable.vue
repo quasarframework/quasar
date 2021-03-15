22cbe9b7cbd6681a87c2f9fbc1b0e9be3aa5a927
Dan Popescu <pdan.popescu@gmail.com>
2021-03-15 19:08:27 +0200

    docs(QDialog): add example of full page scroll dialog #8380
    #8380

--- docs/src/examples/QDialog/Scrollable.vue
+++ docs/src/examples/QDialog/Scrollable.vue
@@ -2,6 +2,7 @@
   <div class="q-pa-md q-gutter-sm">
     <q-btn label="Basic scroll" color="primary" @click="basic = true" />
     <q-btn label="Fixed size" color="primary" @click="fixed = true" />
+    <q-btn label="Page scroll" color="primary" @click="extended = true" />
 
     <q-dialog v-model="basic" transition-show="rotate" transition-hide="rotate">
       <q-card>
@@ -40,6 +41,31 @@
         </q-card-actions>
       </q-card>
     </q-dialog>
+
+    <q-dialog
+      v-model="extended"
+      maximized
+      no-focus
+      transition-show="jump-down"
+      transition-hide="jump-up"
+    >
+      <div class="bg-transparent shadow-0 q-pa-lg" @click.self="extended = false">
+        <q-card class="q-mx-auto" style="width: 600px; max-width: 80vw">
+          <q-card-section>
+            <div class="text-h6">Terms of Agreement</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <p v-for="n in 30" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
+          </q-card-section>
+
+          <q-card-actions align="right">
+            <q-btn flat label="Decline" color="primary" v-close-popup />
+            <q-btn flat label="Accept" color="primary" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </div>
+    </q-dialog>
   </div>
 </template>
 
@@ -48,7 +74,8 @@ export default {
   data () {
     return {
       basic: false,
-      fixed: false
+      fixed: false,
+      extended: false
     }
   }
 }
