f58955567de0df61d087d1ebe7939c2ddf19fed7
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-03-20 15:33:16 +0200

    feat(LocalStorage/SessionStorage): prepare the plugin as soon as it is imported #8595

--- ui/src/plugins/LocalStorage.js
+++ ui/src/plugins/LocalStorage.js
@@ -1,19 +1,16 @@
 import { client } from './Platform.js'
 import { getEmptyStorage, getStorage } from '../utils/private/web-storage.js'
 
-export default {
+const storage = __QUASAR_SSR_SERVER__ || client.has.webStorage === false
+  ? getEmptyStorage()
+  : getStorage('local')
+
+const Plugin = {
   install ({ $q }) {
-    if (this.__installed === true) {
-      const { install, __installed, ...storage } = this
-      $q.localStorage = storage
-      return
-    }
-
-    const storage = __QUASAR_SSR_SERVER__ || client.has.webStorage === false
-      ? getEmptyStorage()
-      : getStorage('local')
-
     $q.localStorage = storage
-    Object.assign(this, storage)
   }
 }
+
+Object.assign(Plugin, storage)
+
+export default Plugin
--- ui/src/plugins/SessionStorage.js
+++ ui/src/plugins/SessionStorage.js
@@ -1,19 +1,16 @@
 import { client } from './Platform.js'
 import { getEmptyStorage, getStorage } from '../utils/private/web-storage.js'
 
-export default {
+const storage = __QUASAR_SSR_SERVER__ || client.has.webStorage === false
+  ? getEmptyStorage()
+  : getStorage('session')
+
+const Plugin = {
   install ({ $q }) {
-    if (this.__installed === true) {
-      const { install, __installed, ...storage } = this
-      $q.sessionStorage = storage
-      return
-    }
-
-    const storage = __QUASAR_SSR_SERVER__ || client.has.webStorage === false
-      ? getEmptyStorage()
-      : getStorage('session')
-
     $q.sessionStorage = storage
-    Object.assign(this, storage)
   }
 }
+
+Object.assign(Plugin, storage)
+
+export default Plugin
