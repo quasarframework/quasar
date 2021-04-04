82f48c2568ddd235fd71ff461c485ee5ec19840e
Tobias Mesquita <tobias.mesquita@gmail.com>
2021-04-04 14:49:58 -0300

    ts defs - ssr middlewares

--- app/types/ssrmiddleware.d.ts
+++ app/types/ssrmiddleware.d.ts
@@ -1,4 +1,4 @@
-import { Express, Request, Response } from "express";
+import { Express, Request, Response, ServeStaticOptions, RequestHandler } from "express";
 
 interface RenderParams {
   req: Request;
@@ -16,22 +16,56 @@ interface RenderErrorParams extends RenderParams {
   err: RenderError;
 }
 
-interface SsrMiddlewareRender {
-  vue: (ssrContext: RenderVueParams) => Promise<string>;
+interface SsrMiddlewareServe {
+  /**
+   * It's essentially a wrapper over express.static() with a few convenient tweaks:
+   * - the pathFromPublicFolder is a path resolved to the "public" folder out of the box
+   * - the opts are the same as for express.static()
+   * - opts.maxAge is used by default, taking into account the quasar.conf.js > ssr > maxAge configuration; this sets how long the respective file(s) can live in browser's cache
+   */
+  static: (path: string, options: ServeStaticOptions<Response>) => RequestHandler<Response>;
+  /**
+   * Displays a wealth of useful debug information (including the stack trace).
+   * Warning: It's available only in development and NOT in production.
+   */
   error: (ssrContext: RenderErrorParams) => void;
 }
+
+type SsrMiddlewareRender = (ssrContext: RenderVueParams) => Promise<string>;
 
 interface SsrMiddlewareFolders {
   root: string;
   public: string;
 }
 
+interface SsrMiddlewareResolve {
+  /**
+   * Whenever you define a route (with app.use(), app.get(), app.post() etc), you should use the resolve.urlPath() method so that you'll also keep into account the configured publicPath (quasar.conf.js > build > publicPath).
+   */
+  urlPath: (url: string) => string;
+  /**
+   * Resolve folder path to the root (of the project in dev and of the distributables in production). Under the covers, it does a path.join()
+   * @param paths paths to join
+   */
+  root: (...paths: string[]) => string;
+  /**
+   * Resolve folder path to the "public" folder. Under the covers, it does a path.join()
+   * @param paths paths to join
+   */
+  public: (...paths: string[]) => string;
+}
+
 interface SsrMiddlewareParams {
   app: Express;
-  resolveUrlPath: (url: string) => string;
+  resolve: SsrMiddlewareResolve;
   publicPath: string;
   folders: SsrMiddlewareFolders;
+  /**
+   * Uses Vue and Vue Router to render the requested URL path. 
+   * @returns the rendered HTML string to return to the client
+   */
   render: SsrMiddlewareRender;
+  serve: SsrMiddlewareServe;
 }
 
 export type SsrMiddlewareCallback = (
