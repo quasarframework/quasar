0672f71f94dfeb9f2d33d69695e384e6689a5006
Paolo Caleffi <p.caleffi@dreamonkey.com>
2021-04-19 17:05:12 +0200

    feat(app/ts): updates types for webpack5 support (#8957)
    * feat(app): add type wrappers into all template files
    
    * feat(app/ts): updates types for webpack5 support
    
    * Update compression.js
    
    * Update render.js
    
    Co-authored-by: Razvan Stoenescu <razvan.stoenescu@gmail.com>

--- app/types/ssrmiddleware.d.ts
+++ app/types/ssrmiddleware.d.ts
@@ -1,4 +1,5 @@
-import { Express, Request, Response, ServeStaticOptions, RequestHandler } from "express";
+import { Express, Request, RequestHandler, Response } from "express";
+import { ServeStaticOptions } from "serve-static";
 
 interface RenderParams {
   req: Request;
@@ -23,12 +24,15 @@ interface SsrMiddlewareServe {
    * - the opts are the same as for express.static()
    * - opts.maxAge is used by default, taking into account the quasar.conf.js > ssr > maxAge configuration; this sets how long the respective file(s) can live in browser's cache
    */
-  static: (path: string, options?: ServeStaticOptions<Response>) => RequestHandler<Response>;
+  static(
+    path: string,
+    options?: ServeStaticOptions<Response>
+  ): RequestHandler<Response>;
   /**
    * Displays a wealth of useful debug information (including the stack trace).
    * Warning: It's available only in development and NOT in production.
    */
-  error: (ssrContext: RenderErrorParams) => void;
+  error(ssrContext: RenderErrorParams): void;
 }
 
 type SsrMiddlewareRender = (ssrContext: RenderVueParams) => Promise<string>;
@@ -42,17 +46,17 @@ interface SsrMiddlewareResolve {
   /**
    * Whenever you define a route (with app.use(), app.get(), app.post() etc), you should use the resolve.urlPath() method so that you'll also keep into account the configured publicPath (quasar.conf.js > build > publicPath).
    */
-  urlPath: (url: string) => string;
+  urlPath(url: string): string;
   /**
    * Resolve folder path to the root (of the project in dev and of the distributables in production). Under the covers, it does a path.join()
    * @param paths paths to join
    */
-  root: (...paths: string[]) => string;
+  root(...paths: string[]): string;
   /**
    * Resolve folder path to the "public" folder. Under the covers, it does a path.join()
    * @param paths paths to join
    */
-  public: (...paths: string[]) => string;
+  public(...paths: string[]): string;
 }
 
 interface SsrMiddlewareParams {
@@ -61,7 +65,7 @@ interface SsrMiddlewareParams {
   publicPath: string;
   folders: SsrMiddlewareFolders;
   /**
-   * Uses Vue and Vue Router to render the requested URL path. 
+   * Uses Vue and Vue Router to render the requested URL path.
    * @returns the rendered HTML string to return to the client
    */
   render: SsrMiddlewareRender;
