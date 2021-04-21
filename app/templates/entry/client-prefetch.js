3317899440419b313807b62113fbd09c3037a368
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-04-21 14:06:14 +0300

    fix(app): preFetch's currentRoute is a ref instead of the actual value #8974

--- app/templates/entry/client-prefetch.js
+++ app/templates/entry/client-prefetch.js
@@ -104,8 +104,8 @@ export function addPreFetchHooks (router<%= store ? ', store' : '' %>, publicPat
     preFetchList.reduce(
       (promise, preFetch) => promise.then(() => hasRedirected === false && preFetch({
         <% if (store) { %>store,<% } %>
-        currentRoute: to.value,
-        previousRoute: from.value,
+        currentRoute: to,
+        previousRoute: from,
         redirect,
         urlPath,
         publicPath
