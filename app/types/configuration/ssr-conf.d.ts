d480630ebfad21015a422238ade8e1dfc15b6be0
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-04-23 20:38:48 +0300

    feat(app/TS): update the quasar.conf.js > ssr definition

--- app/types/configuration/ssr-conf.d.ts
+++ app/types/configuration/ssr-conf.d.ts
@@ -1,3 +1,5 @@
+import { Configuration as WebpackConfiguration } from "webpack";
+import * as WebpackChain from "webpack-chain";
 export interface QuasarSsrConfiguration {
   /**
    * If a PWA should take over or just a SPA.
@@ -9,7 +11,38 @@ export interface QuasarSsrConfiguration {
   pwa?: boolean | object;
 
   /**
+   * Manually call store.replaceState(window.__INITIAL_STATE__)
+   * instead of letting Quasar CLI do it
+   */
+  manualStoreHydration?: boolean;
+
+  /**
+   * Manually call $q.onSSRHydrated()
+   * instead of letting Quasar CLI do it
+   */
+  manualPostHydrationTrigger?: boolean;
+
+  /**
+   * Tell browser when a file from the server should expire from cache
+   * (the default value, in ms)
+   * Has effect only when server.static() is used
+   */
+  maxAge?: number;
+
+  /**
    * Add/remove/change properties of production generated package.json
    */
   extendPackageJson?: (pkg: { [index in string]: any }) => void;
+
+  /**
+   * Webpack config object for the Webserver
+   * which includes the SSR middleware
+   */
+  extendWebpackWebserver?: (config: WebpackConfiguration) => void;
+
+  /**
+   * Equivalent to `extendWebpackWebserver()` but uses `webpack-chain` instead.
+   * Handles the Webserver webpack config ONLY which includes the SSR middleware
+   */
+  chainWebpackWebserver?: (chain: WebpackChain) => void;
 }
