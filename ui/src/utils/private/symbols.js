b95196e4d443ab8f9591acb6c93b56785635abbf
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-04-26 19:51:56 +0300

    feat(ui): temporarily use string keys for Quasar's provide/inject

--- ui/src/utils/private/symbols.js
+++ ui/src/utils/private/symbols.js
@@ -1,38 +1,9 @@
-const hasSymbol = typeof Symbol === 'function'
-  && typeof Symbol.toStringTag === 'symbol'
-
-export const quasarKey = hasSymbol === true
-  ? Symbol('_q_')
-  : '_q_'
-
-export const timelineKey = hasSymbol === true
-  ? Symbol('_q_t_')
-  : '_q_t_'
-
-export const stepperKey = hasSymbol === true
-  ? Symbol('_q_s_')
-  : '_q_s_'
-
-export const layoutKey = hasSymbol === true
-  ? Symbol('_q_l_')
-  : '_q_l_'
-
-export const pageContainerKey = hasSymbol === true
-  ? Symbol('_q_pc_')
-  : '_q_pc_'
-
-export const fabKey = hasSymbol === true
-  ? Symbol('_q_f_')
-  : '_q_f_'
-
-export const formKey = hasSymbol === true
-  ? Symbol('_q_fo_')
-  : '_q_fo_'
-
-export const tabsKey = hasSymbol === true
-  ? Symbol('_q_tabs_')
-  : '_q_tabs_'
-
-export const uploaderKey = hasSymbol === true
-  ? Symbol('_q_u_')
-  : '_q_u_'
+export const quasarKey = '_q_'
+export const timelineKey = '_q_t_'
+export const stepperKey = '_q_s_'
+export const layoutKey = '_q_l_'
+export const pageContainerKey = '_q_pc_'
+export const fabKey = '_q_f_'
+export const formKey = '_q_fo_'
+export const tabsKey = '_q_tabs_'
+export const uploaderKey = '_q_u_'
