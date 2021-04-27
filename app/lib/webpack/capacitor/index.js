541153d3e7e3025159748447d7edf5c892c4fe8b
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-04-27 03:29:04 +0300

    fix(app): Capacitor mode - take into account /src-capacitor deps also

--- app/lib/webpack/capacitor/index.js
+++ app/lib/webpack/capacitor/index.js
@@ -1,7 +1,18 @@
+const appPaths = require('../../app-paths')
 const injectHtml = require('../inject.html')
 const injectHotUpdate = require('../inject.hot-update')
 
+const capNodeModules = appPaths.resolve.capacitor('node_modules')
+
 module.exports = function (chain, cfg) {
+  // need to also look into /src-capacitor
+  // for deps like @capacitor/core
+  chain.resolve.modules
+    .merge([ capNodeModules ])
+
+  chain.resolveLoader.modules
+    .merge([ capNodeModules ])
+
   injectHtml(chain, cfg)
   injectHotUpdate(chain, cfg)
 }
