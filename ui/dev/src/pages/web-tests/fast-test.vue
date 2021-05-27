92f0850c22cc8c493fabcd47256500ab3b0e374d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-05-27 15:40:43 +0300

    chore(ui): clear fast-test

--- ui/dev/src/pages/web-tests/fast-test.vue
+++ ui/dev/src/pages/web-tests/fast-test.vue
@@ -8,25 +8,6 @@
       For some test that you think it should be persistent,
       make a new *.vue file here or in another folder under /dev/components.
     -->
-    <q-dialog v-model="dlg">
-      <q-card>
-        <q-stepper v-model="stepNum"
-          vertical
-          header-nav
-        >
-          <q-step v-for="(step, index) in steps" :key="index"
-            :name="index"
-            :title="step.title"
-          >
-            <!-- <div>gigi</div>
-            <q-toggle v-model="tog" label="Toggle me" /> -->
-            <q-input v-model="text" autofocus />
-          </q-step>
-        </q-stepper>
-      </q-card>
-    </q-dialog>
-
-    <q-btn label="Show Popup" @click="dlg = true" />
   </div>
 </template>
 
@@ -37,23 +18,6 @@ import { h, ref, computed, watch, nextTick, onBeforeUnmount, onMounted, getCurre
 export default {
   setup () {
     return {
-      text: ref(''),
-      tog: ref(false),
-      dlg: ref(false),
-      stepNum: ref(0),
-      steps: [{
-        title: 'Step 1',
-      }, {
-        title: 'Step 2',
-      }, {
-        title: 'Step 3',
-      }, {
-        title: 'Step 4',
-      }, {
-        title: 'Step 5',
-      // }, {
-      //   title: 'Step 6',
-      }]
     }
   }
 }
