8ce7bb107f32962ee2776022e829c3ec9092ca7a
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-06-06 02:30:33 +0300

    fix(app): improve module not found error reporting #9459

--- app/lib/helpers/print-webpack-issue/formatters/moduleNotFound.js
+++ app/lib/helpers/print-webpack-issue/formatters/moduleNotFound.js
@@ -10,10 +10,18 @@ const cmd = nodePackager === 'yarn'
   : 'npm install --save'
 
 module.exports = function format (error, printLog, titleFn) {
-  const dependency = error.message.match(depRE)[1]
-
   printLog(titleFn(removeFileLoaders(error.file)))
   printLog()
+
+  const depMatch = error.message.match(depRE)
+
+  if (depMatch === null) {
+    printLog(error.message.replace('Module not found: Error: ', ''))
+    return
+  }
+
+  const dependency = depMatch[1]
+
   printLog(`Module not found: Can't resolve imported dependency "${bold.underline.yellow(dependency)}"`)
 
   if (relativeRE.test(dependency) === false) {
