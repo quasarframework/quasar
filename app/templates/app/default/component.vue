0bd5fd3bd3231bb1aa0f37cde86569b5bad6728a
Camilo M. Hernandez <cmilohernandez@gmail.com>
2021-07-02 18:16:42 -0400

    Components V2
    Update Components to V2 compliance.

--- app/templates/app/default/component.vue
+++ app/templates/app/default/component.vue
@@ -3,10 +3,12 @@
 </template>
 
 <script>
-export default {
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   // name: 'ComponentName',
   setup () {
     return {}
   }
-}
+})
 </script>
