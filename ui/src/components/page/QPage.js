e08691e68deba8cd5e0eee2d672ee15b4723362f
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2021-09-23 12:48:15 +0800

    feat(QPage): Suggestion for not using QPageContainer (#10788)
    see #10788

--- ui/src/components/page/QPage.js
+++ ui/src/components/page/QPage.js
@@ -1,7 +1,7 @@
 import { h, defineComponent, computed, inject, getCurrentInstance } from 'vue'
 
 import { hSlot } from '../../utils/private/render.js'
-import { pageContainerKey, layoutKey } from '../../utils/private/symbols.js'
+import { layoutKey } from '../../utils/private/symbols.js'
 
 export default defineComponent({
   name: 'QPage',
@@ -14,12 +14,29 @@ export default defineComponent({
   setup (props, { slots }) {
     const { proxy: { $q } } = getCurrentInstance()
 
-    const $layout = inject(layoutKey)
-    inject(pageContainerKey, () => {
-      console.error('QPage needs to be child of QPageContainer')
+    const $layout = inject(layoutKey, () => {
+      console.error('QPage needs to be child of QLayout')
     })
 
     const style = computed(() => {
+
+      const css = {}
+
+      css.boxSizing = 'content-box'
+      
+      if ($layout.header.space === true) {
+        css.marginTop = `${ $layout.header.size }px`
+      }
+      if ($layout.right.space === true) {
+        css[ `margin${ $q.lang.rtl === true ? 'Left' : 'Right' }` ] = `${ $layout.right.size }px`
+      }
+      if ($layout.footer.space === true) {
+        css.marginBottom = `${ $layout.footer.size }px`
+      }
+      if ($layout.left.space === true) {
+        css[ `margin${ $q.lang.rtl === true ? 'Right' : 'Left' }` ] = `${ $layout.left.size }px`
+      }
+
       const offset
         = ($layout.header.space === true ? $layout.header.size : 0)
         + ($layout.footer.space === true ? $layout.footer.size : 0)
@@ -31,16 +48,16 @@ export default defineComponent({
 
         return props.styleFn(offset, height)
       }
+      
+      css.minHeight = $layout.isContainer.value === true
+                      ? ($layout.containerHeight.value - offset) + 'px'
+                      : (
+                          $q.screen.height === 0
+                            ? (offset !== 0 ? `calc(100vh - ${ offset }px)` : '100vh')
+                            : ($q.screen.height - offset) + 'px'
+                        )
 
-      return {
-        minHeight: $layout.isContainer.value === true
-          ? ($layout.containerHeight.value - offset) + 'px'
-          : (
-              $q.screen.height === 0
-                ? (offset !== 0 ? `calc(100vh - ${ offset }px)` : '100vh')
-                : ($q.screen.height - offset) + 'px'
-            )
-      }
+      return css
     })
 
     const classes = computed(() =>
--- ui/src/components/page/QPageContainer.js
+++ ui/src/components/page/QPageContainer.js
@@ -1,42 +1,14 @@
-import { h, defineComponent, computed, provide, inject, getCurrentInstance } from 'vue'
+import { h, defineComponent } from 'vue'
 
 import { hSlot } from '../../utils/private/render.js'
-import { pageContainerKey, layoutKey } from '../../utils/private/symbols.js'
 
 export default defineComponent({
   name: 'QPageContainer',
 
   setup (_, { slots }) {
-    const { proxy: { $q } } = getCurrentInstance()
-
-    const $layout = inject(layoutKey, () => {
-      console.error('QPageContainer needs to be child of QLayout')
-    })
-
-    provide(pageContainerKey, true)
-
-    const style = computed(() => {
-      const css = {}
-
-      if ($layout.header.space === true) {
-        css.paddingTop = `${ $layout.header.size }px`
-      }
-      if ($layout.right.space === true) {
-        css[ `padding${ $q.lang.rtl === true ? 'Left' : 'Right' }` ] = `${ $layout.right.size }px`
-      }
-      if ($layout.footer.space === true) {
-        css.paddingBottom = `${ $layout.footer.size }px`
-      }
-      if ($layout.left.space === true) {
-        css[ `padding${ $q.lang.rtl === true ? 'Right' : 'Left' }` ] = `${ $layout.left.size }px`
-      }
-
-      return css
-    })
 
     return () => h('div', {
-      class: 'q-page-container',
-      style: style.value
+      class: 'q-page-container'
     }, hSlot(slots.default))
   }
 })
