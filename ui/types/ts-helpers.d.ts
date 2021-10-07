b4024c8282ddbb5e448d8eaab7d76a93755a5fb8
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2021-10-07 20:17:06 +0300

    feat(ts): Generate slot types

--- ui/types/ts-helpers.d.ts
+++ ui/types/ts-helpers.d.ts
@@ -30,8 +30,9 @@ export type PublicProps = VNodeProps & AllowedComponentProps & ComponentCustomPr
 // GlobalComponentConstructor helper is kind of like the ComponentConstructor type helper, but simpler and keeps the Volar errors simpler,
 // and also similar to the usage in official Vue packages: https://github.com/vuejs/vue-next/blob/d84d5ecdbdf709570122175d6565bb61fae877f2/packages/runtime-core/src/components/BaseTransition.ts#L258-L264 or https://github.com/vuejs/vue-router-next/blob/5dd5f47515186ce34efb9118dda5aad0bb773439/src/RouterView.ts#L160-L172 etc.
 // TODO: This can be replaced with `DefineComponent` once this PR gets merged: https://github.com/vuejs/vue-next/pull/4465
-export type GlobalComponentConstructor<Props = {}> = {
+export type GlobalComponentConstructor<Props = {}, Slots = {}> = {
   new (): {
     $props: PublicProps & Props
+    $slots: Slots
   }
 }
