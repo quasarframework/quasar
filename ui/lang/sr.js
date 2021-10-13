aa5c019b0d936f3f8533c9a4d162c41a1f10dffc
MarkoArsic <markoarsic2104@gmail.com>
2021-10-13 15:20:16 +0200

    Minor changes in Serbian Latin + New Serbian Cyrillic lang

--- ui/lang/sr.js
+++ ui/lang/sr.js
@@ -23,20 +23,22 @@ export default {
     monthsShort: 'Jan_Feb_Mar_Apr_Maj_Jun_Jul_Avg_Sep_Okt_Nov_Dec'.split('_'),
     firstDayOfWeek: 1, // 0-6, 0 - Sunday, 1 Monday, ...
     format24h: true,
-    pluralDay: 'dana'
+    pluralDay: 'дана'
   },
   table: {
     noData: 'Nema podataka',
     noResults: 'Nema odgovarajućih zapisa',
     loading: 'Učitavanje...',
-    selectedRecords: rows => (
-      rows > 1
+    selectedRecords: function (rows) {
+      return rows > 1
         ? rows + ' izabranih redova.'
         : (rows === 0 ? 'Nema' : '1') + ' izabranih redova.'
-    ),
+    },
     recordsPerPage: 'Redova po stranici:',
     allRows: 'Sve',
-    pagination: (start, end, total) => start + '-' + end + ' od ' + total,
+    pagination: function (start, end, total) {
+      return start + '-' + end + ' od ' + total
+    },
     columns: 'Kolone'
   },
   editor: {
@@ -56,14 +58,14 @@ export default {
     center: 'Centrirati',
     right: 'Poravnati udesno',
     justify: 'Poravnati obostrano',
-    print: 'Ispis',
+    print: 'Štampa',
     outdent: 'Smanjiti uvlačenje',
     indent: 'Povećati uvlačenje',
     removeFormat: 'Ukloniti formatiranje',
     formatting: 'Formatirati',
     fontSize: 'Veličina slova',
     align: 'Poravnati',
-    hr: 'Ubaciti vodoravni lenjir',
+    hr: 'Ubaciti horizontalnu liniju',
     undo: 'Poništiti',
     redo: 'Vratiti',
     heading1: 'Naslov 1',
@@ -72,17 +74,17 @@ export default {
     heading4: 'Naslov 4',
     heading5: 'Naslov 5',
     heading6: 'Naslov 6',
-    paragraph: 'Odlomak',
+    paragraph: 'Paragraf',
     code: 'Kod',
     size1: 'Najmanje',
     size2: 'Manje',
     size3: 'Normalno',
     size4: 'Srednje Veliko',
     size5: 'Veliko',
-    size6: 'Već',
+    size6: 'Veće',
     size7: 'Najveće',
     defaultFont: 'Podrazumevani font',
-    viewSource: 'Pogledaj izvor'
+    viewSource: 'Погледај извор'
   },
   tree: {
     noNodes: 'Nema nijednog čvora',
