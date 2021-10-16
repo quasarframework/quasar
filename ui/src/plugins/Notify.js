700be95b795d3c0cd66eea4c8b07aa083e9cc0a7
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-10-16 13:58:06 +0300

    Update Notify.js

--- ui/src/plugins/Notify.js
+++ ui/src/plugins/Notify.js
@@ -64,6 +64,8 @@ function logError (error, config) {
 function getComponent ($q) {
   return defineComponent({
     name: 'QNotifications',
+    
+    // hide App from Vue devtools
     devtools: { hide: true },
 
     setup () {
