d70b433ac08777fcea36478228d1df492795089c
Jeff Galbraith <galbraith64@gmail.com>
2021-10-24 15:49:30 -0600

    fix(app) - additional changes to electron-main.js
    The fixes include:
    1. 'process.platform' is undefined if `nodeIntegration` is not true. Fall back to using `os.platform()`
    2. Resolve which icon to use based on platform Electron is running on
    3. Inject icon path into BrowserWindow (with previous commit this now works with both dev and build)

--- app/templates/electron/electron-main.js
+++ app/templates/electron/electron-main.js
@@ -1,11 +1,24 @@
 import { app, BrowserWindow, nativeTheme } from 'electron'
 import path from 'path'
+import os from 'os'
+
+// needed in case nodeIntegration is not true
+const platform = process.platform || os.platform()
 
 try {
-  if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
-    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
+  if (platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
+    require('fs').unlinkSync(path.join(app.getPath('userData'), 'DevTools Extensions'))
   }
-} catch (_) { }
+}
+catch (_) { }
+
+// determine which icon to use for the platform
+// works for both dev and build
+const iconPath = path.resolve(__dirname, 'icons',
+  platform === 'win32' ? 'icon.ico'
+    : platform === 'darwin' ? 'icon.icns'
+      : 'linux-512x512.png'
+)
 
 let mainWindow
 
@@ -14,6 +27,7 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
+    icon: iconPath,
     width: 1000,
     height: 600,
     useContentSize: true,
@@ -29,7 +43,8 @@ function createWindow () {
   if (process.env.DEBUGGING) {
     // if on DEV or Production with debug enabled
     mainWindow.webContents.openDevTools()
-  } else {
+  }
+  else {
     // we're on production; no access to devtools pls
     mainWindow.webContents.on('devtools-opened', () => {
       mainWindow.webContents.closeDevTools()
@@ -44,7 +59,7 @@ function createWindow () {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  if (platform !== 'darwin') {
     app.quit()
   }
 })
