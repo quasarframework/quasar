c2534496aca52787647686d9011582799db1883c
Chiatiah Calson <c.chiatiah@dreamonkey.com>
2021-10-25 12:41:52 +0100

    feat(docs): add footer to main layout

---
+++ docs/src/assets/landing-page/landing-page-footer.ts
@@ -0,0 +1,302 @@
+export default [
+  {
+    label: 'Style & identity',
+    items: [
+      {
+        label: 'Typography',
+        to: '#'
+      },
+      {
+        label: 'Color palette',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Layout & Grid',
+    items: [
+      {
+        label: 'Introduction to flexbox',
+        to: '#'
+      },
+      {
+        label: 'Grid row',
+        to: '#'
+      },
+      {
+        label: 'Grid column',
+        to: '#'
+      },
+      {
+        label: 'Grid gutter',
+        to: '#'
+      },
+      {
+        label: 'Flexbox patterns',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Vue directives',
+    items: [
+      {
+        label: 'Close popup',
+        to: '#'
+      },
+      {
+        label: 'Go back (Handling back button)',
+        to: '#'
+      },
+      {
+        label: 'Intersection',
+        to: '#'
+      },
+      {
+        label: 'Material ripples',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  }
+]
--- docs/src/layouts/MainLayout.vue
+++ docs/src/layouts/MainLayout.vue
@@ -1,8 +1,8 @@
 <template>
-  <q-layout view="hHh lpR fff">
+  <q-layout view="hHh lpR fff" class="bg-lp-dark">
 
-    <q-header class="bg-lp-dark homepage-header">
-      <q-toolbar class="q-py-sm q-px-md">
+    <q-header>
+      <q-toolbar class="bg-lp-dark q-py-sm q-px-md">
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
@@ -65,15 +65,59 @@
       <router-view />
     </q-page-container>
 
-    <q-footer class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          <div>Title</div>
-        </q-toolbar-title>
-      </q-toolbar>
+    <q-footer class="" style="background-color: #dbe1e5;">
+      <div class="row justify-center bg-white">
+        <div class="help">
+          <q-toolbar>
+            <q-tabs v-model="tabs" align="justify" class="text-grey-7">
+              <q-tab name="mails" label="Why quasar" class="help__link" />
+              <q-tab name="alarms" label="Team" class="help__link" />
+              <q-tab
+                name="movies"
+                label="Video tutorials"
+                class="help__link"
+              />
+              <q-tab
+                name="photos"
+                label="Quasar brand resources"
+                class="help__link"
+              />
+              <q-tab name="alarms" label="MIT license" class="help__link" />
+              <q-tab
+                name="alarms"
+                label="Privacy policy"
+                class="help__link"
+              />
+            </q-tabs>
+          </q-toolbar>
+        </div>
+      </div>
+
+      <div class="row justify-center q-pt-xl q-mt-md">
+        <div class="col-10 row q-pt-md">
+          <div class="col">
+            <div class="row q-col-gutter-lg q-gutter-y-lg text-size-12 text-capitalize">
+              <template v-for="footerItem in footerItems" :key="footerItem.label">
+                <div class="col-2">
+                  <q-list>
+                    <q-item-label class="q-pa-none text-lp-dark"
+                    >{{ footerItem.label }}</q-item-label
+                    >
+                    <q-separator spaced color="lp-primary" />
+                    <template v-for="(item, i) in footerItem.items" :key="i">
+                      <q-item dense class="q-pa-none" clickable :to="item.to">
+                        <q-item-section class="text-lp-dark">
+                          {{ item.label }}
+                        </q-item-section>
+                      </q-item>
+                    </template>
+                  </q-list>
+                </div>
+              </template>
+            </div>
+          </div>
+        </div>
+      </div>
     </q-footer>
 
   </q-layout>
@@ -81,6 +125,7 @@
 
 <script lang='ts'>
 import { defineComponent, ref } from 'vue'
+import footerItems from 'assets/landing-page/landing-page-footer.ts'
 
 export default defineComponent({
   name: 'MainLayout',
@@ -88,8 +133,13 @@ export default defineComponent({
     const tab = ref('tab1')
 
     return {
-      tab
+      tab,
+      footerItems
     }
   }
 })
 </script>
+
+<style scoped lang="scss">
+
+</style>
