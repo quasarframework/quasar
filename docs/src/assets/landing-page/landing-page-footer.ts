9ef5ecd7cea8359a1343f2fd19b8be744de0380d
Chiatiah Calson <chiacalson@gmail.com>
2021-10-29 14:50:11 +0100

    feat(landing): landing page sections (#3)

---
+++ docs/src/assets/landing-page/landing-page-footer.ts
@@ -0,0 +1,310 @@
+export const homepageFooterItems = [
+  {
+    label: 'Getting Started',
+    items: [
+      {
+        label: 'Typography',
+        to: '#'
+      },
+      {
+        label: 'Color palette',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Contribution guide',
+    items: [
+      {
+        label: 'Introduction to flexbox',
+        to: '#'
+      },
+      {
+        label: 'Grid row',
+        to: '#'
+      },
+      {
+        label: 'Grid column',
+        to: '#'
+      },
+      {
+        label: 'Grid gutter',
+        to: '#'
+      },
+      {
+        label: 'Flexbox patterns',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar options and helpers',
+    items: [
+      {
+        label: 'Close popup',
+        to: '#'
+      },
+      {
+        label: 'Go back (Handling back button)',
+        to: '#'
+      },
+      {
+        label: 'Intersection',
+        to: '#'
+      },
+      {
+        label: 'Material ripples',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Security',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar CLI',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Icon Genie CLI',
+    items: [
+      {
+        label: 'Introduction',
+        to: '#'
+      },
+      {
+        label: 'Installation/Upgrade notes',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Style & identity',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Layouts & grid',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Vue directives',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar plugins',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'App extensions',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  },
+  {
+    label: 'Quasar Utils',
+    items: [
+      {
+        label: 'Address color',
+        to: '#'
+      },
+      {
+        label: 'App fullscreen',
+        to: '#'
+      },
+      {
+        label: 'Theme Brunhilde',
+        to: '#'
+      },
+      {
+        label: 'Dark mode',
+        to: '#'
+      },
+      {
+        label: 'Spacing',
+        to: '#'
+      }
+    ]
+  }
+]
+
+export const footerToolbar = [
+  { label: 'Why quasar?', to: '#' },
+  { label: 'Team', to: '#' },
+  { label: 'Video tutorials', to: '#' },
+  { label: 'Quasar brand resources', to: '#' },
+  { label: 'Privacy policy', to: '#' }
+]
---
+++ docs/src/components/landing-page/WhyQuasarCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-card class="q-pa-lg text-center column justify-between items-center full-height shadow-bottom-large">
+    <q-card-section class="q-pa-none">
+      <q-icon size="xl" :name="icon"/>
+      <div class="text-uppercase text-bold text-lp-primary text-size-24 q-py-md">
+        {{ title }}
+      </div>
+      <div class="text-lp-light text-size-16 text-weight-medium q-pb-md">
+        {{ body }}
+      </div>
+    </q-card-section>
+
+    <q-card-section class="q-pa-none">
+      <q-btn
+          outline
+          class="text-size-14"
+          color="lp-accent"
+          :label="btnLabel"
+          :to="btnLink"
+      />
+    </q-card-section>
+  </q-card>
+</template>
+<script lang="ts">
+
+export default {
+  props: {
+    icon: {
+      required: true,
+      type: String
+    },
+    title: {
+      required: true,
+      type: String
+    },
+    body: {
+      required: true,
+      type: String
+    },
+    btnLabel: {
+      required: true,
+      type: String
+    },
+    btnLink: {
+      required: true,
+      type: String
+    }
+  },
+  name: 'WhyQuasarCard'
+}
+</script>
