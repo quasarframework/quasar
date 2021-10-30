b0fbe2301767a5032ff2a13686bede682f0c7fa0
Jeff Galbraith <galbraith64@gmail.com>
2021-10-30 07:35:08 -0600

    fix(icon-genie): more refinement for Electron icons
    This is to support changes in App in regards to "icon.png"

--- icongenie/lib/modes/quasar-app-v1/electron.js
+++ icongenie/lib/modes/quasar-app-v1/electron.js
@@ -1,22 +1,29 @@
 module.exports = [
   {
-    // macos
+    // macos (embedded icons)
     generator: 'icns',
     name: 'icon.icns',
     folder: 'src-electron/icons'
   },
 
   {
-    // windows
+    // windows (embedded icon)
     generator: 'ico',
     name: 'icon.ico',
     folder: 'src-electron/icons'
   },
 
   {
-    // linux
+    // linux (not really needed)
     generator: 'png',
     name: 'linux-512x512.png',
+    folder: 'src-electron/icons',
+    sizes: [ 512 ]
+  },
+  {
+    // tray icon (all platforms)
+    generator: 'png',
+    name: 'icon.png',
     folder: 'src-electron/icons',
     sizes: [ 512 ]
   }
