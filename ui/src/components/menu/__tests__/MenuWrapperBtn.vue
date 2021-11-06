172fb7ea416c229c989e7b5ad234bec9181368ae
Evert van der Weit <evertvanderweit@gmail.com>
2021-11-06 22:18:03 +0100

    (testing): add tests for behavior of QMenu

--- ui/src/components/menu/__tests__/MenuWrapperBtn.vue
+++ ui/src/components/menu/__tests__/MenuWrapperBtn.vue
@@ -2,40 +2,28 @@
   <q-btn color="primary" data-cy="wrapper" label="Wrapper">
     <q-menu v-bind="$attrs" data-cy="menu">
       <q-list style="min-width: 100px">
-        <q-item clickable v-close-popup>
-          <q-item-section>New tab</q-item-section>
+        <q-item clickable v-close-popup data-cy="close-popup">
+          <q-item-section>VClosePopup</q-item-section>
         </q-item>
-        <q-item clickable v-close-popup>
-          <q-item-section>New incognito tab</q-item-section>
-        </q-item>
-        <q-separator />
-        <q-item clickable v-close-popup>
-          <q-item-section>Recent tabs</q-item-section>
-        </q-item>
-        <q-item clickable v-close-popup>
-          <q-item-section>History</q-item-section>
-        </q-item>
-        <q-item clickable v-close-popup>
-          <q-item-section>Downloads</q-item-section>
-        </q-item>
-        <q-separator />
-        <q-item clickable v-close-popup>
-          <q-item-section>Settings</q-item-section>
-        </q-item>
-        <q-separator />
-        <q-item clickable v-close-popup>
-          <q-item-section>Help &amp; Feedback</q-item-section>
+        <q-item clickable data-cy="keep-open">
+          <q-item-section>KeepOpen</q-item-section>
         </q-item>
       </q-list>
     </q-menu>
   </q-btn>
+  <div data-cy="other-target" class="other-target bg-red q-pa-lg text-white">
+    Other target
+  </div>
 </template>
 
 <script>
+import { defineComponent } from 'vue'
 
-export default {
+export default defineComponent({
+  inheritAttrs: false,
   setup () {
 
   }
-}
+})
+
 </script>
--- ui/src/components/menu/__tests__/QMenu.spec.js
+++ ui/src/components/menu/__tests__/QMenu.spec.js
@@ -1,17 +1,301 @@
 import { mount } from '@cypress/vue'
 import MenuWrapperBtn from './MenuWrapperBtn.vue'
 
-// const snapshotOptions = { customSnapshotsDir: '../src/components/menu/__tests__' }
-
 describe('QMenu', () => {
-  it('should show a menu at the bottom of the wrapper', () => {
-    mount(MenuWrapperBtn, {})
+  // Position tests
+  describe('Position tests', () => {
+    it('should show a menu at the bottom of the wrapper', () => {
+      mount(MenuWrapperBtn, {})
 
-    cy.dataCy('wrapper')
-      .click()
+      cy.dataCy('wrapper')
+        .click()
 
-    cy.dataCy('menu')
-      .checkPosition('wrapper', 'bottom')
-    // .matchImageSnapshot(snapshotOptions)
+      cy.dataCy('menu')
+        .checkPosition('wrapper', 'bottom')
+    })
+  })
+
+  // Behavior tests
+  describe('Behavior tests', () => {
+    describe('(prop): target', () => {
+      it('should use another target using a CSS selector', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            target: '.other-target'
+          }
+        })
+
+        // Click on parent element should not show menu
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('not.exist')
+
+        // Click on other target should show menu
+        cy.dataCy('other-target')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+      })
+
+      it('should not show when target is false', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            target: false
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('not.exist')
+      })
+    })
+
+    describe('(prop): no-parent-event', () => {
+      it('should not show when clicking parent with no-parent-event true', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            'no-parent-event': true
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('not.exist')
+      })
+
+      it('should show when clicking parent with no-parent-event false', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            'no-parent-event': false
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+      })
+    })
+
+    describe('(prop): context-menu', () => {
+      it('should not show when left clicking parent', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            'context-menu': true
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('not.exist')
+      })
+
+      it('should show when right clicking parent', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            'context-menu': true
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .rightclick()
+        cy.dataCy('menu')
+          .should('exist')
+      })
+    })
+
+    describe('(prop): touch-position', () => {
+      it('should show menu at the position of the click', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            'touch-position': true
+          }
+        })
+
+        const mouseX = 100
+        const mouseY = 25
+
+        cy.dataCy('wrapper')
+          .click(mouseX, mouseY)
+        cy.dataCy('menu')
+          .should('exist')
+          .then(($el) => {
+            expect($el[ 0 ].offsetLeft).to.equal(mouseX)
+            // TODO: check if the Y position being off by 1 is intentional
+            expect($el[ 0 ].offsetTop).to.equal(mouseY + 1)
+          })
+      })
+    })
+
+    describe('(prop): persistent', () => {
+      it('should close the menu when clicking outside the menu', () => {
+        mount(MenuWrapperBtn)
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+        cy.get('body')
+          .click(499, 0)
+          .dataCy('menu')
+          .should('not.exist')
+      })
+
+      it('should close the menu when hitting the escape key', () => {
+        mount(MenuWrapperBtn)
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+        cy.get('body')
+          .type('{esc}')
+          .dataCy('menu')
+          .should('not.exist')
+      })
+
+      it('should not close the menu when clicking outside the menu when persistent', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            persistent: true
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+        cy.get('body')
+          .click(499, 0)
+          .wait(300) // Await menu animation otherwise it always passes
+          .dataCy('menu')
+          .should('exist')
+      })
+
+      it('should not close the menu when hitting the escape key when persistent', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            persistent: true
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+        cy.get('body')
+          .type('{esc}')
+          .wait(300) // Await menu animation otherwise it always passes
+          .dataCy('menu')
+          .should('exist')
+      })
+    })
+
+    describe('(prop): auto-close', () => {
+      it('should not close the menu when clicking a menu child without v-close-popup', () => {
+        mount(MenuWrapperBtn)
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+          .dataCy('keep-open')
+          .click()
+          .wait(300) // Await menu animation otherwise it always passes
+        cy.dataCy('menu')
+          .should('exist')
+      })
+
+      it('should close the menu when clicking a menu child without v-close-popup when auto-close is true', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            'auto-close': true
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+          .dataCy('keep-open')
+          .click()
+          .wait(300) // Await menu animation otherwise it always passes
+        cy.dataCy('menu')
+          .should('not.exist')
+      })
+    })
+
+    describe('(prop): no-refocus', () => {
+      // TODO: it is not clear from the docs that refocussing does not happen when clicking outside or closing by escape/programatticaly. Should this be added?
+      it('should switch focus back to parent element when closing', () => {
+        mount(MenuWrapperBtn)
+
+        cy.dataCy('wrapper')
+          .focus()
+          .should('have.focus')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+          .should('have.focus')
+          .dataCy('close-popup')
+          .click()
+          .wait(300) // Wait for menu transition
+        cy.dataCy('wrapper')
+          .get('.q-focus-helper')
+          .should('have.focus')
+      })
+
+      it('should not switch focus back to parent element when closing if no-refocus is true', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            'no-refocus': true
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .focus()
+          .should('have.focus')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+          .should('have.focus')
+          .dataCy('close-popup')
+          .click()
+          .wait(300) // Wait for menu transition
+        cy.dataCy('wrapper')
+          .get('.q-focus-helper')
+          .should('not.have.focus')
+      })
+    })
+
+    describe('(prop): no-focus', () => {
+      it('should switch focus to the menu when opening', () => {
+        mount(MenuWrapperBtn)
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+          .should('have.focus')
+      })
+
+      it('should no switch focus to the menu when opening with no-focus is true', () => {
+        mount(MenuWrapperBtn, {
+          attrs: {
+            'no-focus': true
+          }
+        })
+
+        cy.dataCy('wrapper')
+          .click()
+        cy.dataCy('menu')
+          .should('exist')
+          .should('not.have.focus')
+      })
+    })
   })
 })
