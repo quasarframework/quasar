d385090eac7e3225cd537d6949458b12aca99dea
Evert van der Weit <evertvanderweit@gmail.com>
2021-11-06 22:50:31 +0100

    (chore): extends cypress eslint plugin

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/vue3-essential',
+    'plugin:cypress/recommended',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
