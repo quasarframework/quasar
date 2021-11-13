fda9a988a5ad341db7882343b38670a67fb6dab3
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-11-14 00:41:28 +0200

    Update electron.js

--- icongenie/lib/modes/quasar-app-v1/electron.js
+++ icongenie/lib/modes/quasar-app-v1/electron.js
@@ -14,16 +14,9 @@ module.exports = [
   },
 
   {
-    // linux (not really needed)
+    // linux
     generator: 'png',
     name: 'linux-512x512.png',
-    folder: 'src-electron/icons',
-    sizes: [ 512 ]
-  },
-  {
-    // tray icon (all platforms)
-    generator: 'png',
-    name: 'icon.png',
     folder: 'src-electron/icons',
     sizes: [ 512 ]
   }
