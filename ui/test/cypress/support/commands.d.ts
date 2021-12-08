8b6497183afdfc74d484dcccb7aafad705423a60
Evert van der Weit <evertvanderweit@gmail.com>
2021-12-08 14:19:40 +0100

    testing(fix): fix autocompletion within spec files

--- ui/test/cypress/support/commands.d.ts
+++ ui/test/cypress/support/commands.d.ts
@@ -1,31 +1,28 @@
 /// <reference types="Cypress" />
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      /**
-       * Custom command to select DOM element by data-cy attribute.
-       * @example cy.dataCy('menu')
-       *            .checkVerticalPosition('wrapper', 'bottom')
-       */
-      checkVerticalPosition<E extends Node = HTMLElement>(
-        anchor: string,
-        anchorOrigin: "bottom" | "top" | "center",
-        selfOrigin: "bottom" | "top" | "center",
-        offset: number = 0
-      ): Chainable<JQuery<E>>;
-  
-      /**
-       * Custom command to select DOM element by data-cy attribute.
-       * @example cy.dataCy('menu')
-       *            .checkHorizontalPosition('wrapper', 'left')
-       */
-      checkHorizontalPosition<E extends Node = HTMLElement>(
-        anchor: string,
-        anchorOrigin: "left" | "middle" | "right",
-        selfOrigin: "left" | "middle" | "right",
-        offset: number = 0
-      ): Chainable<JQuery<E>>;
-    }
-  }
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to select DOM element by data-cy attribute.
+     * @example cy.dataCy('menu')
+     *            .checkVerticalPosition('wrapper', 'bottom')
+     */
+    checkVerticalPosition<E extends Node = HTMLElement>(
+      anchor: string,
+      anchorOrigin: "bottom" | "top" | "center",
+      selfOrigin: "bottom" | "top" | "center",
+      offset: number = 0
+    ): Chainable<JQuery<E>>;
 
+    /**
+     * Custom command to select DOM element by data-cy attribute.
+     * @example cy.dataCy('menu')
+     *            .checkHorizontalPosition('wrapper', 'left')
+     */
+    checkHorizontalPosition<E extends Node = HTMLElement>(
+      anchor: string,
+      anchorOrigin: "left" | "middle" | "right",
+      selfOrigin: "left" | "middle" | "right",
+      offset: number = 0
+    ): Chainable<JQuery<E>>;
+  }
 }
