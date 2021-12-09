1eca971860e1372ce97b5092dd4f50d739e88fd8
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-12-09 05:22:22 +0200

    Update QFooter.js

--- ui/src/components/footer/QFooter.js
+++ ui/src/components/footer/QFooter.js
@@ -46,6 +46,7 @@ export default createComponent({
     const fixed = computed(() =>
       props.reveal === true
       || $layout.view.value.indexOf('F') > -1
+      || ($q.platform.is.ios && $layout.isContainer.value === true)
     )
 
     const containerHeight = computed(() => (
