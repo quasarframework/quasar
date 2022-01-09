bc452e7da2705946f54c648f963c71bc1739bac7
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-01-10 01:34:48 +0200

    Update QPagination.js

--- ui/src/components/pagination/QPagination.js
+++ ui/src/components/pagination/QPagination.js
@@ -302,9 +302,6 @@ export default createComponent({
         const style = {
           minWidth: `${ Math.max(2, String(props.max).length) }em`
         }
-
-        const activeColor = props.activeColor || props.color
-
         if (boundaryStart) {
           const active = props.min === props.modelValue
           contentStart.push(getBtn({
@@ -312,9 +309,8 @@ export default createComponent({
             style,
             disable: props.disable,
             flat: !active,
-            color: active ? activeColor : props.color,
-            textColor: active ? props.textColor : void 0,
-            label: props.min
+            label: props.min,
+            ...(active ? activeBtnProps.value : {})
           }, props.min))
         }
         if (boundaryEnd) {
@@ -324,9 +320,8 @@ export default createComponent({
             style,
             disable: props.disable,
             flat: !active,
-            color: active ? activeColor : props.color,
-            textColor: active ? props.textColor : void 0,
-            label: props.max
+            label: props.max,
+            ...(active ? activeBtnProps.value : {})
           }, props.max))
         }
         if (ellipsesStart) {
