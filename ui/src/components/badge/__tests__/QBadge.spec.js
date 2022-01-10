ac5b0c2d9956acf015c1c8f4329824c9a71d269f
Evert van der Weit <evertvanderweit@gmail.com>
2022-01-10 21:11:49 +0100

    chore(testing): refactor QBadge to use API scaffolded tests

--- ui/src/components/badge/__tests__/QBadge.spec.js
+++ ui/src/components/badge/__tests__/QBadge.spec.js
@@ -1,23 +1,74 @@
-import { mount } from '@cypress/vue'
-import QBadge from '../QBadge'
+// import { mount } from '@cypress/vue'
+// import QBadge from '../QBadge'
 
-const snapshotOptions = { customSnapshotsDir: '../src/components/badge/__tests__' }
+// const snapshotOptions = { customSnapshotsDir: '../src/components/badge/__tests__' }
 
-describe('QBadge', () => {
-  const label = 'Test'
-  const color = 'primary'
+describe('Badge API', () => {
+  describe('Props', () => {
+    describe('Category: content', () => {
+      describe('(prop): floating', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
 
-  it('should render a QBadge with a label and color', () => {
-    mount(QBadge, {
-      props: {
-        label,
-        color
-      }
+      describe('(prop): multi-line', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): label', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): align', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
     })
 
-    cy.get('.q-badge')
-      .should('have.text', label)
-      .should('have.class', `bg-${ color }`)
-      .matchImageSnapshot(snapshotOptions)
+    describe('Category: style', () => {
+      describe('(prop): color', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): text-color', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): transparent', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): outline', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): rounded', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+    })
+  })
+
+  describe('Slots', () => {
+    describe('(slot): default', () => {
+      it.skip(' ', () => {
+        //
+      })
+    })
   })
 })
