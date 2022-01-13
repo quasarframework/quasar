490d5b411499ba111e4cdaed7977dfd83f1da87c
Popescu Dan <pdan.popescu@gmail.com>
2022-01-13 13:46:02 +0200

    feat(QField/QSelect): improve a11n for clear button (#12017)

--- ui/src/mixins/attrs.js
+++ ui/src/mixins/attrs.js
@@ -2,4 +2,6 @@ import { getPropCacheMixin } from '../utils/cache.js'
 
 export const ariaHidden = { 'aria-hidden': 'true' }
 
+export const iconAsButton = { tabindex: 0, type: 'button', 'aria-hidden': false, role: null }
+
 export default getPropCacheMixin('$attrs', 'qAttrs')
