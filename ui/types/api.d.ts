922739b842e1a92355f2c7afbd8363174715652a
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-01-14 18:45:02 +0100

    feat(ui/ts): add QTableColumn generic type

--- ui/types/api.d.ts
+++ ui/types/api.d.ts
@@ -1,6 +1,7 @@
 export * from "./api/cookies";
 export * from "./api/qfile";
 export * from "./api/qselect";
+export * from "./api/qtable";
 export * from "./api/web-storage";
 export * from "./api/validation";
 export * from "./api/slider";
