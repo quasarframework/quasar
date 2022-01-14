a0cc6231f230b7774c7078bd4ae5620e78b84247
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-01-14 18:44:29 +0100

    feat(ui/ts): add QSelectOption type

--- ui/types/api.d.ts
+++ ui/types/api.d.ts
@@ -1,5 +1,6 @@
 export * from "./api/cookies";
 export * from "./api/qfile";
+export * from "./api/qselect";
 export * from "./api/web-storage";
 export * from "./api/validation";
 export * from "./api/slider";
