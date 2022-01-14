2a2a9fdeb593ca1557a51bb4431860f0189af615
Evert van der Weit <evertvanderweit@gmail.com>
2022-01-14 22:04:29 +0100

    chore(testing): create boilerplate for all components on shortlist

---
+++ ui/src/components/input/__tests__/QInput.spec.js
@@ -0,0 +1,105 @@
+describe('Input API', () => {
+  describe('Props', () => {
+    describe('Category: content', () => {
+      describe('(prop): shadow-text', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): autogrow', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+    })
+
+    describe('Category: general', () => {
+      describe('(prop): type', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+    })
+
+    describe('Category: model', () => {
+      describe('(prop): model-value', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): debounce', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): maxlength', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+    })
+
+    describe('Category: style', () => {
+      describe('(prop): input-class', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+
+      describe('(prop): input-style', () => {
+        it.skip(' ', () => {
+          //
+        })
+      })
+    })
+  })
+
+  describe('Events', () => {
+    describe('(event): update:model-value', () => {
+      it.skip(' ', () => {
+        //
+      })
+    })
+
+    describe('(event): focus', () => {
+      it.skip(' ', () => {
+        //
+      })
+    })
+
+    describe('(event): blur', () => {
+      it.skip(' ', () => {
+        //
+      })
+    })
+  })
+
+  describe('Methods', () => {
+    describe('(method): focus', () => {
+      it.skip(' ', () => {
+        //
+      })
+    })
+
+    describe('(method): blur', () => {
+      it.skip(' ', () => {
+        //
+      })
+    })
+
+    describe('(method): select', () => {
+      it.skip(' ', () => {
+        //
+      })
+    })
+
+    describe('(method): getNativeElement', () => {
+      it.skip(' ', () => {
+        //
+      })
+    })
+  })
+})
