ad833d1d43bd25bfee91770d14f93ca09a058f2d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-01-29 18:13:18 +0200

    fix(Screen): use viewport to get window size to avoid problems with zoom/HiDPI #10120

--- ui/src/plugins/Screen.js
+++ ui/src/plugins/Screen.js
@@ -58,12 +58,19 @@ export default defineReactivePlugin({
       return
     }
 
+    const { visualViewport } = window
+    const target = visualViewport || window
+    const getSize = visualViewport === void 0
+      ? () => [ window.innerWidth, window.innerHeight ]
+      : () => [
+          visualViewport.width * visualViewport.scale + window.innerWidth - document.scrollingElement.clientWidth,
+          visualViewport.height * visualViewport.scale + window.innerHeight - document.scrollingElement.clientHeight
+        ]
+
     const classes = $q.config.screen !== void 0 && $q.config.screen.bodyClasses === true
 
     this.__update = force => {
-      const
-        w = window.innerWidth,
-        h = window.innerHeight
+      const [ w, h ] = getSize()
 
       if (h !== this.height) {
         this.height = h
@@ -121,11 +128,7 @@ export default defineReactivePlugin({
     }
 
     const start = () => {
-      const
-        style = getComputedStyle(document.body),
-        target = window.visualViewport !== void 0
-          ? window.visualViewport
-          : window
+      const style = getComputedStyle(document.body)
 
       // if css props available
       if (style.getPropertyValue('--q-size-sm')) {
