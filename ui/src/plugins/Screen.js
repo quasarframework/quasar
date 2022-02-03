e485202b16ad1394202d2f1a15e60ca8dce098c4
Popescu Dan <pdan.popescu@gmail.com>
2022-02-03 13:47:21 +0200

    fix(Screen): fallback to documentElement when scrollElement is missing (jest) #12318 (#12320)
    #12318

--- ui/src/plugins/Screen.js
+++ ui/src/plugins/Screen.js
@@ -60,14 +60,15 @@ export default defineReactivePlugin({
 
     const { visualViewport } = window
     const target = visualViewport || window
+    const scrollingElement = document.scrollingElement || document.documentElement
     const getSize = visualViewport === void 0 || client.is.mobile === true
       ? () => [
-          Math.max(window.innerWidth, document.scrollingElement.clientWidth),
-          Math.max(window.innerHeight, document.scrollingElement.clientHeight)
+          Math.max(window.innerWidth, scrollingElement.clientWidth),
+          Math.max(window.innerHeight, scrollingElement.clientHeight)
         ]
       : () => [
-          visualViewport.width * visualViewport.scale + window.innerWidth - document.scrollingElement.clientWidth,
-          visualViewport.height * visualViewport.scale + window.innerHeight - document.scrollingElement.clientHeight
+          visualViewport.width * visualViewport.scale + window.innerWidth - scrollingElement.clientWidth,
+          visualViewport.height * visualViewport.scale + window.innerHeight - scrollingElement.clientHeight
         ]
 
     const classes = $q.config.screen !== void 0 && $q.config.screen.bodyClasses === true
