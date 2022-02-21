33b3dbd4713f4a46a690471deb6fdffb26f46a9c
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-02-21 22:15:52 +0200

    feat(app-vite): new syntax for build > vitePlugins for better conf diff

--- app-vite/lib/create-vite-config.js
+++ app-vite/lib/create-vite-config.js
@@ -1,22 +1,59 @@
 const { quasar: quasarVitePlugin } = require('@quasar/vite-plugin')
 const vueVitePlugin = require('@vitejs/plugin-vue')
+const getPackage = require('./helpers/get-package')
 
 const appPaths = require('./app-paths')
 const parseEnv = require('./parse-env')
 
 const quasarVitePluginIndexHtmlTransform = require('./vite-plugins/index-html-transform')
 
+function parseVitePlugins (entries) {
+  const acc = []
 
-module.exports = function (quasarConf) {
+  entries.forEach(([ name, opts ]) => {
+    if (typeof name === 'function') {
+      acc.push(name(opts))
+      return
+    }
+
+    if (typeof name !== 'string') {
+      console.error('[Quasar CLI] quasar.config.js > invalid Vite plugin specified:', name)
+      console.error(`Correct form: [ 'my-vite-plugin-name', { /* opts */ } ]`)
+      return
+    }
+
+    const plugin = getPackage(name)
+
+    if (!plugin) {
+      console.error('[Quasar CLI] quasar.config.js > invalid Vite plugin specified (cannot find it):', name)
+      return
+    }
+
+    acc.push(plugin(opts))
+  })
+
+  return acc
+}
+
+module.exports = function (quasarConf, quasarRunMode) {
   const { ctx, build } = quasarConf
+  const quasarPluginOptions = {
+    autoImportComponentCase: quasarConf.framework.autoImportComponentCase,
+    sassVariables: quasarConf.metaConf.css.variablesFile
+  }
+
+  if (quasarRunMode !== void 0) {
+    quasarPluginOptions.runMode = quasarRunMode
+  }
 
   const viteConf = {
     configFile: false,
     root: appPaths.appDir,
     base: build.publicPath,
-    publicDir: build.ignorePublicFolder
+    publicDir: build.ignorePublicFolder === true
       ? false
       : appPaths.publicDir,
+    clearScreen: false,
     mode: ctx.dev === true ? 'development' : 'production',
     resolve: build.resolve,
     define: parseEnv(build.env, build.rawDefine),
@@ -32,16 +69,15 @@ module.exports = function (quasarConf) {
       chunkSizeWarningLimit: build.chunkSizeWarningLimit,
       assetsInlineLimit: build.assetsInlineLimit,
       cssCodeSplit: build.cssCodeSplit,
-      sourcemap: build.sourcemap
+      sourcemap: build.sourcemap === true
+        ? 'inline'
+        : build.sourcemap || false
     },
     plugins: [
       quasarVitePluginIndexHtmlTransform(quasarConf),
       vueVitePlugin(build.viteVuePluginOptions),
-      quasarVitePlugin({
-        autoImportComponentCase: quasarConf.framework.autoImportComponentCase,
-        sassVariables: quasarConf.metaConf.css.variablesFile
-      }),
-      ...build.vitePlugins
+      quasarVitePlugin(quasarPluginOptions),
+      ...parseVitePlugins(build.vitePlugins)
     ]
   }
 
