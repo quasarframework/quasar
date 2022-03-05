20cc82007258ae25e304a18dc23847f478b7715e
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2022-03-05 23:01:13 +0100

    fix: add missing QSelect emits

--- ui/src/components/select/QSelect.js
+++ ui/src/components/select/QSelect.js
@@ -129,7 +129,8 @@ export default createComponent({
     ...useFieldEmits,
     'add', 'remove', 'input-value',
     'keyup', 'keypress', 'keydown',
-    'filter-abort'
+    'filter-abort', 'new-value',
+    'virtual-scroll'
   ],
 
   setup (props, { slots, emit }) {
