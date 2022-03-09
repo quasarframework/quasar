86e19f05885d4e80b2e3f93067da2dd8d2a7defc
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-03-09 02:09:18 +0200

    feat(create-app): rtl support

--- create-app/templates/quasar-v2/js-vite/BASE/postcss.config.js
+++ create-app/templates/quasar-v2/js-vite/BASE/postcss.config.js
@@ -3,7 +3,25 @@
 
 module.exports = {
   plugins: [
-    // to edit target browsers: use "browserslist" field in package.json
-    require('autoprefixer')
+    // https://github.com/postcss/autoprefixer
+    require('autoprefixer')({
+      overrideBrowserslist: [
+        'last 4 Chrome versions',
+        'last 4 Firefox versions',
+        'last 4 Edge versions',
+        'last 4 Safari versions',
+        'last 4 Android versions',
+        'last 4 ChromeAndroid versions',
+        'last 4 FirefoxAndroid versions',
+        'last 4 iOS versions'
+      ]
+    })
+
+    // https://github.com/elchininet/postcss-rtlcss
+    // If you want to support RTL css, then
+    // 1. yarn/npm install postcss-rtlcss
+    // 2. optionally set quasar.config.js > framework > lang to an RTL language
+    // 3. uncomment the following line:
+    // require('postcss-rtlcss')
   ]
 }
