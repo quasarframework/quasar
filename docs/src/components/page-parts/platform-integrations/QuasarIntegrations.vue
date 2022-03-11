b040ef0f04a9676a3186c13641a90c6d558037fa
Calson Chiatiah <c.chiatiah@dreamonkey.com>
2022-03-11 09:50:07 +0100

    feat(integration-page): update integrations page following new design, fix #85

--- docs/src/components/page-parts/platform-integrations/QuasarIntegrations.vue
+++ docs/src/components/page-parts/platform-integrations/QuasarIntegrations.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="column justify-center items-center text-center q-px-xl font-monserrat">
+  <q-page class="column justify-center items-center text-center q-px-xl font-monserrat landing-mb--large">
     <h1 class="page-title landing-heading landing-heading--large normal-line-height">All platforms in one go</h1>
     <p class="letter-spacing-300 text-size-16 text-weight-bold text-center">
       Focus only on your mission and forget about the spaceship.
@@ -19,29 +19,54 @@
     <h2
       class="landing-heading landing-heading--medium q-mb-lg normal-line-height"
     >We have an impressive gear</h2>
-    <div class="integration-options row justify-center q-gutter-lg q-mb-xl">
+    <div class="row justify-center q-gutter-sm q-mb-xl">
       <q-card
         v-for="({path, label, icon, name}, cardIndex) in integrationOptions"
         :key="cardIndex"
-        class="card raise-on-hover column justify-center items-center cursor-pointer bg-brand-secondary"
+        class="card raise-on-hover column justify-center items-center cursor-pointer"
         flat
         @click="$router.push(`/${path}`)"
       >
         <q-icon
           :name="icon"
-          class="card__icon"
+          class="text-size-24 q-my-sm"
           color="brand-accent"
         />
-
-        <q-card-section class="text-white text-size-16 text-weight-bold letter-spacing-300">
+        <div class="text-size-14 text-weight-bold letter-spacing-263 text-brand-secondary">
           {{ label }}
-        </q-card-section>
-
-        <q-card-section class="text-white text-size-14 q-py-none q-px-xs letter-spacing-263 primary-line-height">
+        </div>
+        <div class="text-size-12 q-px-sm letter-spacing-100 text-brand-secondary">
           {{ name }}
-        </q-card-section>
+        </div>
       </q-card>
     </div>
+    <h2 class="landing-heading landing-heading--medium q-mb-lg normal-line-height">Discover Quasar Ecosystem</h2>
+    <template v-for="({about, options}, partIndex) in ecosystemParts" :key="`part-${partIndex}`">
+      <p class="text-size-16 letter-spacing-40 primary-line-height text-brand-secondary">{{ about }}</p>
+      <div class="row justify-center q-gutter-sm margin-bottom-36">
+        <router-link
+          v-for="({icon, label, path, isInternal, iconColor}, cardIndex) in options"
+          :key="cardIndex"
+          :target="isInternal? '_self' : '_blank'"
+          :to="isInternal? `/${path}` : `//${path}`"
+          class="remove-a-tag-styles"
+        >
+          <q-card
+            class="card card--bordered raise-on-hover column justify-center items-center cursor-pointer"
+            flat
+          >
+            <q-icon
+              :name="icon"
+              class="text-size-36 q-my-sm"
+              :color="iconColor || 'brand-primary'"
+            />
+            <div class="text-size-12 q-px-sm letter-spacing-100 text-brand-secondary">
+              {{ label }}
+            </div>
+          </q-card>
+        </router-link>
+      </div>
+    </template>
   </q-page>
 </template>
 
@@ -49,24 +74,29 @@
 import { defineComponent } from 'vue'
 import { useMeta } from 'quasar'
 import { useDocStore } from 'assets/doc-store.js'
-import { fabChrome } from '@quasar/extras/fontawesome-v5'
 import {
   mdiAndroid,
   mdiApple,
   mdiAppleSafari,
+  mdiApplicationOutline,
+  mdiCalendar,
+  mdiDevices,
+  mdiFileDownload,
   mdiFirefox,
+  mdiGoogleChrome,
+  mdiGraphql,
+  mdiLanguageMarkdown,
   mdiLinux,
   mdiMicrosoftEdge,
   mdiMicrosoftWindows,
+  mdiPuzzle,
   mdiServer,
-  mdiApplicationOutline,
-  mdiWidgets,
-  mdiDevices,
-  mdiPuzzle
+  mdiSvg,
+  mdiTestTube
 } from '@quasar/extras/mdi-v6'
 
 const platformIntegrationIcons = [
-  fabChrome,
+  mdiGoogleChrome,
   mdiAppleSafari,
   mdiFirefox,
   mdiMicrosoftEdge,
@@ -76,30 +106,88 @@ const platformIntegrationIcons = [
   mdiAndroid
 ]
 
-const integrationOptions = [
+const ecosystemParts = [
   {
-    label: 'UI',
-    name: 'User Interface Components',
-    icon: mdiWidgets,
-    path: 'components'
+    about: 'App Extensions that help you design your UI',
+    options: [
+      {
+        label: 'QCalendar',
+        icon: mdiCalendar,
+        path: 'github.com/quasarframework/quasar-ui-qcalendar'
+      },
+      {
+        label: 'QMarkdown',
+        icon: mdiLanguageMarkdown,
+        path: 'github.com/quasarframework/app-extension-qmarkdown'
+      },
+      {
+        label: 'QMediaPlayer',
+        icon: 'ondemand_video',
+        path: 'github.com/quasarframework/app-extension-qmediaplayer'
+      },
+      {
+        label: 'All AEs',
+        icon: 'extension',
+        path: 'quasar.dev/app-extensions/discover',
+        iconColor: 'brand-accent'
+      }
+    ]
   },
+  {
+    about: 'App Extensions for your app low tech integrations',
+    options: [
+      {
+        label: 'Testing',
+        icon: mdiTestTube,
+        path: 'github.com/quasarframework/quasar-testing'
+      },
+      {
+        label: 'Apollo GraphQL',
+        icon: mdiGraphql,
+        path: 'github.com/quasarframework/app-extension-apollo'
+      },
+      {
+        label: 'SSG Mode',
+        icon: mdiFileDownload,
+        path: 'github.com/freddy38510/quasar-app-extension-ssg'
+      },
+      {
+        label: 'All AEs',
+        icon: 'extension',
+        path: 'quasar.dev/app-extensions/discover',
+        iconColor: 'brand-accent'
+      }
+    ]
+  },
+  {
+    about: 'Our tools and resources that help you manage icons in your projects',
+    options: [
+      {
+        label: 'Icon Genie',
+        icon: 'stars',
+        path: 'icongenie/installation',
+        isInternal: true
+      },
+      {
+        label: 'Icon Explorer',
+        icon: 'search',
+        path: 'iconexplorer.app'
+      },
+      {
+        label: 'Extra SVG Icons',
+        icon: mdiSvg,
+        path: 'github.com/quasarframework/extra-svg-icons'
+      }
+    ]
+  }
+]
+
+const integrationOptions = [
   {
     label: 'SPA',
     name: 'Single Page Application',
     icon: mdiApplicationOutline,
     path: 'quasar-cli/developing-spa/introduction'
-  },
-  {
-    label: 'PWA',
-    name: 'Progressive Web App',
-    icon: 'web',
-    path: 'quasar-cli/developing-pwa/introduction'
-  },
-  {
-    label: 'BEX',
-    name: 'Browser Extension',
-    icon: mdiPuzzle,
-    path: 'quasar-cli/developing-browser-extensions/introduction'
   },
   {
     label: 'SSR',
@@ -108,10 +196,22 @@ const integrationOptions = [
     path: 'quasar-cli/developing-ssr/introduction'
   },
   {
+    label: 'PWA',
+    name: 'Progressive Web App',
+    icon: 'web',
+    path: 'quasar-cli/developing-pwa/introduction'
+  },
+  {
     label: 'HMA',
     name: 'Hybrid Mobile App',
     icon: 'phone_iphone',
     path: 'quasar-cli/developing-mobile-apps'
+  },
+  {
+    label: 'BEX',
+    name: 'Browser Extension',
+    icon: mdiPuzzle,
+    path: 'quasar-cli/developing-browser-extensions/introduction'
   },
   {
     label: 'MPDA',
@@ -132,33 +232,25 @@ export default defineComponent({
     const $store = useDocStore()
     $store.toc = []
 
-    return { integrationOptions, platformIntegrationIcons }
+    return {
+      integrationOptions,
+      platformIntegrationIcons,
+      ecosystemParts
+    }
   }
 })
 
 </script>
 
 <style lang="scss" scoped>
-.integration-options {
-  width: 85%;
-  @media screen and (min-width: $breakpoint-lg-max) {
-    width: 50%;
-  }
-}
-
 .card {
-  height: 200px;
-  width: 200px;
-  border-radius: 8px;
-  border: solid 1px rgba(0, 0, 0, 0.12);
+  height: 120px;
+  width: 120px;
+  border-radius: 4px;
 
-  &__icon {
-    font-size: 36px;
-  }
-
-  @media screen and (min-width: $breakpoint-xs-max) {
-    height: 160px;
-    width: 160px;
+  &--bordered {
+    border-radius: 8px;
+    border: solid 1px rgba(0, 0, 0, 0.12);
   }
 }
 
@@ -179,5 +271,13 @@ export default defineComponent({
 
 .page-title {
   margin-top: 72px;
+}
+
+.remove-a-tag-styles {
+  text-decoration: none;
+}
+
+.margin-bottom-36 {
+  margin-bottom: 36px;
 }
 </style>
