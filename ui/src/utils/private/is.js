cc4d861ca0ac226801a7b3997ac2a1855c173cb8
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-03-11 09:19:22 +0200

    fix(ui): modify isPlainObject to account for various use-cases but handle the main idea (Arrays should not pass) #12715

--- ui/src/utils/private/is.js
+++ ui/src/utils/private/is.js
@@ -123,7 +123,7 @@ export function isDeepEqual (a, b) {
 // not perfect, but what we ARE interested is for Arrays not to slip in
 // as spread operator will mess things up in various areas
 export function isObject (v) {
-  return Object(v) === v && Array.isArray(v) !== true
+  return v !== null && typeof v === 'object' && Array.isArray(v) !== true
 }
 
 export function isDate (v) {
