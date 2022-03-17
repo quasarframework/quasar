f9823a5d0bba494a434bdcd08a46510b0ad1564d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-03-17 15:32:16 +0200

    fix(extras): help vite dep analysis determine type of script #12833
    Vite reports this:
    
       @quasar/extras doesn't appear to be written in CJS, but also doesn't appear
       to be a valid ES module (i.e. it doesn't have "type": "module" or an .mjs
       extension for the entry point). Please contact the package author to fix.

--- extras/index.js
+++ extras/index.js
@@ -1,2 +1,5 @@
-// empty file
+// this file serves no purpose other than
 // to help q/app-webpack discover where this package is installed
+
+// also helping Vite's dep analysis the type of script
+module.exports = ''
