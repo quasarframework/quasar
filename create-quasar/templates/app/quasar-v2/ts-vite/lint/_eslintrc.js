5f8a2dcf1488678cdeffadf47b145b5234e91e74
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-03-23 12:47:41 +0300

    fix(create-quasar): Remove unused variables

--- create-quasar/templates/app/quasar-v2/ts-vite/lint/_eslintrc.js
+++ create-quasar/templates/app/quasar-v2/ts-vite/lint/_eslintrc.js
@@ -1,5 +1,3 @@
-const { resolve } = require('path')
-
 module.exports = {
   // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
   // This option interrupts the configuration hierarchy at this file
