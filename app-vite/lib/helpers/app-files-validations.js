fcee17c29a17b75ea70af15595d7935debda3afc
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-04-03 14:43:16 +0300

    fix(app-vite): check the existence of /index.html before trying to read it

--- app-vite/lib/helpers/app-files-validations.js
+++ app-vite/lib/helpers/app-files-validations.js
@@ -4,12 +4,17 @@ const { warn } = require('./logger')
 const appPaths = require('../app-paths')
 const { entryPointMarkup, attachMarkup } = require('../helpers/html-template')
 
+const file = appPaths.resolve.app('index.html')
+
 module.exports = function (_cfg) {
-  let file
   let content
   let valid = true
 
-  file = appPaths.resolve.app('index.html')
+  if (fs.existsSync(file) === false) {
+    warn(`The file /index.html is missing. Please add it back.\n`)
+    return false
+  }
+
   content = fs.readFileSync(file, 'utf-8')
 
   if (content.indexOf(attachMarkup) !== -1) {
