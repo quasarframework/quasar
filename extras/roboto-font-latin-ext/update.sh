6a742b85cd587e38666d7e53b6ad62bee05104cb
Ulrich <ulrichhuet@hotmail.com>
2022-04-12 11:03:00 +0200

    feat(font): added font-display on roboto fonts

--- extras/roboto-font-latin-ext/update.sh
+++ extras/roboto-font-latin-ext/update.sh
@@ -12,7 +12,7 @@ AGENT_WOFF="Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko"
 AGENT_WOFF2="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"
 
 # download css as IE11 for .woff
-wget 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&subset=latin-ext' -O - --header="User-Agent: ${AGENT_WOFF}" | \
+wget 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&subset=latin-ext&display=optional' -O - --header="User-Agent: ${AGENT_WOFF}" | \
   sed "s/local('.*'), //" > $FILE
 
 # get links dirname
