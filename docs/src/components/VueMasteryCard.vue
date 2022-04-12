6ddeb9d1b500a76367c569c725bab7247de96ca3
Calson Chiatiah <c.chiatiah@dreamonkey.com>
2022-04-12 10:19:58 +0100

    fix(docs/vue-mastery-card): fix padding and change vue-mastery logo

--- docs/src/components/VueMasteryCard.vue
+++ docs/src/components/VueMasteryCard.vue
@@ -3,23 +3,23 @@
     <q-card-section :horizontal="$q.screen.gt.xs">
       <q-card-section :class="$q.screen.xs? 'row justify-between' : 'column justify-center items-center'">
         <a href="https://www.vuemastery.com/">
-          <img src="~assets/homepage/logo-vue-mastery.png" width="93" class="q-mb-sm" alt="Vue Mastery Logo">
+          <img src="~assets/vuemastery-logo.svg" width="93" class="q-mb-sm" alt="Vue Mastery Logo">
         </a>
-        <q-btn label="Know More" color="brand-accent" rounded class="text-size-10" padding="2px sm" :href="to" target="_blank"/>
+        <q-btn label="Know More" color="brand-accent" rounded class="text-size-10 text-weight-bold" padding="2px sm" :href="to" target="_blank"/>
       </q-card-section>
 
       <q-separator :vertical="$q.screen.gt.xs" color="white-12" />
 
-      <q-card-section class="text-size-14">
-        <p class="q-mb-xs text-brand-primary letter-spacing-263 text-weight-bold text-size-14 primary-line-height">
+      <q-card-section class="text-size-14 padding-y-12">
+        <p class="q-mb-none text-brand-primary letter-spacing-263 text-weight-bold text-size-14 primary-line-height">
           Do you want to know more about {{ subject }}?
         </p>
         <p class="q-mb-sm letter-spacing-25 text-white">
           Learn it faster with out partner Vue Mastery and get into Quasar at light-speed
         </p>
-        <p class="q-mb-none text-size-12 text-brand-accent letter-spacing-40">
+        <div class="text-size-12 text-brand-accent letter-spacing-40">
           By attending this course, you will even support our beloved open source framework!
-        </p>
+        </div>
       </q-card-section>
     </q-card-section>
   </q-card>
@@ -46,5 +46,9 @@ export default defineComponent({
 .margin-y-12 {
   margin-bottom: 12px;
   margin-top: 12px;
+}
+.padding-y-12 {
+  padding-bottom: 12px;
+  padding-top: 12px;
 }
 </style>
