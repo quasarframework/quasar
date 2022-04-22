eed85f7e49d3e7231141530231e309025d2599e7
Jeewhan Ryu <distinct19@gmail.com>
2022-04-22 21:35:30 +0900

    chore(create-quasar): fix typo (#13232)

--- create-quasar/templates/app/quasar-v2/ts-vite/lint/_.eslintrc.js
+++ create-quasar/templates/app/quasar-v2/ts-vite/lint/_.eslintrc.js
@@ -120,7 +120,7 @@ module.exports = {
     // in plain CommonJS modules, you can't use `import foo = require('foo')` to pass this rule, so it has to be disabled
     '@typescript-eslint/no-var-requires': 'off',
 
-    // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
+    // The core 'no-unused-vars' rules (in the eslint:recommended ruleset)
     // does not work with type definitions
     'no-unused-vars': 'off',
 
