2f9f5f5a5233ccdf46236544c94c6c3f92f524f1
Dan Popescu <pdan.popescu@gmail.com>
2022-04-22 23:19:50 +0300

    fix(QInput): fix extra space on bottom of autogrow QInput on large texts on one line #13196
    #13196

--- ui/src/components/input/QInput.js
+++ ui/src/components/input/QInput.js
@@ -286,13 +286,16 @@ export default createComponent({
       const inp = inputRef.value
       if (inp !== null) {
         const parentStyle = inp.parentNode.style
+        const { overflow } = inp.style
 
         // reset height of textarea to a small size to detect the real height
         // but keep the total control size the same
         parentStyle.marginBottom = (inp.scrollHeight - 1) + 'px'
         inp.style.height = '1px'
+        inp.style.overflow = 'hidden'
 
         inp.style.height = inp.scrollHeight + 'px'
+        inp.style.overflow = overflow
         parentStyle.marginBottom = ''
       }
     }
