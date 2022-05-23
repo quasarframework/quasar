cfcb23921be20fced87c6af7dfa56a52631a511d
Aldrin Marquez <aldringm321@gmail.com>
2022-05-23 09:00:44 +0800

    fix prefix
    https://github.com/quasarframework/quasar/discussions/13473

--- ui/icon-set/material-symbols-outlined.js
+++ ui/icon-set/material-symbols-outlined.js
@@ -6,128 +6,128 @@
 module.exports = {
   name: 'material-symbols-outlined',
   type: {
-    positive: 'sym_r_check_circle',
-    negative: 'sym_r_warning',
-    info: 'sym_r_info',
-    warning: 'sym_r_priority_high'
+    positive: 'sym_o_check_circle',
+    negative: 'sym_o_warning',
+    info: 'sym_o_info',
+    warning: 'sym_o_priority_high'
   },
   arrow: {
-    up: 'sym_r_arrow_upward',
-    right: 'sym_r_arrow_forward',
-    down: 'sym_r_arrow_downward',
-    left: 'sym_r_arrow_back',
-    dropdown: 'sym_r_arrow_drop_down'
+    up: 'sym_o_arrow_upward',
+    right: 'sym_o_arrow_forward',
+    down: 'sym_o_arrow_downward',
+    left: 'sym_o_arrow_back',
+    dropdown: 'sym_o_arrow_drop_down'
   },
   chevron: {
-    left: 'sym_r_chevron_left',
-    right: 'sym_r_chevron_right'
+    left: 'sym_o_chevron_left',
+    right: 'sym_o_chevron_right'
   },
   colorPicker: {
-    spectrum: 'sym_r_gradient',
-    tune: 'sym_r_tune',
-    palette: 'sym_r_style'
+    spectrum: 'sym_o_gradient',
+    tune: 'sym_o_tune',
+    palette: 'sym_o_style'
   },
   pullToRefresh: {
-    icon: 'sym_r_refresh'
+    icon: 'sym_o_refresh'
   },
   carousel: {
-    left: 'sym_r_chevron_left',
-    right: 'sym_r_chevron_right',
-    up: 'sym_r_keyboard_arrow_up',
-    down: 'sym_r_keyboard_arrow_down',
-    navigationIcon: 'sym_r_lens'
+    left: 'sym_o_chevron_left',
+    right: 'sym_o_chevron_right',
+    up: 'sym_o_keyboard_arrow_up',
+    down: 'sym_o_keyboard_arrow_down',
+    navigationIcon: 'sym_o_lens'
   },
   chip: {
-    remove: 'sym_r_cancel',
-    selected: 'sym_r_check'
+    remove: 'sym_o_cancel',
+    selected: 'sym_o_check'
   },
   datetime: {
-    arrowLeft: 'sym_r_chevron_left',
-    arrowRight: 'sym_r_chevron_right',
-    now: 'sym_r_schedule',
-    today: 'sym_r_today'
+    arrowLeft: 'sym_o_chevron_left',
+    arrowRight: 'sym_o_chevron_right',
+    now: 'sym_o_schedule',
+    today: 'sym_o_today'
   },
   editor: {
-    bold: 'sym_r_format_bold',
-    italic: 'sym_r_format_italic',
-    strikethrough: 'sym_r_strikethrough_s',
-    underline: 'sym_r_format_underlined',
-    unorderedList: 'sym_r_format_list_bulleted',
-    orderedList: 'sym_r_format_list_numbered',
-    subscript: 'sym_r_vertical_align_bottom',
-    superscript: 'sym_r_vertical_align_top',
-    hyperlink: 'sym_r_link',
-    toggleFullscreen: 'sym_r_fullscreen',
-    quote: 'sym_r_format_quote',
-    left: 'sym_r_format_align_left',
-    center: 'sym_r_format_align_center',
-    right: 'sym_r_format_align_right',
-    justify: 'sym_r_format_align_justify',
-    print: 'sym_r_print',
-    outdent: 'sym_r_format_indent_decrease',
-    indent: 'sym_r_format_indent_increase',
-    removeFormat: 'sym_r_format_clear',
-    formatting: 'sym_r_text_format',
-    fontSize: 'sym_r_format_size',
-    align: 'sym_r_format_align_left',
-    hr: 'sym_r_remove',
-    undo: 'sym_r_undo',
-    redo: 'sym_r_redo',
-    heading: 'sym_r_format_size',
-    code: 'sym_r_code',
-    size: 'sym_r_format_size',
-    font: 'sym_r_font_download',
-    viewSource: 'sym_r_code'
+    bold: 'sym_o_format_bold',
+    italic: 'sym_o_format_italic',
+    strikethrough: 'sym_o_strikethrough_s',
+    underline: 'sym_o_format_underlined',
+    unorderedList: 'sym_o_format_list_bulleted',
+    orderedList: 'sym_o_format_list_numbered',
+    subscript: 'sym_o_vertical_align_bottom',
+    superscript: 'sym_o_vertical_align_top',
+    hyperlink: 'sym_o_link',
+    toggleFullscreen: 'sym_o_fullscreen',
+    quote: 'sym_o_format_quote',
+    left: 'sym_o_format_align_left',
+    center: 'sym_o_format_align_center',
+    right: 'sym_o_format_align_right',
+    justify: 'sym_o_format_align_justify',
+    print: 'sym_o_print',
+    outdent: 'sym_o_format_indent_decrease',
+    indent: 'sym_o_format_indent_increase',
+    removeFormat: 'sym_o_format_clear',
+    formatting: 'sym_o_text_format',
+    fontSize: 'sym_o_format_size',
+    align: 'sym_o_format_align_left',
+    hr: 'sym_o_remove',
+    undo: 'sym_o_undo',
+    redo: 'sym_o_redo',
+    heading: 'sym_o_format_size',
+    code: 'sym_o_code',
+    size: 'sym_o_format_size',
+    font: 'sym_o_font_download',
+    viewSource: 'sym_o_code'
   },
   expansionItem: {
-    icon: 'sym_r_keyboard_arrow_down',
-    denseIcon: 'sym_r_arrow_drop_down'
+    icon: 'sym_o_keyboard_arrow_down',
+    denseIcon: 'sym_o_arrow_drop_down'
   },
   fab: {
-    icon: 'sym_r_add',
-    activeIcon: 'sym_r_close'
+    icon: 'sym_o_add',
+    activeIcon: 'sym_o_close'
   },
   field: {
-    clear: 'sym_r_cancel',
-    error: 'sym_r_error'
+    clear: 'sym_o_cancel',
+    error: 'sym_o_error'
   },
   pagination: {
-    first: 'sym_r_first_page',
-    prev: 'sym_r_keyboard_arrow_left',
-    next: 'sym_r_keyboard_arrow_right',
-    last: 'sym_r_last_page'
+    first: 'sym_o_first_page',
+    prev: 'sym_o_keyboard_arrow_left',
+    next: 'sym_o_keyboard_arrow_right',
+    last: 'sym_o_last_page'
   },
   rating: {
-    icon: 'sym_r_grade'
+    icon: 'sym_o_grade'
   },
   stepper: {
-    done: 'sym_r_check',
-    active: 'sym_r_edit',
-    error: 'sym_r_warning'
+    done: 'sym_o_check',
+    active: 'sym_o_edit',
+    error: 'sym_o_warning'
   },
   tabs: {
-    left: 'sym_r_chevron_left',
-    right: 'sym_r_chevron_right',
-    up: 'sym_r_keyboard_arrow_up',
-    down: 'sym_r_keyboard_arrow_down'
+    left: 'sym_o_chevron_left',
+    right: 'sym_o_chevron_right',
+    up: 'sym_o_keyboard_arrow_up',
+    down: 'sym_o_keyboard_arrow_down'
   },
   table: {
-    arrowUp: 'sym_r_arrow_upward',
-    warning: 'sym_r_warning',
-    firstPage: 'sym_r_first_page',
-    prevPage: 'sym_r_chevron_left',
-    nextPage: 'sym_r_chevron_right',
-    lastPage: 'sym_r_last_page'
+    arrowUp: 'sym_o_arrow_upward',
+    warning: 'sym_o_warning',
+    firstPage: 'sym_o_first_page',
+    prevPage: 'sym_o_chevron_left',
+    nextPage: 'sym_o_chevron_right',
+    lastPage: 'sym_o_last_page'
   },
   tree: {
-    icon: 'sym_r_play_arrow'
+    icon: 'sym_o_play_arrow'
   },
   uploader: {
-    done: 'sym_r_done',
-    clear: 'sym_r_close',
-    add: 'sym_r_add_box',
-    upload: 'sym_r_cloud_upload',
-    removeQueue: 'sym_r_clear_all',
-    removeUploaded: 'sym_r_done_all'
+    done: 'sym_o_done',
+    clear: 'sym_o_close',
+    add: 'sym_o_add_box',
+    upload: 'sym_o_cloud_upload',
+    removeQueue: 'sym_o_clear_all',
+    removeUploaded: 'sym_o_done_all'
   }
 }
