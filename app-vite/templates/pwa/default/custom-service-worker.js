71e79eb709c9544db9b7dfda60304eecc0c0d0db
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-06-09 12:57:46 +0300

    feat(app-vite&app-webpack): Add TS template for PWA mode (#13624)

--- app-vite/templates/pwa/default/custom-service-worker.js
+++ app-vite/templates/pwa/default/custom-service-worker.js
@@ -1,3 +1,5 @@
+/* eslint-env serviceworker */
+
 /*
  * This file (which will be your service worker)
  * is picked up by the build system ONLY if
---
+++ app-vite/templates/pwa/ts/pwa-env.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    SERVICE_WORKER_FILE: string;
+    PWA_FALLBACK_HTML: string;
+  }
+}
