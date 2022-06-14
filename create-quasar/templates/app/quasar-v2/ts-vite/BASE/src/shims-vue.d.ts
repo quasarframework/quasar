7952781baf37e1b30cb734120130098c4f728a58
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-06-14 12:10:33 +0300

    style(create-quasar): Format shims-vue.d.ts on ts-vite

--- create-quasar/templates/app/quasar-v2/ts-vite/BASE/src/shims-vue.d.ts
+++ create-quasar/templates/app/quasar-v2/ts-vite/BASE/src/shims-vue.d.ts
@@ -5,8 +5,7 @@
 
 // Mocks all files ending in `.vue` showing them as plain Vue instances
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
