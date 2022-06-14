7809bad475d05e4a44ffb2814bbf28333366b15e
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-06-14 12:13:33 +0300

    refactor(create-quasar): Remove vue/ref-macros reference It was used for reactivity transform macros, but that feature is still experimental and is behind a feature flag. So, the users can add the reference along with the configuration to avoid polluting the globals. Also, the new name for this is "vue/macros-global", see: https://github.com/vuejs/core/blob/bdffc143ef3aa27c347b22f19d0052194b54836e/packages/vue/ref-macros.d.ts\#L1

--- create-quasar/templates/app/quasar-v2/ts-vite/BASE/src/shims-vue.d.ts
+++ create-quasar/templates/app/quasar-v2/ts-vite/BASE/src/shims-vue.d.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
 
 /// <reference types="vite/client" />
-/// <reference types="vue/ref-macros" />
 
 // Mocks all files ending in `.vue` showing them as plain Vue instances
 declare module '*.vue' {
