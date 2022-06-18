4b4a57388434cda88bad337c8a4d9da052232968
Popescu Dan <pdan.popescu@gmail.com>
2022-06-18 19:39:08 +0300

    fix(QSelect): fix menu scroll when options list length increases #13700, #13694 (#13711)

--- ui/dev/src/pages/form/select-part-9-dynamic.vue
+++ ui/dev/src/pages/form/select-part-9-dynamic.vue
@@ -2,7 +2,7 @@
   <div class="q-layout-padding column q-gutter-y-lg">
     <q-select
       style="width: 300px"
-      v-model="multiple"
+      v-model="multiple1"
       multiple
       :options="filteredOptions"
       filled
@@ -12,37 +12,90 @@
 
     <q-select
       style="width: 300px"
-      v-model="multiple"
+      v-model="multiple1"
       multiple
       :options="filteredOptions"
       filled
       behavior="dialog"
       label="Multiple - Dialog - google filters"
     />
+
+    <q-select
+      style="width: 300px"
+      v-model="multiple2"
+      multiple
+      :options="options"
+      filled
+      behavior="menu"
+      label="Multiple - Menu - dynamic loading"
+      :loading="loading"
+      @virtual-scroll="onScroll"
+    />
+
+    <q-select
+      style="width: 300px"
+      v-model="multiple2"
+      multiple
+      :options="options"
+      filled
+      behavior="dialog"
+      label="Multiple - Dialog - dynamic loading"
+      :loading="loading"
+      @virtual-scroll="onScroll"
+    />
   </div>
 </template>
 
 <script>
 const optionNames = [ 'Google', 'Twitter', 'Facebook', 'Apple', 'Oracle' ]
-const options = Array(100).fill(null).reduce((acc, _, i) => {
+const options = Array(100000).fill(null).reduce((acc, _, i) => {
   optionNames.forEach(n => {
     acc.push(`${n} - ${i}`)
   })
-
   return acc
 }, [])
+const pageSize = 50
+const lastPage = Math.ceil(options.length / pageSize)
 
 export default {
   data: () => ({
-    multiple: []
+    multiple1: [],
+    multiple2: null,
+
+    nextPage: 2,
+    loading: false
+
   }),
+
   computed: {
     filteredOptions () {
-      if (this.multiple.some(x => x.indexOf('Google') > -1)) {
+      if (this.multiple1.some(x => x.indexOf('Google') > -1)) {
         return options.filter(x => x.indexOf('Twitter') === -1 && x.indexOf('Apple') === -1 && x.indexOf('Oracle') === -1)
       }
-
       return options
+    },
+
+    options () {
+      return options.slice(0, pageSize * (this.nextPage - 1))
+    }
+  },
+
+  methods: {
+    onScroll ({ to, ref }) {
+      const lastIndex = this.options.length - 1
+
+      if (this.loading !== true && this.nextPage < lastPage && to === lastIndex) {
+        this.loading = true
+
+        setTimeout(() => {
+          this.nextPage++
+
+          this.$nextTick(() => {
+            ref.refresh()
+            this.loading = false
+          })
+        }, 500)
+      }
     }
   }
 }
