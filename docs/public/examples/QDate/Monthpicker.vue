0405913efaacb00b6e176e42a69f5d66b6de8974
NicoP-S <paetni1@gmail.com>
2022-06-22 23:29:42 +0200

    qDate Monthpicker example

---
+++ docs/public/examples/QDate/Monthpicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="q-pa-md">
+    <div>Date Model: {{ date }}</div>
+    <q-date v-model="date" years-in-month-view default-view="Months" emit-immediately
+      @update:model-value="onUpdateMv"
+      :key="dpKey" minimal mask="YYYY/MM" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const date = ref('2022/02')
+    const dpKey = ref(Date.now())
+
+    function onUpdateMv (v) {
+      dpKey.value = Date.now()
+    }
+    return {
+      dpKey,
+      onUpdateMv,
+      date
+    }
+  }
+}
+</script>
