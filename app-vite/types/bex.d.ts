a3014846690112bba59018fc61c763dcad7e88c5
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-06-30 14:33:51 +0300

    feat(app-vite): Improve BEX TS types

--- app-vite/types/bex.d.ts
+++ app-vite/types/bex.d.ts
@@ -1,5 +1,23 @@
-export interface GlobalQuasarBex {
-  send(eventName: string, payload?: object): Promise<any>;
-  on(eventName: string, eventListener: (payload?: object) => any): void;
-  off(eventName: string, eventListener: (payload?: object) => any): void;
+import type { EventEmitter } from 'events';
+
+type BexObjectMessage = { event: string; payload: any };
+type BexMessage = string | BexObjectMessage;
+interface BexWall {
+  listen (callback: (messages: BexMessage | BexMessage[]) => void): void;
+  send (data: BexObjectMessage[]): void;
 }
+
+type BexEventPayload = { data: any; eventResponseKey: string; };
+type BexEventListener = (payload: BexEventPayload) => void;
+export interface BexBridge extends EventEmitter {
+  constructor(wall: BexWall): BexBridge;
+
+  send(eventName: string, payload?: Record<string, any>): Promise<any>;
+  getEvents(): { [eventName: string]: (payload?: any) => void };
+
+  on(eventName: string | symbol, listener: BexEventListener): this;
+  once(eventName: string | symbol, listener: BexEventListener): this;
+  off(eventName: string | symbol, listener: BexEventListener): this;
+}
+
+export type GlobalQuasarBex = BexBridge;
