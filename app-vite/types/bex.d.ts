1bc6e2f2872a4239dfea9ec869e7908c4e45508d
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-07-02 13:39:52 +0300

    feat(app-vite): Improve TSDocs

--- app-vite/types/bex.d.ts
+++ app-vite/types/bex.d.ts
@@ -19,14 +19,20 @@ interface BexWall {
  *   }
  * }
  *
- * bridge.send('without-payload-and-response')
+ * bridge.send('without-payload-and-response');
  *
  * bridge.on('with-payload-without-response', ({ data }) => {
  *   data // type: { test: number[] }
- * })
+ * });
  *
+ * bridge.on('with-payload-and-response', ({ data, respond }) => {
+ *   const { foo } = data; // { foo: ['a', 'b'] }
+ *
+ *   const result = foo[0].charCodeAt() + foo[1].charCodeAt(); // 97 + 98
+ *   void respond(result);
+ * });
  * const response = await bridge.send('with-payload-and-response', { foo: ['a', 'b'] });
- * console.log(typeof response) // 'number'
+ * console.log(response); // 195
  */
 export interface BexEventMap {}
 
@@ -44,7 +50,12 @@ type BexEventResponse<T extends BexEventName> = BexEventEntry<T>[1];
 
 type BexPayload<TData, TResponse> = {
   data: TData;
+  /** @deprecated Use {@link BexPayload.respond} instead */
   eventResponseKey: string;
+  /**
+   * Calling this will resolve the Promise of the `send()` call.
+   * You can use this to communicate back with the sender.
+   */
   respond: (
     ...payload: TResponse extends never ? [] : [TResponse]
   ) => Promise<BexPayload<TData, TResponse>>;
