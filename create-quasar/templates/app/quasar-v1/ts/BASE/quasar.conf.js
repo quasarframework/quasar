630996934064cfe584d756982a2a135765ffb50d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-07-04 15:26:18 +0300

    chore(create-quasar): Bump version

--- create-quasar/templates/app/quasar-v1/ts/BASE/quasar.conf.js
+++ create-quasar/templates/app/quasar-v1/ts/BASE/quasar.conf.js
@@ -17,7 +17,10 @@ module.exports = configure(function (/* ctx */) {
     // https://v1.quasar.dev/quasar-cli/supporting-ts
     supportTS: <% if (preset.lint) { %>{
       tsCheckerConfig: {
-        eslint: true
+        eslint: {
+          enabled: true,
+          files: './src/**/*.{ts,tsx,js,jsx,vue}',
+        },
       }
     }<% } else { %>true<% } %>,
 
