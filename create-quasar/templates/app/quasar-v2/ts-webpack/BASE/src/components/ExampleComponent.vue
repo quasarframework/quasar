9cfd43312d40fc701a6ae81b322c61968485214e
AnJ-Github <gumaa95@gmail.com>
2022-07-14 11:07:57 +0200

    Update create-quasar/templates/app/quasar-v2/ts-webpack/BASE/src/components/ExampleComponent.vue
    Co-authored-by: Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>

--- create-quasar/templates/app/quasar-v2/ts-webpack/BASE/src/components/ExampleComponent.vue
+++ create-quasar/templates/app/quasar-v2/ts-webpack/BASE/src/components/ExampleComponent.vue
@@ -28,7 +28,7 @@ const props = withDefaults(defineProps<Props>(), {
 
 const clickCount = ref(0);
 function increment() {
-  clickCount.value += 1
+  clickCount.value += 1;
   return clickCount.value;
 }
 
