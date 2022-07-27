0d6070122dffbb95f11686d650e297a92a3567af
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-07-27 16:36:05 +0300

    Update use-btn.js

--- ui/src/components/btn/use-btn.js
+++ ui/src/components/btn/use-btn.js
@@ -1,8 +1,8 @@
 import { computed } from 'vue'
 
 import useAlign, { useAlignProps } from '../../composables/private/use-align.js'
-import useRouterLink, { useRouterLinkProps } from '../../composables/private/use-router-link.js'
 import useSize, { useSizeProps } from '../../composables/private/use-size.js'
+import useRouterLink, { useRouterLinkProps } from '../../composables/private/use-router-link.js'
 
 const padding = {
   none: 0,
