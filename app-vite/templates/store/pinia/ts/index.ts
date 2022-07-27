2a14af483af162dbae57ac87897e7ae8328fac0d
Rashid Pathiyil <rashidpathiyil@gmail.com>
2022-07-27 12:30:30 +0530

    Extend the router interface in pinia

--- app-vite/templates/store/pinia/ts/index.ts
+++ app-vite/templates/store/pinia/ts/index.ts
@@ -1,6 +1,17 @@
 import { store } from 'quasar/wrappers'
 import { createPinia } from 'pinia'
+import { Router } from 'vue-router';
 
+/*
+ * When adding new properties to stores, you should
+ * also extend the PiniaCustomProperties interface.
+ */
+
+declare module 'pinia' {
+  export interface PiniaCustomProperties {
+    readonly router: Router;
+  }
+}
 /*
  * If not building with SSR mode, you can
  * directly export the Store instantiation;
