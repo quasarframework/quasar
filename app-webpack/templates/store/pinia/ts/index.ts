81b471b6f7a626e33d43784ebe89c50e90e2bcbb
Rashid Pathiyil <rashidpathiyil@gmail.com>
2022-07-27 19:35:05 +0530

    fix(app-vite&app-webpack): Add type for router in Pinia (fix #14061) (#14062)
    Co-authored-by: Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>

--- app-webpack/templates/store/pinia/ts/index.ts
+++ app-webpack/templates/store/pinia/ts/index.ts
@@ -1,5 +1,17 @@
 import { store } from 'quasar/wrappers'
 import { createPinia } from 'pinia'
+import { Router } from 'vue-router';
+
+/*
+ * When adding new properties to stores, you should also
+ * extend the `PiniaCustomProperties` interface.
+ * @see https://pinia.vuejs.org/core-concepts/plugins.html#typing-new-store-properties
+ */
+declare module 'pinia' {
+  export interface PiniaCustomProperties {
+    readonly router: Router;
+  }
+}
 
 /*
  * If not building with SSR mode, you can
