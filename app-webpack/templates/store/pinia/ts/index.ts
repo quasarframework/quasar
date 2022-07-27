c15958bfe6632e444ec96f634f383548c7ef6af4
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-07-27 17:03:19 +0300

    Update app-webpack/templates/store/pinia/ts/index.ts

--- app-webpack/templates/store/pinia/ts/index.ts
+++ app-webpack/templates/store/pinia/ts/index.ts
@@ -3,10 +3,10 @@ import { createPinia } from 'pinia'
 import { Router } from 'vue-router';
 
 /*
- * When adding new properties to stores, you should
- * also extend the PiniaCustomProperties interface.
+ * When adding new properties to stores, you should also
+ * extend the `PiniaCustomProperties` interface.
+ * @see https://pinia.vuejs.org/core-concepts/plugins.html#typing-new-store-properties
  */
-
 declare module 'pinia' {
   export interface PiniaCustomProperties {
     readonly router: Router;
