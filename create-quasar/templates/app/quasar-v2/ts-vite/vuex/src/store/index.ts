06bb3d8a30c7497e12cb9dd14d248a9b1fbdb35b
Rashid Pathiyil <rashidpathiyil@gmail.com>
2022-07-29 00:18:56 +0530

    Update create-quasar/templates/app/quasar-v2/ts-vite/vuex/src/store/index.ts
    Co-authored-by: Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>

--- create-quasar/templates/app/quasar-v2/ts-vite/vuex/src/store/index.ts
+++ create-quasar/templates/app/quasar-v2/ts-vite/vuex/src/store/index.ts
@@ -36,7 +36,7 @@ declare module '@vue/runtime-core' {
 // provide typings for `useStore` helper
 export const storeKey: InjectionKey<VuexStore<StateInterface>> = Symbol('vuex-key')
 
-// Provide typings for `this.$router` inside Vuex store
+// Provide typings for `this.$router` inside Vuex stores
  declare module "vuex" {
    export interface Store<S> {
      readonly $router: Router;
