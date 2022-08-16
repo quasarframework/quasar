18ffce14f47b50a4aa986f757934cf63605e302b
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-08-16 14:24:08 +0300

    chore(create-quasar): Update renderTemplate explanation

--- create-quasar/utils/index.js
+++ create-quasar/utils/index.js
@@ -59,8 +59,8 @@ module.exports.renderTemplate = function (templateDir, scope) {
     const targetRelativePath = rawPath.split('/').map(name => {
       // dotfiles are ignored when published to npm, therefore in templates
       // we need to prefix them with an underscore (e.g. "_.gitignore")
-      // Also, some tools like ESLint excepts valid config files, therefore
-      // we also prefix files like "package.json" too. (e.g. "_.package.json")
+      // Also, some tools like ESLint expect valid config files, therefore
+      // we also prefix files like "package.json" too. (e.g. "_package.json")
       return name.startsWith('_')
         ? name.slice(1)
         : name
