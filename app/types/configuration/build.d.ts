e9f18dd14e77fb7d66c055d5010cb745e0bbe385
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-09-12 14:50:38 +0200

    revert(app/ts): re-add type for vueCompiler option

--- app/types/configuration/build.d.ts
+++ app/types/configuration/build.d.ts
@@ -136,6 +136,8 @@ interface QuasarStaticBuildConfiguration {
    * When providing an object, it represents webpack-bundle-analyzer config options.
    */
   analyze: boolean | BundleAnalyzerPlugin.Options;
+  /** Include vue runtime + compiler version, instead of default Vue runtime-only. */
+  vueCompiler: boolean;
   /**
    * Minification options. [Full list](https://github.com/webpack-contrib/terser-webpack-plugin/#minify).
    */
