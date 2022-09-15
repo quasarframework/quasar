adca7d421e7a27523927d260e87680c036ab108f
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-09-15 17:21:31 +0300

    feat(ui): Offer callable builtin validation rules

--- ui/types/api/validation.d.ts
+++ ui/types/api/validation.d.ts
@@ -1,4 +1,5 @@
 // Keep in sync with ui/src/utils/patterns.js
 export type EmbeddedValidationRule = "date" | "time" | "fulltime" | "timeOrFulltime" | "email" | "hexColor" | "hexaColor" | "hexOrHexaColor" | "rgbColor" | "rgbaColor" | "rgbOrRgbaColor" | "hexOrRgbColor" | "hexaOrRgbaColor" | "anyColor";
 
-export type ValidationRule<T = any> = EmbeddedValidationRule | ((value: T) => boolean | string | Promise<boolean | string>);
+type EmbeddedValidationRuleFn<T = any> = (value: T) => boolean
+export type ValidationRule<T = any> = EmbeddedValidationRule | ((value: T, rules: Record<EmbeddedValidationRule, EmbeddedValidationRuleFn<T>>) => boolean | string | Promise<boolean | string>);
