b98eab57f9773cc28689b1ecf18a93ac74790f7e
Popescu Dan <pdan.popescu@gmail.com>
2022-09-30 19:31:26 +0300

    chore(tabs): (backport from Qv2) add test pages for router navigation (#14526)

---
+++ ui/dev/src/pages/web-tests/tabs-router/One.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Tab <mark>one</mark> Content</div>
+</template>
+
+<script>
+export default {
+  name: 'One',
+
+  props: {
+    allowRouteChange: {
+      type: Boolean
+    }
+  },
+
+  beforeRouteLeave (to, from, next) {
+    next(this.allowRouteChange)
+  }
+}
+</script>
