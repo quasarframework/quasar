dd1fcef3a5960f772011dbe02be3e330babf0be4
Kareem Dabbeet <kareemda1@gmail.com>
2022-10-01 23:56:27 +0300

    test(QBadge): add unit tests

--- ui/src/components/badge/__tests__/QBadge.spec.js
+++ ui/src/components/badge/__tests__/QBadge.spec.js
@@ -1,64 +1,140 @@
-// import { mount } from '@cypress/vue'
-// import QBadge from '../QBadge.js'
+import { mount } from '@cypress/vue'
+import QBadge from '../QBadge'
 
-// const snapshotOptions = { customSnapshotsDir: '../src/components/badge/__tests__' }
+const defaultOptions = {
+  label: 'simple badge'
+}
+
+function mountQBadge (options = {}) {
+  options.props = {
+    ...defaultOptions,
+    ...options.props
+  }
+
+  return mount(QBadge, options)
+}
 
 describe('Badge API', () => {
   describe('Props', () => {
     describe('Category: content', () => {
       describe('(prop): floating', () => {
-        it.skip(' ', () => {
-          //
+        it('should render a floating badge', () => {
+          mountQBadge({
+            props: { floating: true }
+          })
+
+          cy.get('.q-badge')
+            .should('have.class', 'q-badge--floating')
         })
       })
 
       describe('(prop): multi-line', () => {
-        it.skip(' ', () => {
-          //
+        it('should render a content with multiple lines', () => {
+          mountQBadge({
+            props: { multiLine: true }
+          })
+
+          cy.get('.q-badge')
+            .should('have.class', 'q-badge--multi-line')
         })
       })
 
       describe('(prop): label', () => {
-        it.skip(' ', () => {
-          //
+        it('should render a label inside the badge', () => {
+          const label = 'Badge label'
+
+          mountQBadge({
+            props: { label }
+          })
+
+          cy.get('.q-badge')
+            .should('contain', label)
         })
       })
 
       describe('(prop): align', () => {
-        it.skip(' ', () => {
-          //
+        it('should render a badge aligned to the top when align is set to "top"', () => {
+          mountQBadge({
+            props: { align: 'top' }
+          })
+
+          cy.get('.q-badge')
+            .should('have.css', 'vertical-align', 'top')
+        })
+
+        it('should render a badge aligned to the middle when align is set to "middle"', () => {
+          mountQBadge({
+            props: { align: 'middle' }
+          })
+
+          cy.get('.q-badge')
+            .should('have.css', 'vertical-align', 'middle')
+        })
+
+        it('should render a badge aligned to the bottom when align is set to "bottom"', () => {
+          mountQBadge({
+            props: { align: 'bottom' }
+          })
+
+          cy.get('.q-badge')
+            .should('have.css', 'vertical-align', 'bottom')
         })
       })
     })
 
     describe('Category: style', () => {
       describe('(prop): color', () => {
-        it.skip(' ', () => {
-          //
+        it('should change color based on Quasar Color Palette', () => {
+          mountQBadge({
+            props: { color: 'red' }
+          })
+
+          cy.get('.q-badge')
+            .should('have.class', 'bg-red')
         })
       })
 
       describe('(prop): text-color', () => {
-        it.skip(' ', () => {
-          //
+        it('should change text color based on Quasar Color Palette', () => {
+          mountQBadge({
+            props: { textColor: 'red' }
+          })
+
+          cy.get('.q-badge')
+            .should('have.class', 'text-red')
         })
       })
 
       describe('(prop): transparent', () => {
-        it.skip(' ', () => {
-          //
+        it('should have opacity style when "transparent" prop is true', () => {
+          mountQBadge({
+            props: { transparent: true }
+          })
+
+          cy.get('.q-badge')
+            .should('have.class', 'q-badge--transparent')
         })
       })
 
       describe('(prop): outline', () => {
-        it.skip(' ', () => {
-          //
+        it('should have a outline style when "outline" prop is true', () => {
+          mountQBadge({
+            props: { outline: true }
+          })
+
+          cy.get('.q-badge')
+            .should('have.class', 'q-badge--outline')
         })
       })
 
       describe('(prop): rounded', () => {
-        it.skip(' ', () => {
-          //
+        it('should have a rounded style when "rounded" prop is true', () => {
+          mountQBadge({
+            props: { rounded: true }
+          })
+
+          cy.get('.q-badge')
+            .should('have.class', 'q-badge--rounded')
         })
       })
     })
@@ -66,8 +142,21 @@ describe('Badge API', () => {
 
   describe('Slots', () => {
     describe('(slot): default', () => {
-      it.skip(' ', () => {
-        //
+      it('should display the default slot content', () => {
+        const label = 'Badge label'
+
+        mountQBadge({
+          props: {
+            label: undefined
+          },
+
+          slots: {
+            default: label
+          }
+        })
+
+        cy.get('.q-badge')
+          .should('have.text', label)
       })
     })
   })
