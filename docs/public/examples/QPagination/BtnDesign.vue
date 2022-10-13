e9653dafd40228b29824ac547663c010053f7d52
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-10-13 18:50:35 +0300

    feat(docs): update QPagination page with the new features #14577

---
+++ docs/public/examples/QPagination/BtnDesign.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="q-pa-lg">
+    <div class="q-gutter-md">
+      <q-pagination
+        v-model="current"
+        max="5"
+        direction-links
+        flat
+        color="grey"
+        active-color="primary"
+      />
+
+      <q-pagination
+        v-model="current"
+        max="5"
+        direction-links
+        outline
+        color="orange"
+        active-design="unelevated"
+        active-color="brown"
+        active-text-color="orange"
+      />
+
+      <q-pagination
+        v-model="current"
+        max="5"
+        direction-links
+        push
+        color="teal"
+        active-design="push"
+        active-color="orange"
+      />
+
+      <q-pagination
+        v-model="current"
+        :max="5"
+        direction-links
+        unelevated
+        color="black"
+        active-color="purple"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      current: ref(3)
+    }
+  }
+}
+</script>
--- docs/public/examples/QPagination/BtnGutter.vue
+++ docs/public/examples/QPagination/BtnGutter.vue
@@ -3,38 +3,29 @@
     <div class="q-gutter-md">
       <q-pagination
         v-model="current"
-        :max="5"
+        max="5"
         direction-links
-        flat
-        color="grey-4"
-        active-color="primary"
       />
 
       <q-pagination
         v-model="current"
-        :max="5"
+        max="5"
         direction-links
-        outline
-        color="orange"
-        active-color="primary"
+        gutter="sm"
       />
 
       <q-pagination
         v-model="current"
-        :max="5"
+        max="5"
         direction-links
-        push
-        color="teal"
+        gutter="md"
       />
 
       <q-pagination
         v-model="current"
-        :max="5"
+        max="5"
         direction-links
-        unelevated
-        color="black"
-        active-color="yellow"
-        active-text-color="black"
+        gutter="20px"
       />
     </div>
   </div>
@@ -46,7 +37,7 @@ import { ref } from 'vue'
 export default {
   setup () {
     return {
-      current: ref(3)
+      current: ref(2)
     }
   }
 }
