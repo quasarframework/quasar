1a16f3e9a48ec0a046683377fb89445cb03f70de
João Gonçalves <jandretgoncalves@gmail.com>
2022-10-14 15:32:05 +0100

    Add single validation function to forms

--- ui/dev/src/pages/form/form.vue
+++ ui/dev/src/pages/form/form.vue
@@ -1,19 +1,5 @@
 <template>
   <div class="q-pa-md" style="max-width: 500px">
-    <div>{{ native || 'null' }}</div>
-    <div>{{ name || 'null' }}</div>
-    <div>{{ age || 'null' }}</div>
-    <div>{{ modelAsync || 'null' }}</div>
-
-    <q-toggle v-model="show" label="Show form" />
-    <q-toggle v-model="autofocus" label="Autofocus" />
-    <q-toggle v-model="dark" label="Dark" :false-value="null" />
-    <q-toggle v-model="greedy" label="Greedy" />
-    <q-toggle v-model="loading" label="Loading" />
-    <q-toggle v-model="customInput" label="Custom Input" />
-    <q-toggle v-model="titleIsDisabled" label="Disable Title QSelect" />
-    <q-option-group class="q-mb-lg" inline v-model="autofocusEl" dense="dense" :options="autofocusEls" />
-
     <q-form
       v-if="show"
       :autofocus="autofocus"
@@ -27,34 +13,16 @@
       :class="dark ? 'bg-grey-8' : void 0"
     >
       <div class="q-col-gutter-md">
-        <custom-input
-          v-if="customInput"
-          filled
-          v-model="customValue"
-          label="Custom value *"
-          lazy-rules
-          :rules="[val => (val && val.length > 0) || 'Please type something']"
-          hint="This custom input should be validated first on submit"
-        />
-
-        <div>
-          <input v-model="native" :autofocus="autofocusEl === 0">
-        </div>
-
-        <my-comp />
-
-        <q-select
+        <q-input
           ref="title"
-          name="title"
-          v-model="title"
-          :options="titles"
           :dark="dark"
-          :color="dark ? 'yellow' : 'primary'"
           filled
-          :disable="titleIsDisabled"
-          label="Title"
-          :rules="[ val => !!val ]"
-          :autofocus="autofocusEl === 4"
+          v-model="title"
+          label="Your title *"
+          label-color="green"
+          hint="Title"
+          lazy-rules="ondemand"
+          :rules="[ val => val && val.length > 0 || 'Please type something']"
           clearable
         />
 
@@ -66,25 +34,24 @@
           label="Your name *"
           label-color="green"
           hint="Name and surname"
-          lazy-rules
+          lazy-rules="ondemand"
           :rules="[ val => val && val.length > 0 || 'Please type something']"
-          :autofocus="autofocusEl === 1"
           clearable
         />
 
         <q-input
           ref="age"
+          name="age"
           :dark="dark"
           filled
           type="number"
           v-model="age"
           label="Your age * (lazy)"
-          lazy-rules
+          lazy-rules="ondemand"
           :rules="[
             val => val !== null && val !== '' || 'Please type your age',
             val => val > 0 && val < 100 || 'Please type a real age'
           ]"
-          :autofocus="autofocusEl === 2"
           clearable
         />
 
@@ -117,109 +84,19 @@
         <q-toggle :dark="dark" v-model="accept" label="I accept the license and terms" :autofocus="autofocusEl === 3" />
 
         <div>
+          <q-btn class="q-mr-sm" label="Validate Age" @click="validateAgeField" />
           <q-btn label="Submit" type="submit" color="primary" :loading="loading" />
           <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" :loading="loading" />
         </div>
       </div>
     </q-form>
 
-    <div class="q-mt-xl q-pa-sm bg-grey-2 rounded-borders">
-      <q-toggle v-model="nativeSubmit" label="Use native submit (else it calls onSubmit)" />
-    </div>
-
-    <q-form
-      class="q-pa-md"
-      autocomplete="on"
-      :class="dark ? 'bg-grey-8' : void 0"
-      v-on="formListeners"
-      action="http://localhost:4444/upload"
-      method="post"
-      enctype="multipart/form-data"
-      target="wind1"
-    >
-      <div class="q-col-gutter-md">
-        <div class="q-gutter-md">
-          <q-badge :label="user || 'N/A'" />
-          <q-badge :label="pwd || 'N/A'" />
-        </div>
-        <q-select
-          name="title"
-          v-model="title"
-          :options="titles"
-          :dark="dark"
-          :color="dark ? 'yellow' : 'primary'"
-          filled
-          label="Title"
-          :rules="[ val => !!val ]"
-        />
-        <q-input
-          name="user"
-          v-model="user"
-          :dark="dark"
-          :color="dark ? 'yellow' : 'primary'"
-          filled
-          label="Username"
-          autocomplete="username"
-          :rules="[ val => !!val ]"
-        />
-        <q-input
-          name="password"
-          v-model="pwd"
-          :dark="dark"
-          :color="dark ? 'yellow' : 'primary'"
-          filled
-          type="password"
-          label="Password"
-          autocomplete="current-password"
-          :rules="[ val => !!val ]"
-        />
-        <div>
-          <q-btn label="Submit" type="submit" color="primary" />
-        </div>
-      </div>
-    </q-form>
   </div>
 </template>
 
 <script>
-import { h } from 'vue'
-import { QField, QCard, QCardSection, QFormChildMixin } from 'quasar'
 
 export default {
-  components: {
-    customInput: {
-      props: [ 'modelValue' ],
-      render () {
-        return h(QField, {
-          modelValue: this.modelValue,
-          stackLabel: true
-        }, {
-          control: () => this.modelValue || 'null'
-        })
-      }
-    },
-
-    myComp: {
-      mixins: [ QFormChildMixin ],
-
-      render () {
-        return h('div', {}, [
-          h(QCard, {
-            class: 'text-subtitle2',
-            bordered: true,
-            flat: true
-          }, () => h(QCardSection, () => [ 'a custom component' ]))
-        ])
-      },
-
-      methods: {
-        validate () {
-          console.log('called my-comp.validate()')
-          return true
-        }
-      }
-    }
-  },
   data () {
     return {
       loading: false,
@@ -244,7 +121,7 @@ export default {
       autofocusEl: 1,
 
       dark: null,
-      greedy: false,
+      greedy: true,
 
       titles: [ 'Mr.', 'Ms.' ],
 
@@ -279,6 +156,10 @@ export default {
           resolve(!!val || '* Required')
         }, 1000)
       })
+    },
+
+    validateAgeField () {
+      this.$refs.form.validateField('age')
     },
 
     onSubmit (evt) {
