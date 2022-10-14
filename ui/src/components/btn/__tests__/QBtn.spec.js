e42af1ee9161b58bb6f5b30635e64caa37e4d37d
Kareem Dabbeet <kareemda1@gmail.com>
2022-10-14 18:09:34 +0300

    test(QBtn): add unit-test for Qbtn.js - use-btn.js

--- ui/src/components/btn/__tests__/QBtn.spec.js
+++ ui/src/components/btn/__tests__/QBtn.spec.js
@@ -1,23 +1,65 @@
+import { mount } from '@cypress/vue'
+import QBtn from '../QBtn.js'
+
+const defaultOptions = {
+  label: 'simple Btn'
+}
+
+function mountQBtn (options = {}) {
+  options.props = {
+    ...defaultOptions,
+    ...options.props
+  }
+
+  return mount(QBtn, options)
+}
+
 describe('Btn API', () => {
   describe('Props', () => {
     describe('Category: behavior', () => {
       describe('(prop): percentage', () => {
-        it.skip(' ', () => {
-          //
+        it('should render a button with a percentage when "loading" prop is set to true', () => {
+          const percentage = 50
+
+          mountQBtn({
+            props: {
+              percentage,
+              loading: true
+            }
+          })
+
+          cy.get('.q-btn').should('have.attr', 'aria-valuenow', percentage)
         })
       })
 
       describe('(prop): dark-percentage', () => {
-        it.skip(' ', () => {
-          //
+        it('should render a button with a dark percentage when "loading" prop is set to true', () => {
+          const percentage = 50
+
+          mountQBtn({
+            props: {
+              percentage,
+              loading: true,
+              darkPercentage: true
+            }
+          })
+
+          cy.get('.q-btn').should('have.attr', 'aria-valuenow', percentage)
+            .get('.q-btn__progress').should('have.class', 'q-btn__progress--dark')
         })
       })
     })
 
     describe('Category: style', () => {
       describe('(prop): round', () => {
-        it.skip(' ', () => {
-          //
+        it('should render a circle shaped button when "round" prop is set to true', () => {
+          mountQBtn({
+            props: {
+              round: true
+            }
+          })
+
+          cy.get('.q-btn').should('have.class', 'q-btn--round')
         })
       })
     })
@@ -25,30 +67,61 @@ describe('Btn API', () => {
 
   describe('Slots', () => {
     describe('(slot): default', () => {
-      it.skip(' ', () => {
-        //
+      it('should render a button with a label', () => {
+        mountQBtn()
+
+        cy.get('.q-btn').should('contain', defaultOptions.label)
       })
     })
 
     describe('(slot): loading', () => {
-      it.skip(' ', () => {
-        //
+      it('should render a button with a loading slot', () => {
+        const loadingSlot = 'loading slot'
+
+        mountQBtn({
+          props: {
+            loading: true
+          },
+          slots: {
+            loading: loadingSlot
+          }
+        })
+
+        cy.get('.q-btn').contains(loadingSlot)
       })
     })
   })
 
   describe('Events', () => {
     describe('(event): click', () => {
-      it.skip(' ', () => {
-        //
+      it('should emit a click event when the button is clicked', () => {
+        const fn = cy.stub()
+
+        mountQBtn({
+          props: {
+            onClick: fn
+          }
+        })
+
+        cy.get('.q-btn').click()
+          .then(() => expect(fn).to.be.calledOnce)
       })
     })
   })
 
   describe('Methods', () => {
     describe('(method): click', () => {
-      it.skip(' ', () => {
-        //
+      it('should click the button', () => {
+        const fn = cy.stub()
+
+        mountQBtn({
+          props: {
+            onClick: fn
+          }
+        })
+
+        cy.get('.q-btn').invoke('click')
+          .then(() => expect(fn).to.be.calledOnce)
       })
     })
   })
