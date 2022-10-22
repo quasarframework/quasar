f1cbca2aeb4e0eecb897737a9ff03114320d18b9
Popescu Dan <pdan.popescu@gmail.com>
2022-10-22 19:20:12 +0300

    fix(mask): trim input to the length of the mask (if mask is not reverse) to avoid regex lock #14558 (#14684)
    close #14558

--- ui/src/components/input/use-mask.js
+++ ui/src/components/input/use-mask.js
@@ -186,7 +186,7 @@ export default function (props, emit, emitValue, inputRef) {
 
     computedMask = mask
     computedUnmask = val => {
-      const unmaskMatch = unmaskMatcher.exec(val)
+      const unmaskMatch = unmaskMatcher.exec(props.reverseFillMask === true ? val : val.slice(0, mask.length))
       if (unmaskMatch !== null) {
         val = unmaskMatch.slice(1).join('')
       }
