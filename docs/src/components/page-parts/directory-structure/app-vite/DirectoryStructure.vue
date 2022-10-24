398f8ba7eade3f3cbf9c735f5e0aa368a23f852d
Bykov Vasiliy <bykov.vasiliy.s@gmail.com>
2022-10-24 13:53:36 +0300

    docs: Make directory structure pages interactive (fix: #13993) (#14697)
    Co-authored-by: Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>

---
+++ docs/src/components/page-parts/directory-structure/app-vite/DirectoryStructure.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+base-directory-structure(:structure="structure")
+</template>
+
+<script>
+import BaseDirectoryStructure from '../BaseDirectoryStructure.vue'
+import structure from './structure'
+
+export default {
+  name: 'AppViteDirectoryStructure',
+
+  components: {
+    BaseDirectoryStructure
+  },
+
+  setup () {
+    return {
+      structure
+    }
+  }
+}
+</script>
