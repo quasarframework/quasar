3dd645133afd3cbdf824d5d5a3fade0ded4655e7
Dan Popescu <pdan.popescu@gmail.com>
2022-12-02 22:00:08 +0200

    fix(QInput): set margin first and then decrease height to keep parent scroll (regressing from fixing firefox undo) #15023

--- ui/src/components/input/QInput.js
+++ ui/src/components/input/QInput.js
@@ -309,8 +309,8 @@ export default createComponent({
           // but keep the total control size the same
           // Firefox rulez #14263, #14344
           $q.platform.is.firefox !== true && (inp.style.overflow = 'hidden')
-          inp.style.height = '1px'
           parentStyle.marginBottom = (inp.scrollHeight - 1) + 'px'
+          inp.style.height = '1px'
 
           inp.style.height = inp.scrollHeight + 'px'
           inp.style.overflow = overflow
