02f08f5359273da91e33688deb8e8c229380357a
Tuğberk Kılıç <kilictugberk@gmail.com>
2022-12-17 16:57:28 +0300

    chore(ts-vite): fix lint warning

--- create-quasar/templates/app/quasar-v2/ts-vite/i18n/src/boot/i18n.ts
+++ create-quasar/templates/app/quasar-v2/ts-vite/i18n/src/boot/i18n.ts
@@ -5,11 +5,11 @@ import messages from 'src/i18n';
 
 export type MessageLanguages = keyof typeof messages;
 // Type-define 'en-US' as the master schema for the resource
-export type MessageSchema = typeof messages["en-US"];
+export type MessageSchema = typeof messages['en-US'];
 
 // See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
 /* eslint-disable @typescript-eslint/no-empty-interface */
-declare module "vue-i18n" {
+declare module 'vue-i18n' {
   // define the locale messages schema
   export interface DefineLocaleMessage extends MessageSchema {}
 
