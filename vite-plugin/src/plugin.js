27eade83622c4408185fb96192dbbccf99d50f43
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-12-17 10:59:05 +0200

    feat(vite-plugin): support for Sass/Scss <style module> #14847

--- vite-plugin/src/plugin.js
+++ vite-plugin/src/plugin.js
@@ -39,8 +39,8 @@ function getScssTransformsPlugin (opts) {
 
   const scssTransform = createScssTransform('scss', sassVariables)
   const sassTransform = createScssTransform('sass', sassVariables)
-  const scssExt = [ '.scss' ]
-  const sassExt = [ '.sass' ]
+  const scssExt = [ '.scss', '.module.scss' ]
+  const sassExt = [ '.sass', '.module.sass' ]
 
   return {
     name: 'vite:quasar:scss',
