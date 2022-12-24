4195435969ad8ba6cb7b47ccd204677f24ad50f4
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-12-24 14:53:35 +0200

    feat(docs): various tweaks

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -3,8 +3,7 @@
 </template>
 
 <script setup>
-import { useMeta, Platform, Dark } from 'quasar'
-import { onMounted } from 'vue'
+import { useMeta } from 'quasar'
 
 import getMeta from 'assets/get-meta.js'
 
@@ -17,36 +16,4 @@ useMeta({
     'Developer-oriented, front-end framework with VueJS components for best-in-class high-performance, responsive websites, PWA, SSR, Mobile and Desktop apps, all from the same codebase. Sensible people choose Vue. Productive people choose Quasar. Be both.'
   )
 })
-
-if (process.env.CLIENT) {
-  function getMobilePlatform (is) {
-    if (is.ios === true) return 'ios'
-    if (is.android === true) return 'android'
-  }
-
-  function getBodyClasses ({ is, has }) {
-    const cls = [
-      is.desktop === true ? 'desktop' : 'mobile',
-      `${ has.touch === false ? 'no-' : '' }touch`
-    ]
-
-    if (is.mobile === true) {
-      const mobile = getMobilePlatform(is)
-      mobile !== void 0 && cls.push('platform-' + mobile)
-    }
-
-    cls.push(`body--${ Dark.isActive === true ? 'dark' : 'light' }`)
-
-    return cls.join(' ')
-  }
-
-  // need to apply correction from SSG
-  // until Quasar SSG mode is available
-  onMounted(() => {
-    const classes = getBodyClasses(Platform)
-    if (document.body.className !== classes) {
-      document.body.className = classes
-    }
-  })
-}
 </script>
