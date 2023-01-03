74a834d160289d3fd6e2f33d8058c0059a552ff2
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2023-01-03 18:01:34 +0200

    feat(docs): Transitions page -> refine individual transitions (#15048)

--- docs/src/components/page-parts/transitions/TransitionList.vue
+++ docs/src/components/page-parts/transitions/TransitionList.vue
@@ -1,86 +1,104 @@
-<template lang="pug">
-.relative-position
-  img.transition-list-box__ensure-img-loaded.no-pointer-events.absolute-bottom-left(
-    src="https://cdn.quasar.dev/img/parallax1.jpg"
-  )
+<template>
+  <div class="relative-position">
+    <img
+      class="transition-list-box__ensure-img-loaded no-pointer-events absolute-bottom-left"
+      src="https://cdn.quasar.dev/img/parallax1.jpg"
+    />
 
-  q-btn.q-mb-lg(push, color="teal", label="Trigger", @click="triggerAll")
+    <q-btn class="q-mb-lg" push color="teal" label="Trigger All" @click="triggerAll" />
 
-  .q-gutter-md.row.items-start
+    <div class="q-gutter-md row items-start">
+      <div
+        v-for="transition in transitions"
+        :key="transition.name"
+        class="transition-list-box relative-position overflow-hidden rounded-borders shadow-2 cursor-pointer non-selectable"
+        @click="transition.trigger"
+      >
+        <transition :name="transition.css">
+          <img
+            class="transition-list-box__img absolute-full"
+            :key="transition.name + '|' + transition.url"
+            :src="transition.url"
+          />
+        </transition>
 
-    .transition-list-box.relative-position.overflow-hidden.rounded-borders.shadow-2(
-      v-for="(transition,index) in transitions"
-      :key="transition"
-    )
-      transition(:name="'q-transition--' + transition")
-        img.transition-list-box__img.absolute-full.cursor-pointer(
-          :key="url[index]"
-          :src="url[index]"
-          @click="trigger(index)"
-        )
+        <div class="transition-list-box__label absolute-bottom q-pa-sm text-center text-body2">
+          {{ transition.name }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-      .transition-list-box__label.absolute-bottom.q-pa-sm.text-center.text-body2
-        | {{ transition }}
+<script setup>
+import { ref } from 'vue'
 
-  </template>
+const urlFirst = 'https://cdn.quasar.dev/img/parallax2.jpg'
+const urlSecond = 'https://cdn.quasar.dev/img/parallax1.jpg'
 
-<script>
-import { ref } from "vue";
-export default {
-  name: "TransitionList",
-  setup() {
-    const transitions = [
-      "slide-right",
-      "slide-left",
-      "slide-up",
-      "slide-down",
-      "fade",
-      "scale",
-      "rotate",
-      "flip-right",
-      "flip-left",
-      "flip-up",
-      "flip-down",
-      "jump-right",
-      "jump-left",
-      "jump-up",
-      "jump-down",
-    ];
-    const url = ref([
-      ...transitions.map((_) => "https://cdn.quasar.dev/img/parallax2.jpg"),
-    ]);
+const transitions = ref([
+  'slide-right',
+  'slide-left',
+  'slide-up',
+  'slide-down',
+  'fade',
+  'scale',
+  'rotate',
+  'flip-right',
+  'flip-left',
+  'flip-up',
+  'flip-down',
+  'jump-right',
+  'jump-left',
+  'jump-up',
+  'jump-down'
+].map((name, index) => ({
+  name,
+  css: `q-transition--${ name }`,
+  url: urlFirst,
+  trigger: () => {
+    const { url } = transitions.value[ index ]
+    transitions.value[ index ].url = url === urlFirst
+      ? urlSecond
+      : urlFirst
+  }
+})))
 
-    const trigger = (index) => {
-      url.value[index] =
-        url.value[index] === "https://cdn.quasar.dev/img/parallax2.jpg"
-          ? "https://cdn.quasar.dev/img/parallax1.jpg"
-          : "https://cdn.quasar.dev/img/parallax2.jpg";
-    };
-
-    return {
-      url,
-      transitions,
-      trigger,
-      triggerAll() {
-        for (let i = 0; i < transitions.length; i++) trigger(i);
-      },
-    };
-  },
-};
+function triggerAll () {
+  transitions.value.forEach(transition => { transition.trigger() })
+}
 </script>
 
 <style lang="sass">
 .transition-list-box
+
   width: 150px
   height: 150px
+
+  &:after
+    content: ''
+    position: absolute
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    background-color: #000
+    opacity: 0
+    transition: opacity .28s ease-in-out
+
+  &:hover:after
+    opacity: .15
+
   &__img
     height: inherit
     width: inherit
     object-fit: cover
     object-position: 50% 50%
+
   &__label
     color: #fff
-    background-color: rgba(0,0,0,0.2)
+    background-color: rgba(0,0,0,.2)
+
   &__ensure-img-loaded
     opacity: 0
     width: 10px
