4ad7bbf875a08f6bb35c090bb9129bc79339da87
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2023-01-03 18:44:23 +0200

    feat(docs): Improve directory structure pages by adding links (fix: #13993) (#14993)

--- docs/src/components/DocTree.vue
+++ docs/src/components/DocTree.vue
@@ -10,6 +10,20 @@
     <template v-slot:default-header="prop">
       <div class="row items-center no-wrap">
         <div class="doc-tree__label text-weight-bold text-no-wrap">{{ prop.node.l }}</div>
+
+        <q-btn
+          v-if="prop.node.url"
+          class="doc-tree__btn q-ml-xs"
+          size="9px"
+          color="brand-accent"
+          :icon="mdiLaunch"
+          unelevated
+          dense
+          :href="prop.node.url"
+          target="_blank"
+          @click.stop
+        />
+
         <div class="doc-tree__explanation text-grey q-ml-sm" v-if="prop.node.e"># {{ prop.node.e }}</div>
       </div>
     </template>
@@ -17,6 +31,8 @@
 </template>
 
 <script setup>
+import { mdiLaunch } from '@quasar/extras/mdi-v6'
+
 const props = defineProps({
   def: Object
 })
@@ -38,6 +54,8 @@ const nodes = [addId(props.def)]
 .doc-tree
   &__label
     font-size: .9em
+  &__btn .q-icon
+    font-size: 12px
   &__explanation
     font-size: .7em
 </style>
