c8d77d252142210fb8987b3e158ded07c6123f7d
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2023-02-07 14:00:03 +0300

    feat(ui): Improve QForm.getValidationComponents type

---
+++ ui/types/api/qform.d.ts
@@ -0,0 +1,4 @@
+import { QField } from "quasar";
+import { ComponentPublicInstance } from "vue";
+
+export type QFormChildComponent = ComponentPublicInstance & Pick<QField, "validate" | "resetValidation">;
