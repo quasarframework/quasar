7d3cf94a91d2eef9b536d58753bcb939df4d4fff
Popescu Dan <pdan.popescu@gmail.com>
2023-03-06 15:31:36 +0200

    fix(QInput): fix sizing problems for autogrow #15498 (#15521)
    - firefox works good without special tweaks
    - chrome needs adjustments for scroll position
    - chrome reports smaller scrollHeight when it is in a .column container

--- ui/src/components/input/QInput.js
+++ ui/src/components/input/QInput.js
@@ -293,18 +293,29 @@ export default Vue.extend({
         const inp = this.$refs.input
         if (inp !== void 0) {
           const parentStyle = inp.parentNode.style
-          const { overflow } = inp.style
+          // chrome does not keep scroll #15498
+          const { scrollTop } = inp
+          // chrome calculates a smaller scrollHeight when in a .column container
+          const { overflowY, maxHeight } = this.$q.platform.is.firefox === true
+            ? {}
+            : window.getComputedStyle(inp)
+          // on firefox or if overflowY is specified as scroll #14263, #14344
+          // we don't touch overflow
+          // firefox is not so bad in the end
+          const changeOverflow = overflowY !== void 0 && overflowY !== 'scroll'
 
           // reset height of textarea to a small size to detect the real height
           // but keep the total control size the same
-          // Firefox rulez #14263, #14344
-          this.$q.platform.is.firefox !== true && (inp.style.overflow = 'hidden')
+          changeOverflow === true && (inp.style.overflowY = 'hidden')
           parentStyle.marginBottom = (inp.scrollHeight - 1) + 'px'
           inp.style.height = '1px'
 
           inp.style.height = inp.scrollHeight + 'px'
-          inp.style.overflow = overflow
+          // we should allow scrollbars only
+          // if there is maxHeight and content is taller than maxHeight
+          changeOverflow === true && (inp.style.overflowY = parseInt(maxHeight, 10) < inp.scrollHeight ? 'auto' : 'hidden')
           parentStyle.marginBottom = ''
+          inp.scrollTop = scrollTop
         }
       })
     },
