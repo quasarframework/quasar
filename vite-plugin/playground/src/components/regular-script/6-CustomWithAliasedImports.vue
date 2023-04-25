bf9847bcf2f2b24a25ffbd539308fa6441e48a29
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2023-04-25 13:23:44 +0300

    test(vite-plugin): Setup Cypress and add e2e tests

---
+++ vite-plugin/playground/src/components/regular-script/6-CustomWithAliasedImports.vue
@@ -0,0 +1,27 @@
+<template>
+  <h3>With Custom Component and Aliased Imports</h3>
+
+  <div>
+    <q-btn label="Click me" />
+
+    <q-icon name="map" />
+
+    <custom-component />
+  </div>
+</template>
+
+<script>
+import { QBtn as Mimi, Notify } from 'quasar'
+import { defineComponent } from 'vue'
+import CustomComponent from './custom-component.js'
+
+console.log(Mimi)
+console.log('Notify', Notify)
+
+export default defineComponent({
+  name: 'AliasedImports',
+  components: {
+    CustomComponent
+  }
+})
+</script>
---
+++ vite-plugin/test/cypress/script-transform.cy.ts
@@ -0,0 +1,34 @@
+const types = [
+  'basic',
+  'imports',
+  'aliased-imports',
+  'custom',
+  'custom-with-imports',
+  'custom-with-aliased-imports'
+]
+
+describe('Script Transform', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should transform <script setup> correctly', () => {
+    types.forEach((type) => {
+      cy.get(`#script-setup-${type}`)
+        .find('*')
+        .each(($el) => {
+          expect($el.prop('tagName')).not.to.match(/^q-/)
+        })
+    })
+  })
+
+  it('should transform <script> correctly', () => {
+    types.forEach((type) => {
+      cy.get(`#script-${type}`)
+        .find('*')
+        .each(($el) => {
+          expect($el.prop('tagName')).not.to.match(/^q-/)
+        })
+    })
+  })
+})
