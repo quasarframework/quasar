0858ff54a2c43aa111b4b6ceebdafcb476f59f9d
Evertvdw <evertvanderweit@gmail.com>
2023-04-27 13:16:37 +0200

    feat(QDrawer): add prop to disable mini animation (#15670)
    Co-authored-by: Evert van der Weit <evert@mett.nl>

--- ui/src/components/drawer/QDrawer.js
+++ ui/src/components/drawer/QDrawer.js
@@ -41,6 +41,7 @@ export default createComponent({
       type: Number,
       default: 57
     },
+    disableMiniAnimation: Boolean,
 
     breakpoint: {
       type: Number,
@@ -417,6 +418,7 @@ export default createComponent({
     watch(() => $q.lang.rtl, () => { applyPosition() })
 
     watch(() => props.mini, () => {
+      if (props.disableMiniAnimation) return
       if (props.modelValue === true) {
         animateMini()
         $layout.animate()
