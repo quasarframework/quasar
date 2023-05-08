70b76900d049b9c9fa4d2984dfc9671801dfca55
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2023-05-08 17:05:36 +0300

    chore(ui): Utilize mergeConfig helper in dev config

--- ui/dev/quasar.config.js
+++ ui/dev/quasar.config.js
@@ -78,14 +78,16 @@ module.exports = function (ctx) {
       },
 
       extendViteConf (viteConf, { isServer }) {
-        viteConf.server = viteConf.server || {}
-        viteConf.server.fs = viteConf.server.fs || {}
-        viteConf.server.fs.allow = [
-          // for quasar package (ui folder) and related deps
-          '..',
-          // due to workspace hoisting, some deps might come from the root node_modules
-          '../..'
-        ]
+        viteConf.server = mergeConfig(viteConf.server, {
+          fs: {
+            allow: [
+              // for quasar package (ui folder) and related deps
+              '..',
+              // due to workspace hoisting, some deps might come from the root node_modules
+              '../..'
+            ]
+          }
+        })
 
         if (isServer) {
           viteConf.resolve.alias.quasar = join(__dirname, '../src/index.ssr.js')
