3509798a43036e1a47cdfca7ed68c0101a14915d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2023-05-12 12:11:27 +0300

    fix(app-vite/app-webpack): Capacitor ssl-skip uninstall cmd param

--- app-webpack/lib/capacitor/capacitor-config.js
+++ app-webpack/lib/capacitor/capacitor-config.js
@@ -142,8 +142,9 @@ class CapacitorConfig {
       }
 
       const fn = `${ add ? '' : 'un' }installPackage`
+      const nameParam = add ? '@jcesarmobile/ssl-skip@^0.2.0' : '@jcesarmobile/ssl-skip'
 
-      nodePackager[ fn ]('@jcesarmobile/ssl-skip@^0.2.0', {
+      nodePackager[ fn ](nameParam, {
         cwd: appPaths.capacitorDir,
         displayName: 'Capacitor (DEVELOPMENT ONLY) SSL support'
       })
