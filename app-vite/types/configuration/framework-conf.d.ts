41a9e7e9f31f88c2b66700d06fc15355469d3c4c
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2023-07-06 19:39:38 +0300

    fix(ui): fix LoadingBar defaults type (#16049)

--- app-vite/types/configuration/framework-conf.d.ts
+++ app-vite/types/configuration/framework-conf.d.ts
@@ -5,8 +5,15 @@ import {
   QuasarUIConfiguration,
 } from "quasar";
 
+type AnyFn = (...args: any) => any;
+type SerializableConfiguration<T> = {
+  [K in keyof T as AnyFn extends T[K] ? never : K]: object extends T[K]
+    ? SerializableConfiguration<T[K]>
+    : T[K];
+};
+
 interface QuasarFrameworkConfiguration {
-  config?: QuasarUIConfiguration;
+  config?: SerializableConfiguration<QuasarUIConfiguration>;
   iconSet?: QuasarIconSets;
   lang?: QuasarLanguageCodes;
   cssAddon?: boolean;
