b813d4c7fbdf0f2b8ad9f21be2cefed5831056d8
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2023-07-07 21:20:06 +0300

    style: fix ESLint warnings and tidy up indentation

--- create-quasar/templates/app/quasar-v2/ts-vite/vuex/src/store/index.ts
+++ create-quasar/templates/app/quasar-v2/ts-vite/vuex/src/store/index.ts
@@ -37,11 +37,12 @@ declare module '@vue/runtime-core' {
 export const storeKey: InjectionKey<VuexStore<StateInterface>> = Symbol('vuex-key')
 
 // Provide typings for `this.$router` inside Vuex stores
- declare module "vuex" {
-   export interface Store<S> {
-     readonly $router: Router;
-   }
- }
+declare module "vuex" {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  export interface Store<S> {
+    readonly $router: Router;
+  }
+}
 
 export default store(function (/* { ssrContext } */) {
   const Store = createStore<StateInterface>({
