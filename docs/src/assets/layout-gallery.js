6fe874e1ab3ac6b07e83b2f8070fcd547690ac2a
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2023-07-19 13:42:22 +0300

    fix(docs): keep account of docs branch when linking to the layout gallery

--- docs/src/assets/layout-gallery.js
+++ docs/src/assets/layout-gallery.js
@@ -13,5 +13,5 @@ export default list.map(layout => ({
   ...layout,
   screenshot: `https://cdn.quasar.dev/img/layout-gallery/screenshot-${layout.path}.png`,
   demoLink: `/layout/gallery/${layout.path}`,
-  sourceLink: `https://github.com/quasarframework/quasar/blob/dev/docs/src/layouts/gallery/${layout.path}.vue`
+  sourceLink: `https://github.com/quasarframework/quasar/blob/${ process.env.DOCS_BRANCH }/docs/src/layouts/gallery/${layout.path}.vue`
 }))
