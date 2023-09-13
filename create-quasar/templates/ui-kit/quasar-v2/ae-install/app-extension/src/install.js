72e0f8e27ccc6db04e5268f398c7433c4c508efd
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2023-09-13 15:14:39 +0300

    fix(create-quasar): remove obsolete AE links

--- create-quasar/templates/ui-kit/quasar-v2/ae-install/app-extension/src/install.js
+++ create-quasar/templates/ui-kit/quasar-v2/ae-install/app-extension/src/install.js
@@ -2,7 +2,6 @@
  * Quasar App Extension install script
  *
  * Docs: https://quasar.dev/app-extensions/development-guide/install-api
- * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
  */
 
 <%= aeCodeFormat === 'esm' ? 'export default' : 'module.exports =' %> function (api) {
