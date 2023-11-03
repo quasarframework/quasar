d7efaae70d8bf3a26d9ca2f948a1da017db9760b
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2023-11-03 11:44:25 +0300

    docs: add examples and explanation for QTreeNode type

--- ui/types/api/qtree.d.ts
+++ ui/types/api/qtree.d.ts
@@ -1,5 +1,95 @@
 /**
+ * Node type to be used with QTree's `nodes` prop
+ *
+ * @see https://v2.quasar.dev/vue-components/tree#qtree-api
+ *
  * @template TExtra Object type to add extra properties for the node, overrides the existing ones as well
+ *
+ * @example
+ * Basic usage
+ * ```ts
+ * const nodes: QTreeNode[] = [
+ *   // ...
+ * ];
+ * // <q-tree :nodes="nodes" />
+ * ```
+ *
+ * @example
+ * Making some properties required
+ * ```ts
+ * // make label and icon required, the rest will stay optional
+ * const nodes: QTreeNode<{ label: string; icon: string }>[] = [
+ *   // ...
+ * ];
+ * ```
+ *
+ * @example
+ * Adding extra properties
+ * ```ts
+ * // on top of the existing properties, add a foo property
+ * const nodes: QTreeNode<{ foo: number }>[] = [
+ *   // ...
+ * ];
+ * ```
+ *
+ * @example
+ * Using different label/children properties
+ * ```ts
+ * type Node = QTreeNode<{ name: string; subNodes: Node[] }>;
+ * const nodes: Node[] = [
+ *   // ...
+ * ];
+ * // <q-tree :nodes="nodes" label-key="name" children-key="subNodes" />
+ * ```
+ *
+ * @example
+ * Using a different child node type
+ * ```ts
+ * type ChildNode = QTreeNode<{ foo: number }>;
+ * type ParentNode = QTreeNode<{ bar: string; children?: ChildNode[] }>;
+ *
+ * const nodes: ParentNode[] = [
+ *   // ...
+ * ];
+ * ```
+ *
+ * @example
+ * A very basic file system tree
+ * ```ts
+ * interface FileInfo {
+ *   path: string;
+ *   size: number;
+ *   lastModified: number;
+ * }
+ * type FileNode = QTreeNode<FileInfo & { type: "file", children?: never }>;
+ * type DirectoryNode = QTreeNode<FileInfo & { type: "directory", children?: (FileNode | DirectoryNode)[] }>;
+ *
+ * const nodes: DirectoryNode[] = [
+ *   {
+ *     type: "directory",
+ *     path: "/",
+ *     size: 0,
+ *     lastModified: 0,
+ *     // allows both file and directory as children
+ *     children: [
+ *       {
+ *         type: "file",
+ *         path: "/foo.txt",
+ *         size: 100,
+ *         lastModified: 1000,
+ *         // does not allow children
+ *       },
+ *       {
+ *         type: "directory",
+ *         path: "/bar",
+ *         size: 0,
+ *         lastModified: 0,
+ *         // empty folder - doesn't have children
+ *       }
+ *     ]
+ *   }
+ * ]
+ * ```
  */
 export type QTreeNode<TExtra = unknown> = Omit<
   {
