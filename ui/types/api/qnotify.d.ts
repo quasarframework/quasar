4b85a7eac5a926cdc0fabe5bb3966bb09f8a5ff3
AngelHdz Multimedia <angel.hernandez.12@live.com>
2024-01-24 02:09:04 -0400

    feat(ui, plugins): added specific types for QNotifyCreateOptions type

--- ui/types/api/qnotify.d.ts
+++ ui/types/api/qnotify.d.ts
@@ -1,4 +1,4 @@
-import { QNotifyCreateOptions, QBtnProps } from "quasar";
+import { QNotifyCreateOptions, QBtnProps, LiteralUnion  } from "quasar";
 import { ButtonHTMLAttributes } from "vue";
 
 export type QNotifyUpdateOptions = Omit<
@@ -6,6 +6,18 @@ export type QNotifyUpdateOptions = Omit<
   "group" | "position"
 >;
 export type QNotifyOptions = Omit<QNotifyCreateOptions, "ignoreDefaults">;
+
+export interface NotifyCustomTypes {};
+
+type NotifyTypes =  
+  | 'positive'
+  | 'negative'
+  | 'warning'
+  | 'info'
+  | 'ongoing';
+
+export type NotifyType = LiteralUnion<NotifyTypes | keyof NotifyCustomTypes>;
+
 
 export type QNotifyAction = Omit<QBtnProps, "onClick"> &
   ButtonHTMLAttributes & { noDismiss?: boolean; handler?: () => void };
