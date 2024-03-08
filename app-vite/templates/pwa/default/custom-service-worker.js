84cee1427e5ebba9bc42215ea51540ec7135738d
Teodor Atroshenko <thexeos@users.noreply.github.com>
2024-03-08 06:23:47 +0000

    fix: remove regex double-escape in custom service worker denylist

--- app-vite/templates/pwa/default/custom-service-worker.js
+++ app-vite/templates/pwa/default/custom-service-worker.js
@@ -24,7 +24,7 @@ if (process.env.MODE !== 'ssr' || process.env.PROD) {
   registerRoute(
     new NavigationRoute(
       createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
-      { denylist: [new RegExp(process.env.PWA_SERVICE_WORKER_REGEX), /workbox-(.)*\\.js$/] }
+      { denylist: [new RegExp(process.env.PWA_SERVICE_WORKER_REGEX), /workbox-(.)*\.js$/] }
     )
   )
 }
