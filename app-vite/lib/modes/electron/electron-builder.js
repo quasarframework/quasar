3dc01ea9df278b7778c350acdef9ac9700b36864
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2024-03-14 16:55:32 +0200

    feat(app-vite): drop register-service-worker as dependency (should be installed in host project folder)

--- app-vite/lib/modes/electron/electron-builder.js
+++ app-vite/lib/modes/electron/electron-builder.js
@@ -40,6 +40,7 @@ export class QuasarModeBuilder extends AppBuilder {
     if (pkg.dependencies) {
       pkg.dependencies = getFixedDeps(pkg.dependencies, this.ctx.appPaths.appDir)
       delete pkg.dependencies[ '@quasar/extras' ]
+      delete pkg.dependencies[ 'register-service-worker' ]
     }
 
     // we don't need this (also, faster install time & smaller bundles)
