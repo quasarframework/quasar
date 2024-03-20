54e70902741ff1fd4d1c8bf7fa16bb9d2a108d2f
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2024-03-20 14:02:06 +0200

    refactor(ui): umd testing

--- ui/build/script.test-umd.js
+++ ui/build/script.test-umd.js
@@ -2,7 +2,7 @@ const { resolve } = require('node:path')
 const fse = require('fs-extra')
 
 const src = resolve(__dirname, '../dist')
-const dest = resolve(__dirname, '../dev-umd/dist')
+const dest = resolve(__dirname, '../playground-umd/dist')
 
 if (!fse.existsSync(src)) {
   console.error('\nERROR: please run "pnpm build" first\n')
@@ -14,7 +14,7 @@ fse.symlinkSync(src, dest, 'dir')
 
 import('open').then(({ default: open }) => {
   open(
-    resolve(__dirname, '../dev-umd/index.umd.html'),
+    resolve(__dirname, '../playground-umd/index.umd.html'),
     {
       app: { name: 'google chrome' }
     }
