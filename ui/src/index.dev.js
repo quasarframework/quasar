4b95707ca3e4ea6920ce4c20a95ccf91855d27e0
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2024-04-24 14:19:23 +0300

    fdsfds

--- ui/src/index.dev.js
+++ ui/src/index.dev.js
@@ -6,7 +6,7 @@ import installQuasar from './install-quasar.js'
 import Lang from './plugins/lang/Lang.js'
 import IconSet from './plugins/icon-set/IconSet.js'
 
-console.log('test')
+console.log('txest')
 export * from './components.js'
 export * from './directives.js'
 export * from './plugins.js'
