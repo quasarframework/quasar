4bd0821c844ea24793e7d5bf8970727aaa841660
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2024-05-06 12:19:07 +0300

    feat(ui/testing): add spinner tests

---
+++ ui/src/components/spinner/QSpinnerFacebook.test.js
@@ -0,0 +1,59 @@
+import { mount, flushPromises } from '@vue/test-utils'
+import { describe, test, expect } from 'vitest'
+
+import QSpinnerFacebook from './QSpinnerFacebook.js'
+
+describe('[QSpinnerFacebook API]', () => {
+  describe('[Props]', () => {
+    describe('[(prop)size]', () => {
+      test.each([
+        [ 'String', '100px' ],
+        [ 'Number', 100 ]
+      ])('type %s has effect', async (_, propVal) => {
+        const expectedValue = '' + propVal
+        const wrapper = mount(QSpinnerFacebook)
+
+        const target = wrapper.get('.q-spinner')
+
+        expect(
+          target.attributes('width')
+        ).not.toBe(expectedValue)
+
+        expect(
+          target.attributes('height')
+        ).not.toBe(expectedValue)
+
+        await wrapper.setProps({ size: propVal })
+        await flushPromises()
+
+        expect(
+          target.attributes('width')
+        ).toBe(expectedValue)
+
+        expect(
+          target.attributes('height')
+        ).toBe(expectedValue)
+      })
+    })
+
+    describe('[(prop)color]', () => {
+      test('type String has effect', async () => {
+        const propVal = 'red'
+        const wrapper = mount(QSpinnerFacebook)
+
+        const target = wrapper.get('.q-spinner')
+
+        expect(
+          target.classes()
+        ).not.toContain('text-red')
+
+        await wrapper.setProps({ color: propVal })
+        await flushPromises()
+
+        expect(
+          target.classes()
+        ).toContain('text-red')
+      })
+    })
+  })
+})
