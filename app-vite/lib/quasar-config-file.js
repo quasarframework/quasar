b926e3479d8bc53879c522ea22652d8257469e9e
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2024-05-07 11:16:14 +0300

    fix(app-vite/app-webpack): quasar.config > framework > lang/iconSet parsing

--- app-vite/lib/quasar-config-file.js
+++ app-vite/lib/quasar-config-file.js
@@ -91,6 +91,17 @@ function uniquePathFilter (value, index, self) {
   return self.map(obj => obj.path).indexOf(value.path) === index
 }
 
+const extRE = /\.[m|c]?[j|t]s$/
+function formatQuasarAssetPath (asset, type) {
+  return asset.indexOf('/') !== -1
+    ? (
+        extRE.test(asset) === true
+          ? asset
+          : `${ asset }.js`
+      )
+    : `quasar/${ type }/${ asset }.js`
+}
+
 async function onAddress ({ host, port }, mode) {
   if (this.chosenHost) {
     host = this.chosenHost
@@ -415,15 +426,11 @@ class QuasarConfFile {
     const { lang, iconSet } = cfg.framework
 
     if (lang !== void 0) {
-      cfg.framework.lang = lang.indexOf('/') === true
-        ? lang
-        : `quasar/lang/${ lang }.js`
+      cfg.framework.lang = formatQuasarAssetPath(lang, 'lang')
     }
 
     if (iconSet !== void 0) {
-      cfg.framework.iconSet = iconSet.indexOf('/') === true
-        ? iconSet
-        : `quasar/icon-set/${ iconSet }.js`
+      cfg.framework.iconSet = formatQuasarAssetPath(iconSet, 'icon-set')
     }
 
     Object.assign(cfg.metaConf, {
