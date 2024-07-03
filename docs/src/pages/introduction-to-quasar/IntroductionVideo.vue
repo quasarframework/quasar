69057006a99706e42ebfceedcbb003c11087735b
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2024-07-03 16:35:11 +0300

    chore(docs): (intro to quasar) remove obsolete css

--- docs/src/pages/introduction-to-quasar/IntroductionVideo.vue
+++ docs/src/pages/introduction-to-quasar/IntroductionVideo.vue
@@ -44,18 +44,4 @@
 
         &:hover
           opacity: 1
-
-  &__container
-    position: relative
-    padding-bottom: 56.25%
-    height: 0
-    overflow: hidden
-    max-width: 100%
-
-    iframe, object, embed
-      position: absolute
-      top: 0
-      left: 0
-      width: 100%
-      height: 100%
 </style>
