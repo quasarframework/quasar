c83adc2e1422e90b2eb2e2f2dd5711ea9416c8b4
Dtsiantaris <44876922+Dtsiantaris@users.noreply.github.com>
2025-01-15 19:45:36 +0200

    Update _eslint.config.js ts-vite-2.

--- create-quasar/templates/app/quasar-v2/ts-vite-2/eslint/_eslint.config.js
+++ create-quasar/templates/app/quasar-v2/ts-vite-2/eslint/_eslint.config.js
@@ -35,14 +35,27 @@ export default [
    */
   ...pluginVue.configs[ 'flat/essential' ],
 
+  // this rule needs to be above the vueTsEslintConfig to avoid error: 'You have used a rule which requires type information, but don't have parserOptions set to generate type information for this file.'
+  { 
+    rules: {
+        'prefer-promise-reject-errors': 'off',
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { prefer: 'type-imports' }
+        ],
+  
+        // allow debugger during development only
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+      }
+  },
   // https://github.com/vuejs/eslint-config-typescript
   ...vueTsEslintConfig({
     // Optional: extend additional configurations from typescript-eslint'.
     // Supports all the configurations in
     // https://typescript-eslint.io/users/configs#recommended-configurations
     extends: [
-      // By default, only the recommended rules are enabled.
-      'recommended'
+      // By default, only the 'recommendedTypeChecked' rules are enabled on typescript projects.
+      'recommendedTypeChecked'
       // You can also manually enable the stylistic rules.
       // "stylistic",
 
@@ -71,10 +84,6 @@ export default [
     // add your custom rules here
     rules: {
       'prefer-promise-reject-errors': 'off',
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports' }
-      ],
 
       // allow debugger during development only
       'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
