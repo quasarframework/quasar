0375851de560e1963b25577d632dc63e20e9ee4c
Igor <Igorcbraz1@gmail.com>
2025-01-23 16:05:23 -0300

    feat(docs) add clipboard copy option

--- docs/src/pages/style/typography/TypographyHeadings.vue
+++ docs/src/pages/style/typography/TypographyHeadings.vue
@@ -6,7 +6,12 @@
       class="row items-center q-mb-lg"
     >
       <div class="col-sm-3 col-12">
-        <q-badge color="brand-primary" outline :label="heading.cls" />
+        <q-badge
+          color="brand-primary cursor-pointer"
+          outline
+          :label="heading.cls"
+          @click="copyCls(heading.cls)"
+        />
         <q-badge
           v-if="heading.equivalent"
           class="q-ml-sm"
@@ -22,19 +27,41 @@
   </div>
 </template>
 
-<script setup>
-const headings = [
-  { label: 'Headline 1', cls: 'text-h1', equivalent: 'h1' },
-  { label: 'Headline 2', cls: 'text-h2', equivalent: 'h2' },
-  { label: 'Headline 3', cls: 'text-h3', equivalent: 'h3' },
-  { label: 'Headline 4', cls: 'text-h4', equivalent: 'h4' },
-  { label: 'Headline 5', cls: 'text-h5', equivalent: 'h5' },
-  { label: 'Headline 6', cls: 'text-h6', equivalent: 'h6' },
-  { label: 'Subtitle 1', cls: 'text-subtitle1' },
-  { label: 'Subtitle 2', cls: 'text-subtitle2' },
-  { label: 'Body 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.', cls: 'text-body1' },
-  { label: 'Body 2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aliquid ad quas sunt voluptatum officia dolorum cumque, possimus nihil molestias sapiente necessitatibus dolor saepe inventore, soluta id accusantium voluptas beatae.', cls: 'text-body2' },
-  { label: 'Caption text', cls: 'text-caption' },
-  { label: 'Overline', cls: 'text-overline' }
-]
+<script>
+import { copyToClipboard } from 'quasar'
+
+export default {
+  data () {
+    return {
+      headings: [
+        { label: 'Headline 1', cls: 'text-h1', equivalent: 'h1' },
+        { label: 'Headline 2', cls: 'text-h2', equivalent: 'h2' },
+        { label: 'Headline 3', cls: 'text-h3', equivalent: 'h3' },
+        { label: 'Headline 4', cls: 'text-h4', equivalent: 'h4' },
+        { label: 'Headline 5', cls: 'text-h5', equivalent: 'h5' },
+        { label: 'Headline 6', cls: 'text-h6', equivalent: 'h6' },
+        { label: 'Subtitle 1', cls: 'text-subtitle1' },
+        { label: 'Subtitle 2', cls: 'text-subtitle2' },
+        { label: 'Body 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.', cls: 'text-body1' },
+        { label: 'Body 2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aliquid ad quas sunt voluptatum officia dolorum cumque, possimus nihil molestias sapiente necessitatibus dolor saepe inventore, soluta id accusantium voluptas beatae.', cls: 'text-body2' },
+        { label: 'Caption text', cls: 'text-caption' },
+        { label: 'Overline', cls: 'text-overline' }
+      ]
+    }
+  },
+  methods: {
+    copyCls (cls) {
+      copyToClipboard(cls)
+        .then(() => {
+          this.$q.notify('Copied to clipboard')
+        })
+        .catch(() => {
+          this.$q.notify({
+            color: 'negative',
+            message: 'Failed to copy to clipboard'
+          })
+        })
+    }
+  }
+}
 </script>
--- docs/src/pages/style/typography/TypographyWeights.vue
+++ docs/src/pages/style/typography/TypographyWeights.vue
@@ -6,7 +6,12 @@
       class="row items-center q-mb-md"
     >
       <div class="col-sm-3 col-12">
-        <q-badge color="brand-primary" outline :label="`text-weight-${ weight }`" />
+        <q-badge
+          color="brand-primary cursor-pointer"
+          outline
+          :label="`text-weight-${ weight }`"
+          @click="copyWeigh(`text-weight-${ weight }`)"
+        />
       </div>
       <div class="col-sm-9 col-12 q-mb-none q-pl-md q-pt-sm q-pb-sm">
         <div :class="`text-weight-${weight}`">
@@ -17,6 +22,28 @@
   </div>
 </template>
 
-<script setup>
-const weights = [ 'thin', 'light', 'regular', 'medium', 'bold', 'bolder' ]
+<script>
+import { copyToClipboard } from 'quasar'
+
+export default {
+  data () {
+    return {
+      weights: [ 'thin', 'light', 'regular', 'medium', 'bold', 'bolder' ]
+    }
+  },
+  methods: {
+    copyWeigh (weight) {
+      copyToClipboard(weight)
+        .then(() => {
+          this.$q.notify('Copied to clipboard')
+        })
+        .catch(() => {
+          this.$q.notify({
+            color: 'negative',
+            message: 'Failed to copy to clipboard'
+          })
+        })
+    }
+  }
+}
 </script>
