f9390ebb8b9b04e9eaecb3f56e7545e20add020f
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2025-03-01 17:59:58 +0200

    Update QIcon.js

--- ui/src/components/icon/QIcon.js
+++ ui/src/components/icon/QIcon.js
@@ -21,7 +21,7 @@ const libMap = {
   'iconfont ': sameFn,
   'ti-': i => `themify-icon ${ i }`,
   'bi-': i => `bootstrap-icons ${ i }`,
-  'i-': i => `${ i }`
+  'i-': sameFn
 }
 
 const matMap = {
