7758be1d83549bb16341771b82b67bf8c15e7318
SamVanini <vaninisamantha@yahoo.it>
2025-03-09 14:38:57 +0100

    fix(docs): fix QKnob docs when img is in default slot #17812

--- docs/src/examples/QKnob/ShowValue.vue
+++ docs/src/examples/QKnob/ShowValue.vue
@@ -59,7 +59,7 @@
       track-color="grey-3"
     >
       <q-avatar size="60px">
-        <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
+        <img draggable="false" src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
       </q-avatar>
     </q-knob>
   </div>
