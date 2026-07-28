81887068c7e57183724cfdd741facc170dfddacf
Jeff Galbraith <galbraith64@gmail.com>
2026-07-28 01:27:57 -0600

    fix(icongenie): ignore no-color as profile input (#18454)

--- icongenie/lib/utils/filter-argv-params.js
+++ icongenie/lib/utils/filter-argv-params.js
@@ -2,7 +2,7 @@ export function filterArgvParams(argv) {
   const params = {}
 
   Object.keys(argv).forEach(key => {
-    if (key.length > 1 && key !== 'help') {
+    if (key.length > 1 && key !== 'help' && key !== 'noColor') {
       // kebab to camel case
       const prop = key.replaceAll(/(-\w)/g, m => m[1].toUpperCase())
 
