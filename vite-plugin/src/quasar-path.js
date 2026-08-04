077c6e848b26467372f6314b3b24db0c570b8b31
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-08-04 16:06:28 +0300

    feat(plugin-vite): drop createRequire in favor of ESM import.meta.resolve

--- vite-plugin/src/quasar-path.js
+++ vite-plugin/src/quasar-path.js
@@ -1,6 +1,6 @@
-import { createRequire } from 'node:module'
 import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const require = createRequire(import.meta.url)
-
-export const quasarPath = dirname(require.resolve('quasar/package.json'))
+export const quasarPath = dirname(
+  fileURLToPath(import.meta.resolve('quasar/package.json'))
+)
